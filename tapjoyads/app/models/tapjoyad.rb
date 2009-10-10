e2144680405f8a4e1ddbc78de82b3c747873dff7
ben <ben@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-10-10 22:16:42 +0000

    The TapjoyAd class is used to return an XML object to the SDK.  The SDK already knows how to render this object.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@6 6df48309-bd3c-4361-8df8-ec25136b6021

---
+++ tapjoyads/app/models/tapjoyad.rb
@@ -0,0 +1,12 @@
+class TapjoyAd
+  attr_accessor: ClickURL, Image, AdMessage, AdImpressionID, AdID, AdHTML, OpenIn
+  
+  def initialize()
+    @AdImpressionID = "00000000-0000-0000-0000-000000000000"
+    @AdID = "00000000-0000-0000-0000-000000000000"
+    @AdHTML = nil
+    @OpenIn = "Safari"
+    @AdMessage = nil
+  end
+  
+end
