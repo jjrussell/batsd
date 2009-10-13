b2eb0beaf62513de8b7668f3064d77465c93b802
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-10-13 00:54:36 +0000

    Adding support for Crisp Wireless ads
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@17 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/getad_controller.rb
+++ tapjoyads/app/controllers/getad_controller.rb
@@ -1,3 +1,5 @@
+require 'net/http'
+
 class GetadController < ApplicationController
   def adfonic
     respond_to do |f|  
@@ -9,7 +11,36 @@ class GetadController < ApplicationController
   end
   
   def crisp
-    
+    respond_to do |f|
+      partner_key = params[:partner_key]
+      site_key = params[:site_key]
+      zone_key = params[:zone_key]
+      user_agent = request.headers['User-Agent']
+           
+      host = 'api.crispwireless.com'
+      path = "/adRequest.v1/single/ad.html" +
+          "?partnerkey=#{partner_key}" + 
+          "&sitekey=#{site_key}" +
+          "&random=#{rand(9999999)}" +
+          "&rspid=" +
+          "&zonekey=#{zone_key}" +
+          "&sectionkey"
+      
+      html = ''
+      Net::HTTP.start(host) do |http|
+        html = http.get(path, "User-Agent" =>user_agent).body
+      end
+      
+      if html.include? 'Error: Empty ad'
+        @ad_return_obj = nil
+        f.html {render(:text => "no ad")}
+      else
+        @ad_return_obj = TapjoyAd.new
+        @ad_return_obj.ClickURL = host + path
+        @ad_return_obj.AdHTML = html
+        f.xml {render(:partial => 'tapjoy_ad')}
+      end
+    end
   end
   
 end
