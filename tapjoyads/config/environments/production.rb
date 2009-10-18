00dc756d583e3f9382a79e5d14cf4d5409874546
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-10-18 16:30:03 +0000

    Changes configuration of Memcached for production to use ip adresses rather than dns names.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@65 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/config/environments/production.rb
+++ tapjoyads/config/environments/production.rb
@@ -24,5 +24,5 @@ config.action_controller.perform_caching             = true
 # config.action_mailer.raise_delivery_errors = false
 
 # Memcache
-CACHE = Memcached.new(['ec2-75-101-247-213.compute-1.amazonaws.com', 
-                       'ec2-174-129-66-251.compute-1.amazonaws.com'])
+CACHE = Memcached.new(['10.240.117.174', 
+                       '10.254.106.99'])
