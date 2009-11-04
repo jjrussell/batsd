3099277e9af485015b3bcb1f108989eb0e1575ca
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-11-04 22:48:24 +0000

    Forgot to add this file last commit.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@145 6df48309-bd3c-4361-8df8-ec25136b6021

---
+++ tapjoyads/app/models/queue_message.rb
@@ -0,0 +1,23 @@
+require 'cgi'
+
+class QueueMessage
+  
+  def self.serialize(args)
+    escaped_args = []
+    args.each do |arg|
+      escaped_args.push CGI::escape(arg)
+    end
+    
+    return escaped_args.join(' ')
+  end
+  
+  def self.deserialize(message)
+    values = message.split(' ')
+    args = []
+    values.each do |value|
+      args.push(CGI::unescape(value))
+    end
+    
+    return args
+  end
+end
