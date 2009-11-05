d853139c6aa516929fbc6c8c0ea2b5fb174cf023
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-11-05 01:21:46 +0000

    Fixes error in adshown with renderint xml
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@149 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/adshown_controller.rb
+++ tapjoyads/app/controllers/adshown_controller.rb
@@ -3,21 +3,21 @@ class AdshownController < ApplicationController
   
   include ActiveMessaging::MessageSender
   
-  @success_xml = <<XML_END
+  def index
+    xml = <<XML_END
 <?xml version="1.0" encoding="UTF-8"?>
 <TapjoyConnectReturnObject>
 <Success>true</Success>
 </TapjoyConnectReturnObject>
 XML_END
-  
-  def index
+    
     message = QueueMessage.serialize([params[:campaign_id], params[:app_id], params[:udid],
         Time.now.to_f.to_s])
     
     publish :adshown_stats, message
     
     respond_to do |f|
-      f.xml {render(:text => @success_xml)}
+      f.xml {render(:text => xml)}
     end
   end
 end
