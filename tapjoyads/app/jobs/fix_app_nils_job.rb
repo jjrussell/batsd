6ab7af56c85c0951f2e4dd9cd17c98a2d6a04fdc
ben <ben@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-11-09 22:47:24 +0000

    Logging when I create nils
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@172 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/jobs/fix_app_nils_job.rb
+++ tapjoyads/app/jobs/fix_app_nils_job.rb
@@ -7,6 +7,7 @@ class FixAppNils
       next_run_time = (Time.now + 1.minutes).to_f.to_s
       app.put('next_run_time', next_run_time)
       app.save
+      Rails.logger.info("Added next_run_time to #{app_id} for #{next_run_time}")
     end
   end
 end
