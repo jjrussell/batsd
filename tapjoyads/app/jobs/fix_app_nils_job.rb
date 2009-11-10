e828eb552d4626da75e7a8526ba7bb387bea21ca
ben <ben@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-11-10 09:12:28 +0000

    fix campaigns and apps in one function
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@192 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/jobs/fix_app_nils_job.rb
+++ tapjoyads/app/jobs/fix_app_nils_job.rb
@@ -1,24 +1,30 @@
 class FixAppNilsJob
   def run
-    response = SimpledbResource.query('app', 'next_run_time, interval_update_time, next_daily_run_time', 
-      "next_run_time is null or interval_update_time is null", '')
+    fix_times('app', App)
+    fix_times('campaign', Campaign)
+    
+  end
+  
+  def fix_times(domain, domain_class)
+    response = SimpledbResource.query(domain, 'next_run_time, interval_update_time, next_daily_run_time', 
+      "next_run_time is null or interval_update_time is null or next_daily_run_time is null", '')
       
-    response.items.each do |item| 
-      app_id = item.name
-      app = App.new(app_id)
+    response.items.each do |response_item| 
+      item_id = response_item.name
+      item = domain_class.new(item_id)
       
-      unless (item.attributes[0])
+      unless (response_item.attributes[0])
         next_run_time = (Time.now.utc + 1.minutes).to_f.to_s
         app.put('next_run_time', next_run_time)     
         Rails.logger.info("Added next_run_time to #{app_id} for #{next_run_time}")
       end
       
-      unless (item.attributes[1])
+      unless (response_item.attributes[1])
         app.put('interval_update_time','60')
         Rails.logger.info("Added interval time to #{app_id} for 60 seconds")
       end
       
-      unless (item.attributes[2])
+      unless (response_item.attributes[2])
         next_run_time = (Time.now.utc + 4.hours).to_f.to_s
         app.put('next_daily_run_time', next_run_time)
         Rails.logger.info("Added next_daily_run_time to #{app_id} for #{next_run_time}")
@@ -28,4 +34,5 @@ class FixAppNilsJob
 
     end
   end
+  
 end
