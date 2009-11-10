739fdaab4ccb7c8da6def36cba81d0e17ee20bcb
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-11-10 09:07:13 +0000

    Adds next_daily_run_time to fix_app_nils
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@191 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/jobs/fix_app_nils_job.rb
+++ tapjoyads/app/jobs/fix_app_nils_job.rb
@@ -1,6 +1,6 @@
 class FixAppNilsJob
   def run
-    response = SimpledbResource.query('app', 'next_run_time, interval_update_time', 
+    response = SimpledbResource.query('app', 'next_run_time, interval_update_time, next_daily_run_time', 
       "next_run_time is null or interval_update_time is null", '')
       
     response.items.each do |item| 
@@ -16,6 +16,12 @@ class FixAppNilsJob
       unless (item.attributes[1])
         app.put('interval_update_time','60')
         Rails.logger.info("Added interval time to #{app_id} for 60 seconds")
+      end
+      
+      unless (item.attributes[2])
+        next_run_time = (Time.now.utc + 4.hours).to_f.to_s
+        app.put('next_daily_run_time', next_run_time)
+        Rails.logger.info("Added next_daily_run_time to #{app_id} for #{next_run_time}")
       end
       
       app.save
