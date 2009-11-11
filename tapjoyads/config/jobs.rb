6f2497eaa6de4b5957b8e92fa36a277ce8f134ea
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-11-11 00:29:55 +0000

    Change campaign stats to run every 2 seconds.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@207 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/config/jobs.rb
+++ tapjoyads/config/jobs.rb
@@ -4,7 +4,7 @@ JobRunner::Gateway.define do |s|
   s.add_job :app_stats, AppStatsJob, 12.seconds
   s.add_job :daily_app_stats, DailyAppStatsJob, 10.minutes
   
-  s.add_job :campaign_stats, CampaignStatsJob, 12.seconds
+  s.add_job :campaign_stats, CampaignStatsJob, 2.seconds
   s.add_job :daily_campaign_stats, DailyCampaignStatsJob, 10.minutes
   
   s.add_job :fix_app_nils, FixAppNilsJob, 10.minutes
