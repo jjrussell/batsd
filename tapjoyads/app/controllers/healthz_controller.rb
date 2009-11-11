327b0e25d8e44fbc3eb6469ad0df35071ab3cd4b
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-11-11 23:38:42 +0000

    Add healthz controller
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@213 6df48309-bd3c-4361-8df8-ec25136b6021

---
+++ tapjoyads/app/controllers/healthz_controller.rb
@@ -0,0 +1,15 @@
+##
+# A controller that is used to easily tell if the system is up.
+#
+# Possible future additions:
+#  Tell if poller is running
+#  Tell if job_runner is running
+#  Tell if simpledb inserts are working
+#  Give information about sqs queues.
+
+class HealthzController < ActionController::Base
+
+  def index
+    render :text => "OK"
+  end
+end
