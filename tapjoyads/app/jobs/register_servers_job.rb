60b14f01953704388b0fb42e9cf8a6f7223db1ff
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-11-12 23:56:05 +0000

    Set up base_url for different run modes.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@235 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/jobs/register_servers_job.rb
+++ tapjoyads/app/jobs/register_servers_job.rb
@@ -3,19 +3,23 @@ class RegisterServersJob
   
   def run
     dns_names = []
+    base_url = ''
     if ENV['RAILS_ENV'] == 'production'
       dns_names = get_dns_names('webserver') | get_dns_names('jobserver')
+      base_url = 'http://localhost:9898'
     elsif ENV['RAILS_ENV'] == 'testing'
       dns_names = get_dns_names('testserver')
+      base_url = 'http://localhost:9898'
     else
       dns_names = ['127.0.0.1']
+      base_url = 'http://localhost:3000'
     end
     
     Rails.logger.info("RegisterServersJob: registering on machines: #{dns_names}")
     
     dns_names.each do |dns_name|
       sess = Patron::Session.new
-      sess.base_url = 'http://localhost:9898'
+      sess.base_url = base_url
     
       sess.username = 'internal'
       sess.password = AuthenticationHelper::USERS[sess.username]
