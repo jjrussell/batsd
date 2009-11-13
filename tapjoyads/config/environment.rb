2b01e1a5df5a8fb12778af4af1b56f324245cac0
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-11-13 01:21:14 +0000

    Fix bugs in restart and configure script. Bump deploy version to 3.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@247 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/config/environment.rb
+++ tapjoyads/config/environment.rb
@@ -83,8 +83,8 @@ Rails::Initializer.run do |config|
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
   
   # These verion numbers indicate the version that is currently deployed.
-  ENV['prod_version'] = '2'
-  ENV['test_version'] = '2'
+  ENV['prod_version'] = '3'
+  ENV['test_version'] = '3'
   
   require 'memcached'
   
