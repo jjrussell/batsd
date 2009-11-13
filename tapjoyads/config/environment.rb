b4b371040a5b701e8fec2e939eb0ecb6ed2fbc2e
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-11-13 00:17:59 +0000

    Change environment to be version 1.1
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@241 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/config/environment.rb
+++ tapjoyads/config/environment.rb
@@ -83,8 +83,8 @@ Rails::Initializer.run do |config|
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
   
   # These verion numbers indicate the version that is currently deployed.
-  ENV['prod_version'] = '1.0'
-  ENV['test_version'] = '1.0'
+  ENV['prod_version'] = '1.1'
+  ENV['test_version'] = '1.1'
   
   require 'memcached'
   
