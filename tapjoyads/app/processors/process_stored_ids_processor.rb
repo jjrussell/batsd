420bcf9bc77e4f18e827b3a8390712148440469c
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-11-16 06:28:24 +0000

    Adds ProcessStoredIdsProcessor, which will process old mssql device id's and put them into simpledb.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@292 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/processors/process_stored_ids_processor.rb
+++ tapjoyads/app/processors/process_stored_ids_processor.rb
@@ -1,10 +1,62 @@
 class ProcessStoredIdsProcessor < ApplicationProcessor
-  
   subscribes_to :process_stored_ids
   
   def on_message(message)
+    start_time = Time.new
+    Rails.logger.debug "ProcessStoredIds: Recieved message: #{message}"
+    
+    sdb = SDB::SDB.new(ENV['AMAZON_ACCESS_KEY_ID'], ENV['AMAZON_SECRET_ACCESS_KEY'])
+    
+    app_items_array = []
+    lookup_item_array = []
+    
+    updated_at = Time.now.utc.to_f.to_s
+    
+    triplets = JSON.parse(message)
+    devices = {}
+    triplets.each do |triplet|
+      device_id = triplet[0]
+      app_id = triplet[1]
+      timestamp = triplet[2]
+      
+      unless devices[device_id]
+        devices[device_id] = []
+      end
+      
+      devices[device_id].push([app_id, timestamp])
+    end
     
     
+    devices.each do |device_id, app_id_list|
+      
+      app_item_attribues = [SDB::Attribute.new('updated-at', updated_at)]
+      app_id_list.each do |pair|
+        app_id = pair[0]
+        timestamp = pair[1]
+        app_item_attribues.push(SDB::Attribute.new("app.#{app_id}", timestamp))
+      end
+      
+      app_item = SDB::Item.new(device_id, app_item_attribues)
+      lookup_item = SDB::Item.new(device_id, [SDB::Attribute.new('app_list', '1'), SDB::Attribute.new('updated-at', updated_at)])
+   
+      app_items_array.push(app_item)
+      lookup_item_array.push(lookup_item)
+      
+      if (app_items_array.length == 25)
+        sdb.batch_put_attributes("#{RUN_MODE_PREFIX}device_app_list_1", app_items_array)
+        sdb.batch_put_attributes("#{RUN_MODE_PREFIX}device_lookup", lookup_item_array)
+        
+        app_items_array = []
+        lookup_item_array = []
+      end
+    end
+    
+    if (app_items_array.length > 0)
+      sdb.batch_put_attributes("#{RUN_MODE_PREFIX}device_app_list_1", app_items_array)
+      sdb.batch_put_attributes("#{RUN_MODE_PREFIX}device_lookup", lookup_item_array)
+    end
+    
+    Rails.logger.info("ProcessStoredIds: Processed #{triplets.length} triplets (#{Time.now - start_time})")
   end
   
 end
