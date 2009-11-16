f39038cef08d512c1852a6bfb6eb1ec3a92e1d50
ben <ben@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-11-16 06:36:29 +0000

    script to add devices to a message
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@295 6df48309-bd3c-4361-8df8-ec25136b6021

---
+++ tapjoyads/script/process_stored_udid_apps.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby
+require 'activemessaging/processor'
+
+include ActiveMessaging::MessageSender
+
+arr = []
+count = 0
+messages = 0
+bad = 0
+
+name = ARGV.first
+File.open(name, "r") do |file|
+  while (line = file.gets) 
+    begin
+      items = line.split(',')
+      udid = items[0]
+      app_id = items[1]
+      timestamp  = Time.parse(items[2].gsub(/\s/,'T')).to_f.to_s
+
+      set = [udid, app_id, timestamp]
+      arr.push(set)
+      count += 1
+      if count == 50
+        message = arr.to_json
+        publish :process_stored_ids, message
+        arr = []
+        messages += 1
+      end
+    rescue => e
+      puts 'Bad_Line: ' + line + e
+      bad += 1
+    end
+  end
+end
+
+if arr.length > 0
+  message = arr.to_json
+  publish :process_stored_ids, message
+  messages += 1
+  arr = []
+end
+
+print "Messages sent: #{messages}"
+print "Bad lines: #{bad}"
+#publish :process_stored_ids, arr.serialize
