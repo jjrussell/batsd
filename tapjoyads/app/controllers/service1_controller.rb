9a0f539782b7d978a6575de0f33535da6b063c7b
ben <ben@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-11-18 01:27:29 +0000

    Redirecting traffic to the right server type (win/ruby) based on the webmethod
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@305 6df48309-bd3c-4361-8df8-ec25136b6021

---
+++ tapjoyads/app/controllers/service1_controller.rb
@@ -0,0 +1,32 @@
+class Service1Controller < ApplicationController
+  
+  before_filter :redirect
+  
+  
+  private
+    def redirect
+      ruby_lb = 'http://webservice-lb-624573684.us-east-1.elb.amazonaws.com/'
+      win_lb = 'http://winweb-lb-1369109554.us-east-1.elb.amazonaws.com/Service1.asmx/'
+      
+      standard_params = "?udid=#{get_param(:DeviceTag, true)}&app_id=#{get_param(:AppID, true)}" +
+        "&device_type=#{get_param(:DeviceType)}&app_version=#{get_param(:AppVersion)}" +
+        "&library_version=#{get_param(:ConnectLibraryVersion)}" +
+        "&device_os_version=#{get_param(:DeviceOSVersion)}"
+      
+      url = case params[:action]
+        when 'Connect' then ruby_lb + "connect" + standard_params
+        when 'AdShown' then ruby_lb + "adshown" + standard_params + "&campaign_id=#{get_param(:CampaignID, true)}"
+        else win_lb + params[:action] + "?" + request.query_string
+      end
+      
+      redirect_to url
+    end
+    
+    def get_param(label, d = false)
+      p = params[label]
+      return "" unless p
+      p = p.downcase if d
+      return CGI::escape(p)
+    end
+    
+end
