4a62165578cf19999c65678281e0986f41a49148
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-11-19 04:04:19 +0000

    Adds /ad action to submit_click controller.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@344 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/submit_click_controller.rb
+++ tapjoyads/app/controllers/submit_click_controller.rb
@@ -39,4 +39,35 @@ XML_END
       end
   end
   
+  def ad
+    xml = <<XML_END
+<?xml version="1.0" encoding="UTF-8"?>
+<TapjoyConnectReturnObject>
+<Success>true</Success>
+</TapjoyConnectReturnObject>
+XML_END
+    
+    if (not params[:campaign_id]) || (not params[:app_id]) || (not params[:udid])
+      error = Error.new
+      error.put('request', request.url)
+      error.put('function', 'adclick')
+      error.put('ip', request.remote_ip)
+      error.save
+      Rails.logger.info "missing required params"
+      render :text => "missing required params"
+      return
+    end
+
+    web_request = WebRequest.new('adclick')
+    web_request.put('campaign_id', params[:campaign_id])
+    web_request.put('app_id', params[:app_id])
+    web_request.put('udid', params[:udid])
+    web_request.put('ip_address', request.remote_ip)
+    
+    web_request.save
+
+    respond_to do |f|
+      f.xml {render(:text => xml)}
+    end
+  end
 end
