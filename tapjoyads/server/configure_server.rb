d2ab1e76cefadc666ee8bf1a7b3fdffe74e30fab
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-11-20 11:59:10 +0000

    Change configure_server so that it doesn't ping register_server.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@412 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/server/configure_server.rb
+++ tapjoyads/server/configure_server.rb
@@ -3,32 +3,21 @@
 # A script that will run when a new ec2 instance is brought up.
 # This script will run as webuser.
 
-require 'rubygems'
-require 'patron'
+unless rails_mode == 'development'
+  security_groups = `curl -s http://169.254.169.254/latest/meta-data/security-groups`.split("\n")
+  if security_groups.include? 'testserver'
+    machine_type = :test
+  elsif security_groups.include? 'masterjobs'
+    machine_type = :master
+  elsif security_groups.include? 'webserver'
+    machine_type = :web
+  elsif security_groups.include? 'website'
+    machine_type = :website
+  end
+else
+  machine_type = :jobs
+end
+
+puts machine_type.to_s
 
 puts `/home/webuser/server/deploy.rb`
-
-type = Base64::decode64(`curl -s http://169.254.169.254/1.0/user-data`)
-
-# Call the register_servers job on all machines, so that they pick up this new machine's memcache.
-server_list = case
-when 'testserver'
-  `script/ec2servers testserver`.split("\n")
-else
-  `script/ec2servers mc`.split("\n")
-end
-
-server_list.each do |server|
-  sess = Patron::Session.new
-  sess.base_url = "#{server}:9898"
-  sess.timeout = 30
-  sess.username = 'internal'
-  sess.password = 'r3sU0oQav2Nl'
-  sess.auth_type = :digest
-
-  begin
-    sess.get("/job/register_servers")
-  rescue Patron::TimeoutError
-    puts "Timed out when registering #{server}"
-  end
-end
