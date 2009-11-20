601359eda80bd2c8802e82c93e9318610a6c075e
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-11-20 11:32:51 +0000

    Set up jobs so that a master server can run different jobs.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@410 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/config/jobs.rb
+++ tapjoyads/config/jobs.rb
@@ -2,10 +2,20 @@ require 'base64'
 
 JobRunner::Gateway.define do |s|
   
-  machine_type = Base64::decode64(`curl -s http://169.254.169.254/1.0/user-data`) if ENV['RAILS_ENV'] != "development"
+  unless ENV['RAILS_ENV'] == 'development'
+    security_groups = Base64::decode64(`curl -s http://169.254.169.254/latest/meta-data/security-groups`).split("\n")
+    if security_groups.include? 'testserver'
+      machine_type = :jobs
+    elsif security_groups.include? 'masterjobs'
+      machine_type = :master
+    else
+      machine_type = :web
+    end
+  else
+    machine_type = :jobs
+  end
   
-  # Expensive jobs. Only run these on job servers.
-  if machine_type == 'jobserver' || ENV['RAILS_ENV'] == "development"
+  if machine_type == :jobs
     s.add_job 'get_ad_network_data', 8.minutes
   
     s.add_job 'app_stats', 30.seconds
@@ -15,8 +25,12 @@ JobRunner::Gateway.define do |s|
     s.add_job 'yesterday_campaign_stats', 30.minutes
   
     s.add_job 'fix_app_nils', 60.minutes
+  elsif machine_type == :master
+    
   end
   
   # Maintenance jobs. Run on all servers:
-  s.add_job 'register_servers', 30.minutes
+  
+  # Memcache servers are hard-coded. Register-servers does not need to run.
+  # s.add_job 'register_servers', 30.minutes
 end
