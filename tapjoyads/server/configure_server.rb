fddd2d09cbeed09429d34eab2ac9623500012a21
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-11-20 12:13:40 +0000

    Fix configure_server to not check for rails_mode.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@417 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/server/configure_server.rb
+++ tapjoyads/server/configure_server.rb
@@ -3,19 +3,15 @@
 # A script that will run when a new ec2 instance is brought up.
 # This script will run as webuser.
 
-unless rails_mode == 'development'
-  security_groups = `curl -s http://169.254.169.254/latest/meta-data/security-groups`.split("\n")
-  if security_groups.include? 'testserver'
-    machine_type = :test
-  elsif security_groups.include? 'masterjobs'
-    machine_type = :master
-  elsif security_groups.include? 'webserver'
-    machine_type = :web
-  elsif security_groups.include? 'website'
-    machine_type = :website
-  end
-else
-  machine_type = :jobs
+security_groups = `curl -s http://169.254.169.254/latest/meta-data/security-groups`.split("\n")
+if security_groups.include? 'testserver'
+  machine_type = :test
+elsif security_groups.include? 'masterjobs'
+  machine_type = :master
+elsif security_groups.include? 'webserver'
+  machine_type = :web
+elsif security_groups.include? 'website'
+  machine_type = :website
 end
 
 puts machine_type.to_s
