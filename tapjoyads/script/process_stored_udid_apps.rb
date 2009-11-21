7bd5c039b3a464bfc66a456c09e0a0bc5c8d3ae6
ben <ben@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-11-21 02:31:28 +0000

    fixed bugs in send stored ids
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@446 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/script/process_stored_udid_apps.rb
+++ tapjoyads/script/process_stored_udid_apps.rb
@@ -12,15 +12,13 @@ name = ARGV.first
 File.open(name, "r") do |file|
   while (line = file.gets) 
     begin
-      items = line.split(',')
-      udid = items[0]
-      app_id = items[1]
-      timestamp  = Time.parse(items[2].gsub(/\s/,'T')).to_f.to_s
+      udid = line
+      app_id = 'f8751513-67f1-4273-8e4e-73b1e685e83d'
 
-      set = [udid, app_id, timestamp]
+      set = [udid, app_id]
       arr.push(set)
       count += 1
-      if count == 50
+      if count % 50
         message = arr.to_json
         publish :process_stored_ids, message
         arr = []
