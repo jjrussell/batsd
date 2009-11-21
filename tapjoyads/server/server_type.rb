d21c7d913cffbea44fc94fd96e9fb4e5d8e76ca2
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-11-21 05:41:49 +0000

    temp change.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@463 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/server/server_type.rb
+++ tapjoyads/server/server_type.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/env ruby
 # Returns the type of server that this server is, by looking at this server's security group.
 
-hostname = `hostname`
-unless (hostname.start_with? 'ip-' or hostname.start_with? 'domU-')
-  # This is not an amazon instance.
-  print 'dev'
-  exit
-end
+# hostname = `hostname`
+# unless (hostname.start_with? 'ip-' or hostname.start_with? 'domU-')
+#   # This is not an amazon instance.
+#   print 'dev'
+#   exit
+# end
 
 
 security_groups = `curl -s http://169.254.169.254/latest/meta-data/security-groups`.split("\n")
