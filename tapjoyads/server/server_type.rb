2c08159571f6be4bbe76981423b314f62ea50015
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-11-21 05:37:32 +0000

    Fix for server_type, so it works on dev machines.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@460 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/server/server_type.rb
+++ tapjoyads/server/server_type.rb
@@ -1,11 +1,12 @@
 #!/usr/bin/env ruby
 # Returns the type of server that this server is, by looking at this server's security group.
 
-# For now, assume this script is run from within amazon.
-# hostname = `hostname`
-# unless (hostname.start_with? 'ip-' or hostname.start_with? 'domU-')
-#   # This is not an amazon instance.
-# end
+hostname = `hostname`
+unless (hostname.start_with? 'ip-' or hostname.start_with? 'domU-')
+  # This is not an amazon instance.
+  print 'dev'
+  exit
+end
 
 
 security_groups = `curl -s http://169.254.169.254/latest/meta-data/security-groups`.split("\n")
