e471bc3e8706a7304440f00f8c9511c566c8b164
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-11-30 20:28:02 +0000

    Adds new method to ec2_helper, which gets the server type.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@506 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/helpers/ec2_helper.rb
+++ tapjoyads/app/helpers/ec2_helper.rb
@@ -3,19 +3,42 @@ require 'AWS'
 module Ec2Helper
   ##
   # Returns an array of the public dns names of all running EC2 servers.
+  # If the group_id is specified, then only servers withing that security group are returned.
   def get_dns_names(group_id = nil)
-    get_field('dnsName', group_id)
+    get_server_field('dnsName', group_id)
   end  
   
   ##
   # Returns an array of the local/private dns names of all running EC2 servers.
+  # If the group_id is specified, then only servers withing that security group are returned.
   def get_local_dns_names(group_id = nil)
-    get_field('privateDnsName', group_id)
+    get_server_field('privateDnsName', group_id)
+  end
+  
+  def get_server_type
+    hostname = `hostname`
+    unless hostname =~ /^ip-|^domU-/
+      # This is not an amazon instance.
+      return 'dev'
+    end
+
+    security_groups = `curl -s http://169.254.169.254/latest/meta-data/security-groups`.split("\n")
+    if security_groups.include? 'testserver'
+      machine_type = 'test'
+    elsif security_groups.include? 'jobserver'
+      machine_type = 'jobs'
+    elsif security_groups.include? 'masterjobs'
+      machine_type = 'masterjobs'
+    else
+      machine_type = 'web'
+    end
+
+    return machine_type
   end
   
   private
   
-  def get_field(field_name, group_id)
+  def get_server_field(field_name, group_id)
     ec2 = AWS::EC2::Base.new({:access_key_id => ENV['AMAZON_ACCESS_KEY_ID'], 
         :secret_access_key => ENV['AMAZON_SECRET_ACCESS_KEY']})
 
