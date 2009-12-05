2471ed74d7aab44eca57fb3e8f42766d6abd34e1
ben <ben@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-12-05 02:44:28 +0000

    removing bad xml
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@558 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/models/simpledb_resource.rb
+++ tapjoyads/app/models/simpledb_resource.rb
@@ -164,7 +164,8 @@ class SimpledbResource
   # Puts a value to be associated with an attribute name.
   def put(attr_name, value, replace = true)
     return if value.nil?
-    value = value.to_s
+    illegal_xml_chars = /\x00|\x01|\x02|\x03|\x04|\x05|\x06|\x07|\x08|\x0B|\x0C|\x0E|\x0F|\x10|\x11|\x12|\x13|\x14|\x15|\x16|\x17|\x18|\x19|\x1A|\x1B|\x1C|\x1D|\x1E|\x1F/
+    value = value.to_s.gsub(illegal_xml_chars,'')
     
     value = value.gsub("\r\n", "^^TAPJOY_NEWLINE^^")
     value = value.gsub("\n", "^^TAPJOY_NEWLINE^^")
