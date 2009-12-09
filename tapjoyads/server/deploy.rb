54e494f892c9b895ad7849a63d4dee1ee6fde26f
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-12-09 03:40:18 +0000

    Add testwebsite to not copy newrelic-web
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@687 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/server/deploy.rb
+++ tapjoyads/server/deploy.rb
@@ -30,7 +30,7 @@ puts `cd /home/webuser/tapjoyads && svn switch #{svn_url}`
 
 if server_type == 'jobs' || server_type == 'masterjobs'
   `mv /home/webuser/tapjoyads/config/newrelic-jobs.yml /home/webuser/tapjoyads/config/newrelic.yml`
-elsif server_type == 'website'
+elsif server_type == 'website' || server_type == 'testwebsite'
   # Do nothing, newrelic.yml is already named correctly.
 else
   `mv /home/webuser/tapjoyads/config/newrelic-web.yml /home/webuser/tapjoyads/config/newrelic.yml`
