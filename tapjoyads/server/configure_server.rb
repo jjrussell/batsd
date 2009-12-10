d8ee79a76577bb8759dade14b7c15ed9ab186c4b
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-12-10 06:29:04 +0000

    Change configure_server to change the svn repo if the server is a website server.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@719 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/server/configure_server.rb
+++ tapjoyads/server/configure_server.rb
@@ -3,5 +3,11 @@
 # A script that will run when a new ec2 instance is brought up.
 # This script will run as webuser.
 
+server_type = `/home/webuser/server/server_type.rb`
+if server_type == 'website' || server_type == 'testwebsite'
+  `rm -rf /home/webuser/tapjoyads`
+  `svn co https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyrailswebsite/trunk/tapjoywebsite /home/webuser/tapjoyads`
+end
+
 puts `/home/webuser/server/deploy.rb`
   
