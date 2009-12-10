05e736a9f37f656e8471cb534d4bb1156038f4c1
ben <ben@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-12-10 06:31:11 +0000

    lookup by store-click
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@720 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/lookup_offerpal_id_controller.rb
+++ tapjoyads/app/controllers/lookup_offerpal_id_controller.rb
@@ -25,4 +25,16 @@ class LookupOfferpalIdController < ApplicationController
     end
   end
   
+  def store
+    record_id = params[:record_id]
+    user = SimpledbResource.select('store-click','*', "publisher_user_record_id = '#{record_id}'")
+    record = user.items.first
+    
+    if record
+      render :text => record.key  #this contains app_id.publisher_user_id
+    else
+      render :text => "not_found"
+    end
+  end
+  
 end
