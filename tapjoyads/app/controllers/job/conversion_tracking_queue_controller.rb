02d2c738bf35a9a07879794cd93685a0ed458d65
ben <ben@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-12-15 10:42:35 +0000

    install currency  through ruby!
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@848 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/job/conversion_tracking_queue_controller.rb
+++ tapjoyads/app/controllers/job/conversion_tracking_queue_controller.rb
@@ -19,66 +19,43 @@ class Job::ConversionTrackingQueueController < Job::SqsReaderController
 
     if (click.get('click_date') && (not click.get('installed')) ) #there has been a click but no install
       
-      if click.get('publisher_app_id') == '48707b62-2cda-47c2-85e7-6e7998dd914d' #EasyApp
-        #for now, make easyapp numbers go to the new system
-        adv_app = App.new(advertiser_app_id)
-        
-        publisher_app_id = click.get('publisher_app_id')
-        currency = Currency.new(publisher_app_id)
-        
-        values = calculate_install_payouts(:currency => currency, :advertiser_app => adv_app)
-        
-        user = SimpledbResource.select('publisher-user-record','*', "record_id = '#{click.get('publisher_user_record_id')}'")
-        if user.items.length == 0
-          click.put('publisher_user_record_id_not_found',click.get('publisher_user_record_id'))
-          click.save #save this item so we can look it up later
-          raise("Install record_id not found: #{record_id} with store click id: #{click.key}")
-        end
-        
-        record = user.items.first
-        publisher_user_id = record.key.split('.')[1]
-        
-        reward = Reward.new
-        reward.put('type', 'install')
-        reward.put('publisher_app_id', publisher_app_id)
-        reward.put('advertiser_app_id', advertiser_app_id)
-        reward.put('publisher_user_id', publisher_user_id)
-        reward.put('advertiser_amount', click.get('advertiser_amount'))
-        reward.put('publisher_amount', click.get('publisher_amount'))
-        reward.put('currency_reward', click.get('currency_reward'))
-        reward.put('tapjoy_amount', click.get('tapjoy_amount'))
-        reward.put('offerpal_amount', click.get('offerpal_amount'))
-        
-        reward.save
-        
-        message = reward.serialize()
-        
-        SqsGen2.new.queue(QueueNames::SEND_CURRENCY).send_message(message)
-        SqsGen2.new.queue(QueueNames::SEND_MONEY_TXN).send_message(message)
-        
-        click.put('installed', "#{install_date.to_s}")
-        click.save
-        
-      else
-        publisher_app_id = click.get('publisher_app_id')
-        publisher_user_record_id = click.get('publisher_user_record_id')
-        click_date = click.get('click_date').to_f
+      adv_app = App.new(advertiser_app_id)
       
-        Rails.logger.info "Processing conversion for #{udid} on #{advertiser_app_id} is for " +
-          "#{publisher_app_id} with record #{publisher_user_record_id}"            
+      publisher_app_id = click.get('publisher_app_id')
+      currency = Currency.new(publisher_app_id)
       
-        win_lb = 'http://winweb-lb-1369109554.us-east-1.elb.amazonaws.com/Service1.asmx/'
-        url = win_lb + "SubmitRewardedInstallConversion?password=asfyrexvlkjewr214314" + 
-          "&PublisherAppId=#{publisher_app_id}&AdvertiserAppId=#{advertiser_app_id}" +
-          "&ClickDate=#{click_date.to_i}&InstallDate=#{install_date.to_i}" +
-          "&DeviceTag=#{udid}&PublisherUserID=#{publisher_user_record_id}" +
-          "&MoneyPaidForInstall=0"
-        
-        download_content(url, {:timeout => 15})
+      values = calculate_install_payouts(:currency => currency, :advertiser_app => adv_app)
       
-        click.put('installed', "#{install_date.to_s}")
-        click.save
+      user = SimpledbResource.select('publisher-user-record','*', "record_id = '#{click.get('publisher_user_record_id')}'")
+      if user.items.length == 0
+        click.put('publisher_user_record_id_not_found',click.get('publisher_user_record_id'))
+        click.save #save this item so we can look it up later
+        raise("Install record_id not found: #{record_id} with store click id: #{click.key}")
       end
+      
+      record = user.items.first
+      publisher_user_id = record.key.split('.')[1]
+      
+      reward = Reward.new
+      reward.put('type', 'install')
+      reward.put('publisher_app_id', publisher_app_id)
+      reward.put('advertiser_app_id', advertiser_app_id)
+      reward.put('publisher_user_id', publisher_user_id)
+      reward.put('advertiser_amount', click.get('advertiser_amount'))
+      reward.put('publisher_amount', click.get('publisher_amount'))
+      reward.put('currency_reward', click.get('currency_reward'))
+      reward.put('tapjoy_amount', click.get('tapjoy_amount'))
+      reward.put('offerpal_amount', click.get('offerpal_amount'))
+      
+      reward.save
+      
+      message = reward.serialize()
+      
+      SqsGen2.new.queue(QueueNames::SEND_CURRENCY).send_message(message)
+      SqsGen2.new.queue(QueueNames::SEND_MONEY_TXN).send_message(message)
+      
+      click.put('installed', "#{install_date.to_s}")
+      click.save
     end
   end
 end
--- tapjoyads/app/controllers/rate_app_offer_controller.rb
+++ tapjoyads/app/controllers/rate_app_offer_controller.rb
@@ -11,44 +11,37 @@ class RateAppOfferController < ApplicationController
     
       app = App.new(app_id)
       
-      if app_id == '48707b62-2cda-47c2-85e7-6e7998dd914d'
-        currency = Currency.new(app_id)
-      
-        values = calculate_offer_payouts(:currency => currency, :offer_amount => 10)
-      
-        ##
-        # Find the user record by snuid
-        user = SimpledbResource.select('publisher-user-record','*', "record_id = '#{record_id}'")
-        if user.items.length == 0
-          raise("Install record_id not found: #{record_id} with rate app_id: #{app_id}")
-        end
-        
-        record = user.items.first
-        publisher_user_id = record.key.split('.')[1]
-        
-        #create the reward item and push to the queues
-        reward = Reward.new
-        reward.put('type', 'rating')
-        reward.put('publisher_app_id', app_id)
-        reward.put('publisher_user_id', publisher_user_id)
-        reward.put('advertiser_amount', '0')
-        reward.put('publisher_amount', '0')
-        reward.put('currency_reward', values[:currency_reward])
-        reward.put('tapjoy_amount', '0')
-        reward.put('offerpal_amount', '0')
-
-        reward.save
-
-        message = reward.serialize()
-
-        SqsGen2.new.queue(QueueNames::SEND_CURRENCY).send_message(message)
-      else
-        
-        message = {:udid => udid, :app_id => app_id, 
-            :record_id => record_id}.to_json
-        SqsGen2.new.queue(QueueNames::RATE_OFFER).send_message(message)
-        
+      currency = Currency.new(app_id)
+    
+      values = calculate_offer_payouts(:currency => currency, :offer_amount => 10)
+    
+      ##
+      # Find the user record by snuid
+      user = SimpledbResource.select('publisher-user-record','*', "record_id = '#{record_id}'")
+      if user.items.length == 0
+        raise("Install record_id not found: #{record_id} with rate app_id: #{app_id}")
       end
+      
+      record = user.items.first
+      publisher_user_id = record.key.split('.')[1]
+      
+      #create the reward item and push to the queues
+      reward = Reward.new
+      reward.put('type', 'rating')
+      reward.put('publisher_app_id', app_id)
+      reward.put('publisher_user_id', publisher_user_id)
+      reward.put('advertiser_amount', '0')
+      reward.put('publisher_amount', '0')
+      reward.put('currency_reward', values[:currency_reward])
+      reward.put('tapjoy_amount', '0')
+      reward.put('offerpal_amount', '0')
+
+      reward.save
+
+      message = reward.serialize()
+
+      SqsGen2.new.queue(QueueNames::SEND_CURRENCY).send_message(message)
+
       redirect_to app.get('store_url')
       return
     end
