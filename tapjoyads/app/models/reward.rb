d1e9e771463689e11d8c38ffebc141fb61eb3362
ben <ben@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-12-15 10:56:56 +0000

    created in reward
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@851 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/models/reward.rb
+++ tapjoyads/app/models/reward.rb
@@ -2,5 +2,6 @@ class Reward < SimpledbResource
   def initialize(key = nil, options = {})
     key = UUIDTools::UUID.random_create.to_s unless key
     super 'reward', key, options
+    put('created', Time.now.utc.to_f.to_s) unless get('created')
   end
 end
