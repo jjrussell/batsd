87f6eb7f70e86c445db1c5562e5c00b6eb2f7e4d
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-12-16 03:30:21 +0000

    Fix unknown variable record_id in conversion_tracker queue
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@885 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/job/conversion_tracking_queue_controller.rb
+++ tapjoyads/app/controllers/job/conversion_tracking_queue_controller.rb
@@ -30,7 +30,7 @@ class Job::ConversionTrackingQueueController < Job::SqsReaderController
       if user.items.length == 0
         click.put('publisher_user_record_id_not_found',click.get('publisher_user_record_id'))
         click.save #save this item so we can look it up later
-        raise("Install record_id not found: #{record_id} with store click id: #{click.key}")
+        raise("Install record_id not found: #{click.get('publisher_user_record_id')} with store click id: #{click.key}")
       end
       
       unless click.get('currency_reward')
