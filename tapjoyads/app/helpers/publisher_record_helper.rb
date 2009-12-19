262a82c7c225a753c60e5ba517f8ecd2147b9a23
ben <ben@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-12-19 10:59:55 +0000

    fix publisher record lookpu
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@963 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/helpers/publisher_record_helper.rb
+++ tapjoyads/app/helpers/publisher_record_helper.rb
@@ -14,7 +14,7 @@ module PublisherRecordHelper
   end
   
   def lookup_by_int_record(int_record_id)
-    record = get_from_cache_and_save("record_id.#{record_id}") do
+    record = get_from_cache_and_save("int_record_id.#{int_record_id}") do
       user = SimpledbResource.select('publisher-user-record','*', "int_record_id = '#{int_record_id}'")
       if user.items.length == 0
         raise("int_record_id not found: #{int_record_id}")
