481ea109a92f4a9d9e8ceefc2db01e5bb3b05ad8
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-12-19 11:02:34 +0000

    Really fix Stats.get_memcache_count_key
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@966 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/models/reward.rb
+++ tapjoyads/app/models/reward.rb
@@ -16,15 +16,15 @@ class Reward < SimpledbResource
     case get('type')
     when 'install'
       increment_count_in_cache(
-        WebRequest.get_memcache_count_key('installs_revenue', get('publisher_app_id'), Time.at(get('created').to_f)), 
+        Stats.get_memcache_count_key('installs_revenue', get('publisher_app_id'), Time.at(get('created').to_f)), 
         false, 1.week, reward.get('publisher_amount').to_i)
         
       increment_count_in_cache(
-        WebRequest.get_memcache_count_key('installs_spend', get('advertiser_app_id'), Time.at(get('created').to_f)), 
+        Stats.get_memcache_count_key('installs_spend', get('advertiser_app_id'), Time.at(get('created').to_f)), 
         false, 1.week, reward.get('advertiser_amount').to_i)
     when 'offer'
       increment_count_in_cache(
-        WebRequest.get_memcache_count_key('offers_revenue', get('publisher_app_id'), Time.at(get('created').to_f)), 
+        Stats.get_memcache_count_key('offers_revenue', get('publisher_app_id'), Time.at(get('created').to_f)), 
         false, 1.week, reward.get('publisher_amount').to_i)      
     end
     
