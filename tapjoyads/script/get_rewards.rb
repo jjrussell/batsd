578aaa2410059ee853e9bfa93173421ecd42a0a9
ben <ben@6df48309-bd3c-4361-8df8-ec25136b6021>
2009-12-31 07:34:48 +0000

    a script to output userids from rewards
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@1078 6df48309-bd3c-4361-8df8-ec25136b6021

---
+++ tapjoyads/script/get_rewards.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+
+# Gets a list of udids who earned currency within a certain period of time
+
+include RightAws
+
+start_time = Time.parse("12:00 AM EST", Time.utc(2009, 12, 23)).utc
+end_time = Time.parse("9:00 PM EST", Time.utc(2009, 12, 29)).utc
+
+app_id = ARGV[2]
+outfile = ARGV[3]
+
+next_token = nil
+count = 0
+
+file = File.new(outfile, "w")
+
+begin
+  where_clause = "publisher_app_id = '#{app_id}' and (type='offer' or type='install') and created >'#{start_time.to_f}' and created< '#{end_time.to_f}'"
+  response = SimpledbResource.select('reward', '*', where_clause, nil, next_token)
+  next_token = response[:next_token]
+  items = response[:items]
+  
+  items.each do |item|
+    userid = item.get('publisher_user_id')
+    currency = item.get('currency_reward')
+    time = item.get('sent_currency')
+    file.syswrite("#{userid},#{currency},#{time}\n")
+    count += 1
+  end
+  puts "Wrote #{count} items to #{outfile}."
+end while next_token != nil
+
+file.close
+
+puts "DONE. Wrote a total of #{count} items to #{outfile}."
