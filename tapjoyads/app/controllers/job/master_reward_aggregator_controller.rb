6be88d72f4aad8abee99f050b06e85b74be699f8
ben <ben@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-01-05 01:56:22 +0000

    fixed last hour of reward aggregator
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@1092 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/job/master_reward_aggregator_controller.rb
+++ tapjoyads/app/controllers/job/master_reward_aggregator_controller.rb
@@ -2,8 +2,8 @@ class Job::MasterRewardAggregatorController < Job::JobController
   include RightAws
   
   def index
-    time = Time.now.utc
-    min_time = Time.utc(time.year, time.month, time.day, time.hour - 1, 0, 0, 0)
+    time = Time.now.utc - 1.hour
+    min_time = Time.utc(time.year, time.month, time.day, time.hour, 0, 0, 0)
     max_time = min_time + 1.hour
     
     msg = { 'start_hour' => min_time.to_f.to_s, 'last_hour' => max_time.to_f.to_s }.to_json
