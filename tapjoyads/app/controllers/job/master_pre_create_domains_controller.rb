0ed763d3aa762841c6766c3961762dd40ce99ea6
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-01-13 01:12:35 +0000

    Fix errors in PreCreateDomains
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@1133 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/job/master_pre_create_domains_controller.rb
+++ tapjoyads/app/controllers/job/master_pre_create_domains_controller.rb
@@ -1,8 +1,9 @@
 # Creates domains that will be used tomorrow
 
 class Job::MasterPreCreateDomainsController < Job::JobController
+  include RightAws
+  
   def index
-    
     msg = (Time.now.utc + 1.day).iso8601[0,10]
     
     SqsGen2.new.queue(QueueNames::PRE_CREATE_DOMAINS).send_message(msg)
--- tapjoyads/app/controllers/job/queue_pre_create_domains_controller.rb
+++ tapjoyads/app/controllers/job/queue_pre_create_domains_controller.rb
@@ -1,6 +1,13 @@
 # Creates domains that will be used tomorrow
 
 class Job::QueuePreCreateDomainsController < Job::SqsReaderController
+  
+  def initialize
+    super QueueNames::PRE_CREATE_DOMAINS
+  end
+  
+  private
+  
   def on_message(message)
     date_string = message.to_s
     
