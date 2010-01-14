ebac44576cbbb3034f0870ca3a84407fdb7f45fd
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-01-14 01:00:52 +0000

    Adds launch_memcached script.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@1167 6df48309-bd3c-4361-8df8-ec25136b6021

---
+++ tapjoyads/server/launch_memcached.rb
@@ -0,0 +1,9 @@
+# Launches memcached servers. This will be run as webuser on startup on all memcached servers.
+
+`killall memcached`
+
+# Get internal ip address
+ifconfig = `/sbin/ifconfig`
+ip_address = ifconfig.match(/inet addr:(.*?)\s/)[1]
+
+`memcached -d -m 1200 -l #{ip_address}`
