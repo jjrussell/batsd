707a8ab0aa6e79af46625a848f9f25e75156345d
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-01-18 19:05:19 +0000

    Get rid of deletion thread, it seems to cause a segfault. Instead just log udid's to be deleted, and we'll delete them later.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@1300 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/script/rebalance_devices.rb
+++ tapjoyads/script/rebalance_devices.rb
@@ -9,37 +9,37 @@ STDOUT.sync = true
 items_to_delete = []
 
 # Deletion thread. Deletes items that have been rebalanced 15 seconds after their rebalancing.
-Thread.new do
-  logger = Logger.new('delete_thread.log')
-  loop do
-    sleep(0.1)
-    item_to_delete = items_to_delete.first
-    next unless item_to_delete
-    next unless Time.now.utc > item_to_delete[:time] + 15
-    old_device_app_list = SimpledbResource.new({:domain_name => "device_app_list_1",
-        :key => item_to_delete[:key]})
-    new_device_app_list = SimpledbResource.new({
-      :domain_name => "device_app_list_#{item_to_delete[:domain_number]}",
-      :key => item_to_delete[:key],
-      :load_from_memcache => false})
-      
-    if old_device_app_list.attributes != new_device_app_list.attributes
-      logger.info "Change in #{old_device_app_list.key}. Resaving."
-      logger.info "Old attrs: #{old_device_app_list.attributes.to_json}"
-      logger.info "New attrs: #{new_device_app_list.attributes.to_json}"
-      
-      new_device_app_list = SimpledbResource.new({
-        :domain_name => "device_app_list_#{item_to_delete[:domain_number]}",
-        :key => item_to_delete[:key],
-        :attrs_to_add => old_device_app_list.attributes})
-      new_device_app_list.save(:updated_at => false)
-    end
-    logger.info "Deleting #{old_device_app_list.key} from device_app_list_1"
-    old_device_app_list.delete_all
-    
-    items_to_delete = items_to_delete[1,items_to_delete.length]
-  end
-end
+# Thread.new do
+#   logger = Logger.new('delete_thread.log')
+#   loop do
+#     sleep(0.1)
+#     item_to_delete = items_to_delete.first
+#     next unless item_to_delete
+#     next unless Time.now.utc > item_to_delete[:time] + 15
+#     old_device_app_list = SimpledbResource.new({:domain_name => "device_app_list_1",
+#         :key => item_to_delete[:key]})
+#     new_device_app_list = SimpledbResource.new({
+#       :domain_name => "device_app_list_#{item_to_delete[:domain_number]}",
+#       :key => item_to_delete[:key],
+#       :load_from_memcache => false})
+#       
+#     if old_device_app_list.attributes != new_device_app_list.attributes
+#       logger.info "Change in #{old_device_app_list.key}. Resaving."
+#       logger.info "Old attrs: #{old_device_app_list.attributes.to_json}"
+#       logger.info "New attrs: #{new_device_app_list.attributes.to_json}"
+#       
+#       new_device_app_list = SimpledbResource.new({
+#         :domain_name => "device_app_list_#{item_to_delete[:domain_number]}",
+#         :key => item_to_delete[:key],
+#         :attrs_to_add => old_device_app_list.attributes})
+#       new_device_app_list.save(:updated_at => false)
+#     end
+#     logger.info "Deleting #{old_device_app_list.key} from device_app_list_1"
+#     old_device_app_list.delete_all
+#     
+#     items_to_delete = items_to_delete[1,items_to_delete.length]
+#   end
+# end
 
 
 to_delete_logger = Logger.new('to_delete.log')
@@ -72,8 +72,8 @@ loop do
       
       to_delete_logger.info device_app_list.key
       
-      items_to_delete.push({:time => Time.now.utc, :key => device_app_list.key, 
-          :domain_number => new_domain_number})
+      #items_to_delete.push({:time => Time.now.utc, :key => device_app_list.key, 
+      #    :domain_number => new_domain_number})
     end
     
     device_lookup.put('app_list', new_domain_number)
