c1d77710251de48ec248a5dd259ed3497c2a9ae7
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-01-21 03:15:51 +0000

    Remove code that does saving - we'll write to sqs and do the saving from queue readers. Work-in-progress.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@1399 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/script/import_udids.rb
+++ tapjoyads/script/import_udids.rb
@@ -4,6 +4,7 @@
 #   script/runner -e <runmode> script/import_udids.rb <app_id> <filename>
 
 require 'logger'
+include RightAws
 
 logger = Logger.new('import_udids.log')
 
@@ -27,72 +28,8 @@ if !/^y/i.match(answer)
   exit
 end
 
-def batch_put(dal_items)
-  
-  domain_number = rand(MAX_DEVICE_APP_DOMAINS)
-  lookup_items = []
-  fixed_dal_items = []
-
-  # Fix the domain name, so that they are all the same.
-  domain_name = "device_app_list_#{domain_number}"
-  
-  start_time = Time.now
-  
-  dal_items.each do |item|
-    fixed_dal_item = DeviceAppList.new({
-      :domain_name => domain_name,
-      :key => item.key,
-      :attributes => item.attributes,
-      :attrs_to_replace => item.attributes,
-      :load => false
-    })
-    fixed_dal_items.push(fixed_dal_item)
-  
-    lookup = DeviceLookup.new(:key => item.key, :load => false)
-    lookup.put('app_list', domain_number)
-    lookup_items.push(lookup)
-  end
-
-  puts "Object creation: #{Time.now.to_f - start_time.to_f}s"
-
-  dal_items.clear()
-  
-  return Thread.new(lookup_items, fixed_dal_items) do |lookup_items, fixed_dal_items|
-    # Write to memcache.
-    fixed_dal_items.each do |item|
-      item.is_new = false
-      item.save(:write_to_sdb => false)
-    end
-    lookup_items.each do |item|
-      item.save(:write_to_sdb => false)
-    end
-    
-    # Now batch_put the items to sdb
-    begin
-      SimpledbResource.put_items(fixed_dal_items)
-    rescue Exception => e
-      puts "Error batch_putting domain_app_list: #{e}"
-      puts fixed_dal_items.to_json
-      sleep(1)
-      retry
-    end
-    
-    begin
-      SimpledbResource.put_items(lookup_items)
-    rescue Exception => e
-      puts "Error batch_putting lookup_items: #{e}"
-      puts lookup_items.to_json
-      sleep(1)
-      retry
-    end
-  end
-end
-
-num_new = 0
-num_repeat = 0
 count = 0
-dal_items = []
-thread_list = []
+udid_list = []
 
 t = Time.now
 
@@ -107,31 +44,17 @@ File.open(filename, "r") do |file|
     end
     
     udid = line.strip
+    #udid_list.push(udid)
+    
+    #SqsGen2.new.queue(QueueNames::IMPORT_UDIDS).send_message(msg)
+    
     dal = DeviceAppList.new(:key => udid)
     dal.set_app_ran(app_key)
     
     if dal.is_new
       num_new += 1
-      dal_items.push(dal)
-      if dal_items.length == 25
-        thread_list.push(batch_put(dal_items))
-      end
     else
       num_repeat += 1
-      dal.save
-    end
-    
-    if num_repeat % 100 == 0
-      # Sleep every 100 num_repeats, so that the save threads get a chance to catch up.
-      sleep(0.5)
-    end
-    
-    if thread_list.length >= 25
-      5.times do |i|
-        thread_list[i].join
-      end
-      
-      thread_list = thread_list[5,thread_list.length]
     end
     
     if (num_new + num_repeat) % 1000 == 0
@@ -140,13 +63,5 @@ File.open(filename, "r") do |file|
     end
   end
 end
-
-batch_put(dal_items)
-
-thread_list.each do |thread|
-  thread.join
-end
-
-sleep(10)
 
 logger.info "Complete. number of new udids: #{num_new}. number of udids already in system: #{num_repeat}."
