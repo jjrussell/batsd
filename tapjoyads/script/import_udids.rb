6b5c10adac94ef91a280a3fec56d89b6ce425c2a
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-01-21 00:27:04 +0000

    Change batch put's to happen in a separate thread. Also add skip parameter, which will skip over the first X items in the file.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@1382 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/script/import_udids.rb
+++ tapjoyads/script/import_udids.rb
@@ -9,12 +9,14 @@ logger = Logger.new('import_udids.log')
 
 app_key = ARGV[2]
 filename = ARGV[3]
+num_to_skip = ARGV[4] || 0
 
 app = App.new(:key => app_key)
 
 num_udids = `wc #{filename}`.split[0]
 
 puts "Will import #{num_udids} udids in to app: '#{app.get('name')}'"
+puts "Skip the first #{num_to_skip} udids."
 puts "Results will be logged to import_udids.log"
 print "Continue? [y/N] "
 STDOUT.flush
@@ -24,50 +26,80 @@ if !/^y/i.match(answer)
 end
 
 def batch_put(dal_items)
-  domain_number = rand(MAX_DEVICE_APP_DOMAINS)
-  lookup_items = []
-  fixed_dal_items = []
   
-  # Fix the domain name, so that they are all the same.
-  domain_name = "device_app_list_#{domain_number}"
-  dal_items.each do |item|
-    fixed_dal_item = DeviceAppList.new({
-      :domain_name => domain_name,
-      :key => item.key,
-      :attributes => item.attributes,
-      :attrs_to_replace => item.attributes,
-      :load => false
-    })
-    fixed_dal_items.push(fixed_dal_item)
+  Thread.new(dal_items) do |items|
+    batch_put_logger = Logger.new('import_udids_batch_puts.log')
+    
+    domain_number = rand(MAX_DEVICE_APP_DOMAINS)
+    lookup_items = []
+    fixed_dal_items = []
+  
+    # Fix the domain name, so that they are all the same.
+    domain_name = "device_app_list_#{domain_number}"
+    items.each do |item|
+      fixed_dal_item = DeviceAppList.new({
+        :domain_name => domain_name,
+        :key => item.key,
+        :attributes => item.attributes,
+        :attrs_to_replace => item.attributes,
+        :load => false
+      })
+      fixed_dal_items.push(fixed_dal_item)
     
     
-    lookup = DeviceLookup.new(:key => item.key)
-    lookup.put('app_list', domain_number)
-    lookup_items.push(lookup)
-  end
+      lookup = DeviceLookup.new(:key => item.key)
+      lookup.put('app_list', domain_number)
+      lookup_items.push(lookup)
+    end
   
-  # Write to memcache.
-  fixed_dal_items.each do |item|
-    item.is_new = false
-    item.save(:write_to_sdb => false)
-  end
-  lookup_items.each do |item|
-    item.save(:write_to_sdb => false)
-  end
+    # Write to memcache.
+    fixed_dal_items.each do |item|
+      item.is_new = false
+      item.save(:write_to_sdb => false)
+    end
+    lookup_items.each do |item|
+      item.save(:write_to_sdb => false)
+    end
   
-  # Now batch_put the items to sdb
-  SimpledbResource.put_items(fixed_dal_items)
-  SimpledbResource.put_items(lookup_items)
+    # Now batch_put the items to sdb
+    begin
+      SimpledbResource.put_items(fixed_dal_items)
+    rescue Exception => e
+      batch_put_logger.info "Error batch_putting domain_app_list: #{e}"
+      batch_put_logger.info fixed_dal_items.to_json
+      sleep(1)
+      retry
+    end
+    
+    begin
+      SimpledbResource.put_items(lookup_items)
+    rescue Exception => e
+      batch_put_logger.info "Error batch_putting lookup_items: #{e}"
+      batch_put_logger.info lookup_items.to_json
+      sleep(1)
+      retry
+    end
+    
+    batch_put_logger.info "Successfully wrote 25 items to domain #{domain_number}"
+  end
   
   dal_items.clear()
 end
 
 num_new = 0
 num_repeat = 0
+count = 0
 dal_items = []
+
+t = Time.now
 
 File.open(filename, "r") do |file|
   while (line = file.gets)
+    count += 1
+    if count < num_to_skip
+      next
+    end
+    
     udid = line.strip
     dal = DeviceAppList.new(:key => udid)
     dal.set_app_ran(app_key)
@@ -88,8 +120,13 @@ File.open(filename, "r") do |file|
       sleep(0.5)
     end
     
+    if num_new % 250 == 0
+      sleep(1)
+    end
+    
     if (num_new + num_repeat) % 1000 == 0
-      logger.info "*** Put #{num_new} new udids and #{num_repeat} repeat."
+      logger.info "*** Put #{num_new} new udids and #{num_repeat} repeat. #{num_new + num_repeat} total. (#{Time.now - t.to_f}s / 1000)"
+      t = Time.now
     end
   end
 end
