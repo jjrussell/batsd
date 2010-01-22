be3997ae9b828b0a8121311f129028cd433aeb2a
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-01-22 06:42:41 +0000

    Set up calculate_show_rate queue reader to actually read.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@1448 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/config/jobs.rb
+++ tapjoyads/config/jobs.rb
@@ -23,6 +23,7 @@ JobRunner::Gateway.define do |s|
     s.add_job 'queue_reward_aggregator', :interval => 5.seconds
     s.add_job 'queue_pre_create_domains', :interval => 1.minutes
     s.add_job 'queue_import_udids', :interval => 10.seconds
+    s.add_job 'queue_calculate_show_rate', :interval => 20.seconds
   elsif machine_type == 'masterjobs'
     s.add_job 'master_cleanup_web_requests', :daily => 2.hours
     s.add_job 'master_create_offers', :interval => 15.minutes
