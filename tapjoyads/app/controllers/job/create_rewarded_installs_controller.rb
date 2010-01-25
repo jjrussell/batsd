a3de06b67d8ab1215f5882f29f69144433894da4
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-01-25 23:40:22 +0000

    Remove unneeded job code in create_rewared_installs, now that we're only using the saved rewarded_installs_list object.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@1487 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/job/create_rewarded_installs_controller.rb
+++ tapjoyads/app/controllers/job/create_rewarded_installs_controller.rb
@@ -26,59 +26,5 @@ class Job::CreateRewardedInstallsController < Job::SqsReaderController
     
     bucket.put('rewarded_installs_list', serialized_app_list.to_json)
     save_to_cache('installs.rewarded_installs_list', serialized_app_list.to_json)
-    
-    #now get the list of all apps with currency
-    app_currency_list = []
-    Currency.select(:where => "currency_name != ''") do |item|
-      app_currency_list.push(item)
-    end
-      
-    #go through and create app-specific lists for each app
-    app_currency_list.each do |currency|
-      
-      Rails.logger.info "Creating rewarded install xml for #{currency.key} of #{currency.get('currency_name')}"
-      banned_apps = currency.get('disabled_apps').split(';') if currency.get('disabled_apps')
-      
-      xml = ""
-      app_list.each do |app|
-        next if (banned_apps) && (banned_apps.include? app.key)
-        next if app.key == currency.key
-        return_offer = ReturnOffer.new(1, app, currency)
-        xml += return_offer.to_xml
-        xml += "TAPJOY_IPHONE_ONLY" if app.get('iphone_only') == '1'
-        xml += "^^TAPJOY_SPLITTER^^"
-      end
-      
-      bucket.put("installs_#{currency.key}", xml)
-      save_to_cache("installs.s3.#{currency.key}", xml)
-      
-      xml = ""
-      app_list.each do |app|
-        next if (banned_apps) && (banned_apps.include? app.key)
-        next if app.key == currency.key
-        return_offer = ReturnOffer.new(1, app, currency)
-        return_offer.AppID = currency.key
-        xml += return_offer.to_server_xml
-        xml += "TAPJOY_IPHONE_ONLY" if app.get('iphone_only') == '1'
-        xml += "^^TAPJOY_SPLITTER^^"
-      end
-        
-      bucket.put("server.installs_#{currency.key}", xml)
-      save_to_cache("server.installs.s3.#{currency.key}", xml)
-      
-      xml = ""
-      app_list.each do |app|
-        next if (banned_apps) && (banned_apps.include? app.key)
-        next if app.key == currency.key
-        return_offer = ReturnOffer.new(1, app, currency)
-        return_offer.AppID = currency.key
-        xml += return_offer.to_server_xml_redirect
-        xml += "TAPJOY_IPHONE_ONLY" if app.get('iphone_only') == '1'
-        xml += "^^TAPJOY_SPLITTER^^"
-      end
-        
-      bucket.put("redirect.installs_#{currency.key}", xml)
-      save_to_cache("redirect.installs.s3.#{currency.key}", xml)
-    end
   end
 end
