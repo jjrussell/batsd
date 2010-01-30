8ff8084e69255f227c25841704b6c10194463f74
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-01-30 00:26:43 +0000

    Only reduce tapjoy's sort-cvr if app is free.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@1564 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/job/create_rewarded_installs_controller.rb
+++ tapjoyads/app/controllers/job/create_rewarded_installs_controller.rb
@@ -59,7 +59,7 @@ class Job::CreateRewardedInstallsController < Job::SqsReaderController
     elsif app.key == 'f8751513-67f1-4273-8e4e-73b1e685e83d'
       # Movies: boost cvr by 25-25%
       boost = 0.25 + rand * 0.1
-    elsif app.get('partner_id') == '70f54c6d-f078-426c-8113-d6e43ac06c6d'
+    elsif app.get('partner_id') == '70f54c6d-f078-426c-8113-d6e43ac06c6d' and app.is_free
       # Tapjoy apps: reduce cvr by 5%
       boost = -0.05
     end
