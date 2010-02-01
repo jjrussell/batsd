921ab90943fb737c07ffe12336c4d706ca26a3b8
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-02-01 22:26:57 +0000

    Don't process reward messages if advertiser_app_id is nil.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@1591 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/job/send_money_txn_controller.rb
+++ tapjoyads/app/controllers/job/send_money_txn_controller.rb
@@ -14,7 +14,11 @@ class Job::SendMoneyTxnController < Job::SqsReaderController
     unless reward.get('sent_money_txn')
       Rails.logger.info "Sending money transaction to sql: #{reward.key}"
 
-      unless reward.get('publisher_amount') and reward.get('advertiser_app_id')
+      unless reward.get('publisher_amount')
+        if reward.get('advertiser_app_id').nil?
+          return
+        end
+        
         values = calculate_install_payouts(
             :currency => Currency.new(:key => reward.get('publisher_app_id')), 
             :advertiser_app => App.new(:key => reward.get('advertiser_app_id')))
