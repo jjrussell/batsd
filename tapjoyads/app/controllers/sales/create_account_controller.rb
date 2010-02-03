20ac68543a379ac20932c6be7a5ccdc18ad86b25
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-02-03 20:04:10 +0000

    Updates to create_account sales tool. Remove password field, and duplicate email fields. Better messaging.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@1642 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/sales/create_account_controller.rb
+++ tapjoyads/app/controllers/sales/create_account_controller.rb
@@ -6,8 +6,8 @@ class Sales::CreateAccountController < Sales::SalesController
   end
 
   def confirm
-    unless verify_params([:sales_person_id, :email, :password, :company_name, :contact_name,
-        :contact_email, :contact_phone, :free_credits], 
+    unless verify_params([:sales_person_id, :email, :company_name, :contact_name,
+        :contact_phone, :free_credits], 
         {:allow_empty => false, :render_missing_text => false})
       flash[:error] = "Cannot leave any fields blank"
       render 'index'
@@ -18,23 +18,22 @@ class Sales::CreateAccountController < Sales::SalesController
   def create
     free_credits_in_cents = params[:free_credits].to_i * 100
     
-    url = 'http://ws.tapjoyads.com/Service1.asmx/CreateAccount?sales_password=uew862nvm01ds' +
+    url = 'http://winweb-lb-1369109554.us-east-1.elb.amazonaws.com' +
+        '/Service1.asmx/CreateAccount?sales_password=uew862nvm01ds' +
         "&email=#{CGI::escape(params[:email])}" +
-        "&password=#{CGI::escape(params[:password])}" +
         "&sales_person_id=#{CGI::escape(params[:sales_person_id])}" +
         "&company_name=#{CGI::escape(params[:company_name])}" +
         "&contact_name=#{CGI::escape(params[:contact_name])}" +
-        "&contact_email=#{CGI::escape(params[:contact_email])}" +
+        "&contact_email=#{CGI::escape(params[:email])}" +
         "&contact_phone=#{CGI::escape(params[:contact_phone])}" +
         "&free_credits_in_cents=#{free_credits_in_cents}"
     
     response = download_content(url, :timeout => 30)
-    flash[:info] = response
-    # if response.status = 200
-    #   flash[:info] = "Successfully created account for #{params[:email]}"
-    # else
-    #   flash[:error] = "Failed to make account for #{params[:email]}"
-    # end
+    if response =~ />OK<\/string>/
+      flash[:info] = "Successfully created account for #{params[:email]}"
+    else
+      flash[:error] = response
+    end
     
     redirect_to "/sales/create_account?sales_person_id=#{params[:sales_person_id]}"
   end
