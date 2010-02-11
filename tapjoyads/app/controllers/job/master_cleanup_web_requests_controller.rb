442dbc799603b5d0e9303b0c74b0f7855b183e42
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-02-11 02:18:58 +0000

    Change master_cleanup_web_requests to only retry every 3rd day, since cleanup jobs take longer than a day to run. This will stop double-runs.
    Re-enable master_cleanup_web_requests, but 5 hours after the utc day, rather than 2 hours.
    
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@1689 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/job/master_cleanup_web_requests_controller.rb
+++ tapjoyads/app/controllers/job/master_cleanup_web_requests_controller.rb
@@ -2,15 +2,15 @@ class Job::MasterCleanupWebRequestsController < Job::JobController
   include SqsHelper
   
   def index
-    # Cleanup 2 days ago. Also cleanup through 7 days prior, in case any previous cleanups failed.
+    # Cleanup 2 days ago. Also retry cleanups for previous days, in case previous cleanups failed.
     # Attempting to cleanup a domain that has already been erased and backed up
     # doesn't have any adverse affects.
     day = Time.now.utc - 2.days
     
-    7.times do
+    3.times do
       date_string = day.iso8601[0,10]
       send_to_sqs(QueueNames::CLEANUP_WEB_REQUESTS, date_string)
-      day = day - 1.days
+      day = day - 3.days
     end
     
     render :text => 'ok'
