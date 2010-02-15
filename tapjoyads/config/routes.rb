a9ec7b96259c007284a1d5d068da43caacfd03e5
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-02-15 20:30:21 +0000

    Remove :controller.:format route.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@1720 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/config/routes.rb
+++ tapjoyads/config/routes.rb
@@ -104,6 +104,6 @@ ActionController::Routing::Routes.draw do |map|
   # consider removing the them or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
-  map.connect ':controller.:format'
+  #map.connect ':controller.:format'
     
 end
