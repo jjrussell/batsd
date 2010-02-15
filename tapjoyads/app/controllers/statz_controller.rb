3ce0378291b9ac64282dedb81a043aaf95c32b6c
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-02-15 16:45:02 +0000

    Add statz json feed.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@1706 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/statz_controller.rb
+++ tapjoyads/app/controllers/statz_controller.rb
@@ -5,37 +5,40 @@ class StatzController < ApplicationController
   before_filter 'authenticate'
   
   def index
-    @install_count_24hours = get_from_cache('statusz.install_count_24hours')
-    @app_list = get_from_cache('statusz.app_list')
-    @appstats_list = get_from_cache('statusz.appstats_list')
-    @last_updated = get_from_cache('statusz.last_updated')
+    @install_count_24hours = get_from_cache('statz.install_count_24hours')
+    @app_list = get_from_cache('statz.app_list')
+    @appstats_list = get_from_cache('statz.appstats_list')
+    @last_updated = get_from_cache('statz.last_updated')
     
-    if params[:reload] != '1' and @install_count_24hours and @app_list and @appstats_list and @last_updated
-      return
+    unless params[:reload] != '1' and @install_count_24hours and @app_list and @appstats_list and @last_updated
+      @install_count_24hours = StoreClick.count(:where => "installed > '#{Time.now.to_f - 1.day}'")
+
+      @app_list = []
+      App.select(:where => "interval_update_time = '3600'") do |app|
+        @app_list.push(app)
+      end
+
+      now = Time.now.utc
+
+      @appstats_list = []
+      @app_list.each do |app|
+        @appstats_list.push(Appstats.new(app.key, {
+          :stat_types => ['paid_installs', 'paid_clicks', 'logins', 'new_users', 'published_installs', 'installs_opened', 'hourly_impressions'],
+          :start_time => now - 23.hours,
+          :end_time => now + 1.hour}))
+      end
+
+      @last_updated = Time.now
+
+      save_to_cache('statz.install_count_24hours', @install_count_24hours)
+      save_to_cache('statz.app_list', @app_list)
+      save_to_cache('statz.appstats_list', @appstats_list)
+      save_to_cache('statz.last_updated', @last_updated)
     end
     
-    @install_count_24hours = StoreClick.count(:where => "installed > '#{Time.now.to_f - 1.day}'")
-
-    @app_list = []
-    App.select(:where => "interval_update_time = '3600'") do |app|
-      @app_list.push(app)
+    respond_to do |f|
+      f.json
+      f.html
     end
-
-    now = Time.now.utc
-    
-    @appstats_list = []
-    @app_list.each do |app|
-      @appstats_list.push(Appstats.new(app.key, {
-        :stat_types => ['paid_installs', 'paid_clicks', 'logins', 'new_users', 'published_installs', 'installs_opened', 'hourly_impressions'],
-        :start_time => now - 23.hours,
-        :end_time => now + 1.hour}))
-    end
-
-    @last_updated = Time.now
-    
-    save_to_cache('statusz.install_count_24hours', @install_count_24hours)
-    save_to_cache('statusz.app_list', @app_list)
-    save_to_cache('statusz.appstats_list', @appstats_list)
-    save_to_cache('statusz.last_updated', @last_updated)
   end
 end
