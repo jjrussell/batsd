639c38859f0904d00f399b22e37823ad6a58335c
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-02-15 20:45:18 +0000

    Render something for master_reload_statz.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@1726 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/job/master_reload_statz_controller.rb
+++ tapjoyads/app/controllers/job/master_reload_statz_controller.rb
@@ -3,12 +3,14 @@ class Job::MasterReloadStatzController < Job::JobController
   def index
     sess = Patron::Session.new
     sess.base_url = 'localhost:9898'
-    sess.timeout = 60
+    sess.timeout = 120
     sess.username = 'internal'
     sess.password = AuthenticationHelper::USERS[sess.username]
     sess.auth_type = :digest
 
     sess.get("/statz.json?reload=1")
+    
+    render :text => 'ok'
   end
   
 end
