a6394699258d827d6eb65d2124b0786a3f134fe4
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-02-16 22:15:33 +0000

    Add add_virtual_good method to PointPurchases
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@1785 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/models/point_purchases.rb
+++ tapjoyads/app/models/point_purchases.rb
@@ -2,12 +2,25 @@
 # Store information about a single udid's currency, virtual good purchases, and challenges 
 # for a single app.
 class PointPurchases < SimpledbResource
-  self.key_format = "udid.app_guid"
+  self.key_format = "udid.app_id"
   
   def dynamic_domain_name
-    domain_number = @key.hash % 10
+    domain_number = @key.hash % NUM_POINT_PURCHASES_DOMAINS
     
     return "point_purchases_#{domain_number}"
+  end
+  
+  
+  def add_virtual_good(virtual_good_key)
+    virtual_goods_string = get('virtual_goods') || '{}'
+    user_virtual_goods = JSON.parse(virtual_goods_string)
+
+    if user_virtual_goods[virtual_good_key]
+      user_virtual_goods[virtual_good_key] += 1
+    else
+      user_virtual_goods[virtual_good_key] = 1
+    end
+    set('virtual_goods', user_virtual_goods.to_json)
   end
   
 end
