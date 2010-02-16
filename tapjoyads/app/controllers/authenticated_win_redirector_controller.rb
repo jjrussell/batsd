12aff5a179631d6aced40bc9015493a4037cba26
ben <ben@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-02-16 02:43:57 +0000

    win_redirector goes to test link
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@1760 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/authenticated_win_redirector_controller.rb
+++ tapjoyads/app/controllers/authenticated_win_redirector_controller.rb
@@ -11,7 +11,7 @@ class AuthenticatedWinRedirectorController < ApplicationController
   def redirect
     # Note that this method is not fully portable, as request_uri returns different values
     # based on the server type (ie, IIS leaves it blank). This works fine for apache though.
-    redirect_to 'http://winweb-lb-1369109554.us-east-1.elb.amazonaws.com' + request.request_uri
-    #redirect_to 'http://www.tapjoyconnect.com.asp1-3.dfw1-1.websitetestlink.com' + request.request_uri
+    #redirect_to 'http://winweb-lb-1369109554.us-east-1.elb.amazonaws.com' + request.request_uri
+    redirect_to 'http://www.tapjoyconnect.com.asp1-3.dfw1-1.websitetestlink.com' + request.request_uri
   end
 end
