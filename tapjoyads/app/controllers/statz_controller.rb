7bff02de3755be2c55d5a4a7000d176a979805c7
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-02-18 22:33:11 +0000

    Fix error with appstats_list being too large to store in memcache.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@1836 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/statz_controller.rb
+++ tapjoyads/app/controllers/statz_controller.rb
@@ -7,10 +7,13 @@ class StatzController < ApplicationController
   def index
     @install_count_24hours = get_from_cache('statz.install_count_24hours')
     @app_list = get_from_cache('statz.app_list')
-    @appstats_list = get_from_cache('statz.appstats_list')
     @last_updated = get_from_cache('statz.last_updated')
+    @appstats_list = []
+    @app_list.each do |app|
+      @appstats_list.push(get_from_cache("statz.appstats.#{app.key}"))
+    end
     
-    unless params[:reload] != '1' and @install_count_24hours and @app_list and @appstats_list and @last_updated
+    unless params[:reload] != '1' and @install_count_24hours and @app_list and @last_updated
       @install_count_24hours = StoreClick.count(:where => "installed > '#{Time.now.to_f - 1.day}'")
 
       @app_list = []
@@ -23,14 +26,15 @@ class StatzController < ApplicationController
 
       @appstats_list = []
       @app_list.each do |app|
-        @appstats_list.push(Appstats.new(app.key, {
+        appstats = Appstats.new(app.key, {
           :start_time => now - 23.hours,
-          :end_time => now + 1.hour}))
+          :end_time => now + 1.hour})
+        @appstats_list.push(appstats)
+        save_to_cache("statz.appstats.#{app.key}", appstats)
       end
 
       save_to_cache('statz.install_count_24hours', @install_count_24hours)
       save_to_cache('statz.app_list', @app_list)
-      save_to_cache('statz.appstats_list', @appstats_list)
       save_to_cache('statz.last_updated', @last_updated)
     end
     
