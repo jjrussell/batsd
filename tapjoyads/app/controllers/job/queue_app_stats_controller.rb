373d799ff007fb19975e7661329ec5bca0877003
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-02-19 23:38:34 +0000

    Fix queue_app_stats to aggregate using advertiser_app_id only for store_click and store_install paths
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@1927 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/job/queue_app_stats_controller.rb
+++ tapjoyads/app/controllers/job/queue_app_stats_controller.rb
@@ -150,7 +150,11 @@ class Job::QueueAppStatsController < Job::SqsReaderController
       app_condition = "publisher_app_id = '#{app_key}'"
     else
       stat_name = WebRequest::PATH_TO_STAT_MAP[wr_path]
-      app_condition = "(app_id = '#{app_key}' or advertiser_app_id = '#{app_key}')"
+      if WebRequest::USE_ADVERTISER_APP_ID.include?(wr_path)
+        app_condition = "advertiser_app_id = '#{app_key}')"
+      else
+        app_condition = "app_id = '#{app_key}'"
+      end
     end
     date = time.iso8601[0,10]
     
