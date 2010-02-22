9fd2906ab5a59025dfe227e2f50db78ae6d1e78a
ben <ben@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-02-22 04:32:09 +0000

    adding tap_points controller for tap_points currency service
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@1962 6df48309-bd3c-4361-8df8-ec25136b6021

---
+++ tapjoyads/app/controllers/tap_points_controller.rb
@@ -0,0 +1,34 @@
+class TapPointsController < ApplicationController
+  include DownloadHelper
+  
+  def add
+    
+    parts = params[:snuid].split('.')
+    
+    if parts.length < 2
+      url = "http://www.tapjoyconnect.com.asp1-3.dfw1-1.websitetestlink.com/Service1.asmx/LookupPointId?pointid=#{params[:snuid]}"
+      response = download_content(url, :return_response => true)
+      raise "snuid: #{params[:snuid]} not found in mosso lookup: #{response.body}" if response.status != 200 
+      parts = response.body.split('.')
+    end
+      
+    udid = parts[0]
+    app_id = parts[1]
+      
+
+    amount = params[:currency]
+    
+    lock_on_key("lock.purchase_vg.#{udid}.#{app_id}") do
+      point_purchases = PointPurchases.new(:key => "#{params[:udid]}.#{params[:app_id]}")
+      
+      Rails.logger.info "Adding #{amount} from #{params[:snuid]}, to user balance: #{point_purchases.points}"
+  
+      
+      point_purchases.points = point_purchases.points + amount.to_i
+  
+      point_purchases.serial_save(:catch_exceptions => false)
+    end
+  
+  end
+  
+end
