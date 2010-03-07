9547b9f69be433cfe1fd06b99c3ce2bca4958994
ben <ben@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-03-07 10:47:41 +0000

    fix key
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@2140 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/enter_promotion.rb
+++ tapjoyads/app/controllers/enter_promotion.rb
@@ -2,12 +2,13 @@ class EnterPromotionController < ApplicationController
   def index
     return unless verify_params([:app_id, :udid])
     
-    pe = PromotionEntry.new(:key => "#{params[:udid]}.#{params[:promo_id]}")
+    pe = PromotionEntry.new
     pe.put('last5', params[:last5])
     pe.put('email', params[:email])
     pe.put('phone', params[:phone])
     pe.put('udid', params[:udid])
     pe.put('app_id', params[:app_id])
+    pe.put('promo_id', params[:promo_id])
     
     pe.save
 
