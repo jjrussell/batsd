f98ddf8639f1e100e323f88b5a9ab83300913649
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-03-22 21:39:36 +0000

    Fix S3 key usage in FailedSqsWrites
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@2241 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/job/master_failed_sqs_writes_controller.rb
+++ tapjoyads/app/controllers/job/master_failed_sqs_writes_controller.rb
@@ -8,14 +8,14 @@ class Job::MasterFailedSqsWritesController < Job::JobController
   def index
     bucket = S3.new.bucket('failed-sqs-writes')
     bucket.keys.each do |key|
-      json_string = bucket.get(key).to_s
+      json_string = bucket.get(key.to_s).to_s
       json = JSON.parse(json_string)
       message = json['message']
       queue_name = json['queue_name']
       
       begin
         SqsGen2.new.queue(queue_name).send_message(message)
-        bucket.delete_folder(key)
+        bucket.delete_folder(key.to_s)
       rescue RightAws::AwsError => e
         log_line = "FailedSqsWrites job failed to write message #{message} to queue #{queue_name}, with error: #{e}"
         Rails.logger.info log_line
