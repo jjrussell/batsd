992e2d8cdba97e36a5e0d846e25610d73a8e67f1
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-03-23 20:45:39 +0000

    Connect to internal ip address, not 127.0.0.1
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@2249 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/server/memcached_monitor.rb
+++ tapjoyads/server/memcached_monitor.rb
@@ -6,13 +6,17 @@ require 'logger'
 require 'socket'
 logger = Logger.new("/home/webuser/memcached_monitor.log", 7, 10000000)
 
+# Get internal ip address
+ifconfig = `/sbin/ifconfig`
+ip_address = ifconfig.match(/inet addr:(.*?)\s/)[1]
+
 begin
   loop do
-    t = TCPSocket.new('127.0.0.1', 11211)
+    t = TCPSocket.new(ip_address, 11211)
     logger.info "OK"
     sleep(30)
   end
 rescue
-  logger.error "No connection could be made to 127.0.0.1:11211. Restarting memcached"
+  logger.error "No connection could be made to #{ip_address}:11211. Restarting memcached"
   `/home/webuser/server/launch_memcached.rb`
 end
