1a3b3081a69addaf2be62e749180f9d5ab54d8c6
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-04-07 22:12:48 +0000

    add a data miner lib to contain random scripts
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@2435 6df48309-bd3c-4361-8df8-ec25136b6021

---
+++ tapjoyads/lib/data_miner.rb
@@ -0,0 +1,17 @@
+class DataMiner
+
+  # returns a hash of store_click counts by country for the given app_id for yesterday
+  def self.store_clicks_by_country_for_app_id(app_id)
+    day = Time.zone.now.yesterday.to_date.to_s
+    counts = {}
+    0.upto(MAX_WEB_REQUEST_DOMAINS - 1) do |i|
+      items = WebRequest.select(:where => "advertiser_app_id = '#{app_id}' and path = 'store_click'", :domain_name => "web-request-#{day}-#{i}")[:items]
+      items.each do |item|
+        country = item.get('country')
+        counts[country] = ((counts[country] || 0) + 1)
+      end
+    end
+    counts
+  end
+
+end
