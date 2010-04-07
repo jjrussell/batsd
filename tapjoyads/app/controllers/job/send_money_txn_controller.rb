ad92b488753b049ca409e02d7b9261ce6490409a
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-04-07 19:55:09 +0000

    fix error
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@2432 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/job/send_money_txn_controller.rb
+++ tapjoyads/app/controllers/job/send_money_txn_controller.rb
@@ -53,13 +53,13 @@ class Job::SendMoneyTxnController < Job::SqsReaderController
         c.publisher_app_id = reward.get('publisher_app_id')
         c.advertiser_amount = reward.get('advertiser_amount')
         c.publisher_amount = reward.get('publisher_amount')
-        c.tapjoy_amount = values[:tapjoy_amount].to_i + values[:offerpal_amount].to_i
+        c.tapjoy_amount = reward.get('tapjoy_amount').to_i + reward.get('offerpal_amount').to_i
         c.reward_type_string = reward.get('type')
       end
       conversion.save!
       
       reward.put('sent_money_txn', Time.now.utc.to_f.to_s)
-      reward.save      
+      reward.save
       
     end
   end
