25655623ec8cacefb5902c05204b5eb797fe351d
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-04-08 01:17:00 +0000

    fix the import conversions script
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@2440 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/lib/one_offs.rb
+++ tapjoyads/lib/one_offs.rb
@@ -4,27 +4,38 @@ class OneOffs
     file = File.open('tmp/conversions.txt', 'r')
     first_line = true
     file.each_line do |line|
+
       # nasty hack alert!
       # for some reason the first line of this imput file doesn't work so just print
       # it and manually insert the record
       if first_line
+        puts "*** first line ***"
         puts line
         first_line = false
         next
       end
+
       vals = line.split(' ', 11)
-      c = Conversion.new
-      c.id = vals[0].downcase
-      c.reward_id = vals[7].downcase unless vals[7] == 'NULL'
-      c.advertiser_app_id = vals[2].downcase unless vals[2] == 'NULL'
-      c.publisher_app_id = vals[1].downcase
-      c.advertiser_amount = vals[4].to_i
-      c.publisher_amount = vals[3].to_i
-      c.tapjoy_amount = vals[5].to_i + vals[6].to_i
-      c.reward_type = vals[9].to_i
-      c.reward_type = 999 if c.reward_type == 6
-      c.created_at = Time.zone.parse(vals[10])
-      c.save!
+      unless vals.length == 11
+        puts "*** weird line ***"
+        puts line
+        next
+      end
+
+      if Conversion.find_by_id(vals[0].downcase).nil?
+        c = Conversion.new
+        c.id = vals[0].downcase
+        c.reward_id = vals[7].downcase unless vals[7] == 'NULL'
+        c.advertiser_app_id = vals[2].downcase unless vals[2] == 'NULL'
+        c.publisher_app_id = vals[1].downcase
+        c.advertiser_amount = vals[4].to_i
+        c.publisher_amount = vals[3].to_i
+        c.tapjoy_amount = vals[5].to_i + vals[6].to_i
+        c.reward_type = vals[9].to_i
+        c.reward_type = 999 if c.reward_type == 6
+        c.created_at = Time.zone.parse(vals[10])
+        c.save!
+      end
     end
     file.close
   end
