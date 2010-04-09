2c59301d0545317f7d3fa6848496089f01d7d275
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-04-09 22:58:21 +0000

    Actually send email paramater to 4info.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@2478 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/list_signup_controller.rb
+++ tapjoyads/app/controllers/list_signup_controller.rb
@@ -43,7 +43,7 @@ class ListSignupController < ApplicationController
       url = 'http://www.4infoalerts.com/wap/tapjoy/post_email_address' +
           "?campaignId=#{@advertiser_app.custom_app_id}" +
           "&id=#{signup.key}" +
-          "&email=#{signup.email_address}" +
+          "&email=#{params[:email_address]}" +
           "&reward=#{reward_text}" +
           "&udid=#{signup.udid}" +
           "&key=#{key}"
