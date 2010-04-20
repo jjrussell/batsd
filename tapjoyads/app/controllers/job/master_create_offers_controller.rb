32b739b7211db2b7d6f7a156521deab5e19db82d
ben <ben@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-04-20 23:06:35 +0000

    Run create_rewarded_installs every 1 minute instead of every 15 minutes
    And make it so create_offers is run every 10 minutes
    
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@2567 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/job/master_create_offers_controller.rb
+++ tapjoyads/app/controllers/job/master_create_offers_controller.rb
@@ -2,7 +2,7 @@ class Job::MasterCreateOffersController < Job::JobController
   include SqsHelper
   
   def index
-    send_to_sqs(QueueNames::CREATE_OFFERS, 'run')
+    send_to_sqs(QueueNames::CREATE_OFFERS, 'run') if rand < 0.1
     send_to_sqs(QueueNames::CREATE_REWARDED_INSTALLS, 'run')
     
     render :text => 'ok'
--- tapjoyads/config/jobs.rb
+++ tapjoyads/config/jobs.rb
@@ -27,7 +27,7 @@ JobRunner::Gateway.define do |s|
     s.add_job 'queue_calculate_show_rate', :interval => 20.seconds
   elsif machine_type == 'masterjobs'
     s.add_job 'master_cleanup_web_requests', :daily => 5.hours
-    s.add_job 'master_create_offers', :interval => 15.minutes
+    s.add_job 'master_create_offers', :interval => 1.minutes
     s.add_job 'master_app_stats', :interval => 2.minutes
     s.add_job 'master_reward_aggregator', :hourly => 5.minutes
     s.add_job 'master_pre_create_domains', :daily => 6.hours
