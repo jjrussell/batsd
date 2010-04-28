9a9a5733008f5810b572452b53ddc95c42f78b5a
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-04-28 04:42:25 +0000

    use one_offs.rb instead of data_miner.rb
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@3378 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/lib/one_offs.rb
+++ tapjoyads/lib/one_offs.rb
@@ -103,4 +103,18 @@ class OneOffs
     end
   end
   
+  # returns a hash of yesterday's store_click counts by country for the given advertiser_app_id
+  def self.store_clicks_by_country_for_advertiser_app_id(advertiser_app_id)
+    day = Time.zone.now.yesterday.to_date.to_s
+    counts = {}
+    0.upto(MAX_WEB_REQUEST_DOMAINS - 1) do |i|
+      items = WebRequest.select(:where => "advertiser_app_id = '#{advertiser_app_id}' and path = 'store_click'", :domain_name => "web-request-#{day}-#{i}")[:items]
+      items.each do |item|
+        country = item.get('country')
+        counts[country] = ((counts[country] || 0) + 1)
+      end
+    end
+    counts
+  end
+  
 end
