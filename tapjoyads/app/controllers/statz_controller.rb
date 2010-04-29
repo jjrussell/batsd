d5757c8cdc54292fe9fcb23dbc8857df8d707430
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-04-29 00:59:16 +0000

    Don't cache app_list, since it is too large to cache.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@3402 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/statz_controller.rb
+++ tapjoyads/app/controllers/statz_controller.rb
@@ -6,17 +6,16 @@ class StatzController < ApplicationController
   
   def index
     @install_count_24hours = get_from_cache('statz.install_count_24hours')
-    @app_list = get_from_cache('statz.app_list')
     @last_updated = get_from_cache('statz.last_updated')
     @appstats_list = []
     
+    @app_list = []
+    SdbApp.select(:where => "interval_update_time = '3600'") do |app|
+      @app_list.push(app)
+    end
+    
     unless params[:reload] != '1' and @install_count_24hours and @app_list and @last_updated
       @install_count_24hours = StoreClick.count(:where => "installed > '#{Time.now.to_f - 1.day}'")
-
-      @app_list = []
-      SdbApp.select(:where => "interval_update_time = '3600'") do |app|
-        @app_list.push(app)
-      end
 
       now = Time.now.utc
       @last_updated = now
@@ -31,7 +30,6 @@ class StatzController < ApplicationController
       end
 
       save_to_cache('statz.install_count_24hours', @install_count_24hours)
-      save_to_cache('statz.app_list', @app_list)
       save_to_cache('statz.last_updated', @last_updated)
     else
       @app_list.each do |app|
