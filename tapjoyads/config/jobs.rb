970a1e9395211fbf9f06aceff38df7b2dad70d98
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-04-30 01:01:36 +0000

    disable calculate show rates
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@3433 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/config/jobs.rb
+++ tapjoyads/config/jobs.rb
@@ -24,7 +24,7 @@ JobRunner::Gateway.define do |s|
     s.add_job 'queue_reward_aggregator', :interval => 5.seconds
     s.add_job 'queue_pre_create_domains', :interval => 1.minutes
     s.add_job 'queue_import_udids', :interval => 10.seconds
-    s.add_job 'queue_calculate_show_rate', :interval => 20.seconds
+    # s.add_job 'queue_calculate_show_rate', :interval => 20.seconds
     s.add_job 'queue_calculate_next_payout', :interval => 5.minutes
   elsif machine_type == 'masterjobs'
     s.add_job 'master_cleanup_web_requests', :daily => 5.hours
