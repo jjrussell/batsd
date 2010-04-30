bd8bb046751a147d5145f63030399d666049983b
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-04-30 01:03:51 +0000

    disable the master calculate show rate and enable the reader
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@3436 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/config/jobs.rb
+++ tapjoyads/config/jobs.rb
@@ -24,7 +24,7 @@ JobRunner::Gateway.define do |s|
     s.add_job 'queue_reward_aggregator', :interval => 5.seconds
     s.add_job 'queue_pre_create_domains', :interval => 1.minutes
     s.add_job 'queue_import_udids', :interval => 10.seconds
-    # s.add_job 'queue_calculate_show_rate', :interval => 20.seconds
+    s.add_job 'queue_calculate_show_rate', :interval => 20.seconds
     s.add_job 'queue_calculate_next_payout', :interval => 5.minutes
   elsif machine_type == 'masterjobs'
     s.add_job 'master_cleanup_web_requests', :daily => 5.hours
@@ -32,7 +32,7 @@ JobRunner::Gateway.define do |s|
     s.add_job 'master_app_stats', :interval => 2.minutes
     s.add_job 'master_reward_aggregator', :hourly => 5.minutes
     s.add_job 'master_pre_create_domains', :daily => 6.hours
-    s.add_job 'master_calculate_show_rate', :interval => 20.minutes
+    # s.add_job 'master_calculate_show_rate', :interval => 20.minutes
     s.add_job 'master_failed_sqs_writes', :interval => 3.minutes
     s.add_job 'master_reload_statz', :interval => 10.minutes
     s.add_job 'master_calculate_next_payout', :daily => 4.hours
