93029f2b2e52282be19c8bc41848f545263ba0a9
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-05-06 00:39:49 +0000

    Major reworking of statz
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@3517 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/job/master_reload_statz_controller.rb
+++ tapjoyads/app/controllers/job/master_reload_statz_controller.rb
@@ -1,14 +1,43 @@
 class Job::MasterReloadStatzController < Job::JobController
+  include MemcachedHelper
   
   def index
-    sess = Patron::Session.new
-    sess.base_url = 'localhost:9898'
-    sess.timeout = 200
-    sess.username = 'internal'
-    sess.password = 'TJ73business'
-    sess.auth_type = :digest
+    cached_stats = {}
+    
+    apps = []
+    SdbApp.select(:where => "interval_update_time = '3600'") do |app|
+      apps.push(app)
+    end
 
-    sess.get("/statz.json?reload=1")
+    apps.each do |app|
+      appstats = Appstats.new(app.key, { :start_time => now - 23.hours, :end_time => now + 1.hour })
+      
+      this_apps_stats = {}
+      this_apps_stats['icon_url'] = app.get_icon_url
+      this_apps_stats['app_name'] = app.name
+      this_apps_stats['paid_installs'] = appstats['paid_installs'].sum
+      this_apps_stats['connects'] = appstats['logins'].sum
+      this_apps_stats['new_users'] = appstats['new_users'].sum
+      this_apps_stats['daily_active_users'] = appstats['daily_active_users'].sum
+      this_apps_stats['price'] = number_to_currency(app.price / 100.0)
+      this_apps_stats['payment_for_install'] = number_to_currency(app.payment_for_install / 100.0)
+      this_apps_stats['balance'] = number_to_currency(app.balance / 100.0)
+      this_apps_stats['daily_budget'] = app.daily_budget
+      this_apps_stats['show_rate'] = "%.2f" % app.show_rate
+      this_apps_stats['vg_purchases'] = appstats['vg_purchases'].sum
+      this_apps_stats['published_installs'] = appstats['published_installs'].sum
+      this_apps_stats['installs_revenue'] = number_to_currency(appstats['installs_revenue'].sum / 100.0)
+      this_apps_stats['ad_impressions'] = appstats['hourly_impressions'].sum
+      
+      cached_stats[app.key] = this_apps_stats
+    end
+
+    save_to_cache('statz.cached_stats', cached_stats)
+
+    install_count_24hours = StoreClick.count(:where => "installed > '#{Time.now.to_f - 1.day}'")
+    save_to_cache('statz.install_count_24hours', install_count_24hours)
+    
+    save_to_cache('statz.last_updated', Time.zone.now)
     
     render :text => 'ok'
   end
--- tapjoyads/app/controllers/statz_controller.rb
+++ tapjoyads/app/controllers/statz_controller.rb
@@ -1,20 +1,16 @@
 class StatzController < WebsiteController
   include MemcachedHelper
   
-  filter_access_to [ :index ]
+  filter_access_to [ :index, :show ]
   
   def index
     @install_count_24hours = get_from_cache('statz.install_count_24hours')
     @last_updated = get_from_cache('statz.last_updated')
-    @appstats_hash = {}
-    
-    @app_list = []
-    SdbApp.select(:where => "interval_update_time = '3600'") do |app|
-      @app_list.push(app)
-    end
-    
-    @app_list.each do |app|
-      @appstats_hash[app.key] = get_from_cache("statz.appstats.#{app.key}")
-    end
+    @cached_stats = get_from_cache('statz.cached_stats')
+  end
+  
+  def show
+    @app = SdbApp.new :key => params[:id]
+    @stats = Appstats.new(app.key, { :start_time => now - 23.hours, :end_time => now + 1.hour })
   end
 end
