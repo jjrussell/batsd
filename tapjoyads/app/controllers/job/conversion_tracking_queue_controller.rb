b2f6b4de1359fb09c92e9a663daa935ab44a0db2
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-05-12 18:48:48 +0000

    Temp hack to clear conversion tracking queue.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@3601 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/job/conversion_tracking_queue_controller.rb
+++ tapjoyads/app/controllers/job/conversion_tracking_queue_controller.rb
@@ -20,10 +20,11 @@ class Job::ConversionTrackingQueueController < Job::SqsReaderController
     click = StoreClick.new(:key => "#{udid}.#{advertiser_app_id}")
     
     if not click.get('click_date')
-      sleep(5)
+      sleep(10)
       click = StoreClick.new(:key => "#{udid}.#{advertiser_app_id}", :load_from_memcache => false)
       if not click.get('click_date')
-        raise "Click not found, wait for failed sdb saves to catch up.  app_id: #{advertiser_app_id}  udid: #{udid}"
+        return
+        #raise "Click not found, wait for failed sdb saves to catch up.  app_id: #{advertiser_app_id}  udid: #{udid}"
       end
     end
     
