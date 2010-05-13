b5ffd4af368af441b7e1738bff056e329a0e3460
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-05-13 21:24:15 +0000

    Add master cleanup store click, which will cleanup the store clicks that are older than 30 days.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@3627 6df48309-bd3c-4361-8df8-ec25136b6021

---
+++ tapjoyads/app/controllers/job/master_cleanup_store_click_controller.rb
@@ -0,0 +1,16 @@
+class Job::MasterCleanupStoreClickController < Job::JobController
+  include SqsHelper
+  
+  def index
+    time = Time.zone.now.beginning_of_day - 40.days
+    10.times do
+      count = StoreClick.count(:where => "click_date >= '#{time.to_i}' and click_date < '#{(time + 24.hours).to_i}'")
+      if count > 0
+        send_to_sqs(QueueNames::CLEANUP_STORE_CLICK, time.to_date.to_s(:db))
+      end
+      time += 1.day
+    end
+    
+    render :text => 'ok'
+  end
+end
