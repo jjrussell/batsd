191110924d48534b10e4511a677aa756d94e8734
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-05-14 22:29:19 +0000

    add some error checking code into calculate next payout job
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@3684 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/job/queue_calculate_next_payout_controller.rb
+++ tapjoyads/app/controllers/job/queue_calculate_next_payout_controller.rb
@@ -1,16 +1,31 @@
 class Job::QueueCalculateNextPayoutController < Job::SqsReaderController
-
+  include NewRelicHelper
+  
   def initialize
     super QueueNames::CALCULATE_NEXT_PAYOUT
   end
-
+  
   private
-
+  
   def on_message(message)
+    today = Time.zone.now
+    yesterday = today - 1.day
+    
     Partner.to_calculate_next_payout_amount.each do |partner|
+      # store the cutoff dates and current next payout amount for error checking
+      today_cutoff_date = partner.payout_cutoff_date(today)
+      yesterday_cutoff_date = partner.payout_cutoff_date(yesterday)
+      current_next_payout_amount = partner.next_payout_amount
+      
+      # calculate the next payout amount inside a transaction and reload the object first to ensure consistency
       Partner.transaction do
         partner.reload.calculate_next_payout_amount
         partner.save!
+      end
+      
+      # verify that nothing changed if the cutoff date is the same
+      if today_cutoff_date == yesterday_cutoff_date && current_next_payout_amount != partner.next_payout_amount
+        alert_new_relic(CalculateNextPayoutMismatch, "next_payout_amount differs for partner: #{partner.id}, previously: #{current_next_payout_amount}, now: #{partner.next_payout_amount}", request, params)
       end
     end
   end
