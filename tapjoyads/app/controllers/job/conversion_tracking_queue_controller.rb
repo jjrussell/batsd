1f9ca1f1e2e7447515a75ac4a9f305af899aa3d9
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-05-24 23:03:43 +0000

    Temp change to flush out bad messages from conversion tracking queue.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@3797 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/job/conversion_tracking_queue_controller.rb
+++ tapjoyads/app/controllers/job/conversion_tracking_queue_controller.rb
@@ -23,7 +23,8 @@ class Job::ConversionTrackingQueueController < Job::SqsReaderController
       sleep(5)
       click = StoreClick.new(:key => "#{udid}.#{advertiser_app_id}", :load_from_memcache => false)
       if not click.get('click_date')
-        raise "Click not found, wait for failed sdb saves to catch up.  app_id: #{advertiser_app_id}  udid: #{udid}"
+        #raise "Click not found, wait for failed sdb saves to catch up.  app_id: #{advertiser_app_id}  udid: #{udid}"
+        return
       end
     end
     
