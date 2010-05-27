291e3ca3d61af848433e3a0cb04fb5f33081e0ec
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-05-27 00:49:55 +0000

    add the User-Agent HTTP header as a custom parameter to new relic when MissingRequiredParamsErrors happen
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@4047 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/application_controller.rb
+++ tapjoyads/app/controllers/application_controller.rb
@@ -41,6 +41,7 @@ class ApplicationController < ActionController::Base
   def log_missing_required_params
     Rails.logger.info "missing required params"
     if params[:udid] != 'null'
+      NewRelic::Agent.add_custom_parameters({ :user_agent => request.headers['User-Agent'] })
       alert_new_relic(MissingRequiredParamsError, request.url, request, params)
     end
   end
