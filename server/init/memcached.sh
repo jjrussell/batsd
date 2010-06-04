72f17ce6d6091c7abb6d80a76adfa20e285e44c0
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-06-04 10:41:53 +0000

    disable the annoying motd on the memcached servers
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@4094 6df48309-bd3c-4361-8df8-ec25136b6021

--- server/init/memcached.sh
+++ server/init/memcached.sh
@@ -5,5 +5,9 @@ cp /home/webuser/server/init/ssh_host_rsa_key /etc/ssh/
 cp /home/webuser/server/init/ssh_host_rsa_key.pub /etc/ssh/
 /etc/init.d/ssh restart
 
+# kill the motd
+rm /etc/cron.d/cloudinit-updates
+rm /etc/motd
+
 # start god/memcached
 /usr/bin/god -c /home/webuser/server/god/memcached.god
