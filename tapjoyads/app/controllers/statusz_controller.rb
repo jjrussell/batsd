a32673d4a4c29c0c18207fb8441776143235a65c
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-06-05 00:44:34 +0000

    Only auth check statusz/queue_check
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@4135 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/statusz_controller.rb
+++ tapjoyads/app/controllers/statusz_controller.rb
@@ -2,22 +2,11 @@ class StatuszController < ApplicationController
   include AuthenticationHelper
   include MemcachedHelper
   include RightAws
-
-  before_filter 'basic_authenticate'
+  
+  before_filter 'basic_authenticate', :only => :queue_check
   
   def index
-    sdb = SdbInterface.new(nil, nil, {:multi_thread => true, :port => 80, :protocol => 'http'})
-
-    @domain_save_freq = {}
-    
-    sdb.list_domains do |result|
-      result.domains.each do |domain_name|
-        @domain_save_freq[domain_name] = get_count_in_cache(
-            "savefreq.#{domain_name}.#{((Time.now.to_i - 1.minutes) / 1.minutes).to_i}")
-      end
-    end
-  rescue Exception => e
-    render :text => "Exception: #{e}"
+    render :text => "ok"
   end
   
   def queue_check
