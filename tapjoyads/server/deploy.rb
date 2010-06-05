4996d56dc8381edbd77a581f087e28494a06f0cc
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-06-05 06:29:47 +0000

    remove the 'deploy_group' argument from script/deploy. now you just use 'script/deploy production <version>'
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@4143 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/server/deploy.rb
+++ tapjoyads/server/deploy.rb
@@ -7,16 +7,13 @@ require 'yaml'
 server_type = `/home/webuser/server/server_type.rb`
 
 run_mode = 'production'
-if server_type == 'test' || server_type == 'testwebsite'
+if server_type == 'test'
   run_mode = 'test'
 end
 
 settings = YAML::load_file('/home/webuser/server/configuration.yaml')
 
 yaml_version = settings['config']['api_deploy_version']
-if server_type == 'website' || server_type == 'testwebsite'
-  yaml_version = settings['config']['website_deploy_version']
-end
 
 version = ARGV.first || yaml_version
 puts "Deploying version: #{version}"
@@ -26,17 +23,11 @@ if version == 'trunk'
   version_part = 'trunk'
 end
 svn_url = "https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/#{version_part}/tapjoyads"
-if server_type == 'website' || server_type == 'testwebsite'
-  svn_url = "https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyrailswebsite/#{version_part}/tapjoywebsite"
-end
 
 puts `cd /home/webuser/tapjoyads && svn switch #{svn_url}`
 
 if server_type == 'jobs' || server_type == 'masterjobs'
   `mv /home/webuser/tapjoyads/config/newrelic-jobs.yml /home/webuser/tapjoyads/config/newrelic.yml`
-elsif server_type == 'website' || server_type == 'testwebsite'
-  'mv /home/webuser/tapjoyads/config/newrelic.yml.example /home/webuser/tapjoyads/config/newrelic.yml'
-  'mv /home/webuser/tapjoyads/config/database.yml.example /home/webuser/tapjoyads/config/database.yml'
 else
   `mv /home/webuser/tapjoyads/config/newrelic-web.yml /home/webuser/tapjoyads/config/newrelic.yml`
 end
