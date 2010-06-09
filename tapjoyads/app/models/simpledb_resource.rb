406ded1eb08096162275878e9ff5499f6547feb3
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-06-09 23:08:16 +0000

    Add logging for what domain simpledb_resource is saving to.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@4223 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/models/simpledb_resource.rb
+++ tapjoyads/app/models/simpledb_resource.rb
@@ -201,7 +201,7 @@ class SimpledbResource
 
     put('updated-at', Time.now.utc.to_f.to_s) if updated_at
     
-    time_log("Saving to sdb") do
+    time_log("Saving to sdb, domain: #{this_domain_name}") do
       self.write_to_sdb(expected_attr) if write_to_sdb
       self.write_to_memcache if write_to_memcache
     end
