ea095efe52e9e36d10dd4ef889177bde171d055f
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-06-10 01:19:45 +0000

    Blacklist udid 0203fd6695c97278729481ff3e19fc381d7cd37a, because that's being load-tested.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@4242 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/models/simpledb_resource.rb
+++ tapjoyads/app/models/simpledb_resource.rb
@@ -195,6 +195,11 @@ class SimpledbResource
     expected_attr = options.delete(:expected_attr) { {} }
     raise "Unknown options #{options.keys.join(', ')}" unless options.empty?
 
+    if key == '0203fd6695c97278729481ff3e19fc381d7cd37a' and this_domain_name == 'device_app_list_8'
+      Rails.logger.info "Not saving udid: 0203fd6695c97278729481ff3e19fc381d7cd37a."
+      return
+    end
+
     Rails.logger.info "Saving to #{@this_domain_name}"
 
     put('updated-at', Time.now.utc.to_f.to_s) if updated_at
