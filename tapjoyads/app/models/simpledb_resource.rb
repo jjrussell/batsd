23108043a532a7868279f9be3a69636f8c590d24
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-06-10 01:08:13 +0000

    removing 2 second timeout on sdb save.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@4239 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/models/simpledb_resource.rb
+++ tapjoyads/app/models/simpledb_resource.rb
@@ -200,9 +200,7 @@ class SimpledbResource
     put('updated-at', Time.now.utc.to_f.to_s) if updated_at
     
     time_log("Saving to sdb, domain: #{this_domain_name}") do
-      Timeout.timeout(2) do
-        self.write_to_sdb(expected_attr) if write_to_sdb
-      end
+      self.write_to_sdb(expected_attr) if write_to_sdb
       self.write_to_memcache if write_to_memcache
     end
     
