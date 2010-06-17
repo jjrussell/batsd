ae78aaf2615f67878758bb39f56e38c3c526b780
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-06-17 06:20:17 +0000

    Fix email sending for ruby 1.8.7
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@4379 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/config/initializers/smtp_gmail.rb
+++ tapjoyads/config/initializers/smtp_gmail.rb
@@ -1,11 +1,6 @@
 #
-# For Rails 2.x:
-#   A copy of this file should be placed in RAILS_ROOT/initializers/
-#   A file named mailer.yml should be placed in RAILS_ROOT/config/
-#     See mailer.yml.sample
+# Setup emailing through GMail.
 #
-
-require "smtp_tls"
 
 mailer_config = File.open("#{RAILS_ROOT}/config/mailer.yml") 
 mailer_options = YAML.load(mailer_config) 
