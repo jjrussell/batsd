8d5b6fcef9e91d5f90abef561ba34020829db0a6
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-06-17 21:53:31 +0000

    fix icon images
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@4456 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/get_app_image_controller.rb
+++ tapjoyads/app/controllers/get_app_image_controller.rb
@@ -5,18 +5,11 @@ class GetAppImageController < ApplicationController
     return unless verify_params([:app_id])
     
     app_id = params[:app_id].downcase
-    app = SdbApp.new(:key => app_id, :load => false)
-
-    if params[:img] == '1'
-      redirect_to app.get_icon_url
-    else
-      image = get_from_cache_and_save("icon.s3.#{app_id.hash}") do
-        bucket = RightAws::S3.new.bucket('app_data')
-        image_content = bucket.get("icons/#{app_id}.png")
-        Base64.encode64 image_content
-      end
     
-      @icon = image
+    @icon = get_from_cache_and_save("icon.s3.#{app_id}") do
+      bucket = RightAws::S3.new.bucket('app_data')
+      image_content = bucket.get("icons/#{app_id}.png")
+      Base64.encode64 image_content
     end
   end
 end
