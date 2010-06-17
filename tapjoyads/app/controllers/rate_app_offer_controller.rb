abf5ac63465c3cae2e1f77b4326fa9cb6454757c
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-06-17 22:04:58 +0000

    fix rating offers
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@4457 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/rate_app_offer_controller.rb
+++ tapjoyads/app/controllers/rate_app_offer_controller.rb
@@ -6,56 +6,48 @@ class RateAppOfferController < ApplicationController
   layout 'iphone'
   
   def index
+    offer = Offer.find_in_cache(params[:app_id])
+    rating_offer = RatingOffer.find_in_cache(params[:app_id])
+    rating_offer_id_with_version = rating_offer.id
+    rating_offer_id_with_version += ".#{params[:app_version]}" if params[:app_version]
+    device_app_list = DeviceAppList.new(:key => params[:udid])
     
-    if request.headers['User-Agent'] && request.headers['User-Agent'].downcase =~ /safari/ && request.headers['User-Agent'].downcase =~ /mobile/ 
-      record_id = params[:record_id]
-      udid = params[:udid]
-      app_id = params[:app_id]
-      version = ""
-      version = ".#{params[:app_version]}" if params[:app_version]
-      
-      app = SdbApp.new(:key => app_id)
-      
-      offer_id = RatingOffer.find_by_app_id(app_id).id + version
-      device_app_list = DeviceAppList.new(:key => udid)
-      if device_app_list.has_app(offer_id)
-        redirect_to app.get_store_url(udid, params[:app_id])
-        return
-      end
-      device_app_list.set_app_ran(offer_id)
-      device_app_list.save
-      
-      currency = SdbCurrency.new(:key => app_id)
-    
-      values = calculate_offer_payouts(:currency => currency, :offer_amount => 15)
-    
-      record_key = lookup_by_record(record_id)
-      publisher_user_id = record_key.split('.')[1]
-      
-      #create the reward item and push to the queues
-      reward = Reward.new
-      reward.put('type', 'rating')
-      reward.put('publisher_app_id', app_id)
-      reward.put('publisher_user_id', publisher_user_id)
-      reward.put('advertiser_amount', '0')
-      reward.put('publisher_amount', '0')
-      reward.put('currency_reward', values[:currency_reward])
-      reward.put('tapjoy_amount', '0')
-      reward.put('offerpal_amount', '0')
-
-      reward.save
-
-      web_request = WebRequest.new
-      params[:publisher_app_id] = app_id
-      web_request.put_values('rate_app', params, request)
-      web_request.save
-
-      message = reward.serialize(:attributes_only => true)
-
-      send_to_sqs(QueueNames::SEND_CURRENCY, message)
-
-      redirect_to app.get_store_url(udid, params[:app_id])
-      return
+    if device_app_list.has_app(rating_offer_id_with_version)
+      redirect_to(offer.get_destination_url(params[:udid], params[:app_id])) and return
     end
+    
+    device_app_list.set_app_ran(rating_offer_id_with_version)
+    device_app_list.save
+    
+    currency = SdbCurrency.new(:key => app_id)
+  
+    values = calculate_offer_payouts(:currency => currency, :offer_amount => 15)
+  
+    record_key = lookup_by_record(params[:record_id])
+    publisher_user_id = record_key.split('.')[1]
+    
+    #create the reward item and push to the queues
+    reward = Reward.new
+    reward.put('type', 'rating')
+    reward.put('publisher_app_id', params[:app_id])
+    reward.put('publisher_user_id', publisher_user_id)
+    reward.put('advertiser_amount', '0')
+    reward.put('publisher_amount', '0')
+    reward.put('currency_reward', values[:currency_reward])
+    reward.put('tapjoy_amount', '0')
+    reward.put('offerpal_amount', '0')
+
+    reward.save
+
+    web_request = WebRequest.new
+    params[:publisher_app_id] = params[:app_id]
+    web_request.put_values('rate_app', params, request)
+    web_request.save
+
+    message = reward.serialize(:attributes_only => true)
+
+    send_to_sqs(QueueNames::SEND_CURRENCY, message)
+
+    redirect_to offer.get_destination_url(params[:udid], params[:app_id])
   end
 end
