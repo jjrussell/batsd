ee9f432b6cbfdc18bbc0f42d7888be74a5137d38
ben <ben@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-06-22 23:32:56 +0000

    adding lots of money stats and creating /statz/money
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@4578 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/statz_controller.rb
+++ tapjoyads/app/controllers/statz_controller.rb
@@ -4,12 +4,23 @@ class StatzController < WebsiteController
   filter_access_to [ :index, :show, :search ]
   
   def index
-    @install_count_24hours = get_from_cache('statz.install_count_24hours') || "Not Available"
-    @revenue_24hours = get_from_cache('statz.revenue_24hours') || "Not Available"
-    @payout_24hours = get_from_cache('statz.payout_24hours') || "Not Available"
+    money_stats = get_from_cache('statz.money') || {'24_hours' => {}}
+    @cvr_count_24hours = money_stats['24_hours']['conversions'] || "Not Available"
+    @ad_spend_24hours =  money_stats['24_hours']['advertiser_spend'] || "Not Available"
+    @publisher_earnings_24hours =  money_stats['24_hours']['publisher_earnings'] || "Not Available"
     
     @last_updated = get_from_cache('statz.last_updated') || Time.at(8.hours.to_i)
     @cached_stats = get_from_cache('statz.cached_stats') || {}
+  end
+  
+  def money
+    @money_stats = get_from_cache('statz.money') || render(:text => "Not Available") and return
+    @time_ranges = @money_stats.keys
+    
+    @stat_types = @money_stats[@time_ranges.first].keys
+    
+    @last_updated = get_from_cache('statz.last_updated') || Time.at(8.hours.to_i)
+    
   end
   
   def show
