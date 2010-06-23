17bea04ddc0f980b6473ddb46b3c057db481b676
ben <ben@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-06-23 07:47:35 +0000

    show paid installs from TTR3 boost
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@4629 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/tapulous_marketing_controller.rb
+++ tapjoyads/app/controllers/tapulous_marketing_controller.rb
@@ -13,10 +13,14 @@ class TapulousMarketingController < ApplicationController
     appstats = Appstats.new(app_key, {
       :start_time => start_time,
       :end_time => start_time + 24.hours})
+    
+    paidstats = Appstats.new('5597b9ab-8da0-4f17-8430-4e20e8d9e191', { 
+      :start_time => start_time, 
+      :end_time => start_time + 24.hours})  
       
     @date = start_time.iso8601[0,10]
-    @paid_installs = appstats.stats['paid_installs'].sum
-    @spend = appstats.stats['installs_spend'].sum
+    @paid_installs = paidstats.stats['paid_installs'].sum
+    @spend = paidstats.stats['installs_spend'].sum
     @revenue = appstats.stats['installs_revenue'].sum
     @published_installs = appstats.stats['published_installs'].sum
   end
