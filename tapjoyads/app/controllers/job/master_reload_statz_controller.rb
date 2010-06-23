6886b0271f7e4d944c8ad474c6a87a924345d00b
ben <ben@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-06-23 00:07:47 +0000

    fix
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@4592 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/job/master_reload_statz_controller.rb
+++ tapjoyads/app/controllers/job/master_reload_statz_controller.rb
@@ -27,11 +27,11 @@ class Job::MasterReloadStatzController < Job::JobController
       
       money_stats[is]['publisher_earnings'] = number_to_currency(publisher_earnings)  
         
-      marketing_credits = Order.sum(:amount, interval_strings[is].gsub('_TABLE_','orders') + " and payment_method = 2")/100.0
+      marketing_credits = Order.sum(:amount, :conditions => interval_strings[is].gsub('_TABLE_','orders') + " and payment_method = 2")/100.0
       money_stats[is]['marketing_credits'] = number_to_currency(marketing_credits)
       
-      money_stats[is]['orders'] = number_to_currency(Order.sum(:amount, interval_strings[is].gsub('_TABLE_','orders') + " and payment_method != 2")/100.0)
-      money_stats[is]['payouts'] = number_to_currency(Payout.sum(:amount, interval_strings[is].gsub('_TABLE_','payouts'))/100.0)
+      money_stats[is]['orders'] = number_to_currency(Order.sum(:amount, :conditions => interval_strings[is].gsub('_TABLE_','orders') + " and payment_method != 2")/100.0)
+      money_stats[is]['payouts'] = number_to_currency(Payout.sum(:amount, :conditions => interval_strings[is].gsub('_TABLE_','payouts'))/100.0)
       money_stats[is]['revenue'] = number_to_currency(advertiser_spend - marketing_credits)
       money_stats[is]['net_revenue'] = number_to_currency(advertiser_spend - marketing_credits - publisher_earnings)
       
