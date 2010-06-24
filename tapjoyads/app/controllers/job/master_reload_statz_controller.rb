30a5b8f0f07a73dc1a67a6e1b8159c910c4f819b
ben <ben@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-06-24 09:16:47 +0000

    store balance, pending_earnings sums
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@4678 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/job/master_reload_statz_controller.rb
+++ tapjoyads/app/controllers/job/master_reload_statz_controller.rb
@@ -53,6 +53,12 @@ class Job::MasterReloadStatzController < Job::JobController
     
     save_to_cache('statz.money', money_stats)
     
+    total_balance = Partner.sum(:balance, :conditions => "id != '70f54c6d-f078-426c-8113-d6e43ac06c6d'") / 100.0
+    total_pending_earnings = Partner.sum(:pending_earnings, :conditions => "id != '70f54c6d-f078-426c-8113-d6e43ac06c6d'") / 100.0
+    
+    save_to_cache('statz.balance', total_balance)
+    save_to_cache('statz.pending_earnings', total_pending_earnings)
+    
     cached_stats = {}
     
     Offer.find(:all, :conditions => "stats_aggregation_interval = 3600").each do |offer|
