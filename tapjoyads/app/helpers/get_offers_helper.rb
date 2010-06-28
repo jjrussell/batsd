323bd2396e369ac172e1b6012a90cffe93868d78
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-06-28 23:48:11 +0000

    change offers webpage to have next and previous links instead of ajax load more
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@4763 6df48309-bd3c-4361-8df8-ec25136b6021

---
+++ tapjoyads/app/helpers/get_offers_helper.rb
@@ -0,0 +1,19 @@
+module GetOffersHelper
+  
+  def get_previous_link
+    return nil if @start_index == 0
+    
+    tmp_params = params.reject { |k, v| k == 'controller' || k == 'action' }
+    tmp_params['start'] = [@start_index - @max_items, 0].max
+    link_to("&laquo; Previous #{@max_items}", "/get_offers/webpage?#{tmp_params.to_query}")
+  end
+  
+  def get_next_link
+    return nil if @more_data_available < 1
+    
+    tmp_params = params.reject { |k, v| k == 'controller' || k == 'action' }
+    tmp_params['start'] = @start_index + @max_items
+    link_to("Next #{[@more_data_available, @max_items].min} &raquo;", "/get_offers/webpage?#{tmp_params.to_query}")
+  end
+  
+end
