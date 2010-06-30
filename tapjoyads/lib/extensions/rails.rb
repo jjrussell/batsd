269e16645a77936b5a5cc23d0e1f080beef169f6
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-06-30 01:32:57 +0000

    add support for only creating routes for certain hosts.  this feature will only be active in production mode and is completely disabled for now.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@4887 6df48309-bd3c-4361-8df8-ec25136b6021

---
+++ tapjoyads/lib/extensions/rails.rb
@@ -0,0 +1,23 @@
+module ActionController
+  module Routing
+    
+    class RouteSet
+      alias_method :orig_extract_request_environment, :extract_request_environment
+      
+      def extract_request_environment(request)
+        orig_extract_request_environment(request).merge({ :host => request.host })
+      end
+    end
+    
+    class Route
+      alias_method :orig_recognition_conditions, :recognition_conditions
+      
+      def recognition_conditions
+        result = orig_recognition_conditions
+        result << "conditions[:hosts].include?(env[:host])" if false && conditions[:hosts] && Rails.env == 'production'
+        result
+      end
+    end
+    
+  end
+end
