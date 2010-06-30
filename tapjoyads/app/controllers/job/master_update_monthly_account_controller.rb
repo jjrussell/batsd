bab884a8a816754f427a1bcdb4d2c32f68983291
ben <ben@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-06-30 00:53:36 +0000

    change to a 10 second sleep
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@4881 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/job/master_update_monthly_account_controller.rb
+++ tapjoyads/app/controllers/job/master_update_monthly_account_controller.rb
@@ -11,7 +11,7 @@ class Job::MasterUpdateMonthlyAccountController < Job::JobController
       json['year'] = now.year
       message = json.to_json
       send_to_sqs(QueueNames::UPDATE_MONTHLY_ACCOUNT, message)
-      sleep(5) #don't want to overwhelm the job servers
+      sleep(10) #don't want to overwhelm the job servers
     end
     
     render :text => 'ok'
