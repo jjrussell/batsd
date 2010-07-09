17efea77ce3c60f6501288d1f3a2cb7d9afc08cf
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-07-09 05:08:59 +0000

    add spinner to the next/prev links on get offers webpage and other small tweaks
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@5106 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/helpers/get_offers_helper.rb
+++ tapjoyads/app/helpers/get_offers_helper.rb
@@ -5,7 +5,7 @@ module GetOffersHelper
     
     tmp_params = params.reject { |k, v| k == 'controller' || k == 'action' }
     tmp_params['start'] = [@start_index - @max_items, 0].max
-    link_to("&laquo; Previous #{@max_items}", "/get_offers/webpage?#{tmp_params.to_query}")
+    link_to("Previous #{@max_items}", "/get_offers/webpage?#{tmp_params.to_query}", :onclick => "this.className = 'clicked';")
   end
   
   def get_next_link
@@ -13,7 +13,7 @@ module GetOffersHelper
     
     tmp_params = params.reject { |k, v| k == 'controller' || k == 'action' }
     tmp_params['start'] = @start_index + @max_items
-    link_to("Next #{[@more_data_available, @max_items].min} &raquo;", "/get_offers/webpage?#{tmp_params.to_query}")
+    link_to("Next #{[@more_data_available, @max_items].min}", "/get_offers/webpage?#{tmp_params.to_query}", :onclick => "this.className = 'clicked';")
   end
   
 end
