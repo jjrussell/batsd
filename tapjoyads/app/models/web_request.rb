ea22dabd4c5045127141eda9a6054fac0028a6a1
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-07-09 00:46:30 +0000

    go easy on web-request-2010-07-09-(2|12)
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@5093 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/models/web_request.rb
+++ tapjoyads/app/models/web_request.rb
@@ -49,7 +49,7 @@ class WebRequest < SimpledbResource
     @now = Time.now.utc
     date = @now.iso8601[0,10]
     num = rand(MAX_WEB_REQUEST_DOMAINS)
-    if date == '2010-07-07' && num == 15
+    if date == '2010-07-09' && (num == 12 || num == 2)
       num = rand(MAX_WEB_REQUEST_DOMAINS)
     end
     "web-request-#{date}-#{num}"
