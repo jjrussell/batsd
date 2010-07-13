d21b2a2bf7eda4a248f345d5099988c5ff9e968e
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-07-13 06:35:03 +0000

    Fix hash test to actually pass.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@5168 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/test/unit/point_purchases_test.rb
+++ tapjoyads/test/unit/point_purchases_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class PointPurchasesTest < ActiveSupport::TestCase
 
   test "hash method" do
-    assert_equal "test string".hash, -173659658, "Ruby upgrade? Hash must match, or else dynamic_domain_name will be wrong."
+    assert_equal "test string".hash, -1736596589, "Ruby upgrade? Hash must match, or else dynamic_domain_name will be wrong."
+    assert_equal "foo.bar".hash, -230128492
   end
 end
