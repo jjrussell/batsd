f11afce594bf4557f7032030bcc4cd1b3ddde8e9
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-07-14 19:36:55 +0000

    move ec2servers to a lib called Ec2Tasks and make the deploy script remove instances from the LB during deploys
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@5198 6df48309-bd3c-4361-8df8-ec25136b6021

---
+++ tapjoyads/lib/ec2_tasks.rb
@@ -0,0 +1,34 @@
+class Ec2Tasks
+  require 'yaml'
+  require 'rubygems'
+  require 'right_aws'
+  
+  def self.get_dns_names(group_id)
+    get_field_values([ :dns_name ], group_id).collect { |value_hash| value_hash[:dns_name] }
+  end
+  
+  def self.get_dns_names_with_instance_ids(group_id)
+    get_field_values([ :dns_name, :aws_instance_id ], group_id)
+  end
+  
+  def self.get_field_values(field_names, group_id)
+    amazon_credentials = YAML::load_file('config/amazon.yaml')
+    ENV['AWS_ACCESS_KEY_ID'] = amazon_credentials['main']['access_key_id'] unless ENV['AWS_ACCESS_KEY_ID']
+    ENV['AWS_SECRET_ACCESS_KEY'] = amazon_credentials['main']['secret_access_key'] unless ENV['AWS_SECRET_ACCESS_KEY']
+    ec2 = RightAws::Ec2.new(nil, nil, :logger => Logger.new(STDERR))
+    
+    field_names << :aws_groups
+    field_values = []
+    ec2.describe_instances.each do |instance|
+      if instance[:aws_state] == 'running' && instance[:aws_groups].include?(group_id)
+        value_hash = {}
+        field_names.each do |field_name|
+          value_hash[field_name] = instance[field_name]
+        end
+        field_values << value_hash
+      end
+    end
+    field_values
+  end
+  
+end
