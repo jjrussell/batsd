f353a7afc9d60cb9055007cbbcdc76d92ce72b08
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-07-15 21:52:30 +0000

    Add sdb_attr's to reward.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@5223 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/models/reward.rb
+++ tapjoyads/app/models/reward.rb
@@ -1,6 +1,20 @@
 class Reward < SimpledbResource
   self.domain_name = 'reward'
   
+  self.sdb_attr :publisher_app_id
+  self.sdb_attr :advertiser_app_id
+  self.sdb_attr :publisher_user_id
+  self.sdb_attr :advertiser_amount, :type => :int
+  self.sdb_attr :publisher_amount,  :type => :int
+  self.sdb_attr :tapjoy_amount,     :type => :int
+  self.sdb_attr :offerpal_amount,   :type => :int
+  self.sdb_attr :currency_reward,   :type => :int
+  self.sdb_attr :source
+  self.sdb_attr :type
+  self.sdb_attr :created,           :type => :time
+  self.sdb_attr :sent_currency,     :type => :time
+  self.sdb_attr :sent_money_txn,    :type => :time
+  
   def initialize(options = {})
     super
     put('created', Time.now.utc.to_f.to_s) unless get('created')
