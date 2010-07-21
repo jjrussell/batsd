929ccc51157dae1b9f43fa6664de9f742f2fd444
ben <ben@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-07-21 01:03:01 +0000

    return country of install in Connect for paid installs (for smaato and other server to server partners)
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@5284 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/connect_controller.rb
+++ tapjoyads/app/controllers/connect_controller.rb
@@ -4,10 +4,13 @@ class ConnectController < ApplicationController
     params[:app_id] = '7e81549a-7fc5-4940-9435-11371ee47fa9' if request.headers['User-Agent'] =~ /DeerHunting/
     return unless verify_params([:app_id, :udid], {:allow_empty => false})
     
+    @country = nil
+    
     #add this app to the device list
     Rails.logger.info_with_time("Check conversions and maybe add to sqs") do
       click = StoreClick.new(:key => "#{params[:udid]}.#{params[:app_id]}")
       unless (click.attributes.empty? || click.get('installed'))
+        @country = click.country if click.clicked_at > (Time.zone.now - 2.days)
         logger.info "Added conversion to sqs queue"
         message = {:udid => params[:udid], :app_id => params[:app_id], 
             :install_date => Time.now.utc.to_f.to_s}.to_json
