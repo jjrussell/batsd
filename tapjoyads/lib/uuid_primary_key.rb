66428fec1446a64ed2b84ddf9fdcc140b104d589
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-07-22 06:02:41 +0000

    improve the UuidPrimaryKey module
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@5323 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/lib/uuid_primary_key.rb
+++ tapjoyads/lib/uuid_primary_key.rb
@@ -7,12 +7,17 @@ module UuidPrimaryKey
     model.class_eval do
       validates_presence_of :id
       validates_uniqueness_of :id
+
+      before_validation_on_create :set_primary_key
     end
   end
 
+private
+
   # ensures that each new record has a UUID assigned to the 'id' field.
-  def before_validation_on_create
-    self.id = UUIDTools::UUID.random_create.to_s if id.blank?
+  def set_primary_key
+    self.id = UUIDTools::UUID.random_create.to_s unless id =~ /[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}/
+    true
   end
 
 end
