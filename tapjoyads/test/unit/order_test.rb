ce497cb8de32192daa3b856e054b8ba54000d02a
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-07-22 21:44:10 +0000

    more payout and order tests
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@5357 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/test/unit/order_test.rb
+++ tapjoyads/test/unit/order_test.rb
@@ -9,4 +9,19 @@ class OrderTest < ActiveSupport::TestCase
   should ensure_inclusion_of(:status).in_range(Order::STATUS_CODES)
   should ensure_inclusion_of(:payment_method).in_range(Order::PAYMENT_METHODS)
   should validate_numericality_of(:amount)
+  
+  context "An Order" do
+    setup do
+      @partner = Factory(:partner)
+    end
+    
+    should "increase a partner's balance" do
+      assert_equal 0, @partner.balance
+      assert_equal 0, @partner.orders.count
+      Factory(:order, :partner => @partner, :amount => 100)
+      @partner.reload
+      assert_equal 100, @partner.balance
+      assert_equal 1, @partner.orders.count
+    end
+  end
 end
--- tapjoyads/test/unit/payout_test.rb
+++ tapjoyads/test/unit/payout_test.rb
@@ -11,4 +11,19 @@ class PayoutTest < ActiveSupport::TestCase
   should validate_numericality_of(:amount)
   should ensure_inclusion_of(:payment_method).in_range(Payout::PAYMENT_METHODS)
   should ensure_inclusion_of(:status).in_range(Payout::STATUS_CODES)
+  
+  context "A Payout" do
+    setup do
+      @partner = Factory(:partner, :pending_earnings => 100)
+    end
+    
+    should "decrease a partner's pending earnings" do
+      assert_equal 100, @partner.pending_earnings
+      assert_equal 0, @partner.payouts.count
+      Factory(:payout, :partner => @partner, :amount => 100)
+      @partner.reload
+      assert_equal 0, @partner.pending_earnings
+      assert_equal 1, @partner.payouts.count
+    end
+  end
 end
