cc418e5a8ecdaf227758dece3a7ba6b594cf3a0c
hwanjoon <hwanjoon@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-07-23 21:12:42 +0000

    enable export
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@5397 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/billing_controller.rb
+++ tapjoyads/app/controllers/billing_controller.rb
@@ -3,6 +3,7 @@ class BillingController < WebsiteController
 
   filter_access_to :all
   before_filter :get_partner
+  before_filter :get_statements, :only => [:index, :export]
 
   def index
     @payouts = @partner.payouts.sort
@@ -10,7 +11,16 @@ class BillingController < WebsiteController
 
     @current_balance = @partner.balance
     @last_payout = @payouts.last.amount
-    @last_payment = 0
+    @last_payment = @statements.map do |s|
+        s.orders unless s.orders == 0
+      end.compact.last # not sure what this should be
+  end
+
+  def export
+    file_name = "#{@first_statement_date.strftime("%Y-%m")}_#{@last_statement_date.strftime("%Y-%m")}"
+    response.headers['Content-Type'] = "text/csv"
+    response.headers['Content-Disposition'] = "attachment; filename=statements_#{file_name}.csv"
+    render :layout => false
   end
 
   def add_funds
@@ -23,8 +33,16 @@ class BillingController < WebsiteController
     end
   end
 
-  def get_partner
-    # TODO: make these not fake
-    @partner = Partner.find_by_id("ce059644-18a0-4f27-bc2b-c2a2d4d4e7bf")
-  end
+  private
+    def get_partner
+      # TODO: make these not fake
+      @partner = Partner.find_by_id("ce059644-18a0-4f27-bc2b-c2a2d4d4e7bf")
+    end
+
+    def get_statements
+      # TODO: check for begin/end dates
+      @statements = @partner.monthly_accountings
+      @first_statement_date= Time.parse("#{@statements.first.year}-#{@statements.first.month}-01")
+      @last_statement_date = Time.parse("#{@statements.last.year}-#{@statements.last.month}-01")
+    end
 end
--- tapjoyads/config/authorization_rules.rb
+++ tapjoyads/config/authorization_rules.rb
@@ -4,7 +4,7 @@ authorization do
     has_permission_on :home, :to => [ :index ]
     has_permission_on :apps, :to => [ :index, :show, :new, :create, :edit, :update ]
     has_permission_on :reporting, :to => [ :index ]
-    has_permission_on :billing, :to => [ :index, :add_funds, :detail ]
+    has_permission_on :billing, :to => [ :index, :add_funds, :detail, :export ]
     has_permission_on :account, :to => [ :index ]
     has_permission_on :support, :to => [ :index ]
   end
