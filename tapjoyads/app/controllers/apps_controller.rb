52b0c8168d9038cc0ea529b15d0bd104d7aa7e38
hwanjoon <hwanjoon@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-07-27 23:28:12 +0000

    set up something for apps home
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@5515 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/apps_controller.rb
+++ tapjoyads/app/controllers/apps_controller.rb
@@ -1,6 +1,38 @@
 class AppsController < WebsiteController
   layout 'tabbed'
-  
+
   filter_access_to :all
-  
+  before_filter :get_apps
+
+  def index
+    @app_data =
+      unless @my_apps.blank?
+        @my_apps.first.to_json(:include => {
+          :offer => {
+            :methods => [:get_icon_url ]
+          }
+        })
+      end
+  end
+
+  def show
+    respond_to do |format|
+      format.html { redirect_to apps_path }
+      format.json do
+        app = App.find(params[:id])
+        app = nil unless @my_apps.include?(app)
+        render :json => app.to_json(:include => {
+            :offer => {
+              :methods => [:get_icon_url ]
+            }
+          },
+          :methods => [:store_url])
+      end
+    end
+  end
+
+  private
+    def get_apps
+      @my_apps = current_partner.apps
+    end
 end
