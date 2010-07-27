8ec5a6e9487bf04a57e06d9968fb87460dd95907
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-07-27 21:14:33 +0000

    add extra apps to fluent's data feed
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@5472 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/fluent_data_controller.rb
+++ tapjoyads/app/controllers/fluent_data_controller.rb
@@ -7,13 +7,14 @@ class FluentDataController < ApplicationController
     return unless verify_params([:date])
     
     partner = Partner.find('1faa4e38-e118-4249-88f7-258d16ab24cf')
+    extra_apps = [ '91647185-9af5-4da8-80e6-6e66479837f9', '2cf8e550-2dab-432a-b5bb-87c0c7afb5f0', '856f074c-d284-449e-8d2d-f7ef85f257a7' ]
     
     start_time = Time.zone.parse(params[:date])
     
     @date = start_time.iso8601[0,10]
     @appstats_list = []
     
-    partner.apps.each do |app|
+    (partner.apps + App.find(extra_apps)).each do |app|
       appstats = Appstats.new(app.id, {
         :start_time => start_time,
         :end_time => start_time + 24.hours})
