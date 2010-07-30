40e5ff0b72551bda57266ccc6b75f7c33f0f55f9
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-07-30 22:40:01 +0000

    Add activities controller.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@5569 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/config/authorization_rules.rb
+++ tapjoyads/config/authorization_rules.rb
@@ -31,8 +31,9 @@ authorization do
   end
   
   role :statz do
-    has_permission_on :statz, :to => [ :index, :show, :edit, :update, :search, :last_run_times, :udids, :udid, :log ]
+    has_permission_on :statz, :to => [ :index, :show, :edit, :update, :search, :last_run_times, :udids, :udid ]
     has_permission_on :tools, :to => [ :disabled_popular_offers ]
+    has_permission_on :activities, :to => [ :index ]
   end
   
   role :account_mgr do
