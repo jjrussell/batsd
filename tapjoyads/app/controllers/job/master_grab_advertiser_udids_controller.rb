67db4ea425fe515d1653726ae6d2542d760dd139
hwanjoon <hwanjoon@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-08-01 06:00:52 +0000

    disable this job for now
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@5589 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/job/master_grab_advertiser_udids_controller.rb
+++ tapjoyads/app/controllers/job/master_grab_advertiser_udids_controller.rb
@@ -1,10 +1,12 @@
 class Job::MasterGrabAdvertiserUdidsController < Job::JobController
   def index
+    # disable temporarily
+=begin
     Offer.find_each do |offer|
       Sqs.send_message(QueueNames::GRAB_ADVERTISER_UDIDS, offer.id)
       sleep(2) #don't want to overwhelm the job servers
     end
-
+=end
     render :text => 'ok'
   end
 end
