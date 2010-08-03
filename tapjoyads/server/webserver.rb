76755bafc26f839ce04e82e1915e755febe0ef64
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-08-03 06:47:29 +0000

    update webservers.rb for the new AMI
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@5611 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/server/webserver.rb
+++ tapjoyads/server/webserver.rb
@@ -5,9 +5,6 @@
 `cp /home/webuser/server/ssh_host_rsa_key.pub /etc/ssh/`
 `/etc/init.d/ssh restart`
 
-# stop memcached
-`/etc/init.d/memcached stop`
-
 # setup log directories
 `mkdir -p /mnt/log/apache2`
 `mkdir -p /mnt/log/rails`
@@ -15,9 +12,6 @@
 `rm -rf /var/log/apache2`
 `ln -s /mnt/log/apache2 /var/log/apache2`
 `ln -s /mnt/log/rails /home/webuser/tapjoyads/log`
-
-# enable rails log rotation
-`cp /home/webuser/server/rails-logrotate /etc/logrotate.d/rails`
 
 # deploy the latest code
 `su webuser -c '/home/webuser/server/deploy.rb'`
