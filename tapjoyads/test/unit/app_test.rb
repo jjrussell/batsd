c967a9c95ef8f0bb4ef822d4e4082d4f75e90371
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-08-04 21:28:36 +0000

    add some currency tests
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@5690 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/test/unit/app_test.rb
+++ tapjoyads/test/unit/app_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class AppTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
+  subject { Factory(:app) }
+  
+  should have_one(:offer)
+  should have_one(:currency)
+  should have_one(:rating_offer)
+  should have_many(:publisher_conversions)
+  should belong_to(:partner)
+  
+  should validate_presence_of(:partner)
+  should validate_presence_of(:name)
+  
+  
 end
