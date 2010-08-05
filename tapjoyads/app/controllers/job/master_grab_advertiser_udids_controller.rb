c824da178b144349d9194c85e3577da70a50243a
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-08-05 05:49:08 +0000

    fix some runtime bugs
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@5730 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/job/master_grab_advertiser_udids_controller.rb
+++ tapjoyads/app/controllers/job/master_grab_advertiser_udids_controller.rb
@@ -1,16 +1,16 @@
 class Job::MasterGrabAdvertiserUdidsController < Job::JobController
   def index
-    @bucket = S3.bucket(BucketNames::AD_UDIDS)
-    # yesterday
-    date = Time.zone.now.beginning_of_day.to_i
-    previous_day = 1.day.ago(date)
+    bucket = S3.bucket(BucketNames::AD_UDIDS)
+    today = Time.zone.now.beginning_of_day
+    yesterday = today - 1.day
+
     Offer.find_each do |offer|
-      message = [offer.id, date].to_json
+      message = [offer.id, today.to_i].to_json
       Sqs.send_message(QueueNames::GRAB_ADVERTISER_UDIDS, message)
 
       # check if previous day's job failed
-      unless @bucket.key(App.udid_s3_key(app_id, previous_day)).exists?
-        message = [app_id, day_in_the_past.to_i].to_json
+      unless bucket.key(App.udid_s3_key(offer.id, yesterday)).exists?
+        message = [offer.id, yesterday.to_i].to_json
         Sqs.send_message(QueueNames::GRAB_ADVERTISER_UDIDS, message)
       end
 
