43031be099e03d497e3d14bd2e019916b0641278
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-08-05 21:26:04 +0000

    only verify 1/7 of the partner's balances each night and sleep between each one to ease the load on the DB
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@5739 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/job/master_verifications_controller.rb
+++ tapjoyads/app/controllers/job/master_verifications_controller.rb
@@ -8,8 +8,13 @@ class Job::MasterVerificationsController < Job::JobController
 private
   
   def check_partner_balances
+    day_of_week = Date.today.wday
+    
     Partner.find_each do |partner|
+      next unless partner.id.hash % 7 == day_of_week
+      
       partner.recalculate_balances(false, true)
+      sleep(5)
     end
   end
   
