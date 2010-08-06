66ceedd4c794cfea94fbe5cd1ed6c7670dad4a85
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-08-06 02:12:23 +0000

    Don't limit low-balance paid apps to a 10% show rate
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@5752 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/job/master_cache_offers_controller.rb
+++ tapjoyads/app/controllers/job/master_cache_offers_controller.rb
@@ -2,7 +2,7 @@ class Job::MasterCacheOffersController < Job::JobController
   
   def index
     Offer.enabled_offers.each do |offer|
-      if offer.partner.balance <= 10000 && offer.item_type != 'RatingOffer'
+      if offer.partner.balance <= 10000 && offer.is_free? && offer.item_type != 'RatingOffer'
         new_show_rate = [ 0.10, offer.show_rate ].min
         offer_to_update = Offer.find(offer.id)
         offer_to_update.update_attribute(:show_rate, new_show_rate)
