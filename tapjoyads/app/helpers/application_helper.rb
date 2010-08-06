7fb9acd7313e1ea7bfd2d83fc92d039718cde15f
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-08-06 05:31:38 +0000

    add a 'copy to clipboard' button
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@5765 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/helpers/application_helper.rb
+++ tapjoyads/app/helpers/application_helper.rb
@@ -7,4 +7,31 @@ module ApplicationHelper
       '<!--[if IE]><script src="/javascripts/excanvas.js"></script><![endif]-->'
     end
   end
+  
+  def clippy(text, bgcolor = '#FFFFFF')
+    @clippy_id = 0 unless defined?(@clippy_id)
+    @clippy_id += 1
+    
+    html = <<-EOF
+      <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="110" height="14" id="clippy_#{@clippy_id}">
+        <param name="movie" value="/clippy.swf" />
+        <param name="allowScriptAccess" value="always" />
+        <param name="quality" value="high" />
+        <param name="scale" value="noscale" />
+        <param NAME="FlashVars" value="text=#{text}" />
+        <param name="bgcolor" value="#{bgcolor}" />
+        <embed src="/clippy.swf"
+            width="110"
+            height="14"
+            name="clippy_#{@clippy_id}"
+            quality="high"
+            allowScriptAccess="always"
+            type="application/x-shockwave-flash"
+            pluginspage="http://www.macromedia.com/go/getflashplayer"
+            FlashVars="text=#{text}"
+            bgcolor="#{bgcolor}"
+        />
+      </object>
+    EOF
+  end
 end
