bf11580939389145a00a3fada413871b8f98595d
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-08-24 07:19:54 +0000

    slow down this master job to prevent the job error rate from getting too high
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@6212 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/job/master_get_store_info_controller.rb
+++ tapjoyads/app/controllers/job/master_get_store_info_controller.rb
@@ -1,14 +1,12 @@
 class Job::MasterGetStoreInfoController < Job::JobController
-  def initialize
-    @now = Time.now.utc
-  end
   
   def index
-    App.find_each(:conditions => "store_id is not null and platform='iphone'") do |app|
-      
+    App.find_each(:conditions => "store_id is not null and platform = 'iphone'") do |app|
       Sqs.send_message(QueueNames::GET_STORE_INFO, app.id)
+      sleep(1)
     end
     
     render :text => 'ok'
   end
+  
 end
