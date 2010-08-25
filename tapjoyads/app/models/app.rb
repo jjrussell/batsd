46c48c5068fb7cc104af72ba6db37f8a5155cfe7
hwanjoon <hwanjoon@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-08-25 07:59:23 +0000

    allow icon creation for new apps
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@6235 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/models/app.rb
+++ tapjoyads/app/models/app.rb
@@ -15,6 +15,7 @@ class App < ActiveRecord::Base
   after_create :create_primary_offer
   after_update :update_offers
   after_save :update_memcached
+  after_save :save_icon
   before_destroy :clear_memcached
   
   def self.find_in_cache(id, do_lookup = true)
@@ -65,7 +66,30 @@ class App < ActiveRecord::Base
       end
     end
   end
-  
+
+  def attachment=(file)
+    # need ID to process
+    @attachment = file
+  end
+
+  def save_icon
+    return if @attachment.nil?
+    bucket = S3.bucket(BucketNames::APP_DATA)
+    path = "icons/#{id}.png"
+    retries = 3
+    begin
+      @attachment.rewind
+      bucket.put(path, @attachment.read, {}, 'public-read')
+    rescue RightAws::AwsError => e
+      if retries > 0 && e.message =~ /^RequestTimeTooSkewed/
+        retries -= 1
+        retry
+      else
+        raise e
+      end
+    end
+  end
+
   def get_offer_list(udid, options = {})
     currency = options.delete(:currency)
     device_type = options.delete(:device_type)
