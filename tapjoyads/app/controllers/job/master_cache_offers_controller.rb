6f3fedb59310628f72e3814924ce206cf9b9ed8a
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-08-28 00:46:01 +0000

    another attempt at prevent RequestTimeTooSkewed errors
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@6293 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/job/master_cache_offers_controller.rb
+++ tapjoyads/app/controllers/job/master_cache_offers_controller.rb
@@ -4,10 +4,11 @@ class Job::MasterCacheOffersController < Job::JobController
     Offer.enabled_offers.each do |offer|
       if offer.partner.balance <= 10000 && offer.is_free? && offer.item_type != 'RatingOffer'
         new_show_rate = [ 0.10, offer.show_rate ].min
-        offer_to_update = Offer.find(offer.id)
-        offer_to_update.update_attribute(:show_rate, new_show_rate)
+        offer.update_attribute(:show_rate, new_show_rate)
       end
     end
+    
+    S3.reset_connection
     
     Offer.cache_enabled_offers
     Offer.cache_classic_offers
