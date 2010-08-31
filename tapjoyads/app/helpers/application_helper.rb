9d7e2a6d12018439b8f08aa1b07456531dc2610c
hwanjoon <hwanjoon@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-08-31 22:45:02 +0000

    DRY-ing js/css for edit tables
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@6334 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/helpers/application_helper.rb
+++ tapjoyads/app/helpers/application_helper.rb
@@ -2,7 +2,7 @@
 module ApplicationHelper
   def include_tapjoy_graph
     content_for :page_head, stylesheet_link_tag('tapjoy_graph')
-    content_for :page_head, javascript_include_tag('rgraph/RGraph.common.core', 'rgraph/RGraph.common.tooltips', 'rgraph/RGraph.line', 'tapjoy_graph')
+    content_for :page_head, javascript_include_tag('rgraph/RGraph.common.core', 'rgraph/RGraph.common.tooltips', 'rgraph/RGraph.line', 'tapjoy/graph')
     content_for :page_head do
       '<!--[if IE]><script src="/javascripts/excanvas.js"></script><![endif]-->'
     end
--- tapjoyads/public/javascripts/application.js
+++ tapjoyads/public/javascripts/application.js
@@ -1,5 +1,26 @@
+if (typeof(Tapjoy) == "undefined") Tapjoy = {};
+
 $(function(){
   $('.flash').click(function(){
     $(this).slideUp();
-  })
+  });
+
+  var numChanged = 0;
+  Tapjoy.watchTextFields = function() {
+    $('input').focus(function() {
+      $(this).addClass('active');
+    });
+
+    $('input,textarea').blur(function() {
+      $(this).removeClass('active');
+      if ($(this).val() != $(this).attr('init')) {
+        $(this).addClass('changed');
+        numChanged += 1;
+      } else {
+        $(this).removeClass('changed');
+        numChanged -= 1;
+      }
+    });
+  };
 });
+
