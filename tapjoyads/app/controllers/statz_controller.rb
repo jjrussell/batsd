bee24ba38025880cfc54f73bfdc41d22e727a1ab
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-09-02 07:02:57 +0000

    Add Chris's iPhone udid to last run time list.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@6373 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/statz_controller.rb
+++ tapjoyads/app/controllers/statz_controller.rb
@@ -262,6 +262,7 @@ class StatzController < WebsiteController
       { :udid => 'c73e730913822be833766efffc7bb1cf239d855a', :last_run_time => 'Never', :device_label => 'Ben iPhone'       },
       { :udid => '9ac478517b48da604bdb9fc15a3e48139d59660d', :last_run_time => 'Never', :device_label => 'Christine iPhone' },
       { :udid => 'f3de44744a306beb47407b9a23cd97d9fe03339a', :last_run_time => 'Never', :device_label => 'Christine iPad'   },
+      { :udid => 'c0a77ec9da51b722a60295d8cf5e35eee5634e1f', :last_run_time => 'Never', :device_label => 'Chris iPhone'     },
       { :udid => '5c46e034cd005e5f2b08501820ecb235b0f13f33', :last_run_time => 'Never', :device_label => 'Hwan-Joon iPhone' },
       { :udid => '05f900a2b588c4ed140689145ddb4684a1681f20', :last_run_time => 'Never', :device_label => 'Kai iPad'         },
       { :udid => 'c720dd0a5f937735c1a76bce72fcd90ada73ad7d', :last_run_time => 'Never', :device_label => 'Kai iTouch'       },
