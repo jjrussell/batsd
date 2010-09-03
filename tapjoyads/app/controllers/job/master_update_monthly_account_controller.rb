ec28807f034bb479ceaaafbe68b257cc63189c9b
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-09-03 19:59:14 +0000

    setup the update monthly account job to run on the 1st and 15th of each month
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@6399 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/job/master_update_monthly_account_controller.rb
+++ tapjoyads/app/controllers/job/master_update_monthly_account_controller.rb
@@ -1,15 +1,17 @@
 class Job::MasterUpdateMonthlyAccountController < Job::JobController
   def index
-
-    Partner.find_each do |partner|
-      now = Time.zone.now
-      json = {}
-      json['partner_id'] = partner.id
-      json['month'] = now.month
-      json['year'] = now.year
-      message = json.to_json
-      Sqs.send_message(QueueNames::UPDATE_MONTHLY_ACCOUNT, message)
-      sleep(1) #don't want to overwhelm the job servers
+    now = Time.zone.now
+    
+    if now.day == 1 || now.day == 15
+      Partner.find_each do |partner|
+        json = {}
+        json['partner_id'] = partner.id
+        json['month'] = now.month
+        json['year'] = now.year
+        message = json.to_json
+        Sqs.send_message(QueueNames::UPDATE_MONTHLY_ACCOUNT, message)
+        sleep(1) # don't want to overwhelm the job servers
+      end
     end
     
     render :text => 'ok'
--- tapjoyads/config/jobs.rb
+++ tapjoyads/config/jobs.rb
@@ -40,6 +40,7 @@ JobRunner::Gateway.define do |s|
     s.add_job 'master_grab_advertiser_udids', :daily => 7.hours
     s.add_job 'master_grab_disabled_popular_offers', :daily => 8.hours
     s.add_job 'master_set_bad_domains', :interval => 1.minutes
+    s.add_job 'master_update_monthly_account', :daily => 3.hours
   else
     Rails.logger.info "JobRunner: Not running any jobs. Not a job server."
   end
