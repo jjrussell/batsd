5a4af4ecb9cc60c3aec430993277064d20baa22f
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-09-10 00:42:40 +0000

    Add back gamevil_reject
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@6465 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/models/offer.rb
+++ tapjoyads/app/models/offer.rb
@@ -278,7 +278,8 @@ class Offer < ActiveRecord::Base
         show_rate_reject?(device_app_list) ||
         flixter_reject?(publisher_app, device_app_list) ||
         whitelist_reject?(publisher_app) ||
-        secondary_offer_reject?(show_secondary_offers) 
+        secondary_offer_reject?(show_secondary_offers) ||
+        gamevil_reject?(publisher_app)
   end
   
 private
@@ -350,7 +351,7 @@ private
   end
   
   #TO REMOVE
-  def gamevil_reject?(publisher_app) 
+  def gamevil_reject?(publisher_app)
     return publisher_app.partner_id == 'cea789f9-7741-4197-9cc0-c6ac40a0801a' && partner_id != 'cea789f9-7741-4197-9cc0-c6ac40a0801a'
   end
   
