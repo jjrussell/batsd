bbebe39b5fa5410f4db584a89c07b23066cd18ef
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-09-15 07:37:50 +0000

    spread the monthly accounting updates evenly over the course of a week and run the job every night.  also, organize the master jobs config by impact on system performance to make it easier to figure out when to schedule something.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@6513 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/job/master_update_monthly_account_controller.rb
+++ tapjoyads/app/controllers/job/master_update_monthly_account_controller.rb
@@ -2,16 +2,16 @@ class Job::MasterUpdateMonthlyAccountController < Job::JobController
   def index
     now = Time.zone.now
     
-    if now.day == 1 || now.day == 15
-      Partner.find_each do |partner|
-        json = {}
-        json['partner_id'] = partner.id
-        json['month'] = now.month
-        json['year'] = now.year
-        message = json.to_json
-        Sqs.send_message(QueueNames::UPDATE_MONTHLY_ACCOUNT, message)
-        sleep(10) # don't want to overwhelm the job servers
-      end
+    Partner.find_each do |partner|
+      next unless partner.id.hash % 7 == now.wday
+      
+      json = {}
+      json['partner_id'] = partner.id
+      json['month'] = now.month
+      json['year'] = now.year
+      message = json.to_json
+      Sqs.send_message(QueueNames::UPDATE_MONTHLY_ACCOUNT, message)
+      sleep(10)
     end
     
     render :text => 'ok'
--- tapjoyads/config/jobs.rb
+++ tapjoyads/config/jobs.rb
@@ -23,25 +23,31 @@ JobRunner::Gateway.define do |s|
     s.add_job 'queue_update_monthly_account', :interval => 1.minute
     s.add_job 'queue_grab_advertiser_udids', :interval => 5.minutes
   elsif machine_type == 'masterjobs'
-    s.add_job 'master_cleanup_web_requests', :daily => 5.hours
-    s.add_job 'master_create_offers', :interval => 10.minutes
+    # jobs with high impact on overall system performance
+    s.add_job 'master_grab_advertiser_udids', :daily => 7.hours
+    s.add_job 'master_update_monthly_account', :daily => 8.hours
+    s.add_job 'master_verifications', :daily => 5.hours
+    
+    # jobs with moderate impact on overall system performance
     s.add_job 'master_app_stats', :interval => 2.minutes
-    s.add_job 'master_pre_create_domains', :daily => 6.hours
     s.add_job 'master_calculate_show_rate', :interval => 20.minutes
-    s.add_job 'master_failed_sqs_writes', :interval => 3.minutes
+    s.add_job 'master_reload_money', :interval => 1.hour
     s.add_job 'master_reload_statz', :interval => 10.minutes
     s.add_job 'master_reload_statz/daily', :daily => 10.minutes
-    s.add_job 'master_reload_money', :interval => 1.hour
-    s.add_job 'master_calculate_next_payout', :daily => 4.hours
-    s.add_job 'master_select_vg_items', :interval => 5.minutes
-    s.add_job 'master_verifications', :daily => 5.hours
-    s.add_job 'master_get_store_info', :daily => 7.hours
+    
+    # jobs with low impact on overall system performance
     s.add_job 'master_cache_offers', :interval => 1.minute
-    s.add_job 'master_grab_advertiser_udids', :daily => 7.hours
+    s.add_job 'master_calculate_next_payout', :daily => 4.hours
+    s.add_job 'master_cleanup_web_requests', :daily => 5.hours
+    s.add_job 'master_create_offers', :interval => 10.minutes
+    s.add_job 'master_failed_sqs_writes', :interval => 3.minutes
+    s.add_job 'master_get_store_info', :daily => 7.hours
     s.add_job 'master_grab_disabled_popular_offers', :daily => 8.hours
+    s.add_job 'master_pre_create_domains', :daily => 6.hours
+    s.add_job 'master_select_vg_items', :interval => 5.minutes
     s.add_job 'master_set_bad_domains', :interval => 1.minutes
-    s.add_job 'master_update_monthly_account', :daily => 7.hours
   else
     Rails.logger.info "JobRunner: Not running any jobs. Not a job server."
   end
+  
 end
