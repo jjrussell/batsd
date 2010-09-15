7be30aae2f9ef5eb08da4e4a36baf9665a05fd4d
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-09-15 23:48:30 +0000

    Add admin-only fields to currency form. Also add activity-logging to currency form.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@6521 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/currencies_controller.rb
+++ tapjoyads/app/controllers/currencies_controller.rb
@@ -5,16 +5,24 @@ class CurrenciesController < WebsiteController
   filter_access_to :all
 
   before_filter :find_currency, :only => [ :show, :update ]
+  after_filter :save_activity_logs, :only => [ :update, :create ]
 
   def show
   end
   
   def update
+    log_activity(@currency)
+    
     if params[:managed_by_tapjoy]
       params[:currency][:callback_url] = Currency::TAPJOY_MANAGED_CALLBACK_URL
     end
     
-    if @currency.safe_update_attributes(params[:currency], [:name, :conversion_rate, :initial_balance, :callback_url, :secret_key, :test_devices])
+    safe_attributes = [:name, :conversion_rate, :initial_balance, :callback_url, :secret_key, :test_devices]
+    if permitted_to?(:index, :statz)
+      safe_attributes += [:disabled_offers, :disabled_partners, :max_age_rating, :only_free_offers, :offers_money_share, :installs_money_share]
+    end
+    
+    if @currency.safe_update_attributes(params[:currency], safe_attributes)
       flash[:notice] = 'Currency was successfully updated.'
       redirect_to app_currency_path(:app_id => params[:app_id], :id => @currency.id)
     else
