7f762fffe63337bddc019b98f301037f6a23d28f
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-09-16 19:50:57 +0000

    make currency_field automatically add the css class 'currency_field' and automatically convert between dollars and cents
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@6546 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/public/javascripts/application.js
+++ tapjoyads/public/javascripts/application.js
@@ -1,5 +1,13 @@
 if (typeof(Tapjoy) == "undefined") Tapjoy = {};
 
+function numberToCurrency(number) {
+  return '$' + number.toFixed(2);
+}
+
+function stringToNumber(currency) {
+  return Number(currency.replace(/[^\d\.]/g, ''));
+}
+
 $(function($){
   $('.flash').click(function(){
     $(this).slideUp();
@@ -31,5 +39,9 @@ $(function($){
     var content = $('#' + this.id + '_content');
     var position = [$(this).position().left - 300, $(this).position().top - $(window).scrollTop()];
     content.dialog({title: "Help: " + content.attr('name'), position: position, minHeight: 75, dialogClass: 'help-dialog'});
+  });
+
+  $('input.currency_field').change(function() {
+    $(this).val(numberToCurrency(stringToNumber($(this).val())));
   });
 });
