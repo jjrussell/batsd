f6306369f3ceed83422593d4e5f32b2ae53c468d
hwanjoon <hwanjoon@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-09-17 07:07:47 +0000

    maybe it makes sense to redirect users to statz or apps, since the homepage won't really be related to the web console any more?
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@6605 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/homepage_controller.rb
+++ tapjoyads/app/controllers/homepage_controller.rb
@@ -1,6 +1,11 @@
 class HomepageController < WebsiteController
-  
   def index
+    if current_user.nil?
+      redirect_to login_path
+    elsif permitted_to?(:index, :statz)
+      redirect_to statz_path
+    elsif permitted_to?(:index, :apps)
+      redirect_to apps_path
+    end
   end
-  
 end
