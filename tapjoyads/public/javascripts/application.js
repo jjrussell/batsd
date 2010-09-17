e6b5e2db392e4e30287b8465a581c1c61c19d94f
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-09-17 01:35:54 +0000

    add help dialogs to add funds and disable resizing of the ui-dialogs
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@6575 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/public/javascripts/application.js
+++ tapjoyads/public/javascripts/application.js
@@ -17,7 +17,7 @@ $(function($){
     $('.ui-dialog').remove();
     var content = $('#' + this.id + '_content');
     var position = [$(this).position().left - 300, $(this).position().top - $(window).scrollTop()];
-    content.dialog({title: "Help: " + content.attr('name'), position: position, minHeight: 75, dialogClass: 'help-dialog'});
+    content.dialog({title: "Help: " + content.attr('name'), position: position, minHeight: 75, dialogClass: 'help-dialog', resizable: false});
   });
 
   $('input.currency_field').change(function() {
