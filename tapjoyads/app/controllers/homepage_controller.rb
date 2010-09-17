7ca80300ced5f9782e36d41e110862ec530b6218
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-09-17 07:14:24 +0000

    fix path
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@6606 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/homepage_controller.rb
+++ tapjoyads/app/controllers/homepage_controller.rb
@@ -3,7 +3,7 @@ class HomepageController < WebsiteController
     if current_user.nil?
       redirect_to login_path
     elsif permitted_to?(:index, :statz)
-      redirect_to statz_path
+      redirect_to statz_index_path
     elsif permitted_to?(:index, :apps)
       redirect_to apps_path
     end
