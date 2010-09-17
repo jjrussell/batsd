de73d5d71089060a425ea59928aef9bdc4e4f79f
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-09-17 00:01:22 +0000

    style shit
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@6564 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/public/javascripts/application.js
+++ tapjoyads/public/javascripts/application.js
@@ -13,27 +13,6 @@ $(function($){
     $(this).slideUp();
   });
 
-  // watch text fields in editable table
-  var numChanged = 0;
-  $('table.editable input[type=text], table.editable textarea').each(function() {
-    $(this).attr('init', $(this).val());
-  });
-
-  $('table.editable input[type=text], table.editable textarea').focus(function() {
-    $(this).addClass('active');
-  });
-
-  $('table.editable input[type=text], table.editable textarea').blur(function() {
-    $(this).removeClass('active');
-    if ($(this).val() != $(this).attr('init')) {
-      $(this).addClass('changed');
-      numChanged += 1;
-    } else {
-      $(this).removeClass('changed');
-      numChanged -= 1;
-    }
-  });
-
   $('.help').click(function() {
     $('.ui-dialog').remove();
     var content = $('#' + this.id + '_content');
