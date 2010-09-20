280d85b9e38cc66b2bc9d21572194352d9c1fce2
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-09-20 21:27:14 +0000

    link partner accounts together correctly
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@6626 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/import_mssql_controller.rb
+++ tapjoyads/app/controllers/import_mssql_controller.rb
@@ -14,9 +14,11 @@ class ImportMssqlController < ApplicationController
     user.crypted_password = params[:password]
     user.password_salt = params[:salt]
     partner = (params[:partner_id].blank? || params[:partner_id] == 'NULL') ? nil : Partner.find(params[:partner_id])
-    user.current_partner = partner unless partner.nil?
-    if user.new_record? && !partner.nil?
-      user.partners << partner
+    unless partner.nil?
+      user.current_partner = partner
+      unless user.partner_ids.include?(partner.id)
+        user.partners << partner
+      end
     end
     user.created_at = Time.parse(params[:created_at] + ' CST').utc unless params[:created_at].blank?
     user.save(false)
