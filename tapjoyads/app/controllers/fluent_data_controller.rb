3f7072764e4b53853debfaaa11c3a0e5dd3d095a
ben <ben@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-09-21 20:31:51 +0000

    Add HeyWire to the Fluent feed
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@6653 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/fluent_data_controller.rb
+++ tapjoyads/app/controllers/fluent_data_controller.rb
@@ -7,7 +7,7 @@ class FluentDataController < ApplicationController
     return unless verify_params([:date])
     
     partner = Partner.find('1faa4e38-e118-4249-88f7-258d16ab24cf')
-    extra_apps = [ '91647185-9af5-4da8-80e6-6e66479837f9', '2cf8e550-2dab-432a-b5bb-87c0c7afb5f0', '856f074c-d284-449e-8d2d-f7ef85f257a7', '917b59ea-1d45-43e3-8a2f-9b1d1f0e142e', '192e6d0b-cc2f-44c2-957c-9481e3c223a0' ]
+    extra_apps = [ '91647185-9af5-4da8-80e6-6e66479837f9', '2cf8e550-2dab-432a-b5bb-87c0c7afb5f0', '856f074c-d284-449e-8d2d-f7ef85f257a7', '917b59ea-1d45-43e3-8a2f-9b1d1f0e142e', '192e6d0b-cc2f-44c2-957c-9481e3c223a0', '7baa7e9d-6d06-46f8-9909-162312a867de' ]
     
     start_time = Time.zone.parse(params[:date])
     
