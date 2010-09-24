2243198da7bb3745a5d29d310a8b332596d20980
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-09-24 08:00:47 +0000

    sleep longer between next payout calculations
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@6738 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/job/master_calculate_next_payout_controller.rb
+++ tapjoyads/app/controllers/job/master_calculate_next_payout_controller.rb
@@ -2,7 +2,7 @@ class Job::MasterCalculateNextPayoutController < Job::JobController
   def index
     Partner.to_calculate_next_payout_amount.each do |partner|
       partner.calculate_next_payout_amount(true)
-      sleep(5)
+      sleep(10)
     end
     
     render :text => 'ok'
