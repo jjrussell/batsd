5f76150e4be66793028dd735dadbdf3f5651e60c
stephen <stephen@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-09-24 00:25:21 +0000

    Display ad basic image generation.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@6722 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/display_ad_controller.rb
+++ tapjoyads/app/controllers/display_ad_controller.rb
@@ -57,8 +57,56 @@ class DisplayAdController < ApplicationController
     web_request.put_values('display_ad_image', params, get_ip_address, get_geoip_data)
     web_request.save
     
-    # TODO: actually generate an image
+    width = 320
+    height = 50
     
+    publisher = App.find_in_cache(params[:publisher_app_id])
+    offer = Offer.find_in_cache(params[:advertiser_app_id])
+    publisher_icon_blob = Downloader.get("http://s3.amazonaws.com/app_data/icons/#{params[:publisher_app_id]}.png")
+    offer_icon_blob = Downloader.get("http://s3.amazonaws.com/app_data/icons/#{params[:advertiser_app_id]}.png")
+    
+    publisher_icon = Magick::Image.from_blob(publisher_icon_blob)[0].resize(50, 50)
+    offer_icon = Magick::Image.from_blob(offer_icon_blob)[0].resize(50, 50)
+    
+    publisher_icon_color = get_main_color(publisher_icon)
+    offer_icon_color = get_main_color(offer_icon)
+    
+    gradient_fill = Magick::GradientFill.new(0, 0, 0, height, publisher_icon_color, offer_icon_color)
+    
+    img = Magick::Image.new(width, height, gradient_fill)
+    img.format = 'png'
+    
+    img.composite!(publisher_icon, Magick::WestGravity, Magick::AtopCompositeOp)
+    img.composite!(offer_icon, Magick::EastGravity, Magick::AtopCompositeOp)
+    
+    text = "Earn #{publisher.currency.get_reward_amount(offer)} #{publisher.currency.name} in #{publisher.name}!\n Download and run #{offer.name}."
+    
+    draw = Magick::Draw.new
+    
+    img.annotate(draw, width - 100, height, 50, 0, text) {
+      self.pointsize = 14
+      self.stroke = 'white'
+      self.stroke_width = 1
+      self.gravity = Magick::CenterGravity
+      self.font_weight = Magick::BoldWeight
+    }
+    
+    img.annotate(draw, width - 100, height, 50, 0, text) {
+      self.pointsize = 14
+      self.stroke = 'white'
+      self.gravity = Magick::CenterGravity
+      self.font_weight = Magick::BoldWeight
+    }
+    
+    send_data img.to_blob, :type => 'image/png', :disposition => 'inline'
+  end
+  
+private
+
+  def get_main_color(image)
+    hist = image.color_histogram
+    max = hist.values.max
+    hist.each {|k, v| return k.to_color if v == max}
   end
   
 end
