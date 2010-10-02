5076e56848e57cd3a8dfa95f6fc3e33e52aa0aea
hwanjoon <hwanjoon@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-10-02 00:07:51 +0000

    need index.html
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@6890 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/homepage_controller.rb
+++ tapjoyads/app/controllers/homepage_controller.rb
@@ -1,7 +1,7 @@
 class HomepageController < WebsiteController
   def index
     if current_user.nil?
-      redirect_to '/site'
+      redirect_to '/site/index.html'
     elsif permitted_to?(:index, :statz)
       redirect_to statz_index_path
     elsif permitted_to?(:index, :apps)
