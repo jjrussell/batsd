2a9b579f700563dbd356e110732d25cef514fe58
hwanjoon <hwanjoon@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-10-02 00:03:47 +0000

    if not logged in, go to slash site
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@6888 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/homepage_controller.rb
+++ tapjoyads/app/controllers/homepage_controller.rb
@@ -1,7 +1,7 @@
 class HomepageController < WebsiteController
   def index
     if current_user.nil?
-      redirect_to login_path
+      redirect_to '/site'
     elsif permitted_to?(:index, :statz)
       redirect_to statz_index_path
     elsif permitted_to?(:index, :apps)
