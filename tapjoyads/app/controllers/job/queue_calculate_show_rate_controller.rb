2900dfb82fa712bc870f05392fcc789a82c3f0d1
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-10-06 01:27:42 +0000

    beta.tapjoy.com -> www.tapjoy.com
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@7001 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/job/queue_calculate_show_rate_controller.rb
+++ tapjoyads/app/controllers/job/queue_calculate_show_rate_controller.rb
@@ -35,7 +35,7 @@ class Job::QueueCalculateShowRateController < Job::SqsReaderController
     
     min_conversion_rate = offer.min_conversion_rate || (offer.is_paid? ? 0.005 : 0.12)
     if recent_clicks > 200 && conversion_rate < min_conversion_rate
-      Notifier.alert_new_relic(ConversionRateTooLowError, "#{offer.name} (https://beta.tapjoy.com/statz/#{offer.id}) has #{conversion_rate} cvr on #{recent_clicks} clicks.")
+      Notifier.alert_new_relic(ConversionRateTooLowError, "#{offer.name} (https://www.tapjoy.com/statz/#{offer.id}) has #{conversion_rate} cvr on #{recent_clicks} clicks.")
     end
     
     Rails.logger.info "Recent clicks: #{recent_clicks}"
