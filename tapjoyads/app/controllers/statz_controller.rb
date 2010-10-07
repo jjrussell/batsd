651b2dd8fc23a72dde6fe7daeaab69b467498e48
amir <amir@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-10-07 22:48:45 +0000

    use Time.zone.at instead of Time.zone.at
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@7062 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/statz_controller.rb
+++ tapjoyads/app/controllers/statz_controller.rb
@@ -13,7 +13,7 @@ class StatzController < WebsiteController
     
     @money_stats = @timeframe == '1_month' ? Mc.get('money.daily_cached_stats') : Mc.get('money.cached_stats')
     
-    @last_updated = Time.zone.parse(Mc.get("statz.last_updated.#{@timeframe}") || 0)
+    @last_updated = Time.zone.at(Mc.get("statz.last_updated.#{@timeframe}") || 0)
     @cached_stats = Mc.get("statz.cached_stats.#{@timeframe}") || []
   end
 
