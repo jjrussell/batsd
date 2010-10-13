fe724f496864070c4f1d8782d56f62bfd2617871
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-10-13 23:42:24 +0000

    cleanup send_money_txn queue
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@7195 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/job/send_money_txn_controller.rb
+++ tapjoyads/app/controllers/job/send_money_txn_controller.rb
@@ -9,49 +9,44 @@ private
   def on_message(message)
     reward = Reward.deserialize(message.to_s)
     
-    unless reward.get('sent_money_txn')
-      Rails.logger.info "Sending money transaction to sql: #{reward.key}"
-
-      unless reward.get('publisher_amount')
-        if reward.get('advertiser_app_id').nil?
-          return
-        end
-        
-        raise "No amounts set for reward: #{reward.key}"
-      end
-      
+    if reward.sent_money_txn.present?
+      return
+    end
+    
+    if reward.publisher_amount.blank?
+      raise "No amounts set for reward: #{reward.key}"
+    end
+    
+    conversion = Conversion.new do |c|
+      c.id = reward.key
+      c.reward_id = reward.key
+      c.advertiser_offer_id = reward.offer_id
+      c.publisher_app_id = reward.publisher_app_id
+      c.advertiser_amount = reward.advertiser_amount
+      c.publisher_amount = reward.publisher_amount
+      c.tapjoy_amount = reward.tapjoy_amount
+      c.reward_type_string = reward.type
+      c.created_at = reward.created
+    end
+    save_conversion(conversion)
+    
+    if reward.displayer_app_id.present?
       conversion = Conversion.new do |c|
-        c.id = reward.key
+        c.id = reward.reward_key_2
         c.reward_id = reward.key
         c.advertiser_offer_id = reward.offer_id
-        c.publisher_app_id = reward.publisher_app_id
-        c.advertiser_amount = reward.advertiser_amount
-        c.publisher_amount = reward.publisher_amount
-        c.tapjoy_amount = reward.tapjoy_amount
-        c.reward_type_string = reward.type
+        c.publisher_app_id = reward.displayer_app_id
+        c.advertiser_amount = 0
+        c.publisher_amount = reward.displayer_amount
+        c.tapjoy_amount = 0
+        c.reward_type_string_for_displayer = reward.type
         c.created_at = reward.created
       end
       save_conversion(conversion)
-      
-      if reward.displayer_app_id.present?
-        conversion = Conversion.new do |c|
-          c.id = reward.reward_key_2
-          c.reward_id = reward.key
-          c.advertiser_offer_id = reward.offer_id
-          c.publisher_app_id = reward.displayer_app_id
-          c.advertiser_amount = 0
-          c.publisher_amount = reward.displayer_amount
-          c.tapjoy_amount = 0
-          c.reward_type_string_for_displayer = reward.type
-          c.created_at = reward.created
-        end
-        save_conversion(conversion)
-      end
-      
-      reward.put('sent_money_txn', Time.zone.now.to_f.to_s)
-      reward.save
-      
     end
+    
+    reward.sent_money_txn = Time.zone.now
+    reward.save
   end
   
   def save_conversion(conversion)
