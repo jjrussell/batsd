0480eafbacf5839cbda6579eade30bc9ca9cd3ae
ryan <ryan@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-10-21 18:13:59 +0000

    InvalidAppIdForDeviceAppList -> InvalidAppIdForDevices
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@7336 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/models/device.rb
+++ tapjoyads/app/models/device.rb
@@ -18,7 +18,7 @@ class Device < SimpledbShardedResource
     path_list = []
     
     unless app_id =~ /^(\w|\.|-)*$/
-      Notifier.alert_new_relic(InvalidAppIdForDeviceAppList, "udid: #{@key}, app_id: #{app_id}")
+      Notifier.alert_new_relic(InvalidAppIdForDevices, "udid: #{@key}, app_id: #{app_id}")
       return path_list
     end
     
