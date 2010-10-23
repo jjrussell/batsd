8c479c3a403f65ca56376e563fad3dafdbebef2b
amir <amir@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-10-23 22:57:18 +0000

    allow analyzing multiple experiments at once, writing results to csv
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@7455 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/lib/experiments.rb
+++ tapjoyads/lib/experiments.rb
@@ -32,103 +32,92 @@ class Experiments
     
     puts "Report for experiment group '#{experiment_id}' and control group '#{control_id}', from #{start_time.to_s} to #{end_time.to_s}"
     
+    report_multiple(start_time, end_time, [ experiment_id ], control_id)
+  end
+  
+  def self.get_experiment_data(start_time, end_time, experiment_id)
     experiment_id = experiment_id.to_s
-    control_id = control_id.to_s
-    
     viewed_at_condition = "viewed_at >= '#{start_time.to_i}' and viewed_at < '#{end_time.to_i}'"
-    
-    c_offerwall_views = c_clicks = c_conversions = e_offerwall_views = e_clicks = e_conversions = 0
-    
+    offerwall_views = clicks = conversions = 0
+    revenues = []
     date = start_time.to_date
-    while date <= end_time.to_date + 2.days && date <= Time.zone.now.to_date
-      puts "Counting from #{date}..."
-      
-      c_offerwall_views += WebRequest.count :date => date, :where => "path = 'offers' and #{viewed_at_condition} and exp = '#{control_id}'"
-      e_offerwall_views += WebRequest.count :date => date, :where => "path = 'offers' and #{viewed_at_condition} and exp = '#{experiment_id}'"
-      
-      c_clicks += WebRequest.count :date => date, :where => "path = 'offer_click' and #{viewed_at_condition} and exp = '#{control_id}'"
-      e_clicks += WebRequest.count :date => date, :where => "path = 'offer_click' and #{viewed_at_condition} and exp = '#{experiment_id}'"
-      
-      c_conversions += WebRequest.count :date => date, :where => "path = 'conversion' and #{viewed_at_condition} and exp = '#{control_id}'"
-      e_conversions += WebRequest.count :date => date, :where => "path = 'conversion' and #{viewed_at_condition} and exp = '#{experiment_id}'"
-      
+    
+    while date <= end_time.to_date + 2.days && date <= Time.zone.now.to_date      
+      offerwall_views += WebRequest.count :date => date, :where => "path = 'offers' and #{viewed_at_condition} and exp = '#{experiment_id}'"
+      clicks += WebRequest.count :date => date, :where => "path = 'offer_click' and #{viewed_at_condition} and exp = '#{experiment_id}'"
+      conversions += WebRequest.count :date => date, :where => "path = 'conversion' and #{viewed_at_condition} and exp = '#{experiment_id}'"
       date += 1.day
     end
     
-    puts "Aggregating revenues..."
-    c_revenues = []
-    e_revenues = []
     NUM_REWARD_DOMAINS.times do |i|
-      Reward.select :domain_name => "rewards_#{i}", :where => "#{viewed_at_condition} and (exp = '#{control_id}' or exp = '#{experiment_id}')" do |reward|
-        if reward.exp == control_id
-          c_revenues << reward.publisher_amount
-        end
-        if reward.exp == experiment_id
-          e_revenues << reward.publisher_amount
-        end
+      Reward.select :domain_name => "rewards_#{i}", :where => "#{viewed_at_condition} and (exp = '#{experiment_id}')" do |reward|
+        revenues << reward.publisher_amount
       end
     end
     
-    c_ctr = c_clicks.to_f / c_offerwall_views
-    c_cvr = c_conversions.to_f / c_clicks
-    c_avg_rev = c_revenues.sum.to_f / c_offerwall_views
-
-    e_ctr = e_clicks.to_f / e_offerwall_views
-    e_cvr = e_conversions.to_f / e_clicks
-    e_avg_rev = e_revenues.sum.to_f / e_offerwall_views
-
-    c_ctr_variance = c_offerwall_views == 0 ? 0 : (c_ctr * (1 - c_ctr)) / c_offerwall_views
-    e_ctr_variance = e_offerwall_views == 0 ? 0 : (e_ctr * (1 - e_ctr)) / e_offerwall_views
+    ctr       = clicks.to_f / offerwall_views
+    cvr       = conversions.to_f / clicks
+    total_rev = revenues.sum.to_f
+    avg_rev   = total_rev / offerwall_views
+    revenues  = revenues + Array.new(offerwall_views - revenues.length) { 0 }
     
-    c_cvr_variance = c_clicks == 0 ? 0 : (c_cvr * (1 - c_cvr)) / c_clicks
-    e_cvr_variance = e_clicks == 0 ? 0 : (e_cvr * (1 - e_cvr)) / e_clicks
-
-    c_rev_diff_mean_squares = []
-    c_offerwall_views.times do |i|
-      rev = c_revenues[i] || 0
-      c_rev_diff_mean_squares << (rev - c_avg_rev) ** 2
+    ctr_variance = offerwall_views == 0 ? 0 : (ctr * (1 - ctr)) / offerwall_views
+    cvr_variance = clicks == 0 ? 0 : (cvr * (1 - cvr)) / clicks
+    rev_variance = revenues.variance
+    
+    { :clicks => clicks, :offerwall_views => offerwall_views, :conversions => conversions, 
+      :ctr => ctr, :cvr => cvr, :avg_rev => avg_rev, :total_rev => total_rev,
+      :ctr_variance => ctr_variance, :cvr_variance => cvr_variance, :rev_variance => rev_variance, :name => experiment_id }
+  end
+  
+  def self.report_multiple(start_time, end_time, experiment_ids, control_id = EXPERIMENTS[:control], csv_filename = nil)
+    control = get_experiment_data(start_time, end_time, control_id).merge({ :name => 'Control' })
+    
+    if csv_filename
+      csv = File.new(csv_filename, 'w')
+      puts "Writing to #{csv_filename}..."
+      csv.puts <<-END
+Experiment Group,Views,Clicks,Conversions,Revenue,CTR,CVR,AvgRev,CTR-Z,CVR-Z,AvgRev-Z
+Control,#{control[:offerwall_views]},#{control[:clicks]},#{control[:conversions]},$#{control[:total_rev] / 100.0},#{control[:ctr]},#{control[:cvr]},$#{control[:avg_rev] / 100.0},0,0,0
+      END
+      puts "Wrote Control Group"
+    else  
+      print_data(control)
     end
-    c_rev_variance = c_offerwall_views == 0 ? 0 : c_rev_diff_mean_squares.sum.to_f / c_offerwall_views
     
-    e_rev_diff_mean_squares = []
-    e_offerwall_views.times do |i|
-      rev = e_revenues[i] || 0
-      e_rev_diff_mean_squares << (rev - e_avg_rev) ** 2
+    experiment_ids.each do |experiment_id|
+      experiment = calculate_z_scores(get_experiment_data(start_time, end_time, experiment_id), control)
+      if csv_filename
+        puts "Wrote #{experiment_id}"
+        csv.puts "#{experiment_id},#{experiment[:offerwall_views]},#{experiment[:clicks]},#{experiment[:conversions]},$#{experiment[:total_rev] / 100.0},#{experiment[:ctr]},#{experiment[:cvr]},$#{experiment[:avg_rev] / 100.0},#{experiment[:ctr_z_score]},#{experiment[:cvr_z_score]},#{experiment[:avg_rev_z_score]}"
+      else
+        print_data(experiment)
+      end
     end
-    e_rev_variance = e_offerwall_views == 0 ? 0 : e_rev_diff_mean_squares.sum.to_f / e_offerwall_views
-
-    ctr_z_score = (e_ctr - c_ctr) / Math.sqrt(e_ctr_variance + c_ctr_variance)
-    cvr_z_score = (e_cvr - c_cvr) / Math.sqrt(e_cvr_variance + c_cvr_variance)
-    avg_rev_z_score = (e_avg_rev - c_avg_rev) / Math.sqrt(e_rev_variance + c_rev_variance)
     
+    csv.close if csv_filename
+  end
+  
+  def self.calculate_z_scores(experiment, control)
+    experiment[:ctr_z_score] = (experiment[:ctr] - control[:ctr]) / Math.sqrt(experiment[:ctr_variance] + control[:ctr_variance])
+    experiment[:cvr_z_score] = (experiment[:cvr] - control[:cvr]) / Math.sqrt(experiment[:cvr_variance] + control[:cvr_variance])
+    experiment[:avg_rev_z_score] = (experiment[:avg_rev] - control[:avg_rev]) / Math.sqrt(experiment[:rev_variance] + control[:rev_variance])
+    experiment
+  end
+  
+  def self.print_data(experiment)
     puts <<-END
-    Control group
-      Offerwall views:  #{c_offerwall_views}
-      Clicks:           #{c_clicks}
-      Conversions:      #{c_conversions}
-      Revenue:          $#{c_revenues.sum / 100.0}
-      CTR:              #{c_ctr}
-      CVR:              #{c_cvr}
-      Avg revenue:      $#{c_avg_rev / 100.0}
-    Experimental group
-      Offerwall views:  #{e_offerwall_views}
-      Clicks:           #{e_clicks}
-      Conversions:      #{e_conversions}
-      Revenue:          $#{e_revenues.sum / 100.0}
-      CTR:              #{e_ctr}
-      CVR:              #{e_cvr}
-      Avg revenue:      $#{e_avg_rev / 100.0}
-    
-    Variance (used to calculate z-scores)
-      Control
-        ctr: #{c_ctr_variance}, cvr: #{c_cvr_variance}, revenue: #{c_rev_variance}
-      Experimental
-        ctr: #{e_ctr_variance}, cvr: #{e_cvr_variance}, revenue: #{e_rev_variance}
-      
-    Improvement (z-scores >= 1.645 represents 95% confidence that difference is not due to random variance)
-      CTR Z-score:         #{ctr_z_score}
-      CVR Z-score:         #{cvr_z_score}
-      Avg Revenue Z-score: #{avg_rev_z_score}
+    #{experiment[:name]}
+      Offerwall views:  #{experiment[:offerwall_views]}
+      Clicks:           #{experiment[:clicks]}
+      Conversions:      #{experiment[:conversions]}
+      Revenue:          $#{experiment[:total_rev] / 100.0}
+      CTR:              #{experiment[:ctr]}
+      CVR:              #{experiment[:cvr]}
+      Avg revenue:      $#{experiment[:avg_rev] / 100.0}
+      CTR-Z:            #{experiment[:ctr_z_score] || 0}
+      CVR-Z:            #{experiment[:cvr_z_score] || 0}
+      Avg Revenue Z:    #{experiment[:avg_rev_z_score] || 0}
     END
   end
 end
