6ad24bbaf4b0a57c4c692e6e4054c4698b169a42
johnny <johnny@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-10-29 06:31:59 +0000

    Reverted to 7.2.0 for now, decided that it should be QA'd.
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@7607 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/config/initializers/tapjoy.rb
+++ tapjoyads/config/initializers/tapjoy.rb
@@ -9,9 +9,9 @@ UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/
 ANDROID_CONNECT_SDK = 'https://s3.amazonaws.com/tapjoy/sdks/TapjoyConnectSDK_Android_v7.0.0.zip'
 ANDROID_OFFERS_SDK  = 'https://s3.amazonaws.com/tapjoy/sdks/TapjoyConnectOffersSDK_Android_v7.0.0.zip'
 ANDROID_VG_SDK      = 'https://s3.amazonaws.com/tapjoy/sdks/TapjoyHS_VG_OffersSDK.zip'
-IPHONE_CONNECT_SDK  = 'https://s3.amazonaws.com/tapjoy/sdks/TapjoyConnectSDK_iPhone_v7.2.1.zip'
-IPHONE_OFFERS_SDK   = 'https://s3.amazonaws.com/tapjoy/sdks/TapjoyConnectOffersSDK_iPhone_v7.2.1.zip'
-IPHONE_VG_SDK       = 'https://s3.amazonaws.com/tapjoy/sdks/TapjoyConnectVirtualGoodsSDK_iPhone_v7.2.1.zip'
+IPHONE_CONNECT_SDK  = 'https://s3.amazonaws.com/tapjoy/sdks/TapjoyConnectSDK_iPhone_v7.2.0.zip'
+IPHONE_OFFERS_SDK   = 'https://s3.amazonaws.com/tapjoy/sdks/TapjoyConnectOffersSDK_iPhone_v7.2.0.zip'
+IPHONE_VG_SDK       = 'https://s3.amazonaws.com/tapjoy/sdks/TapjoyConnectVirtualGoodsSDK_iPhone_v7.2.0.zip'
 
 unless Rails.env == 'production'
   Mc.cache.flush
