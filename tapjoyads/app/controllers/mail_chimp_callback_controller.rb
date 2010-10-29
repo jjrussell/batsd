805e03cee9b49c2b7d0c792d78d6458c77887a23
hwanjoon <hwanjoon@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-10-29 03:11:56 +0000

    fix mail chimp callback
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@7601 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/mail_chimp_callback_controller.rb
+++ tapjoyads/app/controllers/mail_chimp_callback_controller.rb
@@ -1,11 +1,10 @@
-class MailChimpCallbackController < WebsiteController
-  layout ''
+class MailChimpCallbackController < ApplicationController
   after_filter :save_activity_logs
-  filter_access_to :none
+  protect_from_forgery :only => []
 
   # http://www.mailchimp.com/api/webhooks/
   def callback
-    raise unless params[:key] == MAIL_CHIMP_WEBHOOK_KEY
+    raise unless params[:id] == MAIL_CHIMP_WEBHOOK_KEY
     if params[:type] == "upemail" && params[:data][:list_id] == MAIL_CHIMP_PARTNERS_LIST_ID
       @user = User.find_by_email(params[:data][:old_email])
       log_activity(@user)
