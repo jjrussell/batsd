46a820807f3b2c82c4d9c9d9ec04ff0907ef98c0
hwanjoon <hwanjoon@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-11-05 23:37:16 +0000

    when account manager creates an acount, don't add to mailchimp
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@7760 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/lib/mail_chimp.rb
+++ tapjoyads/lib/mail_chimp.rb
@@ -22,19 +22,21 @@ class MailChimp
   def self.add_partners(partners)
     errors = []
     partners.map do |partner|
-      email = partner.non_managers.first.email
-      if email.blank?
-        nil
-      else
-        name = partner.contact_name
-        name = email if name.blank?
-        {
-          'EMAIL' => email,
-          'NAME' => name,
-          'ID' => partner.id,
-          'IS_PUB' => partner.has_publisher_offer? ? 'true' : 'false',
-          'CAN_EMAIL' => 'true'
-        }
+      unless partner.non_managers.blank?
+        email = partner.non_managers.first.email
+        if email.blank?
+          nil
+        else
+          name = partner.contact_name
+          name = email if name.blank?
+          {
+            'EMAIL' => email,
+            'NAME' => name,
+            'ID' => partner.id,
+            'IS_PUB' => partner.has_publisher_offer? ? 'true' : 'false',
+            'CAN_EMAIL' => 'true'
+          }
+        end
       end
     end.compact.each_slice(100) do |slice|
       results = chimp.subscribe_many(MAIL_CHIMP_PARTNERS_LIST_ID, slice)
