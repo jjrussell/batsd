fc9ec3c4eaad4692f05470b707b4176d47e30fd5
amir <amir@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-11-10 19:08:43 +0000

    add master premier job
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@7816 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/config/jobs.rb
+++ tapjoyads/config/jobs.rb
@@ -46,6 +46,7 @@ JobRunner::Gateway.define do |s|
     s.add_job 'master_pre_create_domains', :daily => 6.hours
     s.add_job 'master_select_vg_items', :interval => 5.minutes
     s.add_job 'master_set_bad_domains', :interval => 1.minutes
+    # s.add_job 'master_set_exclusivity_and_premier_discounts', :daily => 2.hours
   else
     Rails.logger.info "JobRunner: Not running any jobs. Not a job server."
   end
