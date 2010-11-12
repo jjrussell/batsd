e8df233367246cc704207145bc20097805fcfe6a
ben <ben@6df48309-bd3c-4361-8df8-ec25136b6021>
2010-11-12 22:41:57 +0000

    adding esquire to fluent feed
    git-svn-id: https://tapjoy.unfuddle.com/svn/tapjoy_tapjoyads/trunk@7871 6df48309-bd3c-4361-8df8-ec25136b6021

--- tapjoyads/app/controllers/fluent_data_controller.rb
+++ tapjoyads/app/controllers/fluent_data_controller.rb
@@ -11,7 +11,7 @@ class FluentDataController < ApplicationController
       '917b59ea-1d45-43e3-8a2f-9b1d1f0e142e', '192e6d0b-cc2f-44c2-957c-9481e3c223a0', '7baa7e9d-6d06-46f8-9909-162312a867de', 
       'dfb4c57b-c156-4733-b53f-c99161ce4dd1', '92572adf-2cf4-4af6-a293-7715ae0508ff', 'a3f3bf6c-3e9e-4bbe-98ac-6b43d0e3814f', '7591b5ef-4b11-41b8-aed6-c69ecde86503',
       'dae95712-0d9c-44a4-82d5-c73686726f44', 'b0388e2c-dbb5-49f6-8c90-a08d857016a4', '51e42b6c-5393-432d-b058-b9dd10e14a9c', '3edd25b7-0aaa-4f71-be6d-5ec621c86c74',
-      '3e75f700-33fe-4cbd-bf1e-ac84de56605e', '6466e256-2a0e-4b32-b001-859398274856' ]
+      '3e75f700-33fe-4cbd-bf1e-ac84de56605e', '6466e256-2a0e-4b32-b001-859398274856', '0cec66be-51c7-4180-ba06-b06991509c02' ]
     
     start_time = Time.zone.parse(params[:date])
     
