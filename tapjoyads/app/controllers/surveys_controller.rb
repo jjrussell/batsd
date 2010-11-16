abdfb993ecc8d73903754934ec33996d0c47d38f
Stephen McCarthy <stephen@tapjoy.com>
2010-11-16 14:16:27 -0800

    Stop duplicate survey results

--- tapjoyads/app/controllers/surveys_controller.rb
+++ tapjoyads/app/controllers/surveys_controller.rb
@@ -9,25 +9,27 @@ class SurveysController < ApplicationController
   end
   
   def create
-    answers = {
-      :gender => params[:gender][0, 10],
-      :birth_year => params[:birth_year][0, 10],
-      :postal_code => params[:postal_code][0, 10]
-    }
+    if @click.installed_at.nil?
+      answers = {
+        :gender => params[:gender][0, 10],
+        :birth_year => params[:birth_year][0, 10],
+        :postal_code => params[:postal_code][0, 10]
+      }
     
-    survey_result = SurveyResult.new
-    survey_result.udid = params[:udid]
-    survey_result.click_key = params[:click_key]
-    survey_result.geoip_data = get_geoip_data
-    survey_result.answers = answers
-    survey_result.save
+      survey_result = SurveyResult.new
+      survey_result.udid = params[:udid]
+      survey_result.click_key = params[:click_key]
+      survey_result.geoip_data = get_geoip_data
+      survey_result.answers = answers
+      survey_result.save
     
-    device = Device.new(:key => params[:udid])
-    device.survey_answers = answers
-    device.save
+      device = Device.new(:key => params[:udid])
+      device.survey_answers = answers
+      device.save
     
-    if Rails.env == 'production'
-      Downloader.get_with_retry "http://ws.tapjoyads.com/offer_completed?click_key=#{params[:click_key]}"
+      if Rails.env == 'production'
+        Downloader.get_with_retry "http://ws.tapjoyads.com/offer_completed?click_key=#{params[:click_key]}"
+      end
     end
     
     render :template => "surveys/survey_complete"
