a704d09b5f83d54be17c916e8a024d13f922f4e5
Amir Manji <amir@tapjoy.com>
2010-11-16 11:34:33 -0800

    show estimated percentile when changing the offer bid

--- tapjoyads/config/routes.rb
+++ tapjoyads/config/routes.rb
@@ -47,7 +47,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :user_sessions, :only => [ :new, :create, :destroy ]
   map.resources :users, :as => :account, :except => [ :show, :destroy ]
   map.resources :apps, :except => [ :destroy ], :member => { :confirm => :get, :integrate => :get, :publisher_integrate => :get, :archive => :post, :unarchive => :post } do |app|
-    app.resources :offers, :only => [ :show, :update ] , :member => { :download_udids => :get }
+    app.resources :offers, :only => [ :show, :update ] , :member => { :download_udids => :get, :percentile => :post }
     app.resources :currencies, :only => [ :show, :update, :new, :create ],
       :member => { :reset_test_device => :post }
     app.resources :virtual_goods, :as => 'virtual-goods', :only => [ :show, :update, :new, :create, :index ],
