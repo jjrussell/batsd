0af490b4a871c395d13af268cec4bcdb3710da0a
Ben Lewis <ben@tapjoy.com>
2010-11-16 15:21:05 -0800

    get country install count for an advertiser

--- tapjoyads/lib/one_offs.rb
+++ tapjoyads/lib/one_offs.rb
@@ -6,11 +6,12 @@ class OneOffs
   end
   
   # returns a hash of yesterday's store_click counts by country for the given advertiser_app_id
-  def self.store_clicks_by_country_for_advertiser_app_id(advertiser_app_id)
-    day = Time.zone.now.yesterday.to_date.to_s
+  def self.installs_by_country_for_advertiser_app_id(advertiser_app_id, start_date, end_date)
+    st = Time.zone.parse(start_date).to_f.to_s
+    et = Time.zone.parse(end_date).endofday.to_f.to_s
     counts = {}
-    0.upto(MAX_WEB_REQUEST_DOMAINS - 1) do |i|
-      items = WebRequest.select(:where => "advertiser_app_id = '#{advertiser_app_id}' and path = 'store_click'", :domain_name => "web-request-#{day}-#{i}")[:items]
+    50.times do |i|
+      items = Reward.select(:where => "advertiser_app_id = '#{advertiser_app_id}' and created >= '#{st}' and created <= '#{et}'", :domain_name => "rewards_#{i}")[:items]
       items.each do |item|
         country = item.get('country')
         counts[country] = ((counts[country] || 0) + 1)
