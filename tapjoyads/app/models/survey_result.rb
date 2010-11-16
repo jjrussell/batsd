86791528debc7081045e5c488842e23b9ee0ef03
Stephen McCarthy <stephen@tapjoy.com>
2010-11-16 15:09:20 -0800

    cgi_escape survey answers and geoip data.

--- tapjoyads/app/models/survey_result.rb
+++ tapjoyads/app/models/survey_result.rb
@@ -4,7 +4,7 @@ class SurveyResult < SimpledbResource
   
   self.sdb_attr :udid
   self.sdb_attr :click_key
-  self.sdb_attr :answers, :type => :json
-  self.sdb_attr :geoip_data, :type => :json
+  self.sdb_attr :answers, :type => :json, :cgi_escape => true
+  self.sdb_attr :geoip_data, :type => :json, :cgi_escape => true
   
 end
