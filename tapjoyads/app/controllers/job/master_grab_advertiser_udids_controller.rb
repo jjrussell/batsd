3d996a1df14bf3e3961975dd60475f41d1e3fea2
Ryan Johns <ryan@tapjoy.com>
2010-11-19 10:43:16 -0800

    only send grab_advertiser_udids jobs to the queue when paid_installs is greater than 0

--- tapjoyads/app/controllers/job/master_grab_advertiser_udids_controller.rb
+++ tapjoyads/app/controllers/job/master_grab_advertiser_udids_controller.rb
@@ -4,6 +4,9 @@ class Job::MasterGrabAdvertiserUdidsController < Job::JobController
     yesterday = today - 1.day
 
     Offer.find_each do |offer|
+      stats = Appstats.new(offer.id, { :start_time => yesterday, :end_time => today, :granularity => :hourly, :stat_types => [ 'paid_installs' ] }).stats
+      next unless stats['paid_installs'].sum > 0
+
       message = { :offer_id => offer.id, :start_time => yesterday.to_i, :finish_time => today.to_i }.to_json
       Sqs.send_message(QueueNames::GRAB_ADVERTISER_UDIDS, message)
 
