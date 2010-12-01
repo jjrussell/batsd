a794acc946b5b8adcc67c6ef73c61ca3486d9b39
Ryan Johns <ryan@tapjoy.com>
2010-12-01 02:47:45 -0800

    new memcache servers

--- tapjoyads/config/environments/production.rb
+++ tapjoyads/config/environments/production.rb
@@ -26,8 +26,8 @@ config.action_controller.allow_forgery_protection    = false
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
 
-MEMCACHE_SERVERS = [ '10.249.178.127', '10.249.202.48', '10.249.122.224', '10.249.183.15',  '10.248.230.128',  # east-1b
-                     '10.194.211.144', '10.204.114.33', '10.203.71.224',  '10.204.137.154', '10.203.22.36'   ] # east-1d
+MEMCACHE_SERVERS = [ '10.241.110.160', '10.241.57.219', '10.241.58.96',   '10.240.43.96',   '10.241.51.79',   # east-1b
+                     '10.196.55.240',  '10.202.69.124', '10.195.165.124', '10.202.221.219', '10.194.22.4'   ] # east-1d
 
 EXCEPTIONS_NOT_LOGGED = ['ActionController::UnknownAction',
                          'ActionController::RoutingError']
