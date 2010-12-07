71f6f353a34ce850d685e97b0cd5a9c2d3ef5cb8
Ben Lewis <ben@tapjoy.com>
2010-12-06 16:10:23 -0800

    creating a one off to get stats by app by month for a partner

--- tapjoyads/lib/one_offs.rb
+++ tapjoyads/lib/one_offs.rb
@@ -1,5 +1,7 @@
 class OneOffs
   
+  include ActionView::Helpers::NumberHelper
+  
   def self.check_syntax
     Rails::Initializer.run(:load_application_classes)
     true
@@ -127,6 +129,27 @@ class OneOffs
         r.save!
       end
     end
+    
+  end
+  
+  def self.get_monthly_data_by_app(partner_id, year, month)
+    
+    month_start = Time.utc(year, month, 01)
+    
+    total_revenue = 0
+    total_spend = 0
+    
+    Offer.find_all_by_partner_id(partner_id).each do |offer|
+      s = Appstats.new(offer.id, {:type => :sum, :start_time => month_start, :end_time => month_start.end_of_month})
+      revenue = s.stats['rewards_revenue'].first + s.stats['display_revenue'].first
+      spend = s.stats['installs_spend'].first
+      total_revenue += revenue
+      total_spend += spend
+      puts "#{offer.name.gsub(',','_')}, #{number_to_currency(revenue/100.0)}, #{number_to_currency(spend/-100.0)}" if revenue != 0 or spend != 0
+    end
+    
+    puts "Total, #{number_to_currency(total_revenue/100.0)}, #{number_to_currency(total_spend/-100.0)}"
+    
     
   end
   
