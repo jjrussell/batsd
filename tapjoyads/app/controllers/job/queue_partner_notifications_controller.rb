2e75a50bf4c32f6e87c2dd3d5cbbfd7f9403dcbc
Amir Manji <amir@tapjoy.com>
2010-12-13 12:57:14 -0800

    exclude partners with extremely low balances from campaign status emails

--- tapjoyads/app/controllers/job/queue_partner_notifications_controller.rb
+++ tapjoyads/app/controllers/job/queue_partner_notifications_controller.rb
@@ -18,7 +18,7 @@ class Job::QueuePartnerNotificationsController < Job::SqsReaderController
     
     offers.each do |offer|
       if offer.needs_more_funds?
-        offers_needing_more_funds << offer
+        offers_needing_more_funds << offer if offer.payment < partner.balance
       elsif offer.budget_may_not_be_met?
         offers_not_meeting_budget << offer
       elsif offer.needs_higher_bid?
