fad5239b1088a27c8bb06cd0945598a3b726f281
hwanjoon choi <hwanjoon@tapjoy.com>
2010-12-16 18:07:27 -0800

    use tapandmar for android market search

--- tapjoyads/lib/app_store.rb
+++ tapjoyads/lib/app_store.rb
@@ -3,8 +3,10 @@ class AppStore
   APP_URL = 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/wa/wsLookup'
   SEARCH_URL = 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/wa/wsSearch'
 
-  ANDROID_SEARCH_URL = 'http://www.appbrain.com/search'
-  ANDROID_APP_URL = 'http://www.appbrain.com/app/'
+  ANDROID_SEARCH_URL = 'http://tapandmar.appspot.com/search/'
+  ANDROID_APP_URL = 'http://tapandmar.appspot.com/search/'
+  ANDROID_ICON_URL = 'http://tapandmar.appspot.com/icon/'
+
   # returns hash of app info
   def self.fetch_app_by_id(id, platform='iphone', country='')
     if platform == 'android'
@@ -37,26 +39,7 @@ private
   end
 
   def self.fetch_app_by_id_for_android(id)
-    response = request(ANDROID_APP_URL + id)
-    if response.status == 200
-      doc = Hpricot.parse(response.body)
-      app = (doc/"div.appPage").first
-      title = (app/"div h1").text
-      icon_url = (app/"div h1 img").first.attributes['src']
-      publisher = (app/"div div a").first.inner_html
-      price = "%.2f" % (app/"div"/"span.priceFree,span.pricePaid").first.inner_html.gsub(/\$/, '').to_f
-      description = (doc/".appPage p").last.inner_html.split(/Latest version/).first
-      {
-        :item_id => id,
-        :title => title.strip,
-        :icon_url => icon_url,
-        :price => price,
-        :description => description.strip,
-        :publisher => publisher.strip
-      }
-    else
-      raise "Invalid response from Android App Search: #{response.status}"
-    end
+    self.search_android_marketplace(id).first
   end
 
   def self.search_apple_app_store(term, country)
@@ -72,25 +55,21 @@ private
   end
 
   def self.search_android_marketplace(term)
-    response = request(ANDROID_SEARCH_URL, {:market => 'android', :q => term})
+    response = request(ANDROID_SEARCH_URL + term)
     if response.status == 200
-      doc = Hpricot.parse(response.body)
-      if (doc/"a.result").size > 0
-        return (doc/"a.result").map do |link|
-          {
-            :item_id => link.attributes['href'].split(/\//).last,
-            :title => link.attributes['title'],
-            :icon_url => (link/"img.icon").first.attributes['src'],
-            :price => "%.2f" % (link/"span"/"span.priceBox").text.gsub(/\$/, '').to_f,
-            :description => (link/"span"/"span.appSnippet"/"span.snippet").text.strip,
-            :publisher => (link/"span"/"span.appSnippet"/"span.dev").first.inner_html.sub('by ',''),
-          }
-        end
-      else
-        return [ ]
+      items = JSON.load(response.body)
+      return items['app'].map do |hash|
+        {
+          :item_id      => hash['packageName'],
+          :title        => hash['title'],
+          :icon_url     => ANDROID_ICON_URL + hash['packageName'],
+          :price        => hash['price'].nil? ? 0 : hash['price'].gsub(/[^\d\.\-]/,'').to_f,
+          :description  => hash['ExtendedInfo']['description'],
+          :publisher    => hash['creator']
+        }
       end
     else
-      raise "Invalid response from Cyrket"
+      raise "Invalid response."
     end
   end
 
