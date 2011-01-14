9508048808197b2800a5280ea08fc075f9250efa
Ryan Johns <ryan@tapjoy.com>
2011-01-14 14:16:31 -0800

    improve efficiency of master app stats and master cache offers

--- tapjoyads/app/controllers/job/master_app_stats_controller.rb
+++ tapjoyads/app/controllers/job/master_app_stats_controller.rb
@@ -5,7 +5,8 @@ class Job::MasterAppStatsController < Job::JobController
   
   def index
     Offer.to_aggregate_stats.each do |offer|
-      offer.update_attribute(:next_stats_aggregation_time, @now + 1.hour + rand(600))
+      offer.next_stats_aggregation_time = @now + 1.hour + rand(600)
+      offer.save(false)
       Sqs.send_message(QueueNames::APP_STATS, offer.id)
     end
     
