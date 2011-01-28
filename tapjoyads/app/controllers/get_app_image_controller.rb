0802d16976cc22850647a834a7d2324b94cc05d4
Amir Manji <amir@tapjoy.com>
2011-01-27 17:42:46 -0800

    fix icons for actions and secondary offers

--- tapjoyads/app/controllers/get_app_image_controller.rb
+++ tapjoyads/app/controllers/get_app_image_controller.rb
@@ -4,16 +4,21 @@ class GetAppImageController < ApplicationController
     return unless verify_params([:app_id])
     
     app_id = params[:app_id].downcase
+    params[:img] = '1' if app_id.gsub!('img=1', '') # Tap Fish sometimes sends malformated params like: app_id=guidimg=1
     
-    # Tap Fish sometimes sends malformated params like: app_id=guidimg=1
-    if app_id.gsub!('img=1', '') || params[:img] == '1'
-      redirect_to "#{CLOUDFRONT_URL}/icons/#{app_id}.png" and return
+    offer = Offer.find_in_cache app_id
+    return unless verify_records([ offer ])
+    icon_id = offer.icon_id
+    
+    if params[:img] == '1'
+      redirect_to "#{CLOUDFRONT_URL}/icons/#{icon_id}.png" and return
     end
     
-    @icon = Mc.get_and_put("icon.s3.#{app_id}") do
+    @icon = Mc.get_and_put("icon.s3.#{icon_id}", false, 1.day) do
       bucket = S3.bucket(BucketNames::TAPJOY)
-      image_content = bucket.get("icons/#{app_id}.png")
+      image_content = bucket.get("icons/#{icon_id}.png")
       Base64.encode64 image_content
     end
+    
   end
 end
