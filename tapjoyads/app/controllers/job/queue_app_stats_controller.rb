6bd1c90b046ecd29369e38b9a3132c0e5b08584a
Stephen McCarthy <stephen@tapjoy.com>
2011-01-28 11:33:57 -0800

    Count get_vg_items in queue_app_stats

--- tapjoyads/app/controllers/job/queue_app_stats_controller.rb
+++ tapjoyads/app/controllers/job/queue_app_stats_controller.rb
@@ -3,7 +3,7 @@ class Job::QueueAppStatsController < Job::SqsReaderController
   def initialize
     super QueueNames::APP_STATS
     @num_reads = 10
-    @paths_to_aggregate = %w(connect new_user adshown offer_click daily_user monthly_user purchased_vg offers)
+    @paths_to_aggregate = %w(connect new_user adshown offer_click daily_user monthly_user purchased_vg get_vg_items offers)
     @publisher_paths_to_aggregate = %w(offer_click)
     @displayer_paths_to_aggregate = %w(display_ad_requested display_ad_shown offer_click)
   end
