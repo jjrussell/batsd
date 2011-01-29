1f50201d28d9b6a48b042ceefe2fecdf2348de04
Stephen McCarthy <stephen@tapjoy.com>
2011-01-28 16:00:13 -0800

    Make get_user_id_from_udid use the publisher_app_id, not the displayer app id, so that it actually works.

--- tapjoyads/app/controllers/display_ad_controller.rb
+++ tapjoyads/app/controllers/display_ad_controller.rb
@@ -85,9 +85,7 @@ private
     publisher_app_ids = []
     if self_ad
       publisher_app_ids << params[:app_id]
-      user_id = params[:publisher_user_id]
     else
-      user_id = get_user_id_from_udid(params[:udid], params[:app_id])
       @@allowed_publisher_app_ids.each do |app_id|
         last_run_time = device.last_run_time(app_id)
         if last_run_time.present? && last_run_time > now - 1.week
@@ -127,6 +125,7 @@ private
       offer = offer_list[rand(offer_list.size)]
     
       if offer.present?
+        user_id = self_ad ? params[:publisher_user_id] : get_user_id_from_udid(params[:udid], publisher_app_id)
         @click_url = offer.get_click_url(publisher_app, user_id, params[:udid], currency.id, 'display_ad', nil, now, params[:app_id])
         @image = get_ad_image(publisher_app, offer, self_ad, params[:size], currency)
       
