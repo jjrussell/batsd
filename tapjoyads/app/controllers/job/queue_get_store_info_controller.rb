ec83282d3a5475dccf53a8edc9c68a6ae20c0370
Stephen McCarthy <stephen@tapjoy.com>
2011-02-02 11:32:07 -0800

    Update apps prices in nightly job. Also log activity.

--- tapjoyads/app/controllers/job/queue_get_store_info_controller.rb
+++ tapjoyads/app/controllers/job/queue_get_store_info_controller.rb
@@ -8,6 +8,7 @@ private
 
   def on_message(message)
     app = App.find(message.to_s)
+    log_activity(app)
 
     return if app.store_id.nil?
     app_data = AppStore.fetch_app_by_id(app.store_id)
@@ -17,7 +18,10 @@ private
     app.age_rating = app_data[:age_rating]
     app.file_size_bytes = app_data[:file_size_bytes]
     app.supported_devices = app_data[:supported_devices].to_json
+    app.price = app_data[:price]
     app.save!
+    
+    save_activity_logs
   end
 
 end
