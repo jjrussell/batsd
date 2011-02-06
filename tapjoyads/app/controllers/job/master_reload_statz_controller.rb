571738a303399db94c4a55a1b775b1035633ab06
Ryan Johns <ryan@tapjoy.com>
2011-02-05 21:52:12 -0800

    add delimiters to the sums on statz/index for easier reading.  also, speed up master_reload_statz a little bit by checking if the offer's conversions or publisher_offers are > 0 before building the stats hash.

--- tapjoyads/app/controllers/job/master_reload_statz_controller.rb
+++ tapjoyads/app/controllers/job/master_reload_statz_controller.rb
@@ -31,25 +31,26 @@ private
     
     Offer.find_each(:conditions => "stats_aggregation_interval = 3600") do |offer|
       appstats = Appstats.new(offer.id, { :start_time => start_time, :end_time => now + 1.hour, :granularity => granularity }).stats
+      conversions = appstats['paid_installs'].sum
+      published_offers = appstats['rewards'].sum
+      next unless conversions > 0 || published_offers > 0
       
       this_apps_stats = {}
       this_apps_stats['icon_url'] = offer.get_icon_url
       this_apps_stats['offer_name'] = offer.name_with_suffix
-      this_apps_stats['conversions'] = appstats['paid_installs'].sum
-      this_apps_stats['connects'] = appstats['logins'].sum
+      this_apps_stats['conversions'] = number_with_delimiter(conversions)
+      this_apps_stats['connects'] = number_with_delimiter(appstats['logins'].sum)
       this_apps_stats['overall_store_rank'] = (Array(appstats['ranks']['overall.free.united_states']).find_all{|r| r != nil}.last || '-')
       this_apps_stats['price'] = number_to_currency(offer.price / 100.0)
       this_apps_stats['payment'] = number_to_currency(offer.payment / 100.0)
       this_apps_stats['balance'] = number_to_currency(offer.partner.balance / 100.0)
       this_apps_stats['conversion_rate'] = "%.1f%" % ((offer.conversion_rate || 0) * 100.0)
-      this_apps_stats['published_offers'] = appstats['rewards'].sum
+      this_apps_stats['published_offers'] = number_with_delimiter(published_offers)
       this_apps_stats['offers_revenue'] = number_to_currency(appstats['rewards_revenue'].sum / 100.0)
       this_apps_stats['platform'] = offer.get_platform
       this_apps_stats['featured'] = offer.featured?
       
-      if this_apps_stats['conversions'] > 0 || this_apps_stats['published_offers'] > 0
-        cached_stats[offer.id] = this_apps_stats
-      end
+      cached_stats[offer.id] = this_apps_stats
       
       if timeframe != '24_hours'
         sleep(1)
