9bc4fc963de00c24827500cb4a4ba884a07d1086
Amir Manji <amir@tapjoy.com>
2011-02-10 14:43:50 -0800

    return user account on load

--- tapjoyads/app/controllers/game_state_controller.rb
+++ tapjoyads/app/controllers/game_state_controller.rb
@@ -2,14 +2,19 @@ class GameStateController < ApplicationController
   def load
     return unless verify_params([:app_id, :publisher_user_id])
     @game_state = GameState.new :key => "#{params[:app_id]}.#{params[:publisher_user_id]}"
+    @point_purchases = PointPurchases.new :key => "#{params[:publisher_user_id]}.#{params[:app_id]}"
+    @currency = Currency.find_in_cache params[:app_id]
   end
   
   def save
     return unless verify_params([:app_id, :publisher_user_id, :data, :udid])
+    params[:spend] ||= 0
     @game_state = GameState.new :key => "#{params[:app_id]}.#{params[:publisher_user_id]}"
     @game_state.data = params[:data]
     @game_state.version += 1
     @game_state.add_device params[:udid]
+    @game_state.tapjoy_spend += params[:spend].to_i
     @game_state.save
   end
+  
 end
