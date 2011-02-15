588e6d18385d7ceaed7b4d25804aa2a5c1914cde
Amir Manji <amir@tapjoy.com>
2011-02-15 14:02:46 -0800

    2 production game state domains for now

--- tapjoyads/config/environments/production.rb
+++ tapjoyads/config/environments/production.rb
@@ -51,6 +51,7 @@ NUM_POINT_PURCHASES_DOMAINS = 10
 NUM_CLICK_DOMAINS = 50
 NUM_REWARD_DOMAINS = 50
 NUM_DEVICES_DOMAINS = 300
+NUM_GAME_STATE_DOMAINS = 2
 
 mail_chimp = YAML::load_file("#{RAILS_ROOT}/config/mail_chimp.yaml")['production']
 MAIL_CHIMP_API_KEY = mail_chimp['api_key']
