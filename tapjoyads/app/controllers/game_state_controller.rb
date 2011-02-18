7b5cca4689228444c94d568042982a49b27987c9
Amir Manji <amir@tapjoy.com>
2011-02-18 12:09:42 -0800

    make spend a required gamestate paramater

--- tapjoyads/app/controllers/game_state_controller.rb
+++ tapjoyads/app/controllers/game_state_controller.rb
@@ -10,8 +10,7 @@ class GameStateController < ApplicationController
   end
   
   def save
-    return unless verify_params([:app_id, :publisher_user_id, :data, :udid])
-    params[:spend] ||= 0
+    return unless verify_params([:app_id, :publisher_user_id, :data, :udid, :spend])
     @game_state = GameState.new :key => "#{params[:app_id]}.#{params[:publisher_user_id]}"
     @game_state.data = params[:data]
     @game_state.version += 1
