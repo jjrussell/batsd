8cb3269c19e0f5705716fa7d9d2c0b40af6d844a
Ryan Johns <ryan@tapjoy.com>
2011-02-19 02:57:12 -0800

    don't store any country breakdowns for paid_clicks

--- tapjoyads/app/controllers/job/queue_app_stats_controller.rb
+++ tapjoyads/app/controllers/job/queue_app_stats_controller.rb
@@ -65,18 +65,18 @@ private
       stat_row.update_stat_for_hour(stat_name, start_time.hour, count)
     end
     
-    if @skip_hour_counts
-      total_country_clicks = 0
-      Stats::TOP_COUNTRIES.each do |country|
-        stat_name = ['countries', "paid_clicks.#{country}"]
-        count = Mc.get_count(Stats.get_memcache_count_key(stat_name, @offer.id, start_time))
-        total_country_clicks += count
-        stat_row.update_stat_for_hour(stat_name, start_time.hour, count)
-      end
-      total_paid_clicks = stat_row.get_hourly_count('paid_clicks')[start_time.hour]
-      stat_name = ['countries', "paid_clicks.other"]
-      stat_row.update_stat_for_hour(stat_name, start_time.hour, total_paid_clicks - total_country_clicks)
-    end
+    # if @skip_hour_counts
+    #   total_country_clicks = 0
+    #   Stats::TOP_COUNTRIES.each do |country|
+    #     stat_name = ['countries', "paid_clicks.#{country}"]
+    #     count = Mc.get_count(Stats.get_memcache_count_key(stat_name, @offer.id, start_time))
+    #     total_country_clicks += count
+    #     stat_row.update_stat_for_hour(stat_name, start_time.hour, count)
+    #   end
+    #   total_paid_clicks = stat_row.get_hourly_count('paid_clicks')[start_time.hour]
+    #   stat_name = ['countries', "paid_clicks.other"]
+    #   stat_row.update_stat_for_hour(stat_name, start_time.hour, total_paid_clicks - total_country_clicks)
+    # end
     
     paid_installs, installs_spend, jailbroken_installs, paid_installs_by_country, installs_spend_by_country = nil
     Conversion.using_slave_db do
