b74a22421be73d1f0d0f8d0a0fae3df41e5eb898
Ryan Johns <ryan@tapjoy.com>
2011-02-25 14:23:55 -0800

    fix tests

--- tapjoyads/test/functional/enable_offer_requests_controller_test.rb
+++ tapjoyads/test/functional/enable_offer_requests_controller_test.rb
@@ -1,9 +1,7 @@
 require 'test_helper'
 
-class OffersControllerTest < ActionController::TestCase
+class EnableOfferRequestsControllerTest < ActionController::TestCase
   setup :activate_authlogic
-  setup do
-  end
 
   context "Users wishing to tapjoy-enable offers" do
     setup do
@@ -12,16 +10,13 @@ class OffersControllerTest < ActionController::TestCase
       @app = Factory(:app, :partner => @partner)
       @offer = @app.primary_offer
       login_as(@user)
-      @controller = Apps::OffersController.new
     end
 
     should "be able to submit a request" do
-      post 'request_tapjoy_enable', :app_id => @app.id, :id => @offer.id
+      assert_equal 0, @offer.enable_offer_requests.count
+      post :create, :enable_offer_request => { :offer_id => @offer.id }
       assert_response(:redirect)
       assert_equal 0, @offer.enable_offer_requests.first.status
-      assert_equal 'request_tapjoy_enable', assigns['activity_logs'].last.action
-      assert_equal 'EnableOfferRequest', assigns['activity_logs'].last.object_type
-      assert_equal @offer.enable_offer_requests.first.id.to_s, assigns['activity_logs'].last.object_id
     end
   end
 end
