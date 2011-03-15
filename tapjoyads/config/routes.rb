126467e70891251b03864370eed0614df8757d54
Robert Fletcher <lobatifricha@gmail.com>
2011-03-15 14:17:12 -0700

    extra pretties for global

--- tapjoyads/config/routes.rb
+++ tapjoyads/config/routes.rb
@@ -72,9 +72,9 @@ ActionController::Routing::Routes.draw do |map|
                      :unresolved_clicks => :post, :resolve_clicks => :post, :sqs_lengths => :get, :elb_status => :get,
                      :publishers_without_payout_info => :get },
     :member => {  :edit_android_app => :get, :update_android_app => :post, :device_info => :get }
-  map.connect 'statz/global', :controller => 'statz', :action => 'global'
   map.resources :statz, :only => [ :index, :show, :edit, :update, :new, :create ],
-    :member => { :last_run_times => :get, :udids => :get }
+    :member => { :last_run_times => :get, :udids => :get },
+    :collection => { :global => :get }
   map.resources :raffle_manager, :only => [ :index, :edit, :update, :new, :create ]
   map.resources :activities, :only => [ :index ]
   map.resources :partners, :only => [ :index, :show, :new, :create, :update, :edit ],
