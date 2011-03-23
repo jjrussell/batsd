81215cfa65b147fd8ff09794b8d1596cf621bbd1
Ryan Johns <ryan@tapjoy.com>
2011-03-23 02:45:14 -0700

    shouldn't verify authenticity token on PUTs to the agency api since they don't originate from our app

--- tapjoyads/app/controllers/agency_api_controller.rb
+++ tapjoyads/app/controllers/agency_api_controller.rb
@@ -2,6 +2,7 @@ class AgencyApiController < ApplicationController
   include SslRequirement
   
   skip_before_filter :fix_params, :reject_banned_ips
+  skip_before_filter :verify_authenticity_token, :only => [ :update ]
   
 protected
   
