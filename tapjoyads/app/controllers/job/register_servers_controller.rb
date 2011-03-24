c9072195e87f869a9cebbe70c29b4a341df06045
Ryan Johns <ryan@tapjoy.com>
2011-03-24 12:20:58 -0700

    remove unused controller

--- tapjoyads/app/controllers/job/register_servers_controller.rb
+++
@@ -1,9 +0,0 @@
-# Registers all servers in the 'mc' security group to be in this server's memcache pool.
-
-class Job::RegisterServersController < Job::JobController
-  def index
-    #MemcachedModel.instance.register_servers
-    
-    render :text => "ok"
-  end
-end
