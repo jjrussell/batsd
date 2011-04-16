3fa9dc9b0a26096e5aedb0ba5c8c055f9cc913b3
Ryan Johns <ryan@tapjoy.com>
2011-04-16 15:06:28 -0700

    add one-off and remove logic from migration

--- tapjoyads/db/migrate/20110416200308_add_tapjoy_enabled_to_currencies.rb
+++ tapjoyads/db/migrate/20110416200308_add_tapjoy_enabled_to_currencies.rb
@@ -1,7 +1,6 @@
 class AddTapjoyEnabledToCurrencies < ActiveRecord::Migration
   def self.up
     add_column :currencies, :tapjoy_enabled, :boolean, :null => false, :default => false
-    Currency.connection.execute("UPDATE currencies SET tapjoy_enabled = true")
   end
 
   def self.down
