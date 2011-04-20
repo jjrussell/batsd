7de2105ea2f1fbe13bd771b640fffb458c2ec030
Ryan Johns <ryan@tapjoy.com>
2011-04-20 14:48:36 -0700

    create a queue for the apple data job and add the combine_data one-off

--- tapjoyads/config/jobs.rb
+++ tapjoyads/config/jobs.rb
@@ -22,6 +22,7 @@ JobRunner::Gateway.define do |s|
     s.add_job 'queue_mail_chimp_updates', :interval => 1.minute
     s.add_job 'queue_partner_notifications', :interval => 5.minutes
     s.add_job 'queue_recount_stats', :interval => 5.minutes
+    s.add_job 'queue_apple_data', :interval => 5.minutes
   elsif machine_type == 'masterjobs'
     # jobs with high impact on overall system performance
     s.add_job 'master_calculate_next_payout', :daily => 4.hours
