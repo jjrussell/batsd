95d003696b45c03b753b2441b591527cbd0203cf
Ryan Johns <ryan@tapjoy.com>
2011-04-20 13:54:56 -0700

    data mining for apple execs

--- tapjoyads/lib/one_offs.rb
+++ tapjoyads/lib/one_offs.rb
@@ -56,4 +56,22 @@ class OneOffs
     end
   end
 
+  def apple_data(domain_num)
+    count = 0
+    domain_name = "rewards_#{domain_num}"
+    counts = {}
+    start = Time.zone.now.beginning_of_year
+    finish = start + 3.months
+    Reward.select(:domain_name => domain_name, :where => "created >= '#{start.to_i}' AND created < '#{finish.to_i}'") do |r|
+      count += 1
+      puts "#{Time.zone.now.to_s(:db)} - looked at #{count} rewards" if count % 1000 == 0
+      next unless r.udid =~ /^[a-f0-9]{40}$/
+      counts[r.udid] ||= 0
+      counts[r.udid] += 1
+    end
+    b = S3.bucket(BucketNames::TAPJOY)
+    b.put("apple_data/#{domain_name}.json", counts.to_json)
+    counts
+  end
+
 end
