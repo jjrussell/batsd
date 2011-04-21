7cdb041019a0e5c32ab7dd53be7fbb9683edc56c
Amir Manji <amir@tapjoy.com>
2011-04-20 22:16:25 -0700

    always render index with newhome layout

--- tapjoyads/app/controllers/homepage_controller.rb
+++ tapjoyads/app/controllers/homepage_controller.rb
@@ -8,7 +8,7 @@ class HomepageController < WebsiteController
     elsif permitted_to?(:index, :apps)
       redirect_to apps_path
     elsif current_partner.nil?
-      render :action => 'index', :layout => 'newhome.html.haml'
+      render :action => 'index', :layout => 'newhome'
     end
   end
 
@@ -28,10 +28,14 @@ class HomepageController < WebsiteController
   end
   
   def advertisers
-    render :layout => 'homepage.html.haml';
+    render :layout => 'homepage'
   end
   
   def app_developers
-    render :layout => 'newcontent.html.haml'
+    render :layout => 'newcontent'
+  end
+  
+  def index
+    render :layout => 'newhome'
   end
 end
