55fa9af7b97fa9534cc485c76e62b5218b3ff444
Eric Bieller <eric.bieller@tapjoy.com>
2011-04-26 16:01:47 -0700

    Forgot to add some files

---
+++ tapjoyads/app/controllers/homepage/careers_controller.rb
@@ -0,0 +1,30 @@
+## Press Controller
+## @description Controls press section of the website
+## @help News entries are loaded via constructor. Press/index.html.haml contains the main press view but viewing articles uses press.html.haml as the layout
+
+class Homepage::CareersController < WebsiteController
+  layout 'careers'
+  def initialize
+    @careers_list = [
+      [ "Product",
+        ["Name", "URL"],
+        ["Name2", "URL2"],
+        ["Name3", "URL3"],
+      ],
+      [ "Engineering",
+        ["Name", "URL"],
+        ["Name2", "URL2"],
+        ["Name3", "URL3"],
+      ]
+    ]
+  end
+
+  def show
+    sanitized_id = params[:id]
+    render "homepage/careers/#{sanitized_id}"
+  end
+
+  def index
+    render :layout => 'newcontent'
+  end
+end
