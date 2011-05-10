81fd3411d9f45cb0dcd03e6f4443e574834b0889
Hwan-Joon Choi <hwanjoon@tapjoy.com>
2011-05-09 17:14:28 -0700

    oops, left my email in there

--- tapjoyads/app/models/tapjoy_mailer.rb
+++ tapjoyads/app/models/tapjoy_mailer.rb
@@ -63,7 +63,7 @@ class TapjoyMailer < ActionMailer::Base
   def contact_us(info)
     from 'support@tapjoy.com'
     if Rails.env == 'development'
-      recipients "hwanjoon@tapjoy.com"#"dev@tapjoy.com"
+      recipients "dev@tapjoy.com"
     else
       recipients "support+contactus@tapjoy.com"
     end
