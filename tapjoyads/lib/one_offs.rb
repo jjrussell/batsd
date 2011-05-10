c5f0aff034bbdc5f0d9cdf1e71e96cef6ae5c88d
Amir Manji <amir@tapjoy.com>
2011-05-10 00:08:18 -0700

    populate urls for existing action offers

--- tapjoyads/lib/one_offs.rb
+++ tapjoyads/lib/one_offs.rb
@@ -2,7 +2,10 @@ class OneOffs
   
   def self.copy_instructions_into_offers
     ActionOffer.find_each do |action_offer|
-      action_offer.primary_offer.update_attribute(:instructions, action_offer.instructions)
+      offer = action_offer.primary_offer
+      offer.instructions = action_offer.instructions
+      offer.url = action_offer.app.direct_store_url
+      offer.save!
     end
   end
 
