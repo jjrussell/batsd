4484c8932719e6efbc6b672b150ca687402ade1d
Sanjath Shringeri <sshringeri@sshringeri-mbpro.local>
2011-05-19 14:52:27 -0700

    Move filtering into view. Add initial sorting in the controller to avoid JS sorting.

--- tapjoyads/app/controllers/account/whitelist_controller.rb
+++ tapjoyads/app/controllers/account/whitelist_controller.rb
@@ -5,29 +5,41 @@ class Account::WhitelistController < WebsiteController
   filter_access_to :all
 
   def index
-    unless current_partner.use_whitelist?
-      redirect_to apps_path
-    end  
+    redirect_to apps_path unless current_partner.use_whitelist?
     @whitelisted_offers = current_partner.get_offer_whitelist
-    @offers = Offer.enabled_offers.by_name(params[:name]).by_device(params[:device])
-    @status_to_show = params[:status]
+    all_offers = Offer.enabled_offers.by_name(params[:name]).by_device(params[:device] == "all" ? "" : params[:device]).sort_by {|offer| offer.name}
+    approved_offers = all_offers.reject { |offer| !@whitelisted_offers.include?(offer.id) }
+    blocked_offers = all_offers.reject { |offer| @whitelisted_offers.include?(offer.id) }
+    case params[:status]
+    when "a": @offers = approved_offers
+    when "b": @offers = blocked_offers
+    else @offers = approved_offers + blocked_offers
+    end
   end
   
   def enable
-    unless current_partner.use_whitelist?
-      redirect_to apps_path
-    end
+    redirect_to apps_path unless current_partner.use_whitelist?
     current_partner.add_to_whitelist(params[:id])
     current_partner.save!
     redirect_to account_whitelist_index_path
   end
   
   def disable
-    unless current_partner.use_whitelist?
-      redirect_to apps_path
-    end
+    redirect_to apps_path unless current_partner.use_whitelist?
     current_partner.remove_from_whitelist(params[:id])
     current_partner.save!
     redirect_to account_whitelist_index_path
   end
+  
+class ItemForWhitelistSort
+  def initialize( item )
+    @item = item
+  end
+  def item
+    @item
+  end
+  def <=>( target )
+    ( self.item <=> target.item ) * (-1)
+  end
+end  
 end
