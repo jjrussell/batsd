ea852bba78d5b392d44dd453a04f9c3ae0bcf274
Hwan-Joon Choi <hwanjoon@tapjoy.com>
2011-05-18 20:13:26 -0700

    apsalar_sharing defaults to false

--- tapjoyads/db/migrate/20110506095954_add_apsalar_sharing_to_partners.rb
+++ tapjoyads/db/migrate/20110506095954_add_apsalar_sharing_to_partners.rb
@@ -1,6 +1,6 @@
 class AddApsalarSharingToPartners < ActiveRecord::Migration
   def self.up
-    add_column :partners, :apsalar_sharing, :boolean
+    add_column :partners, :apsalar_sharing, :boolean, :null => false, :default => false
   end
 
   def self.down
