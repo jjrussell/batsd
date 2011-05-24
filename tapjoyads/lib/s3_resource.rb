e578238f113dea52a6f3bff6dda468382ad87cdd
Robert Fletcher <lobatifricha@gmail.com>
2011-05-24 11:55:14 -0700

    fix for issue where S3 has timing issue, and log_progress for store ranks job

--- tapjoyads/lib/s3_resource.rb
+++ tapjoyads/lib/s3_resource.rb
@@ -139,7 +139,8 @@ class S3Resource
       Mc.put(get_memcache_key, @attributes) if save_to_memcache
 
       begin
-        bucket = S3.bucket(bucket_name)
+        s3 = RightAws::S3.new(nil, nil, { :multi_thread => true, :port => 80, :protocol => 'http' })
+        bucket = RightAws::S3::Bucket.new(s3, bucket_name)
         bucket.put(id, convert_to_raw_attributes)
       rescue Exception => e
         if retries > 0
