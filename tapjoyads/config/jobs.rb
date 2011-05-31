3267db7232580780e7f59628811266d88be379aa
Ryan Johns <ryan@tapjoy.com>
2011-05-31 15:01:48 -0700

    add job config

--- tapjoyads/config/jobs.rb
+++ tapjoyads/config/jobs.rb
@@ -23,6 +23,7 @@ JobRunner::Gateway.define do |s|
     s.add_job 'queue_partner_notifications', :interval => 5.minutes
     s.add_job 'queue_recount_stats', :interval => 5.minutes
     s.add_job 'queue_limit_app_installs', :interval => 30.seconds
+    s.add_job 'queue_udid_reports', :interval => 5.minutes
   elsif machine_type == 'masterjobs'
     # jobs with high impact on overall system performance
     s.add_job 'master_calculate_next_payout', :daily => 4.hours
