3197f729327fefd3eb3658d3d5a7b805c9b6d2c9
Amir Manji <amir@tapjoy.com>
2011-06-07 14:07:20 -0700

    doh

--- tapjoyads/config/initializers/machine_type.rb
+++ tapjoyads/config/initializers/machine_type.rb
@@ -1,5 +1,1 @@
 MACHINE_TYPE = `"#{Rails.root.to_s}/../server/server_type.rb"`
-Rails.logger.info '************************ FUCK YOU HELLO ************************'
-Rails.logger.info '************************ FUCK YOU HELLO ************************'
-Rails.logger.info '************************ FUCK YOU HELLO ************************'
-Rails.logger.info '************************ FUCK YOU HELLO ************************'
