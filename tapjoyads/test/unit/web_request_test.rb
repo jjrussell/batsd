06c5d06a47c9022f027de4ab58c5d9ea6ac2d12b
Robert Fletcher <lobatifricha@gmail.com>
2011-06-06 18:42:14 -0700

    fixing web request tests, typo in action offer

--- tapjoyads/test/unit/web_request_test.rb
+++ tapjoyads/test/unit/web_request_test.rb
@@ -1,41 +1,26 @@
 require 'test_helper'
 
 class WebRequestTest < ActiveSupport::TestCase
-  ##
-  # All tests are merged into a single test case. This is because in order to guarantee 
-  # consistency, we must sleep between writes. Therefore this is set up to have all writes
-  # occur in write* methods, and all reads occur in read* methods. This test simply calls 
-  # the write and read methods, with a single sleep in between.
-  test "all tests" do
-    write_basic
-    
-    sleep(10)
-    
-    read_basic
-  end
-  
-  def write_basic
+  should "write and read WebRequests consistently" do
     @basic_time = Time.now.utc
     
-    m = WebRequest.new
+    m = WebRequest.new(:consistent => true)
     m.put_values('connect', {:app_id => 'app1', :udid => 'udid1'}, nil, {}, 'UserAgent1')
-    m.save
+    m.serial_save
     
-    m = WebRequest.new
+    m = WebRequest.new(:consistent => true)
     m.put_values('connect', {:app_id => 'app1', :udid => 'udid2'}, nil, {}, 'UserAgent2')
     m.add_path('new_user')
-    m.save
+    m.serial_save
     @basic_key = m.key
     @basic_domain = m.this_domain_name
-  end
-  def read_basic
+
     assert_equal(2, Mc.get_count(Stats.get_memcache_count_key('logins', 'app1', @basic_time)))
     assert_equal(1, Mc.get_count(Stats.get_memcache_count_key('new_users', 'app1', @basic_time)))
     
-    m = WebRequest.new(:domain_name => @basic_domain, :key => @basic_key, :load => true, :load_from_memcache => false)
+    m = WebRequest.new(:domain_name => @basic_domain, :key => @basic_key, :load => true, :load_from_memcache => false, :consistent => true)
     assert_equal(['connect', 'new_user'], m.get('path'))
     assert_equal('app1', m.get('app_id'))
     assert_equal('udid2', m.get('udid'))
   end
-  
 end
