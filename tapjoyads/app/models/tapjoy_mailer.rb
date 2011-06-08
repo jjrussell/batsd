905c5c01b15324ac35e871dbf93e89c215a7915b
Amir Manji <amir@tapjoy.com>
2011-06-07 17:45:46 -0700

    send emails to mobilesupport instead of me

--- tapjoyads/app/models/tapjoy_mailer.rb
+++ tapjoyads/app/models/tapjoy_mailer.rb
@@ -114,7 +114,7 @@ class TapjoyMailer < ActionMailer::Base
   def support_request(description, email_address, app, currency, udid, publisher_user_id, device_type, language_code)
     from 'Online Support Request <noreply@tapjoy.com>'
     reply_to email_address
-    recipients 'amir@tapjoy.com'
+    recipients 'mobilesupport@tapjoy.com'
     content_type 'text/html'
     subject 'Missing Currency'
     body(:description => description, :app => app, :currency => currency, :udid => udid, 
