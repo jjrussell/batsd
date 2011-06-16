a0afc67d14ac103fa9a68f8d597e29fc6653a598
Eric Bieller <eric.bieller@tapjoy.com>
2011-06-15 23:33:54 -0700

    add var before iconid

--- tapjoyads/public/javascripts/android.js
+++ tapjoyads/public/javascripts/android.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $(".icon").click(function() {
-    iconId = $(this).attr('id');
+    var iconId = $(this).attr('id');
     $(".screenshot").removeClass('show');
     $("."+iconId).addClass('show');
     $(".icon").removeClass('selected');
