40a7e2663db79f4c9fcb124a5eeb0626ed746581
Eric Bieller <eric.bieller@tapjoy.com>
2011-06-15 18:35:46 -0700

    Added /androidfund, styled, application form doesn't work, tested in everything but IE

---
+++ tapjoyads/app/controllers/androidfund_controller.rb
@@ -0,0 +1,19 @@
+class AndroidfundController < WebsiteController
+  layout 'androidfund'
+  protect_from_forgery :except => [:contact]
+
+  def index
+  end
+  
+  def apply
+    if params[:info]
+      info = params[:info]
+      if info[:first_name].blank? || info[:last_name].blank? || info[:email_address].blank?
+        @error_msg = "All fields must be filled out."
+      else
+        TapjoyMailer.deliver_androidfund_application(params[:info])
+      end
+    end
+    render :layout => 'androidfund'
+  end
+end
