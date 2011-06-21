ff1ff99f5d8c122ae58ec8370ec783564932e01f
Stephen McCarthy <stephen@tapjoy.com>
2011-06-21 12:25:17 -0700

    Add yyyy_mm_dd time format. Only cache currency id's

--- tapjoyads/app/models/external_publishers.rb
+++ tapjoyads/app/models/external_publishers.rb
@@ -1,7 +1,7 @@
 class ExternalPublishers < S3Resource
   self.bucket_name = BucketNames::OFFER_DATA
   DEFAULT_KEY = 'external_publishers'
-  attribute :currencies, :type => :json, :default => []
+  attribute :currency_ids, :type => :json, :default => []
   
   def self.default
     ExternalPublishers.find(DEFAULT_KEY).currencies
@@ -9,17 +9,16 @@ class ExternalPublishers < S3Resource
   
   def self.cache
     external_publishers = ExternalPublishers.find_or_initialize_by_id(DEFAULT_KEY)
-    external_publishers.currencies = Currency.external_publishers
+    external_publishers.currency_ids = Currency.external_publishers.collect(&:id)
     external_publishers.save!
   end
   
   def self.populate_potential
     now = Time.zone.now
     yesterday = now - 1.day
-    date_string = yesterday.to_date.to_s(:db)
+    date_string = yesterday.to_s(:yyyy_mm_dd)
     
-    # Currency.find_each(:conditions => 'potential_external_publisher = false') do |currency|
-    Currency.find_each do |currency|
+    Currency.find_each(:conditions => 'potential_external_publisher = false') do |currency|
       appstats = Appstats.new(currency.app_id, :start_time => yesterday.beginning_of_day - 1.day, :end_time => now.beginning_of_day, :stat_types => ['offerwall_views', 'display_ads_requested', 'featured_offers_requested'])
       next if appstats.stats['offerwall_views'].sum + appstats.stats['display_ads_requested'].sum + appstats.stats['featured_offers_requested'].sum == 0
       
