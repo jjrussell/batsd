35ccbbc30580743557de3002f39111c16f35dd56
Ryan Johns <ryan@tapjoy.com>
2011-06-24 14:24:23 -0700

    don't overwrite gamers.confirmed_at if the account is already confirmed.

--- tapjoyads/app/controllers/games/confirmations_controller.rb
+++ tapjoyads/app/controllers/games/confirmations_controller.rb
@@ -2,7 +2,7 @@ class Games::ConfirmationsController < GamesController
   
   def create
     @gamer = Gamer.find_using_perishable_token(params[:token], 1.year)
-    if @gamer.present? && @gamer.confirm!
+    if @gamer.present? && (@gamer.confirmed_at? || @gamer.confirm!)
       flash[:notice] = 'Email address confirmed.'
       redirect_to games_root_path
     else
