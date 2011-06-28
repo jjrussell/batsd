9d1a915dbbd7c2ac8fb3466f8cd3ad22644c1556
Johnny Chan <johnnyjchan@gmail.com>
2011-06-28 13:56:50 -0700

    Revert "Updated iOS SDK version."
    This reverts commit 22b5750b67aff71aabb92b2ef915a85c46f510df.

--- tapjoyads/config/initializers/tapjoy.rb
+++ tapjoyads/config/initializers/tapjoy.rb
@@ -14,9 +14,9 @@ ANDROID_CONNECT_SDK = 'https://s3.amazonaws.com/tapjoy/sdks/TapjoyConnectSDK_And
 ANDROID_OFFERS_SDK  = 'https://s3.amazonaws.com/tapjoy/sdks/TapjoyOffersSDK_Android_v8.0.1.zip'
 ANDROID_VG_SDK      = 'https://s3.amazonaws.com/tapjoy/sdks/TapjoyVirtualGoodsSDK_Android_v8.0.1.zip'
 
-IPHONE_CONNECT_SDK  = 'https://s3.amazonaws.com/tapjoy/sdks/TapjoyConnectSDK_iOS_v8.0.1.zip'
-IPHONE_OFFERS_SDK   = 'https://s3.amazonaws.com/tapjoy/sdks/TapjoyConnectOffersSDK_iOS_v8.0.1.zip'
-IPHONE_VG_SDK       = 'https://s3.amazonaws.com/tapjoy/sdks/TapjoyConnectVirtualGoodsSDK_iOS_v8.0.1.zip'
+IPHONE_CONNECT_SDK  = 'https://s3.amazonaws.com/tapjoy/sdks/TapjoyConnectSDK_iOS_v8.0.0.zip'
+IPHONE_OFFERS_SDK   = 'https://s3.amazonaws.com/tapjoy/sdks/TapjoyConnectOffersSDK_iOS_v8.0.0.zip'
+IPHONE_VG_SDK       = 'https://s3.amazonaws.com/tapjoy/sdks/TapjoyConnectVirtualGoodsSDK_iOS_v8.0.0.zip'
 IPHONE_UNITY_PLUGIN = 'https://s3.amazonaws.com/tapjoy/sdks/TapjoyConnectUnityPluginSample.zip'
 
 
