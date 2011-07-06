80851db8ae971c156ed272c5b3cceae8cbd0d0b8
Eric Bieller <eric.bieller@tapjoy.com>
2011-07-06 12:21:45 -0700

    Altered main home page image

--- tapjoyads/db/schema.rb
+++ tapjoyads/db/schema.rb
@@ -287,17 +287,15 @@ ActiveRecord::Schema.define(:version => 20110622232150) do
   add_index "monthly_accountings", ["partner_id", "month", "year"], :name => "index_monthly_accountings_on_partner_id_and_month_and_year", :unique => true
   add_index "monthly_accountings", ["partner_id"], :name => "index_monthly_accountings_on_partner_id"
 
-  create_table "news_coverages", :id => false, :force => true do |t|
-    t.string   "id",           :limit => 36, :null => false
-    t.datetime "published_at",               :null => false
-    t.string   "link_source",                :null => false
-    t.text     "link_text",                  :null => false
-    t.text     "link_href",                  :null => false
+  create_table "news_coverages", :force => true do |t|
+    t.datetime "published_at"
+    t.string   "link_source"
+    t.text     "link_text"
+    t.text     "link_href"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "news_coverages", ["id"], :name => "index_news_coverages_on_id", :unique => true
   add_index "news_coverages", ["published_at"], :name => "index_news_coverages_on_published_at"
 
   create_table "offer_discounts", :id => false, :force => true do |t|
@@ -515,11 +513,10 @@ ActiveRecord::Schema.define(:version => 20110622232150) do
   add_index "payouts", ["id"], :name => "index_payouts_on_id", :unique => true
   add_index "payouts", ["partner_id"], :name => "index_payouts_on_partner_id"
 
-  create_table "press_releases", :id => false, :force => true do |t|
-    t.string   "id",               :limit => 36, :null => false
-    t.datetime "published_at",                   :null => false
-    t.text     "link_text",                      :null => false
-    t.text     "link_href",                      :null => false
+  create_table "press_releases", :force => true do |t|
+    t.datetime "published_at"
+    t.text     "link_text"
+    t.text     "link_href"
     t.string   "link_id"
     t.text     "content_title"
     t.text     "content_subtitle"
@@ -530,7 +527,6 @@ ActiveRecord::Schema.define(:version => 20110622232150) do
     t.datetime "updated_at"
   end
 
-  add_index "press_releases", ["id"], :name => "index_press_releases_on_id", :unique => true
   add_index "press_releases", ["published_at"], :name => "index_press_releases_on_published_at"
 
   create_table "rank_boosts", :id => false, :force => true do |t|
