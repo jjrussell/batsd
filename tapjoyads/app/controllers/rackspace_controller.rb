f9624837ffdf794f33691cf00dfa7a0bbc71c16b
Ryan Johns <ryan@tapjoy.com>
2011-07-13 12:24:18 -0700

    stop traffic to rackspace

---
+++ tapjoyads/app/controllers/rackspace_controller.rb
@@ -0,0 +1,14 @@
+class RackspaceController < ApplicationController
+  skip_before_filter :set_time_zone
+  skip_before_filter :set_locale
+  skip_before_filter :fix_params
+  skip_before_filter :reject_banned_ips
+  before_filter :rackspace_is_gone
+  
+private
+  
+  def rackspace_is_gone
+    render :template => 'layouts/success'
+  end
+  
+end
