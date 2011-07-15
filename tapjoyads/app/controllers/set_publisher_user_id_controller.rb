7cb359dcfef5103cdfe8a6a5e46e38179b4e633f
Stephen McCarthy <sjmccarthy@gmail.com>
2011-07-15 11:45:03 -0700

    Remove commented out code

--- tapjoyads/app/controllers/set_publisher_user_id_controller.rb
+++ tapjoyads/app/controllers/set_publisher_user_id_controller.rb
@@ -5,10 +5,6 @@ class SetPublisherUserIdController < ApplicationController
     device = Device.new(:key => params[:udid])
     device.set_publisher_user_id!(params[:app_id], params[:publisher_user_id])
     
-    # web_request = WebRequest.new
-    # web_request.put_values('set_publisher_user_id', params, get_ip_address, get_geoip_data, request.headers['User-Agent'])
-    # web_request.save
-    
     render :template => 'layouts/success'
   end
 end
