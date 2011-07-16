38f86b1f0de119a9fecd8c8c214560b010015e16
Ryan Johns <ryan@tapjoy.com>
2011-07-16 04:37:41 -0700

    add stomp gem

--- tapjoyads/config/environment.rb
+++ tapjoyads/config/environment.rb
@@ -54,6 +54,7 @@ Rails::Initializer.run do |config|
   config.gem 'curbit', :version => '0.2.0'
   config.gem 'newrelic_rpm', :version => '3.0.1'
   config.gem 'differ', :version => '0.1.2'
+  config.gem 'stomp', :version => '1.1.9'
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
