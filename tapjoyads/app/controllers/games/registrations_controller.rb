25d4cf7b210bc938e86bddaa85bb90e1889030a6
Stephen McCarthy <sjmccarthy@gmail.com>
2011-07-18 13:14:25 -0700

    Lawyers gotta lawyer

--- tapjoyads/app/controllers/games/registrations_controller.rb
+++ tapjoyads/app/controllers/games/registrations_controller.rb
@@ -6,9 +6,10 @@ class Games::RegistrationsController < GamesController
   
   def create
     @gamer = Gamer.new do |g|
-      g.email    = params[:gamer][:email]
-      g.password = params[:gamer][:password]
-      g.referrer = params[:gamer][:referrer]
+      g.email            = params[:gamer][:email]
+      g.password         = params[:gamer][:password]
+      g.referrer         = params[:gamer][:referrer]
+      g.terms_of_service = params[:gamer][:terms_of_service]
     end
     if @gamer.save
       GamesMailer.deliver_gamer_confirmation(@gamer, games_confirm_url(:token => @gamer.perishable_token))
