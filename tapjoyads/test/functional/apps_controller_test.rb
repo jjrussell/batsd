910fc64348a45ea7914fdcbb5db49f41bdde5afa
Amir Manji <amanji75@gmail.com>
2011-07-18 18:02:15 -0700

    shit

--- tapjoyads/test/functional/apps_controller_test.rb
+++ tapjoyads/test/functional/apps_controller_test.rb
@@ -29,7 +29,7 @@ class AppsControllerTest < ActionController::TestCase
         assert_equal last_app, assigns(:app)
       end
 
-      should "ee someone else's app" do
+      should "see someone else's app" do
         someone_else = Factory(:partner, :pending_earnings => 10000, :balance => 10000)
         not_my_app = Factory(:app, :partner => someone_else)
         get 'show', :id => not_my_app.id
