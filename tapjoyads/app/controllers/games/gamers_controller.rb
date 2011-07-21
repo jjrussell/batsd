97a93ddc56ade08f93af7d8bd6a901815194da83
Stephen McCarthy <sjmccarthy@gmail.com>
2011-07-20 18:34:53 -0700

    render the signed mobileconfig in a way that doesn't cause ruby to hang.

--- tapjoyads/app/controllers/games/gamers_controller.rb
+++ tapjoyads/app/controllers/games/gamers_controller.rb
@@ -39,6 +39,9 @@ class Games::GamersController < GamesController
       respond_to do |format|
         format.mobileconfig do
           response.headers['Content-Disposition'] = "attachment; filename=TapjoyGamesProfile.mobileconfig"
+          
+          file = File.open('app/views/games/gamers/start_link.mobileconfig')
+          render :text => file.read
         end
       end
     # else
