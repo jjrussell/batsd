c5295a67e6617c97044a5f10d51bd5cfd8a0ba4e
Ryan Johns <ryan@tapjoy.com>
2011-07-21 02:01:13 -0700

    writing large amounts of data to s3 is far more reliable from a file than from memory

--- tapjoyads/app/controllers/job/queue_activemq_consumer_controller.rb
+++ tapjoyads/app/controllers/job/queue_activemq_consumer_controller.rb
@@ -11,13 +11,16 @@ class Job::QueueActivemqConsumerController < Job::JobController
     consumer.listener_thread.exit
     
     unless messages.empty?
-      data = messages.map { |msg| msg.body }.join("\n")
-      path = "#{params[:queue]}/#{now.to_s(:yyyy_mm_dd)}/#{now.hour}/#{params[:server]}_#{UUIDTools::UUID.random_create.hexdigest}"
+      filename = "#{params[:server]}_#{UUIDTools::UUID.random_create.hexdigest}"
+      path = "#{params[:queue]}/#{now.to_s(:yyyy_mm_dd)}/#{now.hour}/#{filename}"
+      data = File.open("tmp/#{filename}", 'w+')
+      data.write(messages.map { |msg| msg.body }.join("\n"))
       
       retries = 3
       begin
+        data.rewind
         bucket = S3.bucket(BucketNames::ACTIVEMQ_MESSAGES)
-        Timeout.timeout(60) { bucket.put(path, data) }
+        bucket.put(path, data.read)
       rescue Exception => e
         if retries > 0
           retries -= 1
@@ -25,9 +28,14 @@ class Job::QueueActivemqConsumerController < Job::JobController
           retry
         else
           consumer.close
+          data.close
+          File.delete("tmp/#{filename}")
           raise e
         end
       end
+      
+      data.close
+      File.delete("tmp/#{filename}")
       
       messages.each do |msg|
         consumer.acknowledge(msg)
