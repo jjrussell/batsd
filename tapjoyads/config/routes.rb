e354fdfb231ec0e815b7737cb99c590a74640e93
Stephen McCarthy <sjmccarthy@gmail.com>
2011-07-21 17:42:19 -0700

    Remove tapjoygames.mobileconfig, we're using /gamer/start_link instead

--- tapjoyads/config/routes.rb
+++ tapjoyads/config/routes.rb
@@ -5,8 +5,6 @@ ActionController::Routing::Routes.draw do |map|
     m.root :controller => 'games/homepage', :action => :index
     m.real_index 'real_index', :controller => 'games/homepage', :action => :real_index
     
-    m.tapjoygames 'tapjoygames.mobileconfig', :controller => 'games/tapjoygames', :action => :index
-    
     m.resources :gamer_sessions, :controller => 'games/gamer_sessions', :only => [ :new, :create, :destroy ]
     m.login 'login', :controller => 'games/gamer_sessions', :action => :new
     m.logout 'logout', :controller => 'games/gamer_sessions', :action => :destroy
