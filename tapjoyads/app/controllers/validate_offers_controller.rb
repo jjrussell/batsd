7ad5dc40750c5fbc792c67c17cf35f93087e00c1
Jia Feng <jia.feng@tapjoy.com>
2011-08-03 16:10:42 -0700

    basic validate offers controller

---
+++ tapjoyads/app/controllers/validate_offers_controller.rb
@@ -0,0 +1,56 @@
+class ValidateOffersController < ApplicationController
+  
+  before_filter :set_device_type, :setup
+  
+  def index
+    #test url: 
+    #http://localhost:3000/validate_offers?udid=test&type=0&currency_id=2349536b-c810-47d7-836c-2cd47cd3a796&app_id=2349536b-c810-47d7-836c-2cd47cd3a796&offer_id=2349536b-c810-47d7-836c-2cd47cd3a796
+    if @offer.is_valid_for?(@publisher_app, @device, @currency, params[:device_type], @geoip_data, params[:app_version], @direct_pay_providers, params[:type], @hide_app_offers, params[:library_version], params[:os_version], params[:screen_layout_size])
+      render :text => true
+    else
+      render :text => false
+    end
+  end
+  
+private
+
+  def setup
+    params[:currency_id] ||= params[:app_id]
+    return unless verify_params([ :app_id, :udid, :currency_id, :offer_id, :type ])
+    
+    @geoip_data = get_geoip_data
+    @geoip_data[:country] = params[:country_code] if params[:country_code].present?
+    
+    @device = Device.new(:key => params[:udid])
+    @publisher_app = App.find_in_cache(params[:app_id])
+    @currency = Currency.find_in_cache(params[:currency_id])
+    @currency = nil if @currency.present? && @currency.app_id != params[:app_id]
+    @offer = Offer.find_in_cache(params[:offer_id])    
+    return unless verify_records([ @publisher_app, @currency, @offer ], :render_missing_text => false)
+    
+    @hide_app_offers = @currency.hide_rewarded_app_installs_for_version?(params[:app_version], params[:source])
+    @direct_pay_providers = params[:direct_pay_providers].to_s.split(',')
+  end
+  
+  ##
+  # Sets the device_type parameter from the device_ua param, which AdMarvel sends.
+  def set_device_type
+    if params[:device_type].blank? && params[:device_ua].present?
+      params[:device_type] = case params[:device_ua]
+      when /iphone;/i
+        'iphone'
+      when /ipod;/i
+        'ipod'
+      when /ipad;/i
+        'ipad'
+      when /android/i
+        'android'
+      when /windows/i
+        'windows'
+      else
+        nil
+      end
+    end
+  end
+  
+end
