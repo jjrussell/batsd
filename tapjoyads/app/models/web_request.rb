089342c0eba8672a6333e00674c8bfeb58907566
Ryan Johns <ryan@tapjoy.com>
2011-08-05 14:47:17 -0700

    remove activemq

--- tapjoyads/app/models/web_request.rb
+++ tapjoyads/app/models/web_request.rb
@@ -173,12 +173,9 @@ class WebRequest < SimpledbResource
   # Also increments all stats associated with this webrequest.
   def serial_save(options = {})
     self.time = @now unless self.time?
-    # self.put('activemq', '1')
     super({:write_to_memcache => false}.merge(options))
     
     update_realtime_stats
-    
-    # Activemq.publish_message('web_requests', self.serialize)
   end
   
   ##
