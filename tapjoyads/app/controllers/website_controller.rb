b0c9cb9ea6b5f625dedece301a104db9d491656a
Hwan-Joon Choi <hwanjoon@tapjoy.com>
2011-08-09 18:29:00 -0700

    code review

--- tapjoyads/app/controllers/website_controller.rb
+++ tapjoyads/app/controllers/website_controller.rb
@@ -110,4 +110,9 @@ private
     redirect_to login_path
   end
 
+  def nag_user_about_payout_info
+    if current_partner.payout_info.nil? || !current_partner.payout_info.valid?
+      flash.now[:notice] = "Please remember to <a href='/billing/payment-info'>update your W8/W9</a>."
+    end
+  end
 end
