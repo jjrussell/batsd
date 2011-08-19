ee16eba661884ad048a8b0fcf46632869e877613
Amir Manji <amanji75@gmail.com>
2011-08-18 18:38:41 -0700

    fix videos controller

--- tapjoyads/app/controllers/videos_controller.rb
+++ tapjoyads/app/controllers/videos_controller.rb
@@ -22,14 +22,15 @@ private
     
     params[:publisher_app_id] = @publisher_app.id
     
-    @offer_list = OfferList.new(
+    @offer_list, @more_data_available = OfferList.new(
       :device             => device,
+      :publisher_app      => @publisher_app,
       :currency           => @currency,
       :device_type        => params[:device_type],
       :geoip_data         => @geoip_data,
       :os_version         => params[:os_version],
       :type               => Offer::VIDEO_OFFER_TYPE,
       :library_version    => params[:library_version],
-      :screen_layout_size => params[:screen_layout_size])
+      :screen_layout_size => params[:screen_layout_size]).get_offers(0, 100)
   end
 end
