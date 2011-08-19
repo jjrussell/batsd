02c52d70318c7235728210f1d577fb3862961910
Stephen McCarthy <sjmccarthy@gmail.com>
2011-08-18 17:08:13 -0700

    non-apps and apps without store_id's are ineligible to be top popular apps.

--- tapjoyads/app/models/popular_app.rb
+++ tapjoyads/app/models/popular_app.rb
@@ -27,6 +27,9 @@ class PopularApp
         next
       end
       
+      next if offer.item_type != 'App'
+      next if offer.item.store_id.blank?
+      
       appstats = Appstats.new(offer.id, { :start_time => start_time, :end_time => now + 1.hour, :stat_types => [ 'new_users' ] }).stats
       new_users = appstats['new_users'].sum
       
