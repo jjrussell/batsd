6b292d321ad61c3d0473b0da910bb351142fc354
Ryan Johns <ryan@tapjoy.com>
2011-08-24 00:10:48 -0700

    basic syslog-ng 4.0 config

--- server/syslog-ng/configure.rb
+++ server/syslog-ng/configure.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
 
 conf_files = Dir.glob("/home/webuser/tapjoyserver/server/syslog-ng/syslog-ng.conf-client-*")
-`cp #{conf_files[rand(conf_files.size)]} /etc/syslog-ng/syslog-ng.conf`
+`cp #{conf_files[rand(conf_files.size)]} /opt/syslog-ng/etc/syslog-ng.conf`
 `/etc/init.d/syslog-ng restart`
