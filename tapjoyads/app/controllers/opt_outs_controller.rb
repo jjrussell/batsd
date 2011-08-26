b14f6fc288afff72507b4c4d57b417da0695f8a8
Hwan-Joon Choi <hwanjoon@tapjoy.com>
2011-08-25 17:53:42 -0700

    flash msg for opting out

--- tapjoyads/app/controllers/opt_outs_controller.rb
+++ tapjoyads/app/controllers/opt_outs_controller.rb
@@ -1,12 +1,13 @@
 class OptOutsController < ApplicationController
-  
+
   def create
     unless params[:udid].blank?
       d = Device.new(:key => params[:udid])
       d.opted_out = true
       d.save
+      flash[:notice] = "This device (#{params[:udid]}) has been successfully opted out."
     end
-    redirect_to '/site/privacy'
+    redirect_to :back
   end
-  
+
 end
