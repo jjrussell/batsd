aa9c70af9c0ba41ad76142d8ce193aa1349b2c45
Hwan-Joon Choi <hwanjoon@tapjoy.com>
2011-08-29 11:57:24 -0700

    boot admins to tools

--- tapjoyads/app/controllers/homepage_controller.rb
+++ tapjoyads/app/controllers/homepage_controller.rb
@@ -3,7 +3,9 @@ class HomepageController < WebsiteController
   protect_from_forgery :except => [:contact]
 
   def start
-    if permitted_to?(:index, :statz)
+    if has_role_with_hierarchy?(:admin)
+      redirect_to tools_path
+    elsif permitted_to?(:index, :statz)
       redirect_to statz_index_path
     elsif permitted_to?(:index, :apps)
       redirect_to apps_path
