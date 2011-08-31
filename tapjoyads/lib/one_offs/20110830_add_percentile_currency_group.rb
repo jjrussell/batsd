7499e231b91e51cee3c7091c34a1d7f651ee27b9
Amir Manji <amanji75@gmail.com>
2011-08-31 16:00:58 -0700

    ignore randomness in the percentile group

--- tapjoyads/lib/one_offs/20110830_add_percentile_currency_group.rb
+++ tapjoyads/lib/one_offs/20110830_add_percentile_currency_group.rb
@@ -1,6 +1,6 @@
 class OneOffs
   def self.add_percentile_currency_group
     default_group = CurrencyGroup.find_by_name('default')
-    CurrencyGroup.create(default_group.attributes.merge(:rank_boost => 0, :name => 'percentile'))
+    CurrencyGroup.create(default_group.attributes.merge(:rank_boost => 0, :random => 0, :name => 'percentile'))
   end
 end
