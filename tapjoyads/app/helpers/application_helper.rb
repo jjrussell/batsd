7fbb9fe91356fab4ec1194f710210561190c969d
Hwan-Joon Choi <hwanjoon@tapjoy.com>
2011-09-07 02:29:27 -0700

    change this method to be more positive

--- tapjoyads/app/helpers/application_helper.rb
+++ tapjoyads/app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  def sdk_popup_link(text, sdk_type, hide_clippy=false)
+  def sdk_popup_link(text, sdk_type, show_clippy=true)
     concat(link_to(text, popup_sdk_path(:sdk => sdk_type), :rel => 'facebox'))
-    if permitted_to?(:index, :statz) && !hide_clippy
+    if permitted_to?(:index, :statz) && show_clippy
       concat('<br/>')
       concat(clippy(sdk_path(sdk_type, :only_path => false)))
     end
