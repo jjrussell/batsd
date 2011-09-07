271289b5105d650dc6d59172b98dbd7ddb9e7fca
Hwan-Joon Choi <hwanjoon@tapjoy.com>
2011-09-07 15:45:51 -0700

    option to not make sdk link a popup

--- tapjoyads/app/helpers/application_helper.rb
+++ tapjoyads/app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  def sdk_popup_link(text, sdk_type, show_clippy=true)
-    concat(link_to(text, popup_sdk_path(:sdk => sdk_type), :rel => 'facebox'))
-    if permitted_to?(:index, :statz) && show_clippy
-      concat('<br/>')
-      concat(clippy(sdk_path(sdk_type, :only_path => false)))
+  def sdk_link(text, sdk_type, popup=true)
+    if popup
+      concat(link_to(text, popup_sdk_path(:sdk => sdk_type), :rel => 'facebox'))
+    else
+      concat(link_to(text, sdk_path(sdk_type)))
     end
   end
 end
