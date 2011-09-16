b97473ba93699b3adcd316339182a4124f12be30
Norman Chan <norman.chan@tapjoy.com>
2011-09-16 12:01:29 -0700

    Remove unneeded transaction wrapper

--- tapjoyads/app/controllers/games/gamers_controller.rb
+++ tapjoyads/app/controllers/games/gamers_controller.rb
@@ -16,19 +16,15 @@ class Games::GamersController < GamesController
       end
     end
     @gamer_profile = GamerProfile.new( :birthdate => Date.new(params[:date][:year].to_i, params[:date][:month].to_i, params[:date][:day].to_i) )
+    @gamer.gamer_profile = @gamer_profile
 
-    begin
-      Gamer.transaction do
-        @gamer.save!
-        @gamer_profile.gamer = @gamer
-        @gamer_profile.save!
-        GamesMailer.deliver_gamer_confirmation(@gamer, games_confirm_url(:token => @gamer.confirmation_token))
-        render(:json => { :success => true, :link_device_url => new_games_gamer_device_path, :linked => @gamer.udid? }) and return
-      end
-    rescue ActiveRecord::RecordInvalid => e
+    if @gamer.save
+      GamesMailer.deliver_gamer_confirmation(@gamer, games_confirm_url(:token => @gamer.confirmation_token))
+      render(:json => { :success => true, :link_device_url => new_games_gamer_device_path, :linked => @gamer.udid? }) and return
+    else
       errors = []
       @gamer.errors.each do |error|
-        errors << (error)
+        errors << (error) unless error[0] == 'gamer_profile'
       end
       @gamer_profile.errors.each do |error|
         errors << (error)
