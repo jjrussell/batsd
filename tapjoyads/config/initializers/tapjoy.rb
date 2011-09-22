da7aff0cf50b00efc198bfa02cec3c4ad4da5274
Johnny Chan <johnnyjchan@gmail.com>
2011-09-22 13:16:59 -0700

    Updated iOS SDK to 8.1.1

--- tapjoyads/config/initializers/tapjoy.rb
+++ tapjoyads/config/initializers/tapjoy.rb
@@ -15,10 +15,10 @@ ANDROID_OFFERS_SDK   = 'https://s3.amazonaws.com/tapjoy/sdks/TapjoyOffersSDK_And
 ANDROID_VG_SDK       = 'https://s3.amazonaws.com/tapjoy/sdks/TapjoyVirtualGoodsSDK_Android_v8.1.0.zip'
 ANDROID_UNITY_PLUGIN = 'https://s3.amazonaws.com/tapjoy/sdks/TapjoyUnityPlugin_Android_v8.1.0.zip'
 
-IPHONE_CONNECT_SDK  = 'https://s3.amazonaws.com/tapjoy/sdks/TapjoyConnectSDK_iOS_v8.1.0.zip'
-IPHONE_OFFERS_SDK   = 'https://s3.amazonaws.com/tapjoy/sdks/TapjoyConnectOffersSDK_iOS_v8.1.0.zip'
-IPHONE_VG_SDK       = 'https://s3.amazonaws.com/tapjoy/sdks/TapjoyConnectVirtualGoodsSDK_iOS_v8.1.0.zip'
-IPHONE_UNITY_PLUGIN = 'https://s3.amazonaws.com/tapjoy/sdks/TapjoyConnectUnityPluginSample_8.1.0.zip'
+IPHONE_CONNECT_SDK  = 'https://s3.amazonaws.com/tapjoy/sdks/TapjoyConnectSDK_iOS_v8.1.1.zip'
+IPHONE_OFFERS_SDK   = 'https://s3.amazonaws.com/tapjoy/sdks/TapjoyConnectOffersSDK_iOS_v8.1.1.zip'
+IPHONE_VG_SDK       = 'https://s3.amazonaws.com/tapjoy/sdks/TapjoyConnectVirtualGoodsSDK_iOS_v8.1.1.zip'
+IPHONE_UNITY_PLUGIN = 'https://s3.amazonaws.com/tapjoy/sdks/TapjoyConnectUnityPluginSample_8.1.1.zip'
 
 WINDOWS_CONNECT_SDK = 'https://s3.amazonaws.com/tapjoy/sdks/'
 WINDOWS_OFFERS_SDK  = 'https://s3.amazonaws.com/tapjoy/sdks/'
