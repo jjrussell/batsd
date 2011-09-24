d982a8be01a592cb85e0601cb4288bf03685c36e
Stephen McCarthy <sjmccarthy@gmail.com>
2011-09-23 17:15:36 -0700

    Add functionality to use ses

--- tapjoyads/config/environment.rb
+++ tapjoyads/config/environment.rb
@@ -121,7 +121,7 @@ Rails::Initializer.run do |config|
   end
   
   # Mailer:
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :amazon_ses
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   
