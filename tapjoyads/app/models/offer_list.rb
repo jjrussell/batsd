5623c9d143cfc1b9e725cbc38247444c8de29a8c
Eric Tipton <eric.tipton@tapjoy.com>
2011-09-25 01:13:48 -0700

    remove unnecessary whitespace

--- tapjoyads/app/models/offer_list.rb
+++ tapjoyads/app/models/offer_list.rb
@@ -50,7 +50,7 @@ class OfferList
     
     if @device && @device.opted_out?
       @offers = []
-    else    
+    else
       @offers = RailsCache.get_and_put("offers.#{@type}.#{@platform_name}.#{@hide_rewarded_app_installs}.#{@normalized_device_type}") do
         OfferCacher.get_unsorted_offers_prerejected(@type, @platform_name, @hide_rewarded_app_installs, @normalized_device_type)
       end.value
