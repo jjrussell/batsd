10506d5188e63948519f4ba8d27f610abb5cd256
Amir Manji <amanji75@gmail.com>
2011-09-26 16:46:19 -0700

    modify one-off to only generate gamer devices for gamers without one

--- tapjoyads/lib/one_offs/20110914_migrate_gamer_devices.rb
+++ tapjoyads/lib/one_offs/20110914_migrate_gamer_devices.rb
@@ -1,6 +1,6 @@
 class OneOffs
   def self.migrate_gamer_devices
-    Gamer.scoped(:conditions => 'udid').each do |gamer|
+    Gamer.scoped(:include => :gamer_devices, :conditions => 'gamers.udid is not null and gamer_devices.id is null').each do |gamer|
       device = Device.new(:key => gamer.udid)
       gamer_device = gamer.gamer_devices.build(:device => device)
       gamer_device.save!
