a77dec981a25b9c725663ab97810cdb9ef1b67b8
Ryan Johns <ryan@tapjoy.com>
2011-10-05 18:45:25 -0700

    remove dashboard routes from website

--- tapjoyads/config/routes.rb
+++ tapjoyads/config/routes.rb
@@ -3,11 +3,14 @@ ActionController::Routing::Routes.draw do |map|
 
   if MACHINE_TYPE == 'dashboard'
     ActionController::Routing::Routes.add_configuration_file(Rails.root.join('config/routes/dashboard.rb'))
+    ActionController::Routing::Routes.add_configuration_file(Rails.root.join('config/routes/api.rb'))
+    ActionController::Routing::Routes.add_configuration_file(Rails.root.join('config/routes/sdks.rb'))
   elsif MACHINE_TYPE == 'games'
     ActionController::Routing::Routes.add_configuration_file(Rails.root.join('config/routes/games.rb'))
   elsif MACHINE_TYPE == 'website'
     ActionController::Routing::Routes.add_configuration_file(Rails.root.join('config/routes/website.rb'))
-    ActionController::Routing::Routes.add_configuration_file(Rails.root.join('config/routes/dashboard.rb'))
+    ActionController::Routing::Routes.add_configuration_file(Rails.root.join('config/routes/api.rb'))
+    ActionController::Routing::Routes.add_configuration_file(Rails.root.join('config/routes/sdks.rb'))
   elsif MACHINE_TYPE == 'web'
     ActionController::Routing::Routes.add_configuration_file(Rails.root.join('config/routes/web.rb'))
     ActionController::Routing::Routes.add_configuration_file(Rails.root.join('config/routes/legacy.rb'))
@@ -15,7 +18,9 @@ ActionController::Routing::Routes.draw do |map|
   elsif MACHINE_TYPE == 'jobs' || MACHINE_TYPE == 'masterjobs'
     ActionController::Routing::Routes.add_configuration_file(Rails.root.join('config/routes/default.rb'))
   else
-    # jobs/masterjobs/test/dev
+    # test/dev
+    ActionController::Routing::Routes.add_configuration_file(Rails.root.join('config/routes/api.rb'))
+    ActionController::Routing::Routes.add_configuration_file(Rails.root.join('config/routes/sdks.rb'))
     ActionController::Routing::Routes.add_configuration_file(Rails.root.join('config/routes/dashboard.rb'))
     ActionController::Routing::Routes.add_configuration_file(Rails.root.join('config/routes/games.rb'))
     ActionController::Routing::Routes.add_configuration_file(Rails.root.join('config/routes/website.rb'))
