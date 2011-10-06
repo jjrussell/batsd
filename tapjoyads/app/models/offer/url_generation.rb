8e05afc79fa2a325136d7a4ecb058e8d618f69b2
Eric Tipton <eric.tipton@tapjoy.com>
2011-10-06 11:54:18 -0700

    more cache_busting

--- tapjoyads/app/models/offer/url_generation.rb
+++ tapjoyads/app/models/offer/url_generation.rb
@@ -137,16 +137,17 @@ module Offer::UrlGeneration
     "#{click_url}?data=#{SymmetricCrypto.encrypt_object(data, SYMMETRIC_CRYPTO_SECRET)}"
   end
 
-  def display_ad_image_url(publisher_app_id, width, height, currency_id = nil, display_multiplier = nil, bust_cloudfront_cache = false)
+  def display_ad_image_url(publisher_app_id, width, height, currency_id = nil, display_multiplier = nil, bust_cache = false)
     size = "#{width}x#{height}"
 
     if display_custom_banner_for_size?(size)
       url = "#{CLOUDFRONT_URL}/#{banner_creative_path(size)}"
-      url << "?ts=#{Time.now.to_i}" if bust_cloudfront_cache
+      url << "?ts=#{Time.now.to_i}" if bust_cache
     else
       display_multiplier = (display_multiplier || 1).to_f
       # TO REMOVE: displayer_app_id param after rollout.
-      url = "#{API_URL}/display_ad/image?publisher_app_id=#{publisher_app_id}&advertiser_app_id=#{self.id}&displayer_app_id=#{publisher_app_id}&size=#{width}x#{height}&display_multiplier=#{display_multiplier}&currency_id=#{currency_id}"
+      url = "#{API_URL}/display_ad/image?publisher_app_id=#{publisher_app_id}&advertiser_app_id=#{id}&displayer_app_id=#{publisher_app_id}&size=#{size}&display_multiplier=#{display_multiplier}&currency_id=#{currency_id}"
+      url << "&re_cache=#{bust_cache}"
     end
     url
   end
