d75d0ca0f9f25cc7e6ac6340a7b317427a7f5504
Stephen McCarthy <sjmccarthy@gmail.com>
2011-10-11 12:09:27 -0700

    Automatically clear out single-recipient blacklisted emails from FailedEmails queue.

--- tapjoyads/app/controllers/job/queue_send_failed_emails_controller.rb
+++ tapjoyads/app/controllers/job/queue_send_failed_emails_controller.rb
@@ -1,14 +1,24 @@
 class Job::QueueSendFailedEmailsController < Job::SqsReaderController
-  
+
   def initialize
     super QueueNames::FAILED_EMAILS
   end
-  
+
   private
-  
+
   def on_message(message)
     mail = Marshal.restore(Base64::decode64(message.to_s))
-    ActionMailer::Base.deliver_without_rescue_errors(mail)
+
+    begin
+      ActionMailer::Base.deliver_without_rescue_errors(mail)
+    rescue AWS::SimpleEmailService::Errors::MessageRejected => e
+      recipients = mail.to.to_a + mail.cc.to_a + mail.bcc.to_a
+      if e.to_s =~ /Address blacklisted/ && recipients.size == 1
+        Rails.logger.info "Address blackslisted: #{recipients.first}"
+      else
+        raise e
+      end
+    end
   end
-  
+
 end
