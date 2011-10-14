a0c78e71e1f6a52420b755f08fc6d37c8d302ba1
Ryan Johns <ryan@tapjoy.com>
2011-10-14 10:54:47 -0700

    add script to intelligently update geoip database

---
+++ server/update_geoip.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+if ENV['USER'] != 'webuser'
+  puts "This script must be run by webuser."
+  exit
+end
+
+attempts = 0
+
+loop do
+  attempts += 1
+  output = `/usr/bin/geoipupdate -d /home/webuser/GeoIP/ 2>&1`
+  if $?.exitstatus == 0
+    `touch /home/webuser/tapjoyserver/tapjoyads/tmp/restart.txt`
+    puts "Updated GeoIP Database"
+    break
+  elsif $?.exitstatus == 1 && output =~ /GeoIP\ Database\ up\ to\ date/
+    puts "GeoIP Database up to date"
+    break
+  else
+    puts output
+    if attempts > 5
+      puts "Failed to update Geoip Database"
+      break
+    else
+      sleep(0.5)
+    end
+  end
+end
