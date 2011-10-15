0520ae0c26b79d351267b7845a6ab0b02a7ca203
Hwan-Joon Choi <hwanjoon@tapjoy.com>
2011-10-14 22:43:29 -0700

    minor refactor

---
+++ tapjoyads/test/functional/games_gamers_controller_test.rb
@@ -0,0 +1,51 @@
+require 'test_helper'
+
+class Games::GamersControllerTest < ActionController::TestCase
+  setup :activate_authlogic
+
+  context "creating new gamer" do
+    should "return json with no errors" do
+      params = {
+        :gamer => {
+          :email            => Factory.next(:email),
+          :password         => Factory.next(:name),
+          :terms_of_service => '1',
+        },
+        :date => {
+          :year   => '1981',
+          :month  => '10',
+          :day    => '23',
+        },
+      }
+
+      post 'create', params
+
+      assert_response(200)
+      json = JSON.load(@response.body)
+      assert_equal true, json['success']
+    end
+
+    should "return error json if too young" do
+      params = {
+        :gamer => {
+          :email            => Factory.next(:email),
+          :password         => Factory.next(:name),
+          :terms_of_service => '1',
+        },
+        :date => {
+          :year   => Date.today.year,
+          :month  => '10',
+          :day    => '23',
+        },
+      }
+
+      post 'create', params
+
+      assert_response(200)
+      json = JSON.load(@response.body)
+      assert_equal false, json['success']
+      puts json.to_json
+      assert_equal 1, json['error'].length
+    end
+  end
+end
