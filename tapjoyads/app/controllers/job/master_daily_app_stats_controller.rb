5e69e11ae82d14f594071c4b7ae451d8989ffebc
Ryan Johns <ryan@tapjoy.com>
2011-10-15 21:26:02 -0700

    don't attempt to verify stats if vertica is missing data

--- tapjoyads/app/controllers/job/master_daily_app_stats_controller.rb
+++ tapjoyads/app/controllers/job/master_daily_app_stats_controller.rb
@@ -1,6 +1,22 @@
 class Job::MasterDailyAppStatsController < Job::JobController
 
   def index
+    if Offer.to_aggregate_daily_stats.count == 0
+      render :text => 'ok'
+      return
+    end
+
+    start_time    = (Time.zone.now - 1.day).beginning_of_day
+    end_time      = start_time + 1.day
+    stats_count   = Appstats.new(nil, { :stat_prefix => 'global', :start_time => start_time, :end_time => end_time, :stat_types => [ 'featured_offers_requested' ], :granularity => :hourly }).stats['featured_offers_requested'].sum
+    vertica_count = WebRequest.count_with_vertica("path LIKE '%featured_offer_requested%' AND time >= #{start_time.to_i} AND time < #{end_time.to_i}")
+    percentage    = vertica_count / stats_count.to_f
+    if percentage < 0.99999 || percentage > 1.00001
+      Notifier.alert_new_relic(AppStatsVerifyError, "Cannot verify daily stats because Vertica is missing data. Appstats count: #{stats_count}, Vertica count: #{vertica_count}", request, params)
+      render :text => 'ok'
+      return
+    end
+
     next_aggregation_time = Time.zone.now + 1.day
     offer_ids = Offer.to_aggregate_daily_stats.collect(&:id)
 
