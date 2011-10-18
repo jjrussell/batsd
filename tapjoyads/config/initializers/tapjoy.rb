3210bea4c944d615a8fa5509188574b03d6f6a1d
Robert Fletcher <lobatifricha@gmail.com>
2011-10-17 17:05:19 -0700

    adding Knowledge Center links

--- tapjoyads/config/initializers/tapjoy.rb
+++ tapjoyads/config/initializers/tapjoy.rb
@@ -27,6 +27,7 @@ WINDOWS_VG_SDK      = 'https://s3.amazonaws.com/tapjoy/sdks/'
 SCHEMA_VERSION = ActiveRecord::Migrator.current_version
 
 DEV_FORUM_URL = 'https://groups.google.com/group/tapjoy-developer'
+KNOWLEDGE_CENTER_URL = 'http://knowledge.tapjoy.com/'
 TAPJOY_GAMES_REGISTRATION_OFFER_ID = 'f7cc4972-7349-42dd-a696-7fcc9dcc2d03'
 
 WEB_REQUEST_LOGGER = SyslogLogger.new("#{RUN_MODE_PREFIX}rails-web_requests")
