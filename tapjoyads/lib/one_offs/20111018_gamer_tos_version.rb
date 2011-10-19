5a496e738cc8bd6ef4a116aec039f28a7c85aa5f
Norman Chan <norman.chan@tapjoy.com>
2011-10-19 13:52:48 -0700

    One off script only updates tos_version on pre-migration users

--- tapjoyads/lib/one_offs/20111018_gamer_tos_version.rb
+++ tapjoyads/lib/one_offs/20111018_gamer_tos_version.rb
@@ -1,5 +1,5 @@
 class OneOffs
   def self.update_gamer_tos_version
-    Gamer.connection.execute("update gamers set accepted_tos_version = 1")
+    Gamer.connection.execute("update gamers set accepted_tos_version = 1 where accepted_tos_version = 0")
   end
 end
