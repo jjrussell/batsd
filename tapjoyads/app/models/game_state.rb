e7564c6c426885dfb5262fb511f8a06a1cdd4a23
Ryan Johns <ryan@tapjoy.com>
2011-10-20 02:23:38 -0700

    reshard GameStates

--- tapjoyads/app/models/game_state.rb
+++ tapjoyads/app/models/game_state.rb
@@ -1,11 +1,14 @@
 class GameState < SimpledbShardedResource
   # key format: app_id.publisher_user_id
-  
+
   self.num_domains = NUM_GAME_STATE_DOMAINS
   self.sdb_attr :tapjoy_points, :type => :int, :default_value => 0
   self.sdb_attr :version, :type => :int, :default_value => 0
   self.sdb_attr :udids, :force_array => true, :replace => false
-  
+
+  # TO REMOVE: after resharding
+  attr_reader :attributes_to_replace
+
   def dynamic_domain_name
     domain_number = @key.matz_silly_hash % GameState.num_domains
     "game_states_#{domain_number}"
@@ -13,8 +16,16 @@ class GameState < SimpledbShardedResource
 
   def initialize(options = {})
     super({:load_from_memcache => false}.merge(options))
+    # TO REMOVE: after resharding
+    if new_record?
+      correct_domain_name = @this_domain_name
+      old_domain_name = "game_states_#{@key.matz_silly_hash % 2}"
+      super({:load_from_memcache => false, :domain_name => old_domain_name}.merge(options))
+      @this_domain_name = correct_domain_name
+      @attributes_to_replace = @attributes unless new_record?
+    end
   end
-  
+
   def seconds_elapsed
     if updated_at
       (Time.zone.now - updated_at).to_i
@@ -22,19 +33,19 @@ class GameState < SimpledbShardedResource
       0
     end
   end
-  
+
   def app_id
     key.split(".", 2).first
   end
-  
+
   def publisher_user_id
     key.split(".", 2).last
   end
-  
+
   def add_device(udid)
     self.udids = udid unless udids.include? udid
   end
-  
+
   def data
     data_hash = {}
     attributes.each do |k, v|
@@ -43,15 +54,15 @@ class GameState < SimpledbShardedResource
     end
     data_hash
   end
-  
+
   def data=(data_hash = {})
     data_hash.each do |k, v|
       put "d_#{k}", v
     end
   end
-  
+
   def serial_save(options = {})
-    super(:write_to_memcache => false)
+    super({:write_to_memcache => false}.merge(options))
   end
-  
+
 end
---
+++ tapjoyads/lib/one_offs/20111020_reshard_game_states.rb
@@ -0,0 +1,32 @@
+class OneOffs
+
+  def self.reshard_game_states
+    count = 0
+    num_correct = 0
+    num_moved = 0
+    num_already_moved = 0
+    2.times do |i|
+      domain_name = "game_states_#{i}"
+      GameState.select(:domain_name => domain_name) do |gs|
+        if count % 100 == 0
+          puts "total: #{count}, num_correct: #{num_correct}, num_moved: #{num_moved}, num_already_moved: #{num_already_moved}"
+        end
+        count += 1
+        correct_domain_name = "game_states_#{gs.key.matz_silly_hash % NUM_GAME_STATE_DOMAINS}"
+        if gs.this_domain_name == correct_domain_name
+          num_correct += 1
+          next
+        end
+        gs2 = GameState.new(:key => gs.key)
+        if gs2.attributes_to_replace.present?
+          num_moved += 1
+          gs2.save!
+        else
+          num_already_moved += 1
+        end
+        gs.delete_all(false)
+      end
+    end
+  end
+
+end
