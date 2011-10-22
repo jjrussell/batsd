0cb70aa62fa7caf319970ad0929c60e79df11efa
Van Pham <van.pham@yahoo.com>
2011-10-21 20:05:12 -0700

    clean up "games" text

--- tapjoyads/app/models/games_mailer.rb
+++ tapjoyads/app/models/games_mailer.rb
@@ -3,7 +3,7 @@ class GamesMailer < ActionMailer::Base
   def gamer_confirmation(gamer, confirmation_link)
     from 'Tapjoy <noreply@tapjoy.com>'
     recipients gamer.email
-    subject "Welcome to Tapjoy Games!"
+    subject "Welcome to Tapjoy!"
     content_type 'text/html'
     if gamer.gamer_devices.any? && gamer.gamer_devices.first.device_type == 'android'
       device_type = :android
@@ -16,7 +16,7 @@ class GamesMailer < ActionMailer::Base
   def password_reset(gamer, reset_link)
     from 'Tapjoy Support <support@tapjoy.com>'
     recipients gamer.email
-    subject "Password Reset Request - Tapjoy Games"
+    subject "Password Reset Request - Tapjoy"
     content_type 'text/html'
     body :reset_link => reset_link
   end
@@ -24,7 +24,7 @@ class GamesMailer < ActionMailer::Base
   def feedback(gamer, content, user_agent)
     from 'Tapjoy <noreply@tapjoy.com>'
     recipients "feedback@tapjoy.com"
-    subject "User Feedback - Tapjoy Games"
+    subject "User Feedback - Tapjoy"
     content_type 'text/html'
     body(:content => content, :email => gamer.email, :user_agent => user_agent)
   end
@@ -32,7 +32,7 @@ class GamesMailer < ActionMailer::Base
   def report_bug(gamer, content, user_agent, device_id)
     from 'Tapjoy <noreply@tapjoy.com>'
     recipients "mobilehelp@tapjoy.com"
-    subject "Bug Report - Tapjoy Games"
+    subject "Bug Report - Tapjoy"
     content_type 'text/html'
     body(:content => content, :email => gamer.email, :udid => device_id, :user_agent => user_agent)
   end
@@ -40,7 +40,7 @@ class GamesMailer < ActionMailer::Base
   def contact_support(gamer, content, user_agent, device_id)
     from 'Tapjoy <noreply@tapjoy.com>'
     recipients "mobilehelp@tapjoy.com"
-    subject "User Support - Tapjoy Games"
+    subject "User Support - Tapjoy"
     content_type 'text/html'
     body(:content => content, :email => gamer.email, :udid => device_id, :user_agent => user_agent)
   end
@@ -50,7 +50,7 @@ class GamesMailer < ActionMailer::Base
     puts android_link
     from 'Tapjoy <noreply@tapjoy.com>'
     recipients gamer.email
-    subject "Tapjoy Games - Link Device"
+    subject "Tapjoy - Link Device"
     content_type 'text/html'
     body(:ios_link => ios_link, :android_link => android_link)
   end
