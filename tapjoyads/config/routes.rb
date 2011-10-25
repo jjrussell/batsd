ac4a48fef58f83c713b644d8487726bfd761330f
Robert Fletcher <lobatifricha@gmail.com>
2011-10-25 16:33:51 -0700

    refactor routes file

--- tapjoyads/config/routes.rb
+++ tapjoyads/config/routes.rb
@@ -1,26 +1,24 @@
 ActionController::Routing::Routes.draw do |map|
   map.connect 'healthz', :controller => :healthz, :action => :index
 
-  if MACHINE_TYPE == 'dashboard'
-    ActionController::Routing::Routes.add_configuration_file(Rails.root.join('config/routes/dashboard.rb'))
-  elsif MACHINE_TYPE == 'games'
-    ActionController::Routing::Routes.add_configuration_file(Rails.root.join('config/routes/games.rb'))
-  elsif MACHINE_TYPE == 'website'
-    ActionController::Routing::Routes.add_configuration_file(Rails.root.join('config/routes/website.rb'))
-    ActionController::Routing::Routes.add_configuration_file(Rails.root.join('config/routes/dashboard.rb'))
-  elsif MACHINE_TYPE == 'web'
-    ActionController::Routing::Routes.add_configuration_file(Rails.root.join('config/routes/web.rb'))
-    ActionController::Routing::Routes.add_configuration_file(Rails.root.join('config/routes/legacy.rb'))
-    ActionController::Routing::Routes.add_configuration_file(Rails.root.join('config/routes/default.rb'))
-  elsif MACHINE_TYPE == 'jobs' || MACHINE_TYPE == 'masterjobs'
-    ActionController::Routing::Routes.add_configuration_file(Rails.root.join('config/routes/default.rb'))
+  routes = case MACHINE_TYPE
+  when 'dashboard'
+    ['dashboard']
+  when 'games'
+    ['games']
+  when 'website'
+    ['website','dashboard']
+  when 'web'
+    ['web','legacy','default']
+  when 'jobs', 'masterjobs'
+    ['default']
   else
-    # jobs/masterjobs/test/dev
-    ActionController::Routing::Routes.add_configuration_file(Rails.root.join('config/routes/dashboard.rb'))
-    ActionController::Routing::Routes.add_configuration_file(Rails.root.join('config/routes/games.rb'))
-    ActionController::Routing::Routes.add_configuration_file(Rails.root.join('config/routes/website.rb'))
-    ActionController::Routing::Routes.add_configuration_file(Rails.root.join('config/routes/web.rb'))
-    ActionController::Routing::Routes.add_configuration_file(Rails.root.join('config/routes/legacy.rb'))
-    ActionController::Routing::Routes.add_configuration_file(Rails.root.join('config/routes/default.rb'))
+    # test/dev
+    ['dashboard','games','website','web','legacy','default']
+  end
+
+  routes.each do |route|
+    path = Rails.root.join("config/routes/#{route}.rb")
+    ActionController::Routing::Routes.add_configuration_file(path)
   end
 end
