1b1f70bdaa69c72c6d27776a7aac875ae46f929b
Eric Tipton <eric.tipton@tapjoy.com>
2011-10-27 01:28:47 -0400

    create GamesMarketingMailer

--- tapjoyads/app/models/games_mailer.rb
+++ tapjoyads/app/models/games_mailer.rb
@@ -1,41 +1,4 @@
 class GamesMailer < ActionMailer::Base
-  include SendGrid
-
-  self.delivery_method = :smtp
-  self.smtp_settings = {
-    :address => "smtp.sendgrid.net",
-    :port => 587,
-    :domain => "tapjoy.com",
-    :authentication => :plain,
-    :user_name => "erictipton",
-    :password => "shufflethebits"
-  }
-
-  sendgrid_category :use_subject_lines
-  sendgrid_enable :clicktrack, :opentrack
-
-  def gamer_confirmation(gamer, confirmation_link)
-    from 'Tapjoy <noreply@tapjoy.com>'
-    recipients gamer.email
-    subject "Welcome to Tapjoy Games!"
-    content_type 'text/html'
-
-    if gamer.gamer_devices.any? && gamer.gamer_devices.first.device_type == 'android'
-      device_type = :android
-    else
-      device_type = :iphone
-    end
-    body :confirmation_link => confirmation_link, :linked => gamer.gamer_devices.any?, :device_type => device_type
-  end
-
-  def password_reset(gamer, reset_link)
-    from 'Tapjoy Support <support@tapjoy.com>'
-    recipients gamer.email
-    subject "Password Reset Request - Tapjoy Games"
-    content_type 'text/html'
-    body :reset_link => reset_link
-  end
-
   def feedback(gamer, content, user_agent, device_id)
     from 'Tapjoy <noreply@tapjoy.com>'
     reply_to gamer.email
@@ -75,14 +38,5 @@ class GamesMailer < ActionMailer::Base
     from 'Tapjoy <noreply@tapjoy.com>'
     recipients gamer.email
     subject "Tapjoy - Link Device"
-  end
-
-  def invite(gamer_name, recipients_email, link)
-    from "#{gamer_name} <noreply@tapjoy.com>"
-    recipients recipients_email
-    sendgrid_category "Invite"
-    subject "#{gamer_name} has invited you to join Tapjoy"
-    content_type 'text/html'
-    body(:ios_link => ios_link, :android_link => android_link)
   end
 end
