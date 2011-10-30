9d4d31e680c366beeca4cd576d3b1c5b0cc5994d
Ryan Johns <ryan@tapjoy.com>
2011-10-30 04:19:13 -0700

    change the timing of the daily stats aggregation job

--- tapjoyads/app/controllers/job/master_daily_app_stats_controller.rb
+++ tapjoyads/app/controllers/job/master_daily_app_stats_controller.rb
@@ -6,7 +6,8 @@ class Job::MasterDailyAppStatsController < Job::JobController
       return
     end
 
-    start_time    = (Time.zone.now - 1.day).beginning_of_day
+    now           = Time.zone.now
+    start_time    = (now - 1.day).beginning_of_day
     end_time      = start_time + 1.day
     stats_count   = Appstats.new(nil, { :stat_prefix => 'global', :start_time => start_time, :end_time => end_time, :stat_types => [ 'featured_offers_requested' ], :granularity => :hourly }).stats['featured_offers_requested'].sum
     vertica_count = WebRequest.count_with_vertica("path LIKE '%featured_offer_requested%' AND time >= #{start_time.to_i} AND time < #{end_time.to_i}")
@@ -17,13 +18,11 @@ class Job::MasterDailyAppStatsController < Job::JobController
       return
     end
 
-    next_aggregation_time = Time.zone.now + 1.day
-    offer_ids = Offer.to_aggregate_daily_stats.collect(&:id)
-
-    Offer.connection.execute("UPDATE offers SET next_daily_stats_aggregation_time = '#{next_aggregation_time.to_s(:db)}' WHERE id IN ('#{offer_ids.join("','")}')")
-
-    offer_ids.each_slice(StatsAggregation::OFFERS_PER_MESSAGE) do |ids|
-      Sqs.send_message(QueueNames::APP_STATS_DAILY, ids.to_json)
+    next_aggregation_time = (now + 1.day).beginning_of_day + StatsAggregation::DAILY_STATS_START_HOUR.hours
+    Offer.to_aggregate_daily_stats.find_in_batches(:batch_size => StatsAggregation::OFFERS_PER_MESSAGE) do |offers|
+      offer_ids = offers.collect(&:id)
+      Offer.connection.execute("UPDATE #{Offer.quoted_table_name} SET next_daily_stats_aggregation_time = '#{next_aggregation_time.to_s(:db)}' WHERE id IN ('#{offer_ids.join("','")}')")
+      Sqs.send_message(QueueNames::APP_STATS_DAILY, offer_ids.to_json)
     end
 
     render :text => 'ok'
