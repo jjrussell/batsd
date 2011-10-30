fd188005d86f9d3b424820915cee2a3bd69b8995
Ryan Johns <ryan@tapjoy.com>
2011-10-30 14:54:00 -0700

    change VerticaMissingDataError to VerticaDataError and change the error message to show a lot more debugging info.

--- tapjoyads/app/controllers/job/master_daily_app_stats_controller.rb
+++ tapjoyads/app/controllers/job/master_daily_app_stats_controller.rb
@@ -6,14 +6,35 @@ class Job::MasterDailyAppStatsController < Job::JobController
       return
     end
 
-    now           = Time.zone.now
-    start_time    = (now - 1.day).beginning_of_day
-    end_time      = start_time + 1.day
-    stats_count   = Appstats.new(nil, { :stat_prefix => 'global', :start_time => start_time, :end_time => end_time, :stat_types => [ 'featured_offers_requested' ], :granularity => :hourly }).stats['featured_offers_requested'].sum
-    vertica_count = WebRequest.count_with_vertica("path LIKE '%featured_offer_requested%' AND time >= #{start_time.to_i} AND time < #{end_time.to_i}")
-    percentage    = vertica_count / stats_count.to_f
+    now        = Time.zone.now
+    start_time = (now - 1.day).beginning_of_day
+    end_time   = start_time + 1.day
+
+    appstats_counts = Appstats.new(nil, {
+        :stat_prefix => 'global',
+        :start_time  => start_time,
+        :end_time    => end_time,
+        :stat_types  => [ 'featured_offers_requested' ],
+        :granularity => :hourly }).stats['featured_offers_requested']
+    vertica_counts = WebRequest.select_with_vertica(
+        :select     => "count(*), floor((time - #{start_time.to_i}) / #{1.hour.to_i}) as h",
+        :conditions => "path LIKE '%featured_offer_requested%' AND time >= #{start_time.to_i} AND time < #{end_time.to_i}",
+        :group      => 'h',
+        :order      => 'h ASC').map { |result| result[:count] }
+
+    appstats_total = appstats_counts.sum
+    vertica_total  = vertica_counts.sum
+    percentage     = vertica_total / appstats_total.to_f
     if percentage < 0.99999 || percentage > 1.00001
-      Notifier.alert_new_relic(VerticaMissingDataError, "Cannot verify daily stats because Vertica is missing data. Appstats count: #{stats_count}, Vertica count: #{vertica_count}", request, params)
+      message  = "Cannot verify daily stats because Vertica has inaccurate data.\n"
+      message += "Appstats total: #{appstats_total}\n"
+      message += "Vertica total: #{vertica_total}\n"
+      message += "Difference: #{appstats_total - vertica_total}\n\n"
+      message += "hour, appstats, vertica, diff\n"
+      24.times do |i|
+        message += "#{i}, #{appstats_counts[i]}, #{vertica_counts[i]}, #{appstats_counts[i] - vertica_counts[i]}\n"
+      end
+      Notifier.alert_new_relic(VerticaDataError, message, request, params)
       render :text => 'ok'
       return
     end
