a26f6c49266e3c08601dcbf21ea6d4fe2e9e8746
Stephen McCarthy <sjmccarthy@gmail.com>
2011-11-04 14:22:54 -0700

    add period and make date dynamic

--- tapjoyads/app/helpers/application_helper.rb
+++ tapjoyads/app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  def sdk_link(text, sdk_type, popup=true)
+  def sdk_link(text, sdk_type)
     concat(link_to(text, popup_sdk_path(:sdk => sdk_type), :rel => 'facebox'))
   end
 end
