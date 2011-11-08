1d1e465c56b82a425eddaa3e030d4d16c154b3f3
Eric Tipton <eric.tipton@tapjoy.com>
2011-11-08 13:36:45 -0500

    use WEBSITE_URL instead of TJGAMES_URL

--- tapjoyads/app/controllers/job/queue_send_welcome_emails_controller.rb
+++ tapjoyads/app/controllers/job/queue_send_welcome_emails_controller.rb
@@ -24,7 +24,7 @@ class Job::QueueSendWelcomeEmailsController < Job::SqsReaderController
 
     editors_picks = offer_data.any? ? [] : EditorsPick.cached_active(message['using_android'] ? 'android' : 'ios')
 
-    confirm_url = "#{TJGAMES_URL}/confirm?token=#{CGI.escape(gamer.confirmation_token)}"
+    confirm_url = "#{WEBSITE_URL}/confirm?token=#{CGI.escape(gamer.confirmation_token)}"
     GamesMarketingMailer.deliver_welcome_email(gamer, confirm_url, gamer_device, offer_data, editors_picks)
   end
 
