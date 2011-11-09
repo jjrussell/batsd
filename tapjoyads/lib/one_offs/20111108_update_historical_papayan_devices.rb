8b97a88ae8eaea9f4b29c7a6af33cf15a8c08d23
James Huang <jhuang1982@gmail.com>
2011-11-09 12:20:21 -0800

    change PDT to PT in comments

--- tapjoyads/lib/one_offs/20111108_update_historical_papayan_devices.rb
+++ tapjoyads/lib/one_offs/20111108_update_historical_papayan_devices.rb
@@ -1,5 +1,5 @@
 class OneOffs
-  #date string should be PDT in the format of '%yyyy-%mm-%dd'
+  #date string should be PT in the format of '%yyyy-%mm-%dd'
   def self.update_historical_papayan_devices(start_date_str = '2011-05-18', end_date_str = Date.yesterday.to_s(:yyyy_mm_dd))
     updater = Job::MasterUpdatePapayanDeviceController.new
     begin
