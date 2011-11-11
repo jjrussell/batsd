d6215b4371dbfd3f69a803a36de45780da1e748d
James Huang <jhuang1982@gmail.com>
2011-11-10 16:53:15 -0800

    remove garbage db migration file

--- tapjoyads/db/migrate/20111109143200_add_papaya_total_user_to_apps.rb
+++
@@ -1,9 +0,0 @@
-class AddPapayaTotalUserToApps < ActiveRecord::Migration
-  def self.up
-    add_column :apps, :papaya_total_user, :integer, :null => false, :default => 0
-  end
-
-  def self.down
-    remove_column :apps, :papaya_total_user
-  end
-end
