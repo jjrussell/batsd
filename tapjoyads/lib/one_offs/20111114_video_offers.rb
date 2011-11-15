de2b27e5cfcc0caac6824cf33f01c0c34f5b804a
Hwan-Joon Choi <hwanjoon@tapjoy.com>
2011-11-14 22:34:11 -0800

    code review

--- tapjoyads/lib/one_offs/20111114_video_offers.rb
+++ tapjoyads/lib/one_offs/20111114_video_offers.rb
@@ -1,8 +1,8 @@
 class OneOffs
 
   def self.update_video_offer_name_suffix
-    VideoOffer.all.map(&:offers).flatten.each do |offer|
-      offer.update_attribute(:name_suffix, 'video') if offer.name_suffix.blank?
+    Offer.find_each(:conditions => "item_type = 'VideoOffer' and name_suffix != ''") do |offer|
+      offer.update_attribute(:name_suffix, 'Video')
     end
   end
 
