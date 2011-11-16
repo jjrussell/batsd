f6029adab8caaf014f11b2eaaef9ebb0c8336ffd
Adam Sumner <adam.sumner@tapjoy.com>
2011-11-16 11:50:47 -0800

    This commit is broken, I'm working on the routes and _actions.html.haml. I just need to commit this so I can switch branches and work on something else while I wait on something.

--- tapjoyads/db/schema.rb
+++ tapjoyads/db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111027000013) do
+ActiveRecord::Schema.define(:version => 20111111004603) do
 
   create_table "action_offers", :id => false, :force => true do |t|
     t.string   "id",                    :limit => 36,                    :null => false
@@ -726,6 +726,21 @@ ActiveRecord::Schema.define(:version => 20111027000013) do
   add_index "rating_offers", ["app_id"], :name => "index_rating_offers_on_app_id"
   add_index "rating_offers", ["id"], :name => "index_rating_offers_on_id", :unique => true
   add_index "rating_offers", ["partner_id"], :name => "index_rating_offers_on_partner_id"
+
+  create_table "reengagement_offers", :id => false, :force => true do |t|
+    t.string   "id",                    :limit => 36, :null => false
+    t.string   "app_id",                :limit => 36, :null => false
+    t.string   "partner_id",            :limit => 36, :null => false
+    t.string   "currency_id",           :limit => 36, :null => false
+    t.string   "prerequisite_offer_id", :limit => 36
+    t.text     "instructions"
+    t.integer  "reward_value",                        :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "reengagement_offers", ["app_id"], :name => "index_reengagement_offers_on_app_id"
+  add_index "reengagement_offers", ["id"], :name => "index_reengagement_offers_on_id"
 
   create_table "resellers", :id => false, :force => true do |t|
     t.string   "id",                 :limit => 36,                               :null => false
