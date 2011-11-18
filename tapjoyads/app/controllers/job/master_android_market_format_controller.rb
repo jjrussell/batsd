0ab6f1375b7ba24a210af32d69db15add9b9cd7f
Hwan-Joon Choi <hwanjoon@tapjoy.com>
2011-11-18 16:56:35 -0500

    test android search

--- tapjoyads/app/controllers/job/master_android_market_format_controller.rb
+++ tapjoyads/app/controllers/job/master_android_market_format_controller.rb
@@ -1,18 +1,28 @@
 class Job::MasterAndroidMarketFormatController < Job::JobController
 
+  MAX_RETRIES = 5
   def index
 
-    id = 'com.tapjoy.tapjoy'
+    test_search
+    test_fetch
+
+    render :text => 'ok'
+  end
+
+  def test_search
+    fields = [:title, :price, :icon_url, :publisher, :item_id]
+    term = 'tapdefense'
     retries = 0
 
     begin
-      data = AppStore.fetch_app_by_id_for_android(id)
-      changed = [:title, :price, :file_size_bytes, :description, :icon_url,
-        :categories, :publisher, :item_id].any?{|field| data[field].blank?}
-      Notifier.alert_new_relic(AndroidMarketChanged, 'Format may have changed') if changed
+      data = AppStore.search_android_market(term)
+      if data.blank? || fields.any?{|field| data.first[field].blank?}
+        message = 'Search format may have changed'
+        Notifier.alert_new_relic(AndroidMarketChanged, message)
+      end
     rescue
       retries += 1
-      if retries < 5
+      if retries < MAX_RETRIES
         puts "retrying"
         sleep 5
         retry
@@ -20,8 +30,29 @@ class Job::MasterAndroidMarketFormatController < Job::JobController
         Notifier.alert_new_relic(AndroidMarketChanged, 'Site may be down')
       end
     end
+  end
 
+  def test_fetch
+    fields = [:title, :price, :file_size_bytes, :description, :icon_url,
+      :categories, :publisher, :item_id]
+    id = 'com.tapjoy.tapjoy'
+    retries = 0
 
-    render :text => 'ok'
+    begin
+      data = AppStore.fetch_app_by_id_for_android(id)
+      if fields.any?{|field| data[field].blank?}
+        message = 'Listing format may have changed'
+        Notifier.alert_new_relic(AndroidMarketChanged, message)
+      end
+    rescue
+      retries += 1
+      if retries < MAX_RETRIES
+        puts "retrying"
+        sleep 5
+        retry
+      else
+        Notifier.alert_new_relic(AndroidMarketChanged, 'Site may be down')
+      end
+    end
   end
 end
