da7c896cb50648ddfc99c56aaf2430abb5110200
Eric Tipton <eric.tipton@tapjoy.com>
2011-11-18 15:46:06 -0500

    refactor using_android? method

--- tapjoyads/app/controllers/games_controller.rb
+++ tapjoyads/app/controllers/games_controller.rb
@@ -83,10 +83,10 @@ private
 
   def using_android?
     if current_gamer && current_device_id
-      device = current_gamer.gamer_devices.find_by_device_id(current_device_id)
-      device.device_type =~ /android/
-    else
-      HeaderParser.device_type(request.headers['user-agent']) == 'android'
+      device = GamerDevice.find_by_gamer_id_and_device_id(current_gamer.id, current_device_id)
+      return device.device_type == 'android'
     end
+
+    HeaderParser.device_type(request.headers['user-agent']) == 'android'
   end
 end
