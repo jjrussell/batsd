6cb8f3b5330b38aa847a2deffe426dc2e4f48f78
Norman Chan <norman.chan@tapjoy.com>
2011-11-28 14:45:07 -0800

    Update one off script based on review

--- tapjoyads/lib/one_offs/20111027_gamer_profile_image_source.rb
+++ tapjoyads/lib/one_offs/20111027_gamer_profile_image_source.rb
@@ -1,5 +1,5 @@
 class OneOffs
   def self.update_gamer_profile_image_source
-    GamerProfile.connection.execute("update gamer_profiles set image_source = 'facebook' where facebook_id is not null")
+    GamerProfile.update_all("image_source = 'facebook'", 'facebook_id is not null')
   end
 end
