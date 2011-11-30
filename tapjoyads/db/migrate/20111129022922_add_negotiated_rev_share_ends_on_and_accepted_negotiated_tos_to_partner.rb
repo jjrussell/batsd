6d0a703a22f59ea652b8282fa034baa3143655f1
Inderpreet Singh <inderpreet.singh@tapjoy.com>
2011-11-30 15:25:37 -0800

    Set the default value for accepted_negotiated_tos

--- tapjoyads/db/migrate/20111129022922_add_negotiated_rev_share_ends_on_and_accepted_negotiated_tos_to_partner.rb
+++ tapjoyads/db/migrate/20111129022922_add_negotiated_rev_share_ends_on_and_accepted_negotiated_tos_to_partner.rb
@@ -1,7 +1,7 @@
 class AddNegotiatedRevShareEndsOnAndAcceptedNegotiatedTosToPartner < ActiveRecord::Migration
   def self.up
     add_column :partners, :negotiated_rev_share_ends_on, :date
-    add_column :partners, :accepted_negotiated_tos, :boolean
+    add_column :partners, :accepted_negotiated_tos, :boolean, :default => false
   end
 
   def self.down
