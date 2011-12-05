0468784b43f065dddd4314853938cfd3dfe14b18
Stephen McCarthy <sjmccarthy@gmail.com>
2011-12-05 13:56:45 -0800

    Fix logic for OneOff

--- tapjoyads/lib/one_offs/20111205_add_uncapped_ratios_to_spend_shares.rb
+++ tapjoyads/lib/one_offs/20111205_add_uncapped_ratios_to_spend_shares.rb
@@ -2,7 +2,8 @@ class OneOffs
 
   def self.add_uncapped_ratios_to_spend_shares
     SpendShare.find_each do |s|
-      s.uncapped_ratio = [s.ratio, SpendShare::MIN_RATIO].max
+      s.uncapped_ratio = s.ratio
+      s.ratio = [s.uncapped_ratio, SpendShare::MIN_RATIO].max
       s.save!
     end
   end
