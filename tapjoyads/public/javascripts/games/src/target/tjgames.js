a788b6e52d727c337ba1f59f190f36711f647a81
Van Pham <van.pham@yahoo.com>
2011-12-09 18:27:04 -0800

    minify JS

--- tapjoyads/public/javascripts/games/src/target/tjgames.js
+++ tapjoyads/public/javascripts/games/src/target/tjgames.js
@@ -63,9 +63,15 @@ TJG.loadedImages = {};
     TJG.vars.isSafari = nav.appVersion.match(/Safari/gi);
     TJG.vars.hasHomescreen = 'standalone' in nav && TJG.vars.isIos;
     TJG.vars.isStandalone = TJG.vars.hasHomescreen && nav.standalone;
-    TJG.vars.version = nav.appVersion.match(/OS \d+_\d+/g);
-    TJG.vars.platform = nav.platform.split(' ')[0];
-    TJG.vars.language = nav.language.replace('-', '_');
+    if (nav.appVersion) {
+       TJG.vars.version = nav.appVersion.match(/OS \d+_\d+/g);
+    }
+    if (nav.platform) {
+      TJG.vars.platform = nav.platform.split(' ')[0];
+    }
+    if (nav.language) {
+      TJG.vars.language = nav.language.replace('-', '_');
+    }
     if (TJG.vars.isIos || TJG.vars.isMobile) {
      if (TJG.vars.isIPad) {
        classReplaces['mobile'] = 'ipad';
@@ -2382,7 +2388,6 @@ RegExp.escape = function(text) {
           TJG.ui.removeDialogs();
           TJG.repositionDialog = [];
         });
-
         $('#link_device').click(function(){
           if (TJG.vars.isAndroid &&  TJG.android_market_url) {
             document.location.href = TJG.android_market_url;
@@ -2471,6 +2476,52 @@ RegExp.escape = function(text) {
         $('.plus, .mobile_icon').click(function() {
           selectDevice();
         });
+        // Placeholder support for non-supported browsers
+        var input = document.createElement("input");
+        if (('placeholder' in input) == false) {
+          $('[placeholder]').focus(function() {
+            var me = $(this);
+            if (me.val() == '' || me.val() == me.attr('placeholder')) {
+              me.val('').removeClass('placeholder');
+              if (me.hasClass('password')) {
+                me.removeClass('password');
+                if ($.browser.msie) {
+                  me.prev().hide(); // Show label directly above obj
+                }
+                else {
+                  this.type = 'password';
+                }
+              }
+            }
+          }).blur(function() {
+            var me = $(this);
+            if (me.val() == '' || me.val() == me.attr('placeholder')) {
+              if (this.type == 'password') {
+                me.addClass('password');
+                me.addClass('placeholder').val('');
+                if ($.browser.msie) {
+                  me.prev().show(); // Show label directly above obj
+                }
+                else {
+                  this.type = 'text';
+                }
+              }
+              else {
+                me.addClass('placeholder').val(me.attr('placeholder'));
+              }
+            }
+          }).blur().parents('form').submit(function() {
+            $(this).find('[placeholder]').each(function() {
+              var me = $(this);
+              if (me.val() == me.attr('placeholder')) {
+                me.val('');
+              }
+            });
+          });
+          $('label[for=password]').click(function() {
+            $(this).hide();
+          });
+        }
       },
 
       checkFlashMessages: function () {
@@ -2490,11 +2541,6 @@ RegExp.escape = function(text) {
         TJG.onload[key]();
       }
     };
-    if (window.addEventListener) {
-      window.addEventListener("load", TJG.init, false);
-    }
-    else {
-      window.attachEvent("load", TJG.init);
-    }
+    TJG.init();
 
 })(this, document);
