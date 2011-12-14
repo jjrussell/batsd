2ad4700f31e09b888009c5b1f5ca6e596ca56b4e
Robert Fletcher <lobatifricha@gmail.com>
2011-12-13 17:15:08 -0800

    specs for survey_offer and survey_question

--- tapjoyads/spec/spec_helper.rb
+++ tapjoyads/spec/spec_helper.rb
@@ -57,3 +57,14 @@ end
 def login_as(user)
   UserSession.create(user)
 end
+
+def stub_offers
+  mock_bucket = mock()
+  mock_image = mock()
+  mock_image.stubs(:read).returns('fake image')
+  mock_hash = { 'icons/checkbox.jpg' => mock_image }
+  mock_bucket.stubs(:objects).returns(mock_hash)
+  S3.stubs(:bucket).returns(mock_bucket)
+  Offer.any_instance.stubs(:save_icon!)
+  Offer.any_instance.stubs(:sync_banner_creatives!)
+end
