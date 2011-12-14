7b0be8cd8cca942362fc84593ed83cf51ef9590c
Eric Tipton <eric.tipton@tapjoy.com>
2011-12-14 00:42:51 -0500

    don't worry about invalidating unless Rails.env.production?

--- tapjoyads/lib/cloud_front.rb
+++ tapjoyads/lib/cloud_front.rb
@@ -3,6 +3,8 @@ class CloudFront
   ACF = RightAws::AcfInterface.new
 
   def self.invalidate(id, paths)
+    return unless Rails.env.production?
+
     paths = paths.to_a.collect { |path| path.gsub(/^\/?/, "/") }
     begin
       ACF.invalidate('E1MG6JDV6GH0F2', paths, "#{id}.#{Time.now.to_i}")
