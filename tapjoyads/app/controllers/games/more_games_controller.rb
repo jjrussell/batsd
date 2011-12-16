f88802da91c55abe9a500eeedff0cc23417d8c13
James Huang <jhuang1982@gmail.com>
2011-12-15 17:39:46 -0800

    final clean up

--- tapjoyads/app/controllers/games/more_games_controller.rb
+++ tapjoyads/app/controllers/games/more_games_controller.rb
@@ -1,8 +1,8 @@
 class Games::MoreGamesController < GamesController
+
   layout false
 
   def editor_picks
-
     if using_android?
       @editors_picks = EditorsPick.cached_active('android')
     else
