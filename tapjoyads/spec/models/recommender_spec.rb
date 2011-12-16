36e14aa60f3d9c07a8f58df6b1b38c738010bc65
Francisco Gutierrez <freefrancisco@gmail.com>
2011-12-15 17:16:34 -0800

    changed the test to reflect the recommender changes

--- tapjoyads/spec/models/recommender_spec.rb
+++ tapjoyads/spec/models/recommender_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Recommender do
   before do
-    @interface_methods = %w(most_popular_apps recommendations_for_app recommendations_for_udid).map(&:to_sym)
+    @interface_methods = %w(most_popular for_app for_device).map(&:to_sym)
     @recommenders = Recommender::ACTIVE_RECOMMENDERS.keys.map { |x| "Recommenders::#{x.to_s.camelize}".constantize.instance }
   end
 
@@ -11,34 +11,25 @@ describe Recommender do
   end
 
   it "should return recommendations for app in the correct data structure" do
-    app = Recommender.instance(:app_affinity_recommender).random_app
+    app = "9f47822c-2183-4969-98b1-ce64430e4e58"
+    udid = "statz_test_udid"
     @recommenders.each do |r|
-      r.recommendations_for_app(app).should be_an Array
-      r.recommendations_for_app(app, :n => 15).should have_at_most(15).items
-      r.recommendations_for_app(app).first.should be_a Hash
-      r.recommendations_for_app(app).first[:app_id].should be_a String
-      r.recommendations_for_app(app).first[:weight].should be_a Numeric
-    end
-  end
-
-  it "should return recommendations for udid in the correct data structure" do
-    udid = Recommender.instance(:app_affinity_recommender).random_udid
-    @recommenders.each do |r|
-      r.recommendations_for_udid(udid).should be_an Array
-      r.recommendations_for_udid(udid, :n => 15).should have_at_most(15).items
-      r.recommendations_for_udid(udid).first.should be_a Hash
-      r.recommendations_for_udid(udid).first[:app_id].should be_a String
-      r.recommendations_for_udid(udid).first[:weight].should be_a Numeric
-    end
-  end
-
-  it "should return the most popular apps with the correct options" do
-    @recommenders.each do |r|
-      r.most_popular_apps.should be_an Array
-      r.most_popular_apps(:n => 15).should have_at_most(15).items
-      r.most_popular_apps.first.should be_a Hash
-      r.most_popular_apps.first[:app_id].should be_a String
-      r.most_popular_apps.first[:weight].should be_a Numeric
+      r.cache_all
+      r.for_app(app).should be_an Array
+      r.for_app(app, :n => 15).should have_at_most(15).items
+      r.for_app(app).first.should be_a Array
+      r.for_app(app).first.first.should be_a String
+      r.for_app(app).first.last.should be_a Numeric
+      r.for_device(udid).should be_an Array
+      r.for_device(udid, :n => 15).should have_at_most(15).items
+      r.for_device(udid).first.should be_a Array
+      r.for_device(udid).first.first.should be_a String
+      r.for_device(udid).first.last.should be_a Numeric
+      r.most_popular.should be_an Array
+      r.most_popular(:n => 15).should have_at_most(15).items
+      r.most_popular.first.should be_a Array
+      r.most_popular.first.first.should be_a String
+      r.most_popular.first.last.should be_a Numeric
     end
   end
 end
