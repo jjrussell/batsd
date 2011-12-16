2212b2a990213dca0e5569d35a4ce786502e0240
Eric Tipton <eric.tipton@tapjoy.com>
2011-12-15 22:42:29 -0500

    this is better actually

--- tapjoyads/config/initializers/mail_safe.rb
+++ tapjoyads/config/initializers/mail_safe.rb
@@ -1,4 +1,3 @@
 if defined?(MailSafe::Config)
-  LITMUS_SPAM_ADDRESSES = %w(ml@ml.emailtests.com postini_2@postini-mailtest.com barracuda@barracuda.emailtests.com chkemltests@gapps.emailtests.com chkemltests@me.com chkemltests@sg.emailtests.com chkemltests@gmx.com chkemltests@hushmail.com chkemltests@fastmail.fm chkemltests@lycos.com chkemltests@mail.com)
   MailSafe::Config.internal_address_definition = /.*@(tapjoy|emailtests)\.com|#{LITMUS_SPAM_ADDRESSES.join('|')}/i
 end
