2f363de4f61441966c28d90290aa0b999769a9d3
James Huang <jhuang1982@gmail.com>
2011-12-19 15:38:22 -0800

    chage is_wifi_required to wifi_required, use App.find_in_cache instead of App.find_by_id

--- tapjoyads/app/models/app.rb
+++ tapjoyads/app/models/app.rb
@@ -252,12 +252,8 @@ class App < ActiveRecord::Base
     PLATFORM_DETAILS[platform][:screen_layout_sizes].nil? ? [] : PLATFORM_DETAILS[platform][:screen_layout_sizes].sort{ |a,b| a[1] <=> b[1] }
   end
 
-  def is_wifi_required?
-    if PLATFORM_DETAILS[platform][:cell_download_limit_bytes] && file_size_bytes > PLATFORM_DETAILS[platform][:cell_download_limit_bytes]
-      true
-    else
-      false
-    end
+  def wifi_required?
+    PLATFORM_DETAILS[platform][:cell_download_limit_bytes] && file_size_bytes > PLATFORM_DETAILS[platform][:cell_download_limit_bytes]
   end
 
 private
--- tapjoyads/app/models/cached_app.rb
+++ tapjoyads/app/models/cached_app.rb
@@ -1,6 +1,6 @@
 class CachedApp
 
-  attr_accessor :id, :name, :description, :primary_category, :user_rating, :price, :url, :is_wifi_required
+  attr_accessor :id, :name, :description, :primary_category, :user_rating, :price, :url, :wifi_required
 
   def initialize(offer, description = nil)
     self.id = offer.id
@@ -14,7 +14,7 @@ class CachedApp
       self.primary_category = app.primary_category
       self.user_rating = app.user_rating
       self.description ||= app.description
-      self.is_wifi_required = app.is_wifi_required?
+      self.wifi_required = app.wifi_required?
     else
       self.url = offer.url
       self.primary_category = nil
