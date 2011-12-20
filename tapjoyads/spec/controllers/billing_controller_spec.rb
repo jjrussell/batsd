ae8681128c692e92e956155453f6d552cfa3b7f0
Hwan-Joon Choi <hwanjoon@tapjoy.com>
2011-12-19 20:14:04 -0800

    Code review
    * fixed functional tests and moved them to rspec
    * wrote out transfer instead of xfer and trasnfer

--- tapjoyads/spec/controllers/billing_controller_spec.rb
+++ tapjoyads/spec/controllers/billing_controller_spec.rb
@@ -3,25 +3,58 @@ require 'spec/spec_helper'
 describe BillingController do
   before :each do
     activate_authlogic
-    @pending_earnings = 1000
     user = Factory(:user)
-    partner = Factory(:partner, :users => [ user ], :pending_earnings => @pending_earnings)
+    @partner = Factory(:partner, :pending_earnings => 10000, :balance => 10000, :users => [user], :transfer_bonus => 0.1)
     login_as(user)
+    # remove this line after 12/23
+    Time.stubs(:now).returns(Time.parse('2011-12-23'))
   end
 
-  describe 'trasnfer freeze' do
+  describe "admins creating transfers" do
+    it "should log transfer and math should work out" do
+      get :create_transfer, { :transfer_amount => '$1.00' }
+      @partner.reload
+
+      @partner.orders.length.should == 2
+      @partner.payouts.length.should == 1
+      @partner.pending_earnings.should == 9_900
+      @partner.balance.should == 10_110
+    end
+
+    it "should not allow negative transfer" do
+      get :create_transfer, { :transfer_amount => '$-1.00' }
+      @partner.reload
+
+      @partner.orders.should be_blank
+      @partner.payouts.should be_blank
+      @partner.pending_earnings.should == 10_000
+      @partner.balance.should == 10_000
+    end
+
+    it "should not allow transfer greater than pending_earnings amount" do
+      get :create_transfer, { :transfer_amount => '$100.01' }
+      @partner.reload
+
+      @partner.orders.should be_blank
+      @partner.payouts.should be_blank
+      @partner.pending_earnings.should == 10_000
+      @partner.balance.should == 10_000
+    end
+  end
+
+  describe 'transfer freeze' do
     describe 'during freeze' do
       before :each do
         Time.stubs(:now).returns(Time.parse('2011-12-22'))
       end
 
-      it 'should not show xfer page' do
+      it 'should not show transfer page' do
         get :transfer_funds
         @response.should render_template('billing/no_transfer.html.haml')
       end
 
-      it 'should not create xfer' do
-        post :create_transfer, :transfer_amount => 1
+      it 'should not create transfer' do
+        post :create_transfer, { :transfer_amount => '$1.00' }
         flash[:error].should =~ /error/
       end
     end
@@ -31,13 +64,13 @@ describe BillingController do
         Time.stubs(:now).returns(Time.parse('2011-12-23'))
       end
 
-      it 'should show xfer page' do
+      it 'should show transfer page' do
         get :transfer_funds
         @response.should render_template('billing/transfer_funds.html.haml')
       end
 
-      it 'should resume creating xfer' do
-        post :create_transfer, :transfer_amount => 1
+      it 'should resume creating transfer' do
+        post :create_transfer, { :transfer_amount => '$1.00' }
         flash[:error].should be_nil
       end
     end
