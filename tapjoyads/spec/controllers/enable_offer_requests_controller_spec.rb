368220106a3d6ded5bfa28e8e98f04e5865d9832
Hwan-Joon Choi <hwanjoon@tapjoy.com>
2011-12-23 01:48:27 -0800

    test -> spec for enable offer requests controller

---
+++ tapjoyads/spec/controllers/enable_offer_requests_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'spec/spec_helper'
+
+describe EnableOfferRequestsController do
+  before :each do
+    activate_authlogic
+  end
+
+  describe "Users wishing to tapjoy-enable offers" do
+    before :each do
+      @user = Factory(:user)
+      @partner = Factory(:partner, :users => [ @user ])
+      @app = Factory(:app, :partner => @partner)
+      @offer = @app.primary_offer
+      login_as(@user)
+    end
+
+    it 'should be able to submit a request' do
+      @offer.enable_offer_requests.should be_blank
+
+      post :create, :enable_offer_request => { :offer_id => @offer.id }
+
+      @response.should be_redirect
+      @offer.reload.enable_offer_requests.first.status.should == 0
+    end
+  end
+end
