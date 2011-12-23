b7e0aa36cb1ce89daf6d15e22cc23982a93283dc
Hwan-Joon Choi <hwanjoon@tapjoy.com>
2011-12-22 16:25:13 -0800

    Give it a default advertiser app id

--- tapjoyads/app/controllers/games/gamers/devices_controller.rb
+++ tapjoyads/app/controllers/games/gamers/devices_controller.rb
@@ -72,6 +72,7 @@ class Games::Gamers::DevicesController < GamesController
           devices = GamerDevice.find_all_by_device_id(data[:udid])
           if devices.size == 1 && devices[0].gamer_id == current_gamer.id
             invitation_id, advertiser_app_id = ObjectEncryptor.decrypt(current_gamer.referrer).split(',')
+            advertiser_app_id = TAPJOY_GAMES_INVITATION_OFFER_ID if advertiser_app_id.blank?
             referred_by_gamer = Gamer.find_by_id(current_gamer.referred_by)
             invitation = Invitation.find_by_id_and_gamer_id(invitation_id, current_gamer.referred_by)
             if advertiser_app_id && referred_by_gamer && invitation
