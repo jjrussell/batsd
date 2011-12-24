b0712844c46f320b40c5416e922fa7c3e0ad543f
Robert Fletcher <lobatifricha@gmail.com>
2011-12-24 01:31:10 -0800

    remove Mc dependency

--- tapjoyads/lib/fake_aws.rb
+++ tapjoyads/lib/fake_aws.rb
@@ -1,47 +1,51 @@
 module FakeAws
   class RightAws::SdbInterface
+    @@fake_sdb_data ||= {}
+
     def put_attributes(domain, key, attrs_to_put, attrs_to_replace, expected_attr)
-      existing_attrs = Mc.get("fake_sdb.#{key}") || {}
-      Mc.put("fake_sdb.#{key}", existing_attrs.merge(attrs_to_put))
+      data_key = "#{domain}.#{key}"
+
+      existing_attrs = @@fake_sdb_data[data_key] || {}
+      @@fake_sdb_data[data_key] = existing_attrs.merge(attrs_to_put)
     end
 
-    def get_attributes(domain_name, key, something, consistent)
-      attributes = Mc.get("fake_sdb.#{key}") || {}
+    def get_attributes(domain, key, something, consistent)
+      data_key = "#{domain}.#{key}"
+
+      attributes = @@fake_sdb_data[data_key] || {}
       { :attributes => attributes }
     end
 
-    def delete_attributes(domain_name, key, attrs_to_delete, expected_attr)
-      attributes = Mc.get("fake_sdb.#{key}") || {}
+    def delete_attributes(domain, key, attrs_to_delete, expected_attr)
+      data_key = "#{domain}.#{key}"
+
+      attributes = @@fake_sdb_data[data_key] || {}
       attrs_to_delete.each do |attr, value|
         attributes.delete(attr)
       end
-      Mc.put("fake_sdb.#{key}", attributes)
+
+      @@fake_sdb_data[data_key] = attributes
     end
   end
 
   class S3
+    @@fake_buckets ||= {}
+
     def self.bucket(bucket_name)
-      FakeBucket.new(bucket_name)
+      @@fake_buckets[bucket_name] ||= FakeBucket.new
     end
   end
 
   class FakeBucket
-    def initialize(bucket_name)
-      @bucket_name = bucket_name
-    end
-
     def objects
-      FakeObjects.new(@bucket_name)
+      @fake_objects ||= FakeObjects.new
     end
   end
 
   class FakeObjects
-    def initialize(bucket_name)
-      @bucket_name = bucket_name
-    end
-
     def [](key)
-      FakeObject.new(key)
+      @fake_objects ||= {}
+      @fake_objects[key] ||= FakeObject.new
     end
   end
 
@@ -51,7 +55,7 @@ module FakeAws
     end
 
     def write(data)
-      Mc.put("fake_object.#{@key}", data[:data])
+      @data = data
     end
   end
 end
