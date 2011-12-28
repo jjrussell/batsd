f178bea21248b89c633d642cc43d2190223f3b81
Ryan Johns <ryan@tapjoy.com>
2011-12-28 11:08:51 -0800

    add first_effective_on to network_costs so costs can be entered before becoming active

--- tapjoyads/app/models/network_cost.rb
+++ tapjoyads/app/models/network_cost.rb
@@ -1,7 +1,8 @@
 class NetworkCost < ActiveRecord::Base
   include UuidPrimaryKey
 
+  validates_presence_of :first_effective_on
   validates_numericality_of :amount, :only_integer => true, :allow_nil => false
 
-  named_scope :created_between, lambda { |start_time, end_time| { :conditions => [ "created_at >= ? AND created_at < ?", start_time, end_time ] } }
+  named_scope :for_date, lambda { |date| { :conditions => [ "first_effective_on = ? OR (first_effective_on > ? AND first_effective_on < ?)", date, date - 30.days, date ] } }
 end
