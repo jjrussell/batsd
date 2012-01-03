5f2f676ff07c6e1e1c3a3d4ee71c754cf5e5fb9a
Ryan Johns <ryan@tapjoy.com>
2012-01-03 15:22:59 -0800

    move logic to model

--- tapjoyads/app/models/network_cost.rb
+++ tapjoyads/app/models/network_cost.rb
@@ -5,4 +5,9 @@ class NetworkCost < ActiveRecord::Base
   validates_numericality_of :amount, :only_integer => true, :allow_nil => false
 
   named_scope :for_date, lambda { |date| { :conditions => [ "first_effective_on = ? OR (first_effective_on > ? AND first_effective_on < ?)", date, date - 30.days, date ] } }
+
+  def last_effective_on
+    first_effective_on + 29.days
+  end
+
 end
