954c0ed6df0317b21bfb513c3e280cdc7ff1a4c2
Norman Chan <norman.chan@tapjoy.com>
2012-01-04 15:21:02 -0800

    fix categories access in AppMetadata

--- tapjoyads/app/models/app_metadata.rb
+++ tapjoyads/app/models/app_metadata.rb
@@ -6,4 +6,12 @@ class AppMetadata < ActiveRecord::Base
 
   validates_presence_of :store_name, :store_id
   validates_uniqueness_of :store_id, :scope => [ :store_name ]
+
+  def categories=(arr)
+    write_attribute(:categories, arr.join(';'))
+  end
+
+  def categories
+    (read_attribute(:categories)||'').split(';')
+  end
 end
