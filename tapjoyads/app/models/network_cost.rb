75b1c57638763f906130dd58f41c63f0efabfffd
Ryan Johns <ryan@tapjoy.com>
2012-01-03 18:22:13 -0800

    simplify named scope

--- tapjoyads/app/models/network_cost.rb
+++ tapjoyads/app/models/network_cost.rb
@@ -4,7 +4,7 @@ class NetworkCost < ActiveRecord::Base
   validates_presence_of :first_effective_on
   validates_numericality_of :amount, :only_integer => true, :allow_nil => false
 
-  named_scope :for_date, lambda { |date| { :conditions => [ "first_effective_on = ? OR (first_effective_on > ? AND first_effective_on < ?)", date, date - 30.days, date ] } }
+  named_scope :for_date, lambda { |date| { :conditions => [ "first_effective_on > ? AND first_effective_on <= ?", date - 30.days, date ] } }
 
   def last_effective_on
     first_effective_on + 29.days
