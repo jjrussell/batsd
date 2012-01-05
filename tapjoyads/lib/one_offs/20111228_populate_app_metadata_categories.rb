2c8add8ce8cc80f0f0f4347562f7a2c9997ed9a0
Norman Chan <norman.chan@tapjoy.com>
2012-01-04 16:01:36 -0800

    merge one off script change

--- tapjoyads/lib/one_offs/20111228_populate_app_metadata_categories.rb
+++ tapjoyads/lib/one_offs/20111228_populate_app_metadata_categories.rb
@@ -1,7 +1,7 @@
 class OneOffs
 
   def self.populate_app_metadata_categories
-    AppMetadata.connection.execute("update app_metadatas set categories = null")
+    AppMetadata.connection.execute("UPDATE app_metadatas SET categories = NULL")
     App.find_each do |app|
       if app.app_metadatas.present? && app.read_attribute(:categories).present?
         app.app_metadatas.first.write_attribute(:categories, app.read_attribute(:categories))
