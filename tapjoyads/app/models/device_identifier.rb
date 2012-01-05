256ad7fed5e1cd89df221174525f067d59722361
Inderpreet Singh <inderpreet.singh@tapjoy.com>
2012-01-05 11:28:43 -0800

    Rename constant, and add more logging in the OneOff.

--- tapjoyads/app/models/device_identifier.rb
+++ tapjoyads/app/models/device_identifier.rb
@@ -1,12 +1,12 @@
 class DeviceIdentifier < SimpledbShardedResource
   # key_format: (hashed_udid | mac_address)
 
-  self.num_domains = NUM_DEVICE_IDENTIFIERS_DOMAINS
+  self.num_domains = NUM_DEVICE_IDENTIFIER_DOMAINS
 
   self.sdb_attr :udid
 
   def dynamic_domain_name
-    domain_number = @key.matz_silly_hash % NUM_DEVICE_IDENTIFIERS_DOMAINS
+    domain_number = @key.matz_silly_hash % NUM_DEVICE_IDENTIFIER_DOMAINS
     "device_identifiers_#{domain_number}"
   end
 
--- tapjoyads/lib/one_offs/20111215_populate_device_identifiers_and_create_domains.rb
+++ tapjoyads/lib/one_offs/20111215_populate_device_identifiers_and_create_domains.rb
@@ -1,14 +1,18 @@
 class OneOffs
   def self.populate_device_identifiers_and_create_domains
-    NUM_DEVICE_IDENTIFIERS_DOMAINS.times do |i|
+    NUM_DEVICE_IDENTIFIER_DOMAINS.times do |i|
       SimpledbResource.create_domain("device_identifiers_#{i}")
     end
+    num_processed = 0
+
     # This is a long running task, which will every existing device, and create corresponding
     # entries in the device_identifiers table.
     NUM_DEVICES_DOMAINS.times do |i|
       Device.select(:domain_name => "devices_#{i}") do |device|
         begin
           raise "Unable to create identifiers for device: #{device.id}" unless device.create_identifiers
+          num_processed += 1
+          puts "#{Time.zone.now} - #{num_processed} devices processed so far. Iterating on domain number: #{i}" if num_processed % 1000 == 0
         rescue Exception => ex
           puts "#{ex}, retrying"
           sleep 0.2
