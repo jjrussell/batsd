906c703740172af9e4f162b6d3667444b2fbb295
Inderpreet Singh <inderpreet.singh@tapjoy.com>
2012-01-10 16:55:22 -0800

    Update the text to Offer Health for support request to reward ratio.
    Also cache the support requests too.

--- tapjoyads/app/models/offer.rb
+++ tapjoyads/app/models/offer.rb
@@ -625,16 +625,18 @@ class Offer < ActiveRecord::Base
   end
 
   def num_support_requests(start_time = 1.day.ago, end_time = Time.zone.now)
-    conditions = [
-      "offer_id = '#{id}'",
-      "`updated-at` < '#{end_time.to_f}'",
-      "`updated-at` >= '#{start_time.to_f}'",
-    ].join(' and ')
-    SupportRequest.count(:where => conditions)
+    Mc.get_and_put("Offer.SupportRequests.#{id}", false, 1.hour) do
+      conditions = [
+        "offer_id = '#{id}'",
+        "`updated-at` < '#{end_time.to_f}'",
+        "`updated-at` >= '#{start_time.to_f}'",
+      ].join(' and ')
+      SupportRequest.count(:where => conditions)
+    end
   end
 
   def num_clicks_rewarded(start_time = 1.day.ago, end_time = Time.zone.now)
-    Mc.get_and_put("Offer.ClicksRewarded.#{id}", false, 15.minutes) do
+    Mc.get_and_put("Offer.ClicksRewarded.#{id}", false, 1.hour) do
       clicks_rewarded = 0
       conditions = [
         "offer_id = '#{id}'",
