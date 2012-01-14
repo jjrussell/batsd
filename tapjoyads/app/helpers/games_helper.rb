ba759af99624af579a2d514451d159bda031af6c
Ryan Johns <ryan@tapjoy.com>
2012-01-13 16:35:02 -0800

    remove games helper and move the method into games controller

--- tapjoyads/app/helpers/games_helper.rb
+++
@@ -1,6 +0,0 @@
-module GamesHelper
-  def social_feature_redirect_path
-    return request.env['HTTP_REFERER'] if request.env['HTTP_REFERER']
-    "#{WEBSITE_URL}#{edit_games_gamer_path}"
-  end
-end
