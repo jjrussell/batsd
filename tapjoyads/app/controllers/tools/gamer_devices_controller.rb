222e7e1739cc9e91bbc312adb4275855b33dc3e4
Brian Stebar II <brian.stebar@tapjoy.com>
2012-01-25 11:32:23 -0800

    Finished gamer device add and edit form

--- tapjoyads/app/controllers/tools/gamer_devices_controller.rb
+++ tapjoyads/app/controllers/tools/gamer_devices_controller.rb
@@ -3,15 +3,33 @@ class Tools::GamerDevicesController < WebsiteController
   current_tab :tools
   filter_access_to :all
 
+  def create
+    @gamer_device = GamerDevice.new(params[:gamer_device])
+
+    if @gamer_device.save
+      redirect_to tools_gamer_path(@gamer_device.gamer), :notice => 'Gamer Device was successfully created and linked.'
+    else
+      render :action => "new"
+    end
+  end
+
   def edit
     @gamer_device = GamerDevice.find(params[:id])
+  end
+
+  def new
+    if params[:gamer_id].blank?
+      redirect_to tools_gamers_path, :alert => 'A gamer ID is required in order to link a new gamer device.'
+    else
+      @gamer_device = GamerDevice.new(:gamer_id => params[:gamer_id])
+    end
   end
 
   def update
     @gamer_device = GamerDevice.find(params[:id])
 
     if @gamer_device.update_attributes(params[:gamer_device])
-      redirect_to(tools_gamer_path(@gamer_device.gamer), :notice => 'Device updated.')
+      redirect_to tools_gamer_path(@gamer_device.gamer), :notice => 'Device updated.'
     else
       render :action => "edit"
     end
--- tapjoyads/config/routes/dashboard.rb
+++ tapjoyads/config/routes/dashboard.rb
@@ -100,7 +100,7 @@ ActionController::Routing::Routes.draw do |map|
     tools.resources :support_requests, :only => [ :index ], :collection => { :mass_resolve => [ :get, :post ] }
     tools.resources :press_releases, :only => [ :index, :new, :create, :edit, :update ]
     tools.resources :gamers, :only => [ :index, :show ]
-    tools.resources :gamer_devices, :only => [ :edit, :show, :update ]
+    tools.resources :gamer_devices, :only => [ :create, :edit, :new, :show, :update ]
     tools.resources :network_costs, :only => [ :index, :new, :create ]
     tools.resources :partner_program_statz, :only => [ :index ], :collection => { :export => :get }
     tools.resources :payout_freezes, :only => [ :index, :create ], :member => { :disable => :post }
