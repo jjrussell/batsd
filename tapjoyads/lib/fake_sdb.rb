f83bf570c03a66583fe090f87b24045457fb8aca
Robert Fletcher <lobatifricha@gmail.com>
2012-01-25 13:30:29 -0800

    convert points controller tests to specs
    and add expected_attrs support to fake_sdb

--- tapjoyads/lib/fake_sdb.rb
+++ tapjoyads/lib/fake_sdb.rb
@@ -1,10 +1,20 @@
 class FakeSdb
   @@fake_sdb_data ||= {}
 
-  def put_attributes(domain, key, attrs_to_put, attrs_to_replace, expected_attr)
+  def put_attributes(domain, key, attrs_to_put, attrs_to_replace = {}, expected_attrs = {})
     data_key = "#{domain}.#{key}"
 
     existing_attrs = @@fake_sdb_data[data_key] || {}
+    expected_attrs.each do |key, value|
+      if key == 'version'
+        current_version = existing_attrs[key]
+        if current_version && current_version.first.to_i != value.to_i + 1
+          raise Simpledb::ExpectedAttributeError
+        end
+      elsif existing_attrs[key] != value
+        raise Simpledb::ExpectedAttributeError
+      end
+    end
     @@fake_sdb_data[data_key] = existing_attrs.merge(attrs_to_put)
   end
 
@@ -15,7 +25,7 @@ class FakeSdb
     { :attributes => attributes }
   end
 
-  def delete_attributes(domain, key, attrs_to_delete = {}, expected_attr = {})
+  def delete_attributes(domain, key, attrs_to_delete = {}, expected_attrs = {})
     data_key = "#{domain}.#{key}"
 
     attributes = @@fake_sdb_data[data_key] || {}
