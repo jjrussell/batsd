cd41a86d5523eec8b11f597a8ed280f42f7dc634
Francisco Gutierrez <freefrancisco@gmail.com>
2012-01-26 17:22:44 -0800

    change variables to more descriptive names and change puts to should in spec

--- tapjoyads/spec/lib/hash_matrix_spec.rb
+++ tapjoyads/spec/lib/hash_matrix_spec.rb
@@ -1,85 +1,100 @@
 require 'spec_helper'
 
+
 describe HashMatrix do
   before do
-    @m = HashMatrix.eye(3)
-    @m[1, 3] = 1
-    @m[3, 1] = -1
-    @m[6, 6] = 1
-    @v = HashMatrix.vector.from_pairs [[1,2], [2,-2], [3,4], [6,-5]]
+    @matrix = HashMatrix.eye(3)
+    @matrix[1, 3] = 1
+    @matrix[3, 1] = -1
+    @matrix[6, 6] = 1
+    @vector = HashMatrix.vector.from_pairs [[1,2], [2,-2], [3,4], [6,-5]]
   end
 
-  it "should print and not fail with a few different options" do
-    puts "print matrix in sparse mode"
-    puts @m.to_s_sparse
-    puts "print matrix in full mode without labels"
-    puts @m.to_s_full
-    puts "print matrix in full mode with labels"
-    puts @m.to_s_full true
-    idx = (1..8)
-    puts "print matrix in full mode with labels for indices #{idx.to_a.inspect}"
-    puts @m.to_s_full true, idx, idx
-    puts "print matrix default:"
-    puts @m
+  describe "different string representations" do
+    it "should return sparse matrix representation" do
+      @matrix.to_s_sparse.should == <<-STR.gsub(/^\s*/, '').strip
+      (1, 1) -> 1
+      (1, 3) -> 1
+      (2, 2) -> 1
+      (3, 1) -> -1
+      (3, 3) -> 1
+      (6, 6) -> 1
+      STR
+    end
+    it "should return matrix in full mode without labels" do
+      @matrix.to_s_full.should == <<-STR.gsub(/^\s*/, '').strip.gsub(/ +/, "\t")
+      1 0 1 0
+      0 1 0 0
+      -1  0 1 0
+      0 0 0 1
+      STR
+      puts "print matrix in full mode with labels"
+      puts @matrix.to_s_full true
+      idx = (1..8)
+      puts "print matrix in full mode with labels for indices #{idx.to_a.inspect}"
+      puts @matrix.to_s_full true, idx, idx
+      puts "print matrix default:"
+      puts @matrix
+    end
   end
 
   it "should find the p-norms of a vector, and normalize it correctly for different ps" do
-    puts "P_norms for vector \n#{@v}"
-    [1, 1.0000001, 2, 2.0000001, 3, 10, 90, 99, 10000].each{ |p| puts "p_norm(#{p}) = #{@v.p_norm(p)}" }
-    @v.p_norm(1).should == 13
-    @v.p_norm(2).should == 7
-    @v.p_norm(3).should be_close 5.896, 0.001
-    @v.p_norm(1000).should == 5
-    lambda{@v.p_norm(0.5)}.should raise_error
-    [1, 1.0000001, 2, 2.0000001, 3, 10, 90, 99, 10000].each{ |p| puts "normalize for p = #{p}"; puts @v.p_normalize(p); puts "dot product:"; puts(@v.p_normalize(p) * @v.p_normalize(p)) }
+    puts "P_norms for vector \n#{@vector}"
+    [1, 1.0000001, 2, 2.0000001, 3, 10, 90, 99, 10000].each{ |p| puts "p_norm(#{p}) = #{@vector.p_norm(p)}" }
+    @vector.p_norm(1).should == 13
+    @vector.p_norm(2).should == 7
+    @vector.p_norm(3).should be_close 5.896, 0.001
+    @vector.p_norm(1000).should == 5
+    lambda{@vector.p_norm(0.5)}.should raise_error
+    [1, 1.0000001, 2, 2.0000001, 3, 10, 90, 99, 10000].each{ |p| puts "normalize for p = #{p}"; puts @vector.p_normalize(p); puts "dot product:"; puts(@vector.p_normalize(p) * @vector.p_normalize(p)) }
     puts "dot product with itself:"
-    puts @v*@v
-    puts (@v.p_normalize(2) * @v.p_normalize(2))
-    (@v*@v).should == 49
-    (@v.p_normalize(2) * @v.p_normalize(2)).should == 1
+    puts @vector*@vector
+    puts (@vector.p_normalize(2) * @vector.p_normalize(2))
+    (@vector*@vector).should == 49
+    (@vector.p_normalize(2) * @vector.p_normalize(2)).should == 1
   end
 
   it "should projection and print with a subset of keys instead of all its keys" do
     puts "vector"
-    puts @v.to_s
+    puts @vector.to_s
     puts "vector 1..2"
-    puts @v.to_s 1..2
-    @m[7, 1]=20
+    puts @vector.to_s 1..2
+    @matrix[7, 1]=20
     puts "matrix"
-    puts @m
+    puts @matrix
     puts "triplets"
-    p @m.to_triplets
+    p @matrix.to_triplets
     puts "triplets 1..2, 1..3"
-    p @m.to_triplets 1..2, 1..3
+    p @matrix.to_triplets 1..2, 1..3
     puts "matrix projection 1..2, 1..3"
-    puts @m.projection 1..2, 1..3
+    puts @matrix.projection 1..2, 1..3
     puts "matrix projection nil, 1..3"
-    puts @m.projection nil, 1..3
+    puts @matrix.projection nil, 1..3
     puts "matrix projection 1..2, nil"
-    puts @m.projection 1..2, nil
+    puts @matrix.projection 1..2, nil
     puts "matrix projection nil, 1..1"
-    puts @m.projection nil, 1..1
+    puts @matrix.projection nil, 1..1
   end
 
   it "should do matrix * vector and matrix * matrix multiplication, and also do it with a subset of the keys" do
-    (@m * @m).should be_a HashMatrix
-    (@m * @v).should be_a HashMatrix.vector
+    (@matrix * @matrix).should be_a HashMatrix
+    (@matrix * @vector).should be_a HashMatrix.vector
     puts "matrix"
-    puts @m
+    puts @matrix
     puts "vector"
-    puts @v
+    puts @vector
     puts "matrix * vector"
-    puts @m * @v
+    puts @matrix * @vector
     puts "matrix * vector 1..2"
-    puts @m.*(@v, 1..2)
+    puts @matrix.*(@vector, 1..2)
     puts "matrix * vector 1..1"
-    puts @m.*(@v, 1..1)
+    puts @matrix.*(@vector, 1..1)
     puts "matrix * matrix"
-    puts @m * @m
+    puts @matrix * @matrix
     puts "matrix * matrix 1..2"
-    puts @m.*(@m, 1..2)
+    puts @matrix.*(@matrix, 1..2)
     puts "matrix * matrix 1..1"
-    puts @m.*(@m, 1..1)
+    puts @matrix.*(@matrix, 1..1)
     m=HashMatrix.from_triplets [[:a,1,1], [2,:b,2], [1,:c,3]]
     m[1,2] = -1
     puts m
@@ -88,17 +103,17 @@ describe HashMatrix do
     puts m*m
     p idx
     puts (m*m).to_s_full(true, idx, idx)
-    puts m*@v
-    puts @v * {:a => 3, 2 => :a, 3 => 4}
+    puts m*@vector
+    puts @vector * {:a => 3, 2 => :a, 3 => 4}
   end
 
   it "should only store non-zero values" do
-    l1 = @v.length
-    @v[100] = 12
-    l2= @v.length
+    l1 = @vector.length
+    @vector[100] = 12
+    l2= @vector.length
     l2.should == (l1 + 1)
-    @v[200] = 0
-    l3 = @v.length
+    @vector[200] = 0
+    l3 = @vector.length
     l3.should == l2
   end
 end
