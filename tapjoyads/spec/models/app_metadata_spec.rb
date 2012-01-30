5bdfb3dca112584fdcb1ea5fa8de242c7f3ddf14
Norman Chan <norman.chan@tapjoy.com>
2012-01-30 15:04:44 -0800

    Stub AppStore in spec

--- tapjoyads/spec/models/app_metadata_spec.rb
+++ tapjoyads/spec/models/app_metadata_spec.rb
@@ -12,31 +12,35 @@ describe AppMetadata do
     context 'when AppStore returns no data' do
       it 'raises an error' do
         app_metadata = Factory(:app_metadata)
+        AppStore.expects(:fetch_app_by_id).raises(Exception, "Invalid response from app store.")
         expect { app_metadata.update_from_store }.to raise_error
       end
     end
     
     context 'when updating app_metadata only from AppStore' do
       it 'updates metadata name' do
-        app_metadata = Factory(:app_metadata, :name => 'NotTapDefense', :store_id => "297558390")
-        app_metadata.name.should == 'NotTapDefense'
+        app_metadata = Factory(:app_metadata, :name => 'MyApp', :store_id => "abcdefg")
+
+        AppStore.expects(:fetch_app_by_id).returns({:title => 'SomeOtherApp', :price => 0, :categories => []})
         app_metadata.update_from_store
-        app_metadata.name.should == 'TapDefense'
+        
+        app_metadata.name.should == 'SomeOtherApp'
       end
     end
     
     context 'when updating app_metadata and app from AppStore' do
       it 'updates metadata and app name' do
-        app_metadata = Factory(:app_metadata, :name => 'NotTapDefense', :store_id => "297558390")
-        app = Factory(:app, :name => 'SomeApp')
+        app_metadata = Factory(:app_metadata, :name => 'MyApp', :store_id => "abcdefg")
+        app = Factory(:app, :name => 'MyApp')
         app.app_metadatas << app_metadata
         app.save!
 
+        AppStore.expects(:fetch_app_by_id).returns({:title => 'SomeOtherApp', :price => 0, :categories => []})
         app_metadata.update_from_store
         app.reload
 
-        app_metadata.name.should == 'TapDefense'
-        app.name.should == 'TapDefense'
+        app_metadata.name.should == 'SomeOtherApp'
+        app.name.should == 'SomeOtherApp'
       end
     end
   end
