1bee4c3a575007595ca15983620dc60a5e9f7950
Norman Chan <norman.chan@tapjoy.com>
2012-01-31 16:19:04 -0800

    trim whitespace

--- tapjoyads/spec/models/app_metadata_spec.rb
+++ tapjoyads/spec/models/app_metadata_spec.rb
@@ -7,7 +7,7 @@ describe AppMetadata do
   # Check validations
   it { should validate_presence_of :store_name }
   it { should validate_presence_of :store_id }
-  
+
   describe '#update_from_store' do
     context 'when AppStore returns no data' do
       it 'raises an error' do
@@ -16,18 +16,18 @@ describe AppMetadata do
         expect { app_metadata.update_from_store }.to raise_error
       end
     end
-    
+
     context 'when updating app_metadata only from AppStore' do
       it 'updates metadata name' do
         app_metadata = Factory(:app_metadata, :name => 'MyApp', :store_id => "abcdefg")
 
         AppStore.expects(:fetch_app_by_id).returns({:title => 'SomeOtherApp', :price => 0, :categories => []})
         app_metadata.update_from_store
-        
+
         app_metadata.name.should == 'SomeOtherApp'
       end
     end
-    
+
     context 'when updating app_metadata and app from AppStore' do
       it 'updates metadata and app name' do
         app_metadata = Factory(:app_metadata, :name => 'MyApp', :store_id => "abcdefg")
