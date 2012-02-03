c4b235a8eb806efb28ce2e63a2ffdce5a221c659
Ryan Johns <ryan@tapjoy.com>
2012-02-03 14:27:14 -0800

    full path

--- server/start_or_reload_unicorn.rb
+++ server/start_or_reload_unicorn.rb
@@ -5,7 +5,7 @@ pid = `/home/webuser/tapjoyserver/server/unicorn_master_pid.rb`
 if pid == ''
   server_type = `/home/webuser/tapjoyserver/server/server_type.rb`
   env = server_type == 'test' ? 'staging' : 'production'
-  `unicorn_rails -E #{env} -c tapjoyads/config/unicorn.rb -D`
+  `unicorn_rails -E #{env} -c /home/webuser/tapjoyserver/tapjoyads/config/unicorn.rb -D`
 else
   `kill -USR2 #{pid}`
 end
