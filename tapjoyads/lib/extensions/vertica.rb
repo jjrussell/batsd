a437a4d89893182593c20ea61cc34ed782fd4ed3
Ryan Johns <ryan@tapjoy.com>
2012-02-06 17:13:31 -0800

    better patch for vertica gem

--- tapjoyads/lib/extensions/vertica.rb
+++ tapjoyads/lib/extensions/vertica.rb
@@ -1,15 +1,8 @@
 module Vertica
   class Connection
-    def initialize(options = {})
-      reset_values
-
-      @options = options
-      @notices = []
-
-      unless options[:skip_startup]
-        connection.write Messages::Startup.new(@options[:user], @options[:database]).to_bytes
-        process
-      end
+    def reset_notifications
+      @notifications = []
+      @notices       = []
     end
   end
 end
