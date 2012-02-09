5cacab1b3e9a535f43791b828d69ef91695af6ad
Michael Wheeler <michael.wheeler@tapjoy.com>
2012-02-09 15:38:29 -0800

    Using w instead of window to represent global scope.

--- tapjoyads/public/javascripts/games/src/i18n.js
+++ tapjoyads/public/javascripts/games/src/i18n.js
@@ -47,7 +47,7 @@
     result = me.pluralize(result, opt.count);
 
     if(typeof result !== "string") {
-      if(window.ENVIRONMENT === "development") {
+      if(w.ENVIRONMENT === "development") {
         throw customError("Did not find translation string: ",
           {key: key, locale: opt.locale, default_locale: me.default_locale, result: result}, 
           "Tapjoyi18nError");
