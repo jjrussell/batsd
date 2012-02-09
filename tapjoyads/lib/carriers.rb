c03df7187d096c21d1d8a4dcbecf7cea883cad31
James Huang <jhuang1982@gmail.com>
2012-02-09 11:48:58 -0800

    adding carriers.rb to lib

---
+++ tapjoyads/lib/carriers.rb
@@ -0,0 +1,30 @@
+class Carriers
+
+  MCC_MNC_TO_CARRIER_NAME = {
+    '310090' => 'AT&T',
+    '310150' => 'AT&T',
+    '310410' => 'AT&T',
+    '310560' => 'AT&T',
+    '310680' => 'AT&T',
+    '310004' => 'Verizon',
+    '310005' => 'Verizon',
+    '310010' => 'Verizon',
+    '311480' => 'Verizon',
+    '46000'  => 'China Mobile',
+    '46002'  => 'China Mobile',
+    '46007'  => 'China Mobile',
+    '46003'  => 'China Telecom',
+    '46005'  => 'China Telecom',
+    '46020'  => 'China Tietong',
+    '46001'  => 'China Unicom',
+    '46006'  => 'China Unicom',
+    
+  }
+
+  COUNTRY_NAME_TO_CARRIER_NAME = {
+    "United States" => ["AT&T", "Verizon"],
+    "China" => ["China Mobile", "China Telecom"],
+    "Japan" => ["SoftBank"],
+  }
+
+end
