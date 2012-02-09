85c70421dc0501cba4cf88fcb3c3c87cdd205aaa
Robert Fletcher <lobatifricha@gmail.com>
2012-02-09 13:32:03 -0800

    move order tests to specs

--- tapjoyads/spec/models/order_spec.rb
+++ tapjoyads/spec/models/order_spec.rb
@@ -1,17 +1,24 @@
 require 'spec_helper'
 
 describe Order do
+
   before :each do
     @order = Factory(:order)
   end
 
-  it { should belong_to :partner }
+  describe '.belongs_to' do
+    it { should belong_to :partner }
+  end
 
-  it { should validate_presence_of :partner }
-  it { should validate_numericality_of :amount }
+  describe '#valid?' do
+    it { should validate_presence_of :partner }
+    it { should validate_numericality_of :amount }
+    it { should ensure_inclusion_of(:status).in_range(Order::STATUS_CODES.keys) }
+    it { should ensure_inclusion_of(:payment_method).in_range(Order::PAYMENT_METHODS.keys) }
+  end
 
-  describe "an invoice" do
-    it "should be created if the client exists in freshbooks" do
+  describe 'an invoice' do
+    it 'should be created if the client exists in freshbooks' do
       FreshBooks.expects(:get_client_id).returns(5)
       FreshBooks.expects(:create_invoice).returns(7)
 
@@ -26,7 +33,7 @@ describe Order do
       @order.freshbooks_client_id.should == 5
     end
 
-    it "should not be created if there isn't a freshbooks client" do
+    it 'should not be created if there is not a freshbooks client' do
       FreshBooks.expects(:get_client_id).returns(nil)
       FreshBooks.expects(:create_invoice).never
 
@@ -35,7 +42,7 @@ describe Order do
       @order.status.should == 0
     end
 
-    it "should deal if the client disappears from freshbooks" do
+    it 'should deal if the client disappears from freshbooks' do
       FreshBooks.expects(:get_client_id).times(2).returns(2, nil)
       FreshBooks.expects(:create_invoice).once
 
@@ -47,6 +54,21 @@ describe Order do
       order.status.should == 1
       order.create_freshbooks_invoice!
       order.status.should == 0
+    end
+  end
+
+  context 'An Order' do
+    before :each do
+      @partner = Factory(:partner)
+    end
+
+    it 'increases the balance for a partner' do
+      @partner.balance.should == 0
+      @partner.orders.count.should == 0
+      Factory(:order, :partner => @partner, :amount => 100)
+      @partner.reload
+      @partner.balance.should == 100
+      @partner.orders.count.should == 1
     end
   end
 
