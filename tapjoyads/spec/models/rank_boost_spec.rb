29af7a6c855da477ab9c01a1a359743221b89cfd
Robert Fletcher <lobatifricha@gmail.com>
2012-02-10 13:19:05 -0800

    re-nesting for better output

--- tapjoyads/spec/models/rank_boost_spec.rb
+++ tapjoyads/spec/models/rank_boost_spec.rb
@@ -12,45 +12,43 @@ describe RankBoost do
     it { should validate_numericality_of :amount }
   end
 
-  context "A RankBoost" do
+  before :each do
+    @rank_boost = Factory(:rank_boost)
+  end
+
+  it "has its offer's partner_id" do
+    @rank_boost.partner_id.should == @rank_boost.offer.partner_id
+  end
+
+  context "starting before now and ending after now" do
     before :each do
-      @rank_boost = Factory(:rank_boost)
+      @rank_boost = Factory(:rank_boost, :start_time => 1.hour.ago, :end_time => 1.hour.from_now)
     end
 
-    it "has its offer's partner_id" do
-      @rank_boost.partner_id.should == @rank_boost.offer.partner_id
+    it "is active" do
+      @rank_boost.should be_active
     end
 
-    context "starting before now and ending after now" do
+    it "is in the active scope" do
+      RankBoost.active.should include @rank_boost
+    end
+
+    context "that is deactivated" do
       before :each do
-        @rank_boost = Factory(:rank_boost, :start_time => 1.hour.ago, :end_time => 1.hour.from_now)
+        @rank_boost.deactivate!
       end
 
-      it "is active" do
-        @rank_boost.should be_active
+      it "is no longer active" do
+        @rank_boost.should_not be_active
       end
 
-      it "is in the active scope" do
-        RankBoost.active.should include @rank_boost
-      end
-
-      context "that is deactivated" do
-        before :each do
-          @rank_boost.deactivate!
-        end
-
-        it "is no longer active" do
-          @rank_boost.should_not be_active
-        end
-
-        it "is not in the active scope" do
-          RankBoost.active.should_not include @rank_boost
-        end
+      it "is not in the active scope" do
+        RankBoost.active.should_not include @rank_boost
       end
     end
   end
 
-  context "A RankBoost with end_time before start_time" do
+  context "with end_time before start_time" do
     before :each do
       @rank_boost = Factory.build(:rank_boost, :start_time => 1.hour.ago, :end_time => 2.hours.ago)
     end
