16aaa435afe716f187a7d63c78c3800a9d4713c1
Robert Fletcher <lobatifricha@gmail.com>
2012-02-10 15:39:35 -0800

    de-nesting generic offer specs

--- tapjoyads/spec/models/generic_offer_spec.rb
+++ tapjoyads/spec/models/generic_offer_spec.rb
@@ -22,27 +22,25 @@ describe GenericOffer do
     it { should validate_presence_of(:url) }
   end
 
-  # Test category validation
-  context 'A generic offer' do
-    before :each do
-      @generic_offer = Factory(:generic_offer)
-    end
+  before :each do
+    @generic_offer = Factory(:generic_offer)
+  end
 
-    it 'accepts a blank category' do
-      @generic_offer.category = ''
+  it 'accepts a blank category' do
+    @generic_offer.category = ''
+    @generic_offer.should be_valid
+  end
+
+  it 'accepts existing categories' do
+    GenericOffer::CATEGORIES.each do |category|
+      @generic_offer.category = category
       @generic_offer.should be_valid
     end
-
-    it 'accepts existing categories' do
-      GenericOffer::CATEGORIES.each do |category|
-        @generic_offer.category = category
-        @generic_offer.should be_valid
-      end
-    end
-
-    it 'rejects invalid categories' do
-      @generic_offer.category = 'invalid'
-      @generic_offer.should_not be_valid
-    end
   end
+
+  it 'rejects invalid categories' do
+    @generic_offer.category = 'invalid'
+    @generic_offer.should_not be_valid
+  end
+
 end
