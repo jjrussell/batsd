584feda3c527593ffb0fd5d0c446be22c97abaf0
Robert Fletcher <lobatifricha@gmail.com>
2012-02-10 15:32:57 -0800

    refactoring user specs

--- tapjoyads/spec/models/user_spec.rb
+++ tapjoyads/spec/models/user_spec.rb
@@ -18,26 +18,29 @@ describe User do
     it { should validate_uniqueness_of(:username) }
   end
 
-  context 'A regular User' do
-    before :each do
-      @user = Factory(:user)
+  describe '#role_symbols' do
+    context 'given a regular user' do
+      before :each do
+        @user = Factory(:user)
+      end
+
+      it 'includes the partner role' do
+        @user.role_symbols.should include(:partner)
+      end
     end
 
-    it 'has the partner role' do
-      roles = @user.role_symbols
-      roles.should include(:partner)
-    end
-  end
+    context 'given an admin user' do
+      before :each do
+        @admin = Factory(:admin)
+      end
 
-  context 'An admin User' do
-    before :each do
-      @admin = Factory(:admin)
-    end
+      it 'has the partner role' do
+        @admin.role_symbols.should include(:partner)
+      end
 
-    it 'has the partner role and the admin role' do
-      roles = @admin.role_symbols
-      roles.should include(:partner)
-      roles.should include(:admin)
+      it 'includes the admin role' do
+        @admin.role_symbols.should include(:admin)
+      end
     end
   end
 
