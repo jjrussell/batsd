e145be2de037447afc7e1d3a3b86b9cd1985f21f
Robert Fletcher <lobatifricha@gmail.com>
2012-02-10 15:22:17 -0800

    renesting email signup specs

--- tapjoyads/spec/models/email_signup_spec.rb
+++ tapjoyads/spec/models/email_signup_spec.rb
@@ -2,37 +2,37 @@ require 'spec_helper'
 
 describe EmailSignup do
 
-  context 'An EmailSignup' do
-    before :each do
-     @email_signup = Factory(:email_signup)
-     @email_address = @email_signup.email_address
-    end
-
-    it 'is correctly found when searched by dynamic finder attribute methods' do
-      EmailSignup.find_by_email_address(@email_address, :consistent => true).should == @email_signup
-      EmailSignup.find_all_by_email_address(@email_address, :consistent => true).first.should == @email_signup
-    end
-
-    it 'raises errors when dynamic finder attributes are not matched' do
-      expect {
-        EmailSignup.find_by_nonexistent_attribute('hello')
-      }.to raise_error(NoMethodError)
-
-      expect {
-        EmailSignup.do_something_that_doesnt_exist('hello')
-      }.to raise_error(NoMethodError)
-    end
+  before :each do
+    @email_signup = Factory(:email_signup)
+    @email_address = @email_signup.email_address
   end
 
-  context 'Multiple new EmailSignups' do
-    before :each do
-      @count = EmailSignup.count(:consistent => true)
-      @num = 5
-      @num.times { Factory(:email_signup) }
-    end
+  it 'is correctly found when searched by dynamic finder attribute methods' do
+    EmailSignup.find_by_email_address(@email_address, :consistent => true).should == @email_signup
+    EmailSignup.find_all_by_email_address(@email_address, :consistent => true).first.should == @email_signup
+  end
 
-    it 'is counted correctly' do
-      EmailSignup.count(:consistent => true).should == @count + @num
+  it 'raises errors when dynamic finder attributes are not matched' do
+    expect {
+      EmailSignup.find_by_nonexistent_attribute('hello')
+    }.to raise_error(NoMethodError)
+
+    expect {
+      EmailSignup.do_something_that_doesnt_exist('hello')
+    }.to raise_error(NoMethodError)
+  end
+
+  describe '.count' do
+    context 'when given multiple signups' do
+      before :each do
+        @count = EmailSignup.count(:consistent => true)
+        @num = 5
+        @num.times { Factory(:email_signup) }
+      end
+
+      it 'returns the correct count' do
+        EmailSignup.count(:consistent => true).should == @count + @num
+      end
     end
   end
 
