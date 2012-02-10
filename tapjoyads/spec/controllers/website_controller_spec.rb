0b7a00be217fb13f46a31b08f6e01cac8f24e5a2
Hwan-Joon Choi <hwanjoon@tapjoy.com>
2012-02-10 11:13:20 -0800

    change spec structure a little per code review

--- tapjoyads/spec/controllers/website_controller_spec.rb
+++ tapjoyads/spec/controllers/website_controller_spec.rb
@@ -10,24 +10,36 @@ describe WebsiteController do
     @user_partner  = Factory(:partner, :users => [ @user ])
   end
 
-  context '#find_app' do
+  describe '#find_app' do
     before :each do
       @user_app  = Factory(:app, :partner => @user_partner)
       @admin_app = Factory(:app, :partner => @admin_partner)
     end
 
-    it 'should only return my app' do
-      login_as(@user)
-      @controller.send(:find_app, @user_app.id).should  == @user_app
-      lambda do
-        @controller.send(:find_app, @admin_app.id)
-      end.should raise_exception(ActiveRecord::RecordNotFound)
+    context "when given a regular user" do
+      before :each do
+        login_as(@user)
+      end
+
+      it 'finds my app' do
+        @controller.send(:find_app, @user_app.id).should  == @user_app
+      end
+
+      it 'cannot find other apps' do
+        expect {
+          @controller.send(:find_app, @admin_app.id)
+        }.to raise_exception(ActiveRecord::RecordNotFound)
+      end
     end
 
-    it 'should return any app for admin' do
-      login_as(@admin)
-      @controller.send(:find_app, @user_app.id).should  == @user_app
-      @controller.send(:find_app, @admin_app.id).should == @admin_app
+    context "when given an admin user" do
+      before :each do
+        login_as(@admin)
+      end
+
+      it 'finds any app' do
+        @controller.send(:find_app, @user_app.id).should  == @user_app
+      end
     end
   end
 end
