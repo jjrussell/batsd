15e7107502bd4b2f4ddf0d355b74f7ba339896da
Robert Fletcher <lobatifricha@gmail.com>
2012-02-10 14:56:42 -0800

    de-nesting enable offer request specs

--- tapjoyads/spec/models/enable_offer_request_spec.rb
+++ tapjoyads/spec/models/enable_offer_request_spec.rb
@@ -7,42 +7,40 @@ describe EnableOfferRequest do
     it { should belong_to(:assigned_to) }
   end
 
-  context "EnableOfferRequest" do
-    before :each do
-      @enable_offer_request = Factory(:enable_offer_request)
-      @user = Factory(:user)
-      @account_mgr = Factory(:account_mgr_user)
-    end
+  before :each do
+    @enable_offer_request = Factory(:enable_offer_request)
+    @user = Factory(:user)
+    @account_mgr = Factory(:account_mgr_user)
+  end
 
-    it "allows account managers to be assignees" do
-      @enable_offer_request.assigned_to = @account_mgr
+  it "allows account managers to be assignees" do
+    @enable_offer_request.assigned_to = @account_mgr
+    @enable_offer_request.should be_valid
+  end
+
+  it "does not allow non-account managers to be assignees" do
+    @enable_offer_request.assigned_to = @user
+    @enable_offer_request.should_not be_valid
+  end
+
+  it "allows status between 0 and 3" do
+    (0..3).each do |status|
+      @enable_offer_request.status = status
       @enable_offer_request.should be_valid
     end
+    @enable_offer_request.status = -1
+    @enable_offer_request.should_not be_valid
+    @enable_offer_request.status = 4
+    @enable_offer_request.should_not be_valid
+  end
 
-    it "does not allow non-account managers to be assignees" do
-      @enable_offer_request.assigned_to = @user
-      @enable_offer_request.should_not be_valid
-    end
+  it "is not approved with archived apps" do
+    app = @enable_offer_request.offer.item
+    app.hidden = true
+    app.save
 
-    it "allows status between 0 and 3" do
-      (0..3).each do |status|
-        @enable_offer_request.status = status
-        @enable_offer_request.should be_valid
-      end
-      @enable_offer_request.status = -1
-      @enable_offer_request.should_not be_valid
-      @enable_offer_request.status = 4
-      @enable_offer_request.should_not be_valid
-    end
-
-    it "is not approved with archived apps" do
-      app = @enable_offer_request.offer.item
-      app.hidden = true
-      app.save
-
-      @enable_offer_request.reload
-      @enable_offer_request.status = EnableOfferRequest::STATUS_APPROVED
-      @enable_offer_request.should_not be_valid
-    end
+    @enable_offer_request.reload
+    @enable_offer_request.status = EnableOfferRequest::STATUS_APPROVED
+    @enable_offer_request.should_not be_valid
   end
 end
