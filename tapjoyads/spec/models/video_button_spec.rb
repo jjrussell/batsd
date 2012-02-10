3c039ff4ed8afffbc70b54b0f355d950b63b2a0b
Robert Fletcher <lobatifricha@gmail.com>
2012-02-10 15:43:52 -0800

    de-nesting video button specs

--- tapjoyads/spec/models/video_button_spec.rb
+++ tapjoyads/spec/models/video_button_spec.rb
@@ -13,24 +13,23 @@ describe VideoButton do
     it { should validate_numericality_of :ordinal }
   end
 
-  context "A Video button" do
-    before :each do
-      @video_button = Factory(:video_button)
-    end
+  before :each do
+    @video_button = Factory(:video_button)
+  end
 
-    it "is by default enabled" do
-      @video_button.enabled.should == true
-    end
+  it "is by default enabled" do
+    @video_button.enabled.should == true
+  end
 
-    it "updates third party data on offer based on video button" do
-      video_offer = VideoOffer.find(@video_button.video_offer_id)
-      offer = video_offer.primary_offer
+  it "updates third party data on offer based on video button" do
+    video_offer = VideoOffer.find(@video_button.video_offer_id)
+    offer = video_offer.primary_offer
 
+    @video_button.xml_for_offer.should == offer.third_party_data
+
+    video_offer.offers.each do |offer|
       @video_button.xml_for_offer.should == offer.third_party_data
-
-      video_offer.offers.each do |offer|
-        @video_button.xml_for_offer.should == offer.third_party_data
-      end
     end
   end
+
 end
