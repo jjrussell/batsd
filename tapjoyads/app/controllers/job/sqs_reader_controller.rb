9b4dbf1d8272b02f8cbe0c4e3be707153769a21b
Ryan Johns <ryan@tapjoy.com>
2012-02-13 20:15:47 -0800

    add @short_queue_name variable and use in instead of splitting the queue url

--- tapjoyads/app/controllers/job/sqs_reader_controller.rb
+++ tapjoyads/app/controllers/job/sqs_reader_controller.rb
@@ -2,6 +2,7 @@ class Job::SqsReaderController < Job::JobController
 
   def initialize(queue_name)
     @queue_name           = queue_name
+    @short_queue_name     = @queue_name.split('/').last
     @num_reads            = 100
     @raise_on_error       = true
     @break_on_nil_message = true
@@ -21,7 +22,7 @@ class Job::SqsReaderController < Job::JobController
         @break_on_nil_message ? break : next
       end
 
-      Rails.logger.info "#{@queue_name} message received: #{message.body}"
+      Rails.logger.info "#{@short_queue_name} message received: #{message.body}"
 
       begin
         Mc.cache.add(get_memcache_lock_key(message.body), 'locked', visibility)
@@ -33,7 +34,7 @@ class Job::SqsReaderController < Job::JobController
       begin
         on_message(message)
       rescue Exception => e
-        Rails.logger.error("SqsReaderError (#{@queue_name.split('/').last}): #{e.class} - #{e.message}, message: #{message.body}")
+        Rails.logger.error("SqsReaderError (#{@short_queue_name}): #{e.class} - #{e.message}, message: #{message.body}")
         if @raise_on_error
           NewRelic::Agent.add_custom_parameters(split_message_into_params(message.body))
           raise e
@@ -58,7 +59,7 @@ class Job::SqsReaderController < Job::JobController
   private
 
   def get_memcache_lock_key(message_body)
-    "sqslocks.#{@queue_name.hash}.#{message_body.hash}.#{Time.now.to_i / 5.minutes}"
+    "sqslocks.#{@short_queue_name}.#{message_body.hash}.#{Time.now.to_i / 5.minutes}"
   end
 
   # NewRelic truncates parameter length to ~250 chars so split the message up
