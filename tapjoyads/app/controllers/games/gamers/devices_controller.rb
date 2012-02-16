7b6e56c71c578da8b2949e0f11feb0944fb54f01
Jia Feng <jia.feng@tapjoy.com>
2012-02-15 16:26:36 -0800

    minor fix

--- tapjoyads/app/controllers/games/gamers/devices_controller.rb
+++ tapjoyads/app/controllers/games/gamers/devices_controller.rb
@@ -70,7 +70,7 @@ class Games::Gamers::DevicesController < GamesController
         if current_gamer.referrer.present? && !current_gamer.referrer.starts_with?('tjreferrer:')
           devices = GamerDevice.find_all_by_device_id(data[:udid])
           if devices.size == 1 && devices[0].gamer_id == current_gamer.id
-            gamer_or_invitation_id, advertiser_app_id = ObjectEncryptor.decrypt(current_gamer.referrer).split(',')
+            invitation_id, advertiser_app_id = ObjectEncryptor.decrypt(current_gamer.referrer).split(',')
             advertiser_app_id = TAPJOY_GAMES_INVITATION_OFFER_ID if advertiser_app_id.blank?
             referred_by_gamer = Gamer.find_by_id(current_gamer.referred_by)
             if advertiser_app_id && referred_by_gamer
