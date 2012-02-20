a3961bd72d1facd39d228f01a79fbf5c9dc44419
Van Pham <van.pham@yahoo.com>
2012-02-19 22:39:01 -0800

    progress checkin

--- tapjoyads/public/javascripts/marketplace/src/main.js
+++ tapjoyads/public/javascripts/marketplace/src/main.js
@@ -10,5 +10,23 @@ $(document).ready(function() {
       $('.menu-dropdown').addClass('open');
     }
   });
+  
+  $('.device-toggle').bind('click', function(){
+    if ($(this).hasClass('up')) {
+      $(this).removeClass('up').addClass('down');
+    }
+    else {
+      $(this).removeClass('down').addClass('up');
+    }
+  });
+  
+  $('.icon img').each(function(n,o){
+    $(o).attr("src", $(o).attr("source")).load(function(){
+      $(this).fadeIn('slow');
+    });
+    $(o).error(function(){
+      $(o).attr("src", "data:image/gif;base64,R0lGODlhAQABAID/AMDAwAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==");
+    });
+  });
 
 });
