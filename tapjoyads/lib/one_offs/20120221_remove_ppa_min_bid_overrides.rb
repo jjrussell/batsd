fc1be2805f83989e6b041cd63b02df9d09e719ad
Amir Manji <amanji75@gmail.com>
2012-02-21 15:43:07 -0800

    fix method name

--- tapjoyads/lib/one_offs/20120221_remove_ppa_min_bid_overrides.rb
+++ tapjoyads/lib/one_offs/20120221_remove_ppa_min_bid_overrides.rb
@@ -1,5 +1,5 @@
 class OneOffs
-  def self.remove_featured_min_bid_overrides
+  def self.remove_ppa_min_bid_overrides
     Offer.connection.execute("update offers as temp inner join action_offers on temp.item_id = action_offers.id inner join apps on action_offers.app_id = apps.id set temp.min_bid_override = null where apps.platform = 'iphone' and temp.min_bid_override < 35 and temp.min_bid_override >= 10")
     Offer.connection.execute("update offers as temp inner join action_offers on temp.item_id = action_offers.id inner join apps on action_offers.app_id = apps.id set temp.min_bid_override = null where apps.platform = 'android' and temp.min_bid_override < 25 and temp.min_bid_override >= 10")
   end
