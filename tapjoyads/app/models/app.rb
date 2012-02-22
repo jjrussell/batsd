d1e42c48e89c29322d815a68153dfe04e35a57f2
Norman Chan <norman.chan@tapjoy.com>
2012-02-22 09:32:29 -0800

    add papaya_user_count to list of columns in App to be ignored

--- tapjoyads/app/models/app.rb
+++ tapjoyads/app/models/app.rb
@@ -110,7 +110,7 @@ class App < ActiveRecord::Base
     :to => :primary_app_metadata, :allow_nil => true
 
   # TODO: remove these columns from apps table definition and remove this method
-  TO_BE_DELETED = %w(description price store_id age_rating file_size_bytes supported_devices released_at user_rating categories)
+  TO_BE_DELETED = %w(description price store_id age_rating file_size_bytes supported_devices released_at user_rating categories papaya_user_count)
   def self.columns
     super.reject do |c|
       TO_BE_DELETED.include?(c.name)
