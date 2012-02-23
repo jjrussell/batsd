b5f450a1fd632b7c4d23638148cbadf130275cb4
Brian Sanders <brian.sanders@tapjoy.com>
2012-02-23 16:18:33 -0500

    newline fixes

--- tapjoyads/spec/views/tools/users/index.html.haml_spec.rb
+++ tapjoyads/spec/views/tools/users/index.html.haml_spec.rb
@@ -1,18 +1,18 @@
 require 'spec/spec_helper'
 
 describe 'tools/users/index.html.haml' do
-  
+
   context 'with a role manager user' do
     before :each do
       @user = Factory :role_mgr_user
       assigns[:tapjoy_users] = [@user]
       render
     end
-    
+
     it 'displays the link to edit a user' do
       response.should have_tag("a[href=?]", tools_user_path(@user))
     end
-    
+
     it 'displays the roles assigned to that user' do
       response.should have_tag("li.role_mgr")
     end
