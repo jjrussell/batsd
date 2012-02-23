63e4b70bdacda5135e19811b40dff3301d742f35
Neal Wiggins <neal.wiggins@tapjoy.com>
2012-02-23 13:24:18 -0800

    Add clause to ensure we aren't approving based on transfers.

--- tapjoyads/lib/one_offs/20120223_set_valid_partners_to_confirm.rb
+++ tapjoyads/lib/one_offs/20120223_set_valid_partners_to_confirm.rb
@@ -1,5 +1,5 @@
 class OneOffs
   def self.set_valid_partners_to_confirm
-    Partner.connection.execute('update partners as p inner join payouts ps on p.id = ps.partner_id set p.confirmed_for_payout = 1 where ps.status = 1 and ps.month = 1 and ps.year = 2012');
+    Partner.connection.execute('update partners as p inner join payouts ps on p.id = ps.partner_id set p.confirmed_for_payout = 1 where ps.status = 1 and ps.month = 1 and ps.year = 2012 and ps.payment_method = 1');
   end
 end
