0ceebda2330660ca68647ed8ca67c14438140f63
Hwan-Joon Choi <hwanjoon@tapjoy.com>
2012-02-24 11:16:50 -0800

    UserRole#admin?

--- tapjoyads/app/models/role_assignment.rb
+++ tapjoyads/app/models/role_assignment.rb
@@ -7,7 +7,7 @@ class RoleAssignment < ActiveRecord::Base
   validates_presence_of :user, :user_role
   validates_uniqueness_of :user_id, :scope => [ :user_role_id ]
 
-  delegate :name, :to => :user_role
+  delegate :admin?, :name, :to => :user_role
 
   def <=>(other)
     name <=> other.name
