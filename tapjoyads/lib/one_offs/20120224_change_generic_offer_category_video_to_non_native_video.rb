d031c0c88e3788ac70bd165ca4d204112388e8b7
Jeff Dickey <me@jeffdickey.info>
2012-02-24 10:58:06 -0800

    made migration a one-off

--- tapjoyads/lib/one_offs/20120224_change_generic_offer_category_video_to_non_native_video.rb
+++ tapjoyads/lib/one_offs/20120224_change_generic_offer_category_video_to_non_native_video.rb
@@ -1,14 +1,10 @@
-class ChangeGenericOfferCategoryVideoToNonNativeVideo < ActiveRecord::Migration
-  def self.up
+class OneOffs
+  def self.change_generic_offer_category_to_non_native_video
     GenericOffer.all.each do |o|
       o.category = 'Non-Native Video' if o.category == 'Video'
       o.category = nil if o.category.blank?
       o.category = 'Non-Native Video' if o.url =~ /youtube\.com/
       o.save(false)
     end
-  end
-
-  def self.down
-    GenericOffer.update_all({:category => 'Video'}, {:category => 'Non-Native Video'})
   end
 end
