056cdf92821649c8b0fd8a549db3857ad77ca9e9
Peter Holiday <pholiday@ph-tj-mbp>
2012-02-24 13:34:50 -0500

    Minor tweaks to the one-off to exclude one-cent offers and look less like a PHP dev wrote it.

--- tapjoyads/lib/one_offs/20120224_fix_discounts.rb
+++ tapjoyads/lib/one_offs/20120224_fix_discounts.rb
@@ -1,12 +1,11 @@
 class OneOffs
 	def self.fix_offer_discounts
-    count = 0
+		bad_offers = Offer.find(:all, :include => :partner, :conditions => "partners.id = partner_id AND bid > 1 AND premier_discount > 0 AND payment != floor(bid*((100-partners.premier_discount)/100))")
 
-		Offer.find(:all, :include => :partner, :conditions => "partners.id = partner_id AND premier_discount > 0 AND payment != floor(bid*((100-partners.premier_discount)/100))").each { |o|      
+    bad_offers.each { |o|      
       o.update_payment!
-      count += 1
     }
 
-    "Offers fixed: " + count.to_s
+    "Offers fixed: " << bad_offers.count.to_s
 	end
 end
