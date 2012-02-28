56ef0bc6d30d886284827b947e459f85e2c6c5b0
Stephen McCarthy <sjmccarthy@gmail.com>
2012-02-28 11:37:06 -0800

    save activity log for all affected offers

--- tapjoyads/lib/one_offs/20120228_fix_device_types.rb
+++ tapjoyads/lib/one_offs/20120228_fix_device_types.rb
@@ -13,11 +13,23 @@ class OneOffs
 
       offer = Offer.find_by_id(activity_log.object_id)
       next if offer.nil?
+
+      new_activity_log                  = ActivityLog.new({ :load => false })
+      new_activity_log.request_id       = UUIDTools::UUID.random_create.to_s
+      new_activity_log.user             = 'script'
+      new_activity_log.controller       = 'one_off'
+      new_activity_log.action           = 'fix_device_types'
+      new_activity_log.object           = offer
+      new_activity_log.included_methods = []
+
       offer.device_types = activity_log.after_state['device_types']
       if offer.changed?
         puts "Offer #{offer.id} has been fixed from #{offer.device_types_was} to #{offer.device_types}"
         offer.save!
         affected_offers[offer.id] = offer
+
+        new_activity_log.finalize_states
+        new_activity_log.save
       end
 
       offers[offer.id] = true
