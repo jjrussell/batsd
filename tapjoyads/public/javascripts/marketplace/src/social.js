cb02c23f2524e03a3115e426144b054b4b9a0b38
Hwan-Joon Choi <hwanjoon@tapjoy.com>
2012-02-29 09:51:29 -0800

    Code review: TJG -> Tapjoy

--- tapjoyads/public/javascripts/marketplace/src/social.js
+++ tapjoyads/public/javascripts/marketplace/src/social.js
@@ -1,74 +1,71 @@
-(function(TJG) {
+(function(Tap) {
   var _t = window.i18n.t;
-  TJG.social = {
-    doFbLogin : function(connect_acct_path){
-      var scope = 'offline_access,publish_stream';
-      FB.login(function(response, scope) {
-        if (response.authResponse) {
-          FB.api('/me', function(response) {
-            <!--
-            window.location = connect_acct_path;
-            //-->
-          });
-        } else {
-          showError(_t('games.grant_us_access'));
-        }
-      }, {scope: scope});
+  Tap.extend({
+    Social: {
+      doFbLogin: function(connect_acct_path) {
+        FB.login(function(response) {
+          if (response.authResponse) {
+            FB.api('/me', function(response) {
+              <!--
+              window.location = connect_acct_path;
+              //-->
+            });
+          } else {
+            var msg = [
+              '<div id="flash_error" class="dialog_wrapper hide" style="top: 179px; left: 533px; display: block;">',
+              '<div class="close_dialog">',
+              '<div class="close_button"></div>',
+              '</div>',
+              '<div class="dialog">',
+              '<div class="dialog_content">',
+              '<div class="error">', _t('games.grant_us_access'), '</div>',
+              '</div></div></div>',
+            ].join('');
+            $('body').append(msg);
 
-      var showError = function(error){
-        var msg = [
-          '<div id="flash_error" class="dialog_wrapper hide" style="top: 179px; left: 533px; display: block;">',
-          '<div class="close_dialog">',
-          '<div class="close_button"></div>',
-          '</div>',
-          '<div class="dialog">',
-          '<div class="dialog_content">',
-          '<div class="error">', error ,'</div>',
-          '</div></div></div>',
-        ].join('');
-        $('body').append(msg);
+            $(".close_button").click(function(event) {
+              $("#flash_error").fadeOut();
+              $("#flash_error").remove();
+            });
+          }
+        }, {scope: 'offline_access,publish_stream'});
+      },
 
-        $(".close_button").click(function(event) {
-          $("#flash_error").fadeOut();
-          $("#flash_error").remove();
+      doFbLogout: function(){
+        FB.getLoginStatus(function(response) {
+          if (response.authResponse) {
+            FB.logout(function(response) {
+            });
+          }
         });
-      };
-    },
+      },
 
-    doFbLogout : function(){
-      FB.getLoginStatus(function(response) {
-        if (response.authResponse) {
-          FB.logout(function(response) {
-          });
-        }
-      });
-    },
+      checkAndPost: function(currentGamerFbId, link, pictureLink) {
+        FB.getLoginStatus(function(response) {
+          var postToFeed = function() { TJG.social.postToFeed(link, pictureLink); };
+          var currentLoginFbId = response.authResponse && response.authResponse.userID;
+          if (currentLoginFbId && currentGamerFbId && currentGamerFbId != currentLoginFbId) {
+            FB.logout(postToFeed);
+          } else {
+            postToFeed();
+          }
+        });
+      },
 
-    checkAndPost : function(currentGamerFbId, link, pictureLink) {
-      FB.getLoginStatus(function(response) {
-        var postToFeed = function() { TJG.social.postToFeed(link, pictureLink); };
-        var currentLoginFbId = response.authResponse && response.authResponse.userID;
-        if (currentLoginFbId && currentGamerFbId && currentGamerFbId != currentLoginFbId) {
-          FB.logout(postToFeed);
-        } else {
-          postToFeed();
-        }
-      });
-    },
+      postToFeed: function(link, pictureLink) {
+        var obj = {
+          method: 'feed',
+          display: 'popup',
+          name: 'Tapjoy',
+          link: link,
+          picture: pictureLink,
+          caption: ' ',
+          actions: [{ name: _t('shared.join'), link: link}],
+          description: _t('games.post_to_facebook_content')
+        };
 
-    postToFeed : function(link, pictureLink) {
-      var obj = {
-        method: 'feed',
-        display: 'popup',
-        name: 'Tapjoy',
-        link: link,
-        picture: pictureLink,
-        caption: ' ',
-        actions: [{ name: _t('shared.join'), link: link}],
-        description: _t('games.post_to_facebook_content')
-      };
-
-      FB.ui(obj);
-    },
-  };
-}(TJG));
+        FB.ui(obj);
+      },
+    }
+  });
+}(Tapjoy));
