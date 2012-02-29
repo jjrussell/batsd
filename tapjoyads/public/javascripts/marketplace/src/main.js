e83c93308671f124ece8c40e449544c73a6dd62d
Van Pham <van.pham@yahoo.com>
2012-02-28 16:06:10 -0800

    merge

--- tapjoyads/public/javascripts/marketplace/src/main.js
+++ tapjoyads/public/javascripts/marketplace/src/main.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
 
   var _t = window.i18n.t,
-    debounce;
+      debounce,
+			tjmSelectMenu = $('#recommendSelectMenu'),
+			tjmSelectContainer = $('#recommendSelect').parent().closest('.select-container'),
+			selectTrigger = $('#recommendSelect');
 
   // Login Modal
   $('#login, #login-web').bind('click', function() {
@@ -120,7 +123,7 @@ $(document).ready(function() {
     }
   });
 
-  $('.list-button, .app .btn').bind(Tapjoy.EventsMap.start + ' ' + Tapjoy.EventsMap.end, function(e){
+  $('.list-button, .btn').bind(Tapjoy.EventsMap.start + ' ' + Tapjoy.EventsMap.end, function(e){
     var el = $(this),
         which = e.type;
 
@@ -213,6 +216,50 @@ $(document).ready(function() {
     check_valid();
   });
 
+  
+  selectTrigger.bind(Tapjoy.EventsMap.start, function(){
+		var el = $(this),
+				heading = $('.heading', tjmSelectContainer),
+		    fix = $('.fix', tjmSelectContainer);
+				
+	  if(tjmSelectContainer.hasClass('active')){
+      Tapjoy.Utils.removeMask();
+			
+			tjmSelectContainer.removeClass('active');
+      
+			tjmSelectMenu.addClass('hide');
+      
+			heading.text($('li.active', tjmSelectMenu).text());
+						
+		}else{
+			Tapjoy.Utils.mask();
+			
+      tjmSelectContainer.addClass('active');
+			tjmSelectMenu.removeClass('hide');
+
+			heading.text('Choose a Section');
+
+			tjmSelectMenu.css('top', tjmSelectContainer.offset().top + (tjmSelectContainer.outerHeight(true) - 4) + 'px');
+			
+			fix.css({
+				width: tjmSelectContainer.width() - 4 + 'px'
+			});
+		}
+	});
+	
+	$('li', tjmSelectMenu).each(function(){
+		var li = $(this);
+		
+		li.bind('click', function(){
+			$('li', tjmSelectMenu).removeClass('active');
+			li.addClass('active');
+      tjmSelectContainer.removeClass('active');
+			tjmSelectMenu.addClass('hide');
+      Tapjoy.Utils.removeMask();
+
+      $('.heading', tjmSelectContainer).text(li.text())			
+		});
+	});
 	/*
 
 
