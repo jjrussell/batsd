972c1a5315660ea3a49adea49a535f9ec5d64581
Michael Wheeler <michael.wheeler@tapjoy.com>
2012-02-29 17:49:47 -0800

    Invites controller, routes, views sorted out

--- tapjoyads/public/javascripts/marketplace/src/social.js
+++ tapjoyads/public/javascripts/marketplace/src/social.js
@@ -1,4 +1,5 @@
-(function(Tap, $) {
+(function (Tap, $) {
+  "use strict";
   var _t = window.i18n.t,
     FB = window.FB;
   Tap.extend({
@@ -11,27 +12,27 @@
               window.location = $$.data("fb-url");
             });
           } else {
-            Tapjoy.Utils.notification({
+            Tap.Utils.notification({
               message: _t('games.grant_us_access')
             });
           }
         }, {scope: 'offline_access,publish_stream'});
       },
 
-      doFbLogout: function(){
-        FB.getLoginStatus(function(response) {
+      doFbLogout: function () {
+        FB.getLoginStatus(function (response) {
           if (response.authResponse) {
-            FB.logout(function(response) {
+            FB.logout(function (response) {
             });
           }
         });
       },
 
-      checkAndPost: function(currentGamerFbId, link, pictureLink) {
-        FB.getLoginStatus(function(response) {
-          var postToFeed = function() { Tapjoy.Social.postToFeed(link, pictureLink); };
-          var currentLoginFbId = response.authResponse && response.authResponse.userID;
-          if (currentLoginFbId && currentGamerFbId && currentGamerFbId != currentLoginFbId) {
+      checkAndPost: function (currentGamerFbId, link, pictureLink) {
+        FB.getLoginStatus(function (response) {
+          var postToFeed = function () { Tap.Social.postToFeed(link, pictureLink); },
+              currentLoginFbId = response.authResponse && response.authResponse.userID;
+          if (currentLoginFbId && currentGamerFbId && currentGamerFbId !== currentLoginFbId) {
             FB.logout(postToFeed);
           } else {
             postToFeed();
@@ -39,7 +40,7 @@
         });
       },
 
-      postToFeed: function(link, pictureLink) {
+      postToFeed: function (link, pictureLink) {
         var obj = {
           method: 'feed',
           display: 'popup',
@@ -58,6 +59,8 @@
 
 
   $(function () {
+    if (!FB) { return; }
+
     $(".login-to-facebook").click(function () {
       Tap.Social.doFbLogin();
     });
@@ -65,5 +68,16 @@
     if (window.location.search.match(/fb_logout/)) {
       Tap.Social.doFbLogout();
     }
+
+    $(".post-to-facebook").click(function () {
+      var $$ = $(this),
+          icon = $$.data("icon"),
+          callback = $$.data("callback"),
+          facebookId = $$.data("fb-id"),
+          res;
+
+      res = facebookId ? Tap.Social.checkAndPost(facebookId, callback, icon) : Tap.Social.postToFeed(callback, icon);
+      return false;
+    });
   });
-}(Tapjoy, jQuery));
+}(window.Tapjoy, window.jQuery));
