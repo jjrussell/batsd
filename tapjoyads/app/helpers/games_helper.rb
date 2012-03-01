0cf3a9e43d8f631f7ecbd99bc331eac7785dbaa8
Hwan-Joon Choi <hwanjoon@tapjoy.com>
2012-02-29 19:33:05 -0800

    we're not using this helper

--- tapjoyads/app/helpers/games_helper.rb
+++
@@ -1,7 +0,0 @@
-module GamesHelper
-  def change_or_add (action)
-    exists = @current_gamer.send("#{action}?")
-
-    exists ? "(#{t('text.games.change')})" : "(#{t('text.games.add')})"
-  end
-end
