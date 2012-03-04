8e4870e927b68aecb0e57d3e58b1d0aa2e2e5f57
Hwan-Joon Choi <hwanjoon@tapjoy.com>
2012-03-03 23:00:22 -0800

    Refactor "hide login"
    I think this makes a little more sense

--- tapjoyads/app/helpers/application_helper.rb
+++ tapjoyads/app/helpers/application_helper.rb
@@ -3,12 +3,4 @@ module ApplicationHelper
   def sdk_link(text, sdk_type)
     concat(link_to(text, popup_sdk_path(:sdk => sdk_type), :rel => 'facebox'))
   end
-
-  def games_hide_login(val)
-    @games_header_hide = val
-  end
-
-  def get_games_hide_login
-    return @gamer_header_hide || {}
-  end
 end
