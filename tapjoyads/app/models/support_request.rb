e11e595192551b6ee2283691ffc3c06d12f42c38
James Logsdon <jlo@tapjoy.com>
2012-03-07 12:48:41 -0500

    Use `?` as placeholder for consistency

--- tapjoyads/app/models/support_request.rb
+++ tapjoyads/app/models/support_request.rb
@@ -30,7 +30,7 @@ class SupportRequest < SimpledbResource
   end
 
   def get_last_click(udid, offer)
-    conditions = ActiveRecord::Base.sanitize_conditions("udid = '%s' and advertiser_app_id = '%s' manually_resolved_at is null", udid, offer.item_id)
+    conditions = ActiveRecord::Base.sanitize_conditions("udid = ? and advertiser_app_id = ? manually_resolved_at is null", udid, offer.item_id)
     clicks = Click.select_all(:conditions => conditions)
     clicks.sort_by { |c| c.clicked_at.to_f }.last
   end
