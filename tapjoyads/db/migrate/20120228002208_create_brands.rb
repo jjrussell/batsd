5970eac68ecb845a3a0a3301c6558194a2a75184
Neal Wiggins <neal.wiggins@tapjoy.com>
2012-03-09 17:41:56 -0800

    Add support for brand allocations on an offer

--- tapjoyads/db/migrate/20120228002208_create_brands.rb
+++ tapjoyads/db/migrate/20120228002208_create_brands.rb
@@ -3,17 +3,18 @@ class CreateBrands < ActiveRecord::Migration
     create_table :brands, :id => false do |t|
       t.guid :id, :null=> false, :length => 36
       t.string :name
-
       t.timestamps
     end
 
     add_index :brands, :id, :unique => true
     add_index :brands, ['name'], :unique => true
 
-    create_table :brand_offer_mappings, :id => false, :force => true do |t|
-      t.column :id, 'char(36) binary', :null => false
-      t.column :offer_id, 'char(36) binary', :null => false
-      t.column :brand_id, 'char(36) binary', :null => false
+    create_table :brand_offer_mappings, :id => false do |t|
+      t.guid :id, :null => false, :length => 36
+      t.guid :offer_id,:null => false, :length => 36
+      t.guid :brand_id, :null => false, :length => 36
+      t.integer :allocation, :null => false
+      t.timestamps
     end
 
     add_index :brand_offer_mappings, :id, :unique => true
