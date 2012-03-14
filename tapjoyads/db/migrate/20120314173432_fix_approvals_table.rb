5dc0048d21f64178072acdc696a37edf8fd01090
James Logsdon <jlo@tapjoy.com>
2012-03-14 15:56:27 -0400

    `change_table` doesn't alter columns. What?

--- tapjoyads/db/migrate/20120314173432_fix_approvals_table.rb
+++ tapjoyads/db/migrate/20120314173432_fix_approvals_table.rb
@@ -1,16 +1,15 @@
 class FixApprovalsTable < ActiveRecord::Migration
   def self.up
-    change_table :approvals do |t|
-      t.text     :object,    :limit => 16777216
-      t.text     :original,  :limit => 16777216
-    end
+    remove_column :approvals, :object
+
+    add_column    :approvals, :object,    :text, :limit => 16777216
+    add_column    :approvals, :original,  :text, :limit => 16777216
   end
 
   def self.down
-    change_table :approvals do |t|
-      t.text     :object
-    end
-
     remove_column :approvals, :original
+    remove_column :approvals, :object
+
+    add_column    :approvals, :object, :text
   end
 end
