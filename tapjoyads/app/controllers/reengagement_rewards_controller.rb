eb944eabfd973c639b53229cd36466b2b878300e
Adam Sumner <adamsumner@gmail.com>
2012-03-15 11:58:26 -0700

    Fixed pluralization in view, got rid of unneeded @currencies, and reduced number of lookups needed to construct view.

--- tapjoyads/app/controllers/reengagement_rewards_controller.rb
+++ tapjoyads/app/controllers/reengagement_rewards_controller.rb
@@ -7,8 +7,7 @@ class ReengagementRewardsController < ApplicationController
     verify_params([:id])
     @reengagement_offer = ReengagementOffer.find(params[:id])
     @app = App.find(@reengagement_offer.app_id) if @reengagement_offer
-    @currencies = @app.currencies if @app
-    @reengagement_offers = @app.reengagement_campaign if @currencies
+    @reengagement_offers = @app.reengagement_campaign if @app
     @button_link = 'javascript: history.back()'
 
     if @reengagement_offers.try(:present?)
@@ -21,12 +20,12 @@ class ReengagementRewardsController < ApplicationController
   def index
     verify_params([:udid, :timestamp, :publisher_user_id, :app_id])
     @app = App.find_in_cache(params[:app_id])
-    @currencies = Currency.find_all_in_cache_by_app_id(params[:app_id]) if @app
-    @reengagement_offer = ReengagementOffer.resolve(@app, @currencies, params, geoip_data) if @currencies
+    currencies = Currency.find_all_in_cache_by_app_id(params[:app_id]) if @app
+    @reengagement_offer = ReengagementOffer.resolve(@app, currencies, params, geoip_data) if currencies
     @reengagement_offers = @app.reengagement_campaign_from_cache if @reengagement_offer
     @button_link = 'http://ok'
 
-    render :nothing => true, :status => 204 and return unless @app.try(:reengagement_campaign_enabled?) && @reengagement_offers
+    render :nothing => true, :status => 204 and return unless @reengagement_offers && @app.reengagement_campaign_enabled?
   end
 
 end
