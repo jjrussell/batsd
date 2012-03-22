4609411dc5298a16b5856ec5468969ba4b2f337a
Michael Wheeler <michael.wheeler@tapjoy.com>
2012-03-22 12:26:13 -0700

    Editing javascript file so that it only delivers shared and games translations; ignore everything else

--- tapjoyads/config/initializers/available_locales.rb
+++ tapjoyads/config/initializers/available_locales.rb
@@ -5,13 +5,12 @@ AVAILABLE_LOCALES_ARRAY = I18n.backend.send(:translations).keys.collect(&:to_s)
 AVAILABLE_LOCALES = Set.new(AVAILABLE_LOCALES_ARRAY)
 
 AVAILABLE_LOCALES_HASHES = {}
-
 AVAILABLE_LOCALES_ARRAY.each do |locale|
   path = "config/locales/#{locale}.yml"
 
   raise "Missing localization file for: #{locale}. Expected to find: #{path}" unless File.exists? path
 
   File.open(path) do |f|
-    AVAILABLE_LOCALES_HASHES[locale] = f.mtime.to_i
+    AVAILABLE_LOCALES_HASHES[locale] = "locale-#{f.mtime.to_i}"
   end
 end
