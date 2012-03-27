594e30b9cb41cb602f242128e5b6febe32fa50f8
Eric Tipton <eric.tipton@tapjoy.com>
2012-03-27 11:02:21 -0400

    third party tracking url integration for display ads

--- tapjoyads/spec/controllers/display_ad_controller_spec.rb
+++ tapjoyads/spec/controllers/display_ad_controller_spec.rb
@@ -5,6 +5,8 @@ def read_asset(name, directory='banner_ads')
 end
 
 describe DisplayAdController do
+  include ActionView::Helpers
+
   integrate_views
   before :each do
     fake_the_web
@@ -56,10 +58,6 @@ describe DisplayAdController do
           get(:image, @params)
 
           response.content_type.should == 'image/png'
-
-          # To diagnose a mismatch, uncomment the following and compare the new image to #{Rails.root}/spec/assets/banner_ads/custom_320x50.png
-          # File.open("#{Rails.root}/spec/assets/banner_ads/wtf.png", 'w') { |f| f.write(response.body) }
-
           response.body.should == @custom_banner
         end
       end
@@ -96,15 +94,6 @@ describe DisplayAdController do
         it 'returns proper image' do
           get(:image, @params)
           response.content_type.should == 'image/png'
-
-          # Uncomment the following to re-generate the image if needed (e.g. background image changes, text changes, etc)
-          # File.open("#{Rails.root}/spec/assets/banner_ads/generated_320x50.png", 'w') { |f| f.write(response.body) }
-
-          # To diagnose a mismatch, uncomment the following and compare the new image to #{Rails.root}/spec/assets/banner_ads/generated_320x50.png
-          # File.open("#{Rails.root}/spec/assets/banner_ads/wtf.png", 'w') { |f| f.write(response.body) }
-
-          ### The test seems to be failing due to different versions of ImageMagick / different fonts on other developer machines ###
-          # response.body.should == @generated_banner
         end
       end
     end
@@ -127,10 +116,6 @@ describe DisplayAdController do
           get(:index, @params.merge(:format => 'json'))
 
           response.content_type.should == 'application/json'
-
-          # To diagnose a mismatch, uncomment the following and compare the new image to #{Rails.root}/spec/assets/banner_ads/custom_320x50.png
-          # File.open("#{Rails.root}/spec/assets/banner_ads/wtf.png", 'w') { |f| f.write(response.body) }
-
           Base64.decode64(assigns['image']).should == custom_banner
         end
 
@@ -143,10 +128,6 @@ describe DisplayAdController do
 
           get(:index, @params)
           response.content_type.should == 'application/xml'
-
-          # To diagnose a mismatch, uncomment the following and compare the new image to #{Rails.root}/spec/assets/banner_ads/custom_640x100.png
-          # File.open("#{Rails.root}/spec/assets/banner_ads/wtf.png", 'w') { |f| f.write(response.body) }
-
           Base64.decode64(assigns['image']).should == custom_banner
         end
       end
@@ -182,15 +163,6 @@ describe DisplayAdController do
 
           get(:index, @params.merge(:format => 'json'))
           response.content_type.should == 'application/json'
-
-          # Uncomment the following to re-generate the image if needed (e.g. background image changes, text changes, etc)
-          # File.open("#{Rails.root}/spec/assets/banner_ads/generated_320x50.png", 'w') { |f| f.write(Base64.decode64(assigns['image'])) }
-
-          # To diagnose a mismatch, uncomment the following and compare the new image to #{Rails.root}/spec/assets/banner_ads/generated_320x50.png
-          # File.open("#{Rails.root}/spec/assets/banner_ads/wtf.png", 'w') { |f| f.write(response.body) }
-
-          ### The test seems to be failing due to different versions of ImageMagick / different fonts on other developer machines ###
-          # Base64.decode64(assigns['image']).should == File.read("#{Rails.root}/spec/assets/banner_ads/generated_320x50.png")
         end
 
         it 'returns proper image data in xml' do
@@ -201,25 +173,27 @@ describe DisplayAdController do
 
           get(:index, @params)
           response.content_type.should == 'application/xml'
-
-          # Uncomment the following to re-generate the image if needed (e.g. background image changes, text changes, etc)
-          # File.open("#{Rails.root}/spec/assets/banner_ads/generated_640x100.png", 'w') { |f| f.write(Base64.decode64(assigns['image'])) }
-
-          # To diagnose a mismatch, uncomment the following and compare the new image to #{Rails.root}/spec/assets/banner_ads/generated_640x100.png
-          # File.open("#{Rails.root}/spec/assets/banner_ads/wtf.png", 'w') { |f| f.write(response.body) }
-
-          ### The test seems to be failing due to different versions of ImageMagick / different fonts on other developer machines ###
-          # Base64.decode64(assigns['image']).should == File.read("#{Rails.root}/spec/assets/banner_ads/generated_640x100.png")
         end
       end
     end
 
     describe '#webview' do
+      context 'with third party tracking URLs' do
+        it 'should generate hidden image tags' do
+          url = "https://dummyurl.com"
+          @offer.third_party_tracking_urls = [url]
+          @offer.save!
+
+          get(:webview, @params)
+          response.body.should match image_tag(url, :style => 'display:none;')
+        end
+      end
+
       context 'with custom ad' do
         before :each do
-           @offer.banner_creatives = %w(320x50)
-           @offer.approved_banner_creatives = %w(320x50)
-           @offer.rewarded = false
+          @offer.banner_creatives = %w(320x50)
+          @offer.approved_banner_creatives = %w(320x50)
+          @offer.rewarded = false
         end
 
         it 'contains proper image link' do
