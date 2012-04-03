75bf13eb7a3d74a5c5237f9dc031b37b4b61c9e9
Brian Sanders <brian.sanders@tapjoy.com>
2012-04-03 12:26:34 -0400

    Fixes to make OneOff idempotent(-ish)

--- tapjoyads/lib/one_offs/20120321_backfill_deeplink_offers.rb
+++ tapjoyads/lib/one_offs/20120321_backfill_deeplink_offers.rb
@@ -1,19 +1,31 @@
 class OneOffs
   def self.backfill_deeplink_offers
     #find and disable the manually-created deeplinks
-    handmade = GenericOffer.all.find { |go| go.url =~ /earn/ && go.name =~ /^Earn/ }
+    handmade = GenericOffer.find(:all).find_all { |go| go.url =~ /earn/ && go.name =~ /^Earn/ && go.primary_offer.tapjoy_enabled? }
     puts "Disabling #{handmade.count} manually-created deeplinks"
-    handmade.each { |go|
-      go.primary_offer.tapjoy_enabled = false
-      go.primary_offer.save!
-    }
+    handmade.each do |go|
+      begin
+        go.primary_offer.tapjoy_enabled = false
+        go.primary_offer.save!
+      rescue Exception
+        nil
+      end
+    end
 
     #create the new ones
+    puts "Creating new DeeplinkOffers..."
     count = 0
     Currency.all.each do |c|
-      dl = DeeplinkOffer.new(:currency => c, :app => c.app, :partner => c.partner)
-      dl.save!
-      count += 1
+      existing = DeeplinkOffer.find_by_currency_id(c.id)
+      unless existing
+        begin
+          dl = DeeplinkOffer.new(:currency => c, :app => c.app, :partner => c.partner)
+          dl.save!
+          count += 1
+        rescue Exception => e
+          puts "Failed to create DeeplinkOffer for currency #{c.id}: #{e}"
+        end
+      end
     end
     puts "Created #{count} deeplink offers"
   end
