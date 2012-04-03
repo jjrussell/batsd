11de2d6071be293f1aa01a999bfba2428427b6b6
Brian Stebar II <brian.stebar@tapjoy.com>
2012-04-03 16:50:50 -0400

    Progress on specs

--- tapjoyads/spec/controllers/games/support_requests_controller_spec.rb
+++ tapjoyads/spec/controllers/games/support_requests_controller_spec.rb
@@ -1,22 +1,52 @@
-require 'spec_helper'
+require 'spec/spec_helper'
 
-describe Games::SupportController do
+describe Games::SupportRequestsController do
   before :each do
     activate_authlogic
+    fake_the_web
 
-    gamer = Factory(:gamer)
-    gamer.gamer_profile = GamerProfile.create(:facebook_id => '0', :gamer => gamer)
-    login_as(gamer)
+    @device = Factory(:device)
+
+    @click1 = Factory(:click,           :udid => @device.key,
+                                        :clicked_at => 1.hour.ago)
+    @click2 = Factory(:click,           :udid => @device.key,
+                                        :clicked_at => 1.hour.ago)
+    @click1_dupe_old = Factory(:click,  :udid => @device.key,
+                                        :currency_id => @click1.currency_id,
+                                        :advertiser_app_id => @click1.advertiser_app_id,
+                                        :publisher_app_id => @click1.publisher_app_id,
+                                        :offer_id => @click1.offer_id,
+                                        :clicked_at => 2.hours.ago)
+
+    results = [ @click1, @click1_dupe_old, @click2 ]
+    Click.stubs(:select_all).returns(results)
+
+    @gamer = Factory(:gamer)
+    @gamer.gamer_profile = GamerProfile.create(:facebook_id => '0', :gamer => @gamer)
+    @gamer.devices << GamerDevice.new(:device => @device)
+
+    login_as(@gamer)
   end
 
   describe '#create' do
-    context 'without supplying a message' do
-      it "fails validation if no message is provided"
-
-      it "redirects to back to #new if no message is provided"
-    end
+    # context 'without supplying a message' do
+    #   before :each do
+    #     @params = { :type => 'contact_support' }
+    #     @params['support_requests'] = { :content => "" }
+    #   end
+    #
+    #   it "re-renders #new if no message is provided" do
+    #     post :create, @params
+    #     response.should render_template("new")
+    #   end
+    # end
 
     context 'with a message' do
+      before :each do
+        @params = { :type => 'contact_support' }
+        @params['support_requests'] = { :content => "I'm needy and need help!" }
+      end
+
       context 'with a click selected' do
 
       end
@@ -25,13 +55,44 @@ describe Games::SupportController do
 
       end
 
-      it 'creates a SupportRequest'
+      it 'creates a SupportRequest' do
+        puts "Params: #{@params.inspect}"
+        #post :create, @params
+        support_request = SupportRequest.select(:where =>"udid = '#{@device.key}'")
+        puts "Support Request: #{support_request.inspect}"
+      end
     end
   end
 
-  describe '#new' do
-    context 'when format is set to JS' do
-      it 'gathers the correct values for @unresolved_clicks'
-    end
-  end
+  # describe '#new' do
+  #   before :each do
+  #     @params = { :udid => @device.key }
+  #   end
+  #
+  #   context 'when format is set to JS' do
+  #     before :each do
+  #       @params[:format] = 'js'
+  #     end
+  #
+  #     context 'the @unresolved_clicks collection' do
+  #       it 'includes both "correct" clicks' do
+  #         get :new, @params
+  #         assigns(:unresolved_clicks).should include @click1
+  #         assigns(:unresolved_clicks).should include @click2
+  #       end
+  #
+  #       it "doesn't include the older duplicate click" do
+  #         get :new, @params
+  #         assigns(:unresolved_clicks).should_not include @click1_dupe_old
+  #       end
+  #     end
+  #   end
+  #
+  #   context 'when format is not set to JS' do
+  #     it 'finds the current gamer' do
+  #       get :new, @params
+  #       assigns(:current_gamer).should == @gamer
+  #     end
+  #   end
+  # end
 end
