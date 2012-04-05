a8c84ecdcf863e89245373337fac33660d5b1aba
Eric Tipton <eric.tipton@tapjoy.com>
2012-04-04 22:17:02 -0400

    spec for queue_third_party_tracking_requests method

--- tapjoyads/lib/extensions/rails.rb
+++ tapjoyads/lib/extensions/rails.rb
@@ -1,3 +1,64 @@
+#
+# This allows us to do conditional routing based on the requested host.
+#
+# example usage:
+# map.with_options :conditions => { :hosts => [ 'example.com' ] } do |limited_route|
+#   limited_route.resources :some_resource
+# end
+#
+module ActionController
+  module Routing
+
+    class RouteSet
+      alias_method :orig_extract_request_environment, :extract_request_environment
+
+      def extract_request_environment(request)
+        orig_extract_request_environment(request).merge({ :host => request.host })
+      end
+    end
+
+    class Route
+      alias_method :orig_recognition_conditions, :recognition_conditions
+
+      def recognition_conditions
+        result = orig_recognition_conditions
+        result << "conditions[:hosts].include?(env[:host])" if conditions[:hosts] && Rails.env.production?
+        result
+      end
+    end
+
+  end
+end
+
+#
+# This adds a _with_time logging method for each log severity level.
+# It will time how long it takes to execute a block and append the
+# number of seconds to the log message.
+#
+# example usage:
+# Rails.logger.info_with_time('some message') do
+#   method_that_takes_25_seconds
+# end
+#
+# resulting log output: "some message (25s)"
+#
+module ActiveSupport
+  class BufferedLogger
+
+    for severity in Severity.constants
+      class_eval <<-EOT, __FILE__, __LINE__
+        def #{severity.downcase}_with_time(message)                 # def debug_with_time(message)
+          start_time = Time.zone.now                                #   start_time = Time.zone.now
+          yield                                                     #   yield
+          message += ' (' + (Time.zone.now - start_time).to_s + ')' #   message += ' (' + (Time.zone.now - start_time).to_s + ')'
+          add(#{severity}, message)                                 #   add(DEBUG, message)
+        end                                                         # end
+      EOT
+    end
+
+  end
+end
+
 module ActiveRecord
   module ConnectionAdapters
 
@@ -22,11 +83,23 @@ module ActiveRecord
 
   class Base
 
+    # See https://rails.lighthouseapp.com/projects/8994/tickets/2919
+    # allows [attribute]_changed? to behave as expected after cloning a model instance
+    if Rails.version == "2.3.14"
+      def clone
+        attrs = clone_attributes
+        attrs.delete(self.class.primary_key)
+        record = self.class.new
+        record.attributes = attrs # original version is 'record.send :instance_variable_set, '@attributes', attrs'
+        record
+      end
+    end
+
     # ensure API servers are readonly
     alias_method :orig_readonly?, :readonly?
-    cattr_accessor :readonly
+
     def readonly?
-      self.readonly || orig_readonly?
+      (connection.adapter_name == 'SQLite' && Rails.env.production?) || orig_readonly?
     end
 
     # This patch allows us to specify which attributes are safe to update.
