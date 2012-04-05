270c1b656eee79fd6776fc6d85f3f54efa6895e4
Eric Tipton <eric.tipton@tapjoy.com>
2012-04-05 15:50:21 -0700

    fix schema

--- tapjoyads/db/schema.rb
+++ tapjoyads/db/schema.rb
@@ -1,4 +1,4 @@
-# This file is auto-generated from the current state of the database. Instead of editing this file, 
+# This file is auto-generated from the current state of the database. Instead of editing this file,
 # please use the migrations feature of Active Record to incrementally modify your database, and
 # then regenerate this schema definition.
 #
@@ -673,10 +673,7 @@ ActiveRecord::Schema.define(:version => 20120328111003) do
     t.string   "tracking_for_type"
     t.string   "tracking_for_id",                   :limit => 36
     t.text     "cities",                                                                                           :null => false
-<<<<<<< HEAD
-=======
     t.text     "impression_tracking_urls"
->>>>>>> s/third_party_tracking/impression_tracking/g
   end
 
   add_index "offers", ["id"], :name => "index_offers_on_id", :unique => true
