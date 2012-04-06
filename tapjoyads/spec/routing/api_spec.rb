aed72cab96f893a40a9ac6e24770491c9e3ac32f
Jeff Dickey <me@jeffdickey.info>
2012-04-06 19:55:54 -0400

    added more route tests

--- tapjoyads/spec/routing/api_spec.rb
+++ tapjoyads/spec/routing/api_spec.rb
@@ -4,7 +4,13 @@ describe 'routes for api' do
   it 'should be routable' do
     get('/').should route_to 'homepage#start'
     get('/click/app').should route_to 'click#app'
+    get('/connect').should route_to 'connect#index'
     get('/get_offers').should route_to 'get_offers#index'
     get('/get_vg_store_items/user_account').should route_to 'get_vg_store_items#user_account'
+    get('/display_ad').should route_to 'display_ad#index'
+    get('/get_offers/webpage').should route_to 'get_offers#webpage'
+    get('/get_offers/featured').should route_to 'get_offers#featured'
+    get('/videos').should route_to 'videos#index'
+    get('/points/spend').should route_to 'points#spend'
   end
 end
