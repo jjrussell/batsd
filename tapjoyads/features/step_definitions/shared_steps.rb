18cc25e4b786aa648ff81e40fd56a763a956486c
Jeff Dickey <me@jeffdickey.info>
2012-04-06 18:38:08 -0400

    added tests and bug fixes for signup and app creation

--- tapjoyads/features/step_definitions/shared_steps.rb
+++ tapjoyads/features/step_definitions/shared_steps.rb
@@ -1,7 +1,15 @@
+Given /^I go to "([^"]*)"$/ do |text|
+  click_link text
+end
+
 Then /^I should see "([^"]*)"$/ do |text|
   page.should have_content(text)
 end
 
 When /^show me the page$/ do
   save_and_open_page
+end
+
+When /^I press "([^"]*)"$/ do |text|
+  click_button text
 end
