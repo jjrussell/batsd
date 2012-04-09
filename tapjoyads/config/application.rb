1fdc43b084978d3537e93d2c6b33fcb2fe0a3cbd
Jeff Dickey <me@jeffdickey.info>
2012-04-09 14:34:52 -0700

    removed test::unit

--- tapjoyads/config/application.rb
+++ tapjoyads/config/application.rb
@@ -1,6 +1,9 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "active_resource/railtie"
 
 Bundler.require(:default, Rails.env) if defined?(Bundler)
 
