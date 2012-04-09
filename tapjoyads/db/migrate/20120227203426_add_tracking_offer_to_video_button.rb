e34431cc5a8f65186709f7cb2c9442bd068cbdd0
James Logsdon <jlo@tapjoy.com>
2012-04-09 14:04:31 -0400

    Removing tracking_offer_id from video button

--- tapjoyads/db/migrate/20120227203426_add_tracking_offer_to_video_button.rb
+++
@@ -1,9 +0,0 @@
-class AddTrackingOfferToVideoButton < ActiveRecord::Migration
-  def self.up
-    add_guid_column :video_buttons, :tracking_offer_id
-  end
-
-  def self.down
-    remove_column :video_buttons, :tracking_offer_id
-  end
-end
