fa187aa678c633d5427bd805f68f2ab6c32b60f2
Jia Feng <jia.feng@tapjoy.com>
2012-04-11 02:36:13 -0700

    OneOff: delete duplicate Facebook associations, keep the most recent login TJM acct's association

---
+++ tapjoyads/lib/one_offs/2012_04_11_remove_duplicate_facebook_associations.rb
@@ -0,0 +1,17 @@
+class OneOffs
+  def self.remove_duplicate_facebook_associations
+    results = GamerProfile.find_by_sql("SELECT facebook_id, COUNT(*) AS acct_count FROM gamer_profiles WHERE facebook_id IS NOT NULL GROUP BY facebook_id")
+    results.each do |result|
+      if result.acct_count.to_i > 1
+        Gamer.find(
+          :all,
+          :conditions => { :gamer_profiles => { :facebook_id => result.facebook_id } },
+          :include => :gamer_profile,
+          :order => 'last_login_at DESC'
+        ).each_with_index do |gamer, index|
+          gamer.gamer_profile.dissociate_account!(Invitation::FACEBOOK) if index > 0
+        end
+      end
+    end
+  end
+end
