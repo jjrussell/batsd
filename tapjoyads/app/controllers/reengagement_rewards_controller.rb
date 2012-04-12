513a04e3242d68ca49810f2590edf7b23897e1fc
Adam Sumner <adamsumner@gmail.com>
2012-04-12 14:38:12 -0700

    Cache reengagement offer lists.

--- tapjoyads/app/controllers/reengagement_rewards_controller.rb
+++ tapjoyads/app/controllers/reengagement_rewards_controller.rb
@@ -21,7 +21,7 @@ class ReengagementRewardsController < ApplicationController
   def index
     verify_params([:udid, :publisher_user_id, :app_id])
     @app = App.find_in_cache(params[:app_id])
-    @currencies = Currency.find_all_in_cache_by_app_id(params[:app_id]) if @app
+    @currencies = Currency.find_all_in_cache_by_app_id(params[:app_id]) if @app.try(:reengagement_campaign_enabled?)
     @reengagement_offers = @app.reengagement_campaign_from_cache if @currencies.try(:present?)
     @reengagement_offer = ReengagementOffer.resolve(@app, @currencies, @reengagement_offers, params, geoip_data) if @reengagement_offers.try(:present?)
     @button_link = 'http://ok'
