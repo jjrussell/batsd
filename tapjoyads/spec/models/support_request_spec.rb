04ce15dbc9bb74d7407244de8df99db6742150b7
Brian Stebar II <brian.stebar@tapjoy.com>
2012-04-12 12:33:09 -0400

    Specs for `SupportRequest` methods

--- tapjoyads/spec/models/support_request_spec.rb
+++ tapjoyads/spec/models/support_request_spec.rb
@@ -9,7 +9,7 @@ describe SupportRequest do
     @app = Factory(:app)
     @offer = @app.primary_offer
     @currency = Factory(:currency, :app => @app)
-    @click = Factory(:click, :udid => @device.key, :currency_id => @currency.id)
+    @click = Factory(:click, :udid => @device.key, :currency_id => @currency.id, :offer_id => @offer.id)
 
     # Build params array and user_agent
     @user_agent = "rspec"
@@ -72,40 +72,87 @@ describe SupportRequest do
   describe '#fill_from_click' do
     before :each do
       @gamer = Factory(:gamer)
+      @gamer_device = GamerDevice.new(:device => @device)
+      @gamer.devices << @gamer_device
     end
 
     context 'when a click is provided' do
-      it "stores the publisher's app id from the click model"
+      it "stores the publisher's app id from the click model" do
+        @support_request.fill_from_click(@click, @params, @gamer_device, @gamer, @user_agent)
+        @support_request.publisher_app_id.should == @click.publisher_app_id
+      end
 
-      it "stores the publisher's partner id from the click model"
+      it "stores the publisher's partner id from the click model" do
+        @support_request.fill_from_click(@click, @params, @gamer_device, @gamer, @user_agent)
+        @support_request.publisher_partner_id.should == @click.publisher_partner_id
+      end
 
-      it "stores the publisher user id from the click model"
+      it "stores the publisher user id from the click model" do
+        @support_request.fill_from_click(@click, @params, @gamer_device, @gamer, @user_agent)
+        @support_request.publisher_user_id.should == @click.publisher_user_id
+      end
 
-      it "stores the app's id from the click model"
+      it "stores the app's id from the click model" do
+        @support_request.fill_from_click(@click, @params, @gamer_device, @gamer, @user_agent)
+        @support_request.app_id.should == @click.offer.item_id
+      end
 
-      it "stores the currency's id from the click model"
+      it "stores the currency's id from the click model" do
+        @support_request.fill_from_click(@click, @params, @gamer_device, @gamer, @user_agent)
+        @support_request.currency_id.should == @click.currency_id
+      end
 
-      it "stores the offer's id from the click model"
+      it "stores the offer's id from the click model" do
+        @support_request.fill_from_click(@click, @params, @gamer_device, @gamer, @user_agent)
+        @support_request.offer_id.should == @click.offer_id
+      end
 
-      it "stores the click model's id"
+      it "stores the click model's id" do
+        @support_request.fill_from_click(@click, @params, @gamer_device, @gamer, @user_agent)
+        @support_request.click_id.should == @click.id
+      end
     end
 
     context 'when no click is provided' do
-      it "stores the publisher's app id from the params array"
+      it "stores the publisher's app id from the params array" do
+        @support_request.fill_from_click(nil, @params, @gamer_device, @gamer, @user_agent)
+        @support_request.publisher_app_id.should == @params[:publisher_app_id]
+      end
 
-      it "stores the publisher's partner id from the params array"
+      it "stores the publisher's partner id from the params array" do
+        @support_request.fill_from_click(nil, @params, @gamer_device, @gamer, @user_agent)
+        @support_request.publisher_partner_id.should == @params[:publisher_partner_id]
+      end
 
-      it "stores the publisher user id from the params array"
+      it "stores the publisher user id from the params array" do
+        @support_request.fill_from_click(nil, @params, @gamer_device, @gamer, @user_agent)
+        @support_request.publisher_user_id.should == @params[:publisher_user_id]
+      end
 
-      it "stores the app's id from the params array"
+      it "stores the app's id from the params array" do
+        @support_request.fill_from_click(nil, @params, @gamer_device, @gamer, @user_agent)
+        @support_request.app_id.should == @params[:app_id]
+      end
 
-      it "stores the currency's id from the params array"
+      it "stores the currency's id from the params array" do
+        @support_request.fill_from_click(nil, @params, @gamer_device, @gamer, @user_agent)
+        @support_request.currency_id.should == @params[:currency_id]
+      end
 
-      it "stores the offer's id from the params array"
+      it "stores the offer's id from the params array" do
+        @support_request.fill_from_click(nil, @params, @gamer_device, @gamer, @user_agent)
+        @support_request.offer_id.should == @params[:offer_id]
+      end
 
-      it "stores the click's id from the params array"
+      it "stores the click's id from the params array" do
+        @support_request.fill_from_click(nil, @params, @gamer_device, @gamer, @user_agent)
+        @support_request.click_id.should == @params[:click_id]
+      end
     end
 
-    it "store device type from device model"
+    it "store device type from device model" do
+      @support_request.fill_from_click(@click, @params, @gamer_device, @gamer, @user_agent)
+      @support_request.device_type.should == @gamer_device.device_type
+    end
   end
 end
