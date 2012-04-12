cd71defea84893a10c4b3072bc72c962dfd0c5f8
Brian Stebar II <brian.stebar@tapjoy.com>
2012-04-12 17:27:27 -0400

    Refactored `SupportRequest` fill methods; Added lots more specs to make sure it didn't break stuff

--- tapjoyads/app/models/support_request.rb
+++ tapjoyads/app/models/support_request.rb
@@ -26,37 +26,22 @@ class SupportRequest < SimpledbResource
   self.sdb_attr :gamer_id
 
   def fill_from_params(params, app, currency, offer, user_agent)
-    self.description            = params[:description]
-    self.udid                   = params[:udid]
-    self.email_address          = params[:email_address]
-    self.publisher_app_id       = params[:publisher_app_id]
-    self.publisher_partner_id   = params[:publisher_partner_id]
-    self.publisher_user_id      = params[:publisher_user_id]
-    self.device_type            = params[:device_type]
-    self.user_agent             = user_agent
-    self.language_code          = params[:language_code]
-    self.app_id                 = app.id
-    self.currency_id            = currency.id
-    self.offer_id               = offer.id if offer.present?
-    click                       = offer.present? ? get_last_click(params[:udid], offer) : nil
-    self.click_id               = click ? click.id : nil
+    fill( :params     => params,
+          :app        => app,
+          :currency   => currency,
+          :offer      => offer,
+          :user_agent => user_agent )
+
+    # click                       = offer.present? ? get_last_click(params[:udid], offer) : nil
+    # self.click_id               = click ? click.id : nil
   end
 
   def fill_from_click(click, params, device, gamer, user_agent)
-    self.udid                   = device.device_id
-    self.description            = params[:description]
-    self.email_address          = gamer.email
-    self.publisher_app_id       = click.present? ? click.publisher_app_id : params[:publisher_app_id]
-    self.publisher_partner_id   = click.present? ? click.publisher_partner_id : params[:publisher_partner_id]
-    self.publisher_user_id      = click.present? ? click.publisher_user_id : params[:publisher_user_id]
-    self.device_type            = device.device_type
-    self.user_agent             = user_agent
-    self.language_code          = params[:language_code]
-    self.app_id                 = click.present? ? click.offer.item_id : params[:app_id]
-    self.currency_id            = click.present? ? click.currency_id : params[:currency_id]
-    self.offer_id               = click.present? ? click.offer_id : params[:offer_id]
-    self.click_id               = click.present? ? click.id : nil
-    self.gamer_id               = gamer.id
+    fill( :click      => click,
+          :params     => params,
+          :device     => device,
+          :gamer      => gamer,
+          :user_agent => user_agent )
   end
 
   def get_last_click(udid, offer)
@@ -67,5 +52,44 @@ class SupportRequest < SimpledbResource
 
   def click
     Click.find(click_id)
+  end
+
+  private
+
+  def fill(options)
+    app         = options.delete(:app)        { nil }
+    currency    = options.delete(:currency)   { nil }
+    click       = options.delete(:click)      { nil }
+    device      = options.delete(:device)     { nil }
+    gamer       = options.delete(:gamer)      { nil }
+    offer       = options.delete(:offer)      { nil }
+    params      = options.delete(:params)     { |k| raise "#{k} is a required argument" }
+    user_agent  = options.delete(:user_agent) { |k| raise "#{k} is a required argument" }
+    raise "Unknown options #{options.keys.join(', ')}" unless options.empty?
+
+    self.user_agent             = user_agent
+    self.description            = params[:description]
+    self.language_code          = params[:language_code]
+
+    self.publisher_app_id       = click.present? ? click.publisher_app_id : params[:publisher_app_id]
+    self.publisher_partner_id   = click.present? ? click.publisher_partner_id : params[:publisher_partner_id]
+    self.publisher_user_id      = click.present? ? click.publisher_user_id : params[:publisher_user_id]
+    self.udid                   = device.present? ? device.device_id : params[:udid]
+    self.device_type            = device.present? ? device.device_type : params[:device_type]
+    self.email_address          = gamer.present? ? gamer.email : params[:email_address]
+    self.gamer_id               = gamer.present? ? gamer.id : nil
+
+    if click.present?
+      self.app_id               = click.offer.item_id
+      self.currency_id          = click.currency_id
+      self.offer_id             = click.offer_id
+      self.click_id             = click.id
+    else
+      self.app_id               = app.present? ? app.id : params[:app_id]
+      self.currency_id          = currency.present? ? currency.id : params[:currency_id]
+      self.offer_id             = offer.present? ? offer.id : nil
+      offer_click               = offer.present? ? get_last_click(params[:udid], offer) : nil
+      self.click_id             = offer_click.present? ? offer_click.id : nil
+    end
   end
 end
--- tapjoyads/spec/models/support_request_spec.rb
+++ tapjoyads/spec/models/support_request_spec.rb
@@ -5,24 +5,31 @@ describe SupportRequest do
     fake_the_web
     @support_request = SupportRequest.new
 
-    @device = Factory(:device)
     @app = Factory(:app)
     @offer = @app.primary_offer
     @currency = Factory(:currency, :app => @app)
-    @click = Factory(:click, :udid => @device.key, :currency_id => @currency.id, :offer_id => @offer.id)
+    @device = Factory(:device)
+    publisher_app = Factory(:app)
+    @click = Factory(:click,  :udid                 => @device.key,
+                              :currency_id          => @currency.id,
+                              :offer_id             => @offer.id,
+                              :publisher_app_id     => publisher_app.id,
+                              :publisher_partner_id => publisher_app.partner_id,
+                              :publisher_user_id    => "click_test_user" )
 
-    # Build params array and user_agent
     @user_agent = "rspec"
-    @params =  {  :language_code      => "",
-                  :udid               => "test_udid",
-                  :publisher_user_id  => "test_user",
-                  :currency_id        => "test_currency",
-                  :app_id             => "test_app",
-                  :offer_id           => "test_offer",
-                  :device_type        => "android",
-                  :description        => "I'm needy and have lots of problems",
-                  :email_address      => "test@tapjoy.com",
-                }
+    @params =  {  :language_code        => "en",
+                  :udid                 => "test_udid",
+                  :publisher_app_id     => "test_publisher_app",
+                  :publisher_partner_id => "test_publisher_partner",
+                  :publisher_user_id    => "test_publisher_user",
+                  :currency_id          => "test_currency",
+                  :app_id               => "test_app",
+                  :offer_id             => "test_offer",
+                  :device_type          => "android",
+                  :description          => "I'm needy and have lots of problems",
+                  :email_address        => "test@tapjoy.com",
+                  :click_id             => "test_click" }
   end
 
   describe '#fill_from_params' do
@@ -63,9 +70,64 @@ describe SupportRequest do
       end
     end
 
-    it "store device type from params array" do
+    it "stores the description message from the params array" do
+      @support_request.fill_from_params(@params, @app, @currency, nil, @user_agent)
+      @support_request.description.should == @params[:description]
+    end
+
+    it "stores the UDID from the params array" do
+      @support_request.fill_from_params(@params, @app, @currency, nil, @user_agent)
+      @support_request.udid.should == @params[:udid]
+    end
+
+    it "stores the email address from the params array" do
+      @support_request.fill_from_params(@params, @app, @currency, nil, @user_agent)
+      @support_request.email_address.should == @params[:email_address]
+    end
+
+    it "stores the publisher's app id from the params array" do
+      @support_request.fill_from_params(@params, @app, @currency, nil, @user_agent)
+      @support_request.publisher_app_id.should == @params[:publisher_app_id]
+    end
+
+    it "stores the publisher's partner id from the params array" do
+      @support_request.fill_from_params(@params, @app, @currency, nil, @user_agent)
+      @support_request.publisher_partner_id.should == @params[:publisher_partner_id]
+    end
+
+    it "stores the publisher's user id from the params array" do
+      @support_request.fill_from_params(@params, @app, @currency, nil, @user_agent)
+      @support_request.publisher_user_id.should == @params[:publisher_user_id]
+    end
+
+    it "stores device type from params array" do
       @support_request.fill_from_params(@params, @app, @currency, @offer, @user_agent)
       @support_request.device_type.should == @params[:device_type]
+    end
+
+    it "stores the user_agent from the user_agent argument" do
+      @support_request.fill_from_params(@params, @app, @currency, nil, @user_agent)
+      @support_request.user_agent.should == @user_agent
+    end
+
+    it "stores the language code from the params array" do
+      @support_request.fill_from_params(@params, @app, @currency, nil, @user_agent)
+      @support_request.language_code.should == @params[:language_code]
+    end
+
+    it "stores the app's id from the app model" do
+      @support_request.fill_from_params(@params, @app, @currency, nil, @user_agent)
+      @support_request.app_id.should == @app.id
+    end
+
+    it "stores the currency's id from the currency model" do
+      @support_request.fill_from_params(@params, @app, @currency, nil, @user_agent)
+      @support_request.currency_id.should == @currency.id
+    end
+
+    it "leaves gamer_id blank" do
+      @support_request.fill_from_params(@params, @app, @currency, nil, @user_agent)
+      @support_request.gamer_id.should be_blank
     end
   end
 
@@ -73,6 +135,7 @@ describe SupportRequest do
     before :each do
       @gamer = Factory(:gamer)
       @gamer_device = GamerDevice.new(:device => @device)
+      @gamer_device.device_type = "android"
       @gamer.devices << @gamer_device
     end
 
@@ -139,20 +202,50 @@ describe SupportRequest do
         @support_request.currency_id.should == @params[:currency_id]
       end
 
-      it "stores the offer's id from the params array" do
+      it "leaves offer_id blank" do
         @support_request.fill_from_click(nil, @params, @gamer_device, @gamer, @user_agent)
-        @support_request.offer_id.should == @params[:offer_id]
+        @support_request.offer_id.should be_blank
       end
 
-      it "stores the click's id from the params array" do
+      it "leaves click_id blank" do
         @support_request.fill_from_click(nil, @params, @gamer_device, @gamer, @user_agent)
-        @support_request.click_id.should == @params[:click_id]
+        @support_request.click_id.should be_blank
       end
+    end
+
+    it "stores the description message from the params array" do
+      @support_request.fill_from_click(@click, @params, @gamer_device, @gamer, @user_agent)
+      @support_request.description.should == @params[:description]
+    end
+
+    it "stores UDID from device model" do
+      @support_request.fill_from_click(@click, @params, @gamer_device, @gamer, @user_agent)
+      @support_request.udid.should == @gamer_device.device_id
+    end
+
+    it "stores the email address from the gamer model" do
+      @support_request.fill_from_click(@click, @params, @gamer_device, @gamer, @user_agent)
+      @support_request.email_address.should == @gamer.email
     end
 
     it "store device type from device model" do
       @support_request.fill_from_click(@click, @params, @gamer_device, @gamer, @user_agent)
       @support_request.device_type.should == @gamer_device.device_type
+    end
+
+    it "stores the user_agent from the user_agent argument" do
+      @support_request.fill_from_click(@click, @params, @gamer_device, @gamer, @user_agent)
+      @support_request.user_agent.should == @user_agent
+    end
+
+    it "stores the language code from the params array" do
+      @support_request.fill_from_click(@click, @params, @gamer_device, @gamer, @user_agent)
+      @support_request.language_code.should == @params[:language_code]
+    end
+
+    it "stores the gamer's id from the gamer model" do
+      @support_request.fill_from_click(@click, @params, @gamer_device, @gamer, @user_agent)
+      @support_request.language_code.should == @params[:language_code]
     end
   end
 end
