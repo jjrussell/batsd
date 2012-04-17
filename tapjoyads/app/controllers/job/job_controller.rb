fdb3941067f83ae234b35182fc51d88df83e4fbc
Amir Manji <amanji75@gmail.com>
2012-04-16 21:29:14 -0700

    manually notify airbrake of job errors

--- tapjoyads/app/controllers/job/job_controller.rb
+++ tapjoyads/app/controllers/job/job_controller.rb
@@ -2,5 +2,15 @@ class Job::JobController < ApplicationController
   include AuthenticationHelper
 
   before_filter :authenticate
+  around_filter :record_errors
+
+  private
+
+  def record_errors
+    yield
+  rescue => exception
+    notify_airbrake(exception)
+    raise exception
+  end
 
 end
