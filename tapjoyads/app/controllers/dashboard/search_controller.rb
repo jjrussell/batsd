95a9f4dc181b3d9fe7ce8625d2d41d019709e6f2
Brian Stebar II <brian.stebar@tapjoy.com>
2012-04-17 17:08:49 -0400

    Some cleanup

--- tapjoyads/app/controllers/dashboard/search_controller.rb
+++ tapjoyads/app/controllers/dashboard/search_controller.rb
@@ -112,7 +112,7 @@ class Dashboard::SearchController < Dashboard::DashboardController
   end
 
   def currencies
-    term = params[:term].to_s.strip
+    term = params[:term].strip
 
     if term =~ UUID_REGEX
       conditions = [ "id = ? OR app_id = ?", term, term ]
