e301a748f9c5596dbe682d1631d7e548902c1326
Brian Stebar II <brian.stebar@tapjoy.com>
2012-04-17 10:39:52 -0400

    More code review refactoring

--- tapjoyads/app/controllers/games/support_requests_controller.rb
+++ tapjoyads/app/controllers/games/support_requests_controller.rb
@@ -1,5 +1,7 @@
 class Games::SupportRequestsController < GamesController
 
+  before_filter :set_tracking_param
+
   def new
     current_gamer
   end
@@ -12,16 +14,22 @@ class Games::SupportRequestsController < GamesController
       flash.now[:notice] = t("text.games.enter_message");
       render :new and return
     end
+
     case params[:type]
     when "feedback"
       GamesMailer.deliver_feedback(@gamer, data[:content], request.env["HTTP_USER_AGENT"], current_device_id)
     when "report_bug"
       GamesMailer.deliver_report_bug(@gamer, data[:content], request.env["HTTP_USER_AGENT"], current_device_id)
     when "contact_support"
-      GamesMailer.deliver_contact_support(@gamer, current_device, data[:content], request.env["HTTP_USER_AGENT"], params[:language_code])
+      GamesMailer.deliver_contact_support(@gamer, data[:content], request.env["HTTP_USER_AGENT"], current_device_id)
     else
-      GamesMailer.deliver_contact_support(@gamer, current_device, data[:content], request.env["HTTP_USER_AGENT"], params[:language_code])
+      GamesMailer.deliver_contact_support(@gamer, data[:content], request.env["HTTP_USER_AGENT"], current_device_id)
     end
   end
 
+  private
+
+  def set_tracking_param
+    @tjm_request.tracking_param = params[:type] if @tjm_request.present?
+  end
 end
--- tapjoyads/spec/controllers/games/support_requests_controller_spec.rb
+++ tapjoyads/spec/controllers/games/support_requests_controller_spec.rb
@@ -3,101 +3,39 @@ require 'spec/spec_helper'
 describe Games::SupportRequestsController do
   before :each do
     activate_authlogic
-    fake_the_web
-
-    @device = Factory(:device)
-
-    @click1 = Factory(:click,           :udid => @device.key,
-                                        :clicked_at => 1.hour.ago)
-    @click2 = Factory(:click,           :udid => @device.key,
-                                        :clicked_at => 1.hour.ago)
-    @click1_dupe_old = Factory(:click,  :udid => @device.key,
-                                        :currency_id => @click1.currency_id,
-                                        :advertiser_app_id => @click1.advertiser_app_id,
-                                        :publisher_app_id => @click1.publisher_app_id,
-                                        :offer_id => @click1.offer_id,
-                                        :clicked_at => 2.hours.ago)
-
-    results = [ @click1, @click1_dupe_old, @click2 ]
-    Click.stubs(:select_all).returns(results)
-
     @gamer = Factory(:gamer)
-    @gamer.gamer_profile = GamerProfile.create(:facebook_id => '0', :gamer => @gamer)
-    @gamer.devices << GamerDevice.new(:device => @device)
-
     login_as(@gamer)
+    @controller.stubs(:current_gamer).returns(@gamer)
+    flash.stubs(:sweep)
   end
 
-  describe '#create' do
-    # context 'without supplying a message' do
-    #   before :each do
-    #     @params = { :type => 'contact_support' }
-    #     @params['support_requests'] = { :content => "" }
-    #   end
-    #
-    #   it "re-renders #new if no message is provided" do
-    #     post :create, @params
-    #     response.should render_template("new")
-    #   end
-    # end
+  describe '#new' do
+    it 'tracks the event based on the tracking param' do
+      get('new')
+      path = assigns(:tjm_request).path
 
-    context 'with a message' do
-      before :each do
-        @params = { :type => 'contact_support' }
-        @params['support_requests'] = { :content => "I'm needy and need help!" }
-      end
-
-      context 'with a click selected' do
-
-      end
-
-      context 'without a click selected' do
-
-      end
-
-      it 'creates a SupportRequest' do
-        puts "Params: #{@params.inspect}"
-        #post :create, @params
-        support_request = SupportRequest.select(:where =>"udid = '#{@device.key}'")
-        puts "Support Request: #{support_request.inspect}"
-      end
-
-      it 'sends a customer service email' do
-        GamesMailer.expects(:deliver_contact_support).once
-        post :create, @params
-      end
+      get('new', { :type => 'feedback'})
+      assigns(:tjm_request).path.should == [ "#{path}_feedback" ]
     end
   end
 
-  # describe '#new' do
-  #   before :each do
-  #     @params = { :udid => @device.key }
-  #   end
-  #
-  #   context 'when format is set to JS' do
-  #     before :each do
-  #       @params[:format] = 'js'
-  #     end
-  #
-  #     context 'the @unresolved_clicks collection' do
-  #       it 'includes both "correct" clicks' do
-  #         get :new, @params
-  #         assigns(:unresolved_clicks).should include @click1
-  #         assigns(:unresolved_clicks).should include @click2
-  #       end
-  #
-  #       it "doesn't include the older duplicate click" do
-  #         get :new, @params
-  #         assigns(:unresolved_clicks).should_not include @click1_dupe_old
-  #       end
-  #     end
-  #   end
-  #
-  #   context 'when format is not set to JS' do
-  #     it 'finds the current gamer' do
-  #       get :new, @params
-  #       assigns(:current_gamer).should == @gamer
-  #     end
-  #   end
-  # end
+  describe '#create' do
+    before :each do
+      @params = {
+        :support_requests => { :content => 'test message' },
+      }
+    end
+
+    it 'tracks the event based on the tracking param' do
+      get('create', @params)
+      path = assigns(:tjm_request).path
+      get('create', @params.merge(:type => 'feedback'))
+      assigns(:tjm_request).path.should == [ "#{path}_feedback" ]
+    end
+
+    it 'flashes a notice if no data is present' do
+      get('create', { :support_requests => { :content => '' } })
+      flash.now[:notice].should be_present
+    end
+  end
 end
