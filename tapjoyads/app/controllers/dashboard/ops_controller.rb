f5f8bbeecce4185b60814a0386041f19fd3fcc48
Aaron Daniel <amdtech@me.com>
2012-04-18 15:49:54 -0700

    Use hashes for service stats instead

--- tapjoyads/app/controllers/dashboard/ops_controller.rb
+++ tapjoyads/app/controllers/dashboard/ops_controller.rb
@@ -298,7 +298,8 @@ class Dashboard::OpsController < Dashboard::DashboardController
     keys = []
     time = start_time - start_time % mod
 
-    while time.to_i <= end_time.to_i
+    time.to_i.upto(end_time.to_i) do |t|
+      next if t % mod > 0
       if subkeys.is_a? Array
         subkeys.each { |subkey| keys << "#{namespace}.#{subkey}.#{time}" }
       else
