aff7140cba1938ec9f9d2a5cd910af3f0e94b10e
Neal Wiggins <neal.wiggins@tapjoy.com>
2012-04-18 18:53:59 -0700

    progress for sorting column ui

--- tapjoyads/app/controllers/tools/partner_validations_controller.rb
+++ tapjoyads/app/controllers/tools/partner_validations_controller.rb
@@ -3,20 +3,16 @@ class Tools::PartnerValidationsController < WebsiteController
   current_tab :tools
   filter_access_to :all
 
-
   def index
     @partners = Partner.to_payout.all(:include => [:payout_info, :payout_info_confirmation, :payout_threshold_confirmation, {:users => [:user_roles]}])
 
     if params[:acct_mgr_sort]
       @partners = @partners.to_a
       @partners.each { |p|  class << p; attr_accessor :acct_mgr_email; end; p.acct_mgr_email =  p.account_managers.present? ? p.account_managers.first.email.downcase : "\xFF"}
-      @partners = @partners.sort do  |a,b|
-        a.acct_mgr_email <=> b.acct_mgr_email
-      end
-      @partners = @partners.reverse if params[:acct_mgr_sort] == 'DESC'
+      @partners.sort!{ |a,b| a.acct_mgr_email <=> b.acct_mgr_email }
+      @partners.reverse! if params[:acct_mgr_sort] == 'DESC'
     end
     @partners = @partners.paginate(:page => params[:page])
-
   end
 
   def confirm_payouts
