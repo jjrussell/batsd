b737f6c83b83d05e99bda7cdfa9701acd8d973b7
Eric Tipton <eric.tipton@tapjoy.com>
2012-04-20 19:56:41 -0400

    add reload-ability

--- tapjoyads/lib/simpledb/associations.rb
+++ tapjoyads/lib/simpledb/associations.rb
@@ -16,14 +16,16 @@ module Simpledb
         klass = (options[:class_name] || association_name).to_s.classify.constantize
         ivar = "@#{association_name}"
 
-        define_method(association_name) do
-          if instance_variable_get(ivar).present?
+        define_method(association_name) do |*args|
+          reload = args.shift
+
+          if !reload && instance_variable_get(ivar).present?
             return instance_variable_get(ivar)
           end
 
           association_id = send(foreign_key)
-          return nil if association_id.blank?
-          instance_variable_set(ivar, klass.find(association_id))
+          val = association_id.blank? ? nil : klass.find(association_id)
+          instance_variable_set(ivar, val)
         end
       end
     end
