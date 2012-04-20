f2a21f3b3fc168e76f84f1caee1b0c45ffb95e59
Eric Tipton <eric.tipton@tapjoy.com>
2012-04-20 17:34:59 -0400

    apparently memoize is calling freeze on the object's attrs hash

--- tapjoyads/lib/simpledb/associations.rb
+++ tapjoyads/lib/simpledb/associations.rb
@@ -14,13 +14,17 @@ module Simpledb
         association_name = args.first
         foreign_key = options[:foreign_key] || "#{association_name}_id"
         klass = (options[:class_name] || association_name).to_s.classify.constantize
+        ivar = "@#{association_name}"
 
         define_method(association_name) do
+          if instance_variable_get(ivar).present?
+            return instance_variable_get(ivar)
+          end
+
           association_id = send(foreign_key)
           return nil if association_id.blank?
-          klass.find(association_id)
+          instance_variable_set(ivar, klass.find(association_id))
         end
-        memoize association_name
       end
     end
 
