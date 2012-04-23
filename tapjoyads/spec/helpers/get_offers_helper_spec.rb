5e34eaefcdd767182524930105be89c66295bc2f
Jeremy Chotiner <jeremy.chotiner@tapjoy.com>
2012-04-23 11:53:30 -0400

    updated test

--- tapjoyads/spec/helpers/get_offers_helper_spec.rb
+++ tapjoyads/spec/helpers/get_offers_helper_spec.rb
@@ -10,16 +10,16 @@ describe GetOffersHelper do
     end
 
     it 'returns nil if not an app offer' do
-      helper.offer_text(@action_offer, @currency).should == nil
+      helper.featured_offer_text(@action_offer, @currency).should == ""
     end
 
     it 'returns download_and_run text if rewarded' do
-      helper.offer_text(@offer, @currency).should == helper.t('text.featured.download_and_run')
+      helper.featured_offer_text(@offer, @currency).should == helper.t('text.featured.download_and_run')
     end
 
     it 'returns try_out text if not rewarded' do
       @offer.rewarded = false
-      helper.offer_text(@offer, @currency).should == helper.t('text.featured.try_out')
+      helper.featured_offer_text(@offer, @currency).should == helper.t('text.featured.try_out')
     end
   end
 
@@ -32,12 +32,12 @@ describe GetOffersHelper do
     end
 
     it 'returns download text if App Offer' do
-      helper.action_text(@offer).should == helper.t('text.featured.download')
+      helper.featured_offer_action_text(@offer).should == helper.t('text.featured.download')
     end
 
     it 'returns earn_now text if not App offer' do
       @offer.rewarded = false
-      helper.action_text(@action_offer).should == helper.t('text.featured.earn_now')
+      helper.featured_offer_action_text(@action_offer).should == helper.t('text.featured.earn_now')
     end
   end
 end
