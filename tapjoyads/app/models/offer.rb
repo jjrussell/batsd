6409cdd6852b2689b9b42616b94af0d0a9955fed
Brian Stebar II <brian.stebar@tapjoy.com>
2012-04-24 16:23:10 -0400

    Revert "Revert "Merge pull request #2264 from brianstebar/cs_email""
    This reverts commit 03210132bd18e6d67f323762dbc7093637aa5a03.

--- tapjoyads/app/models/offer.rb
+++ tapjoyads/app/models/offer.rb
@@ -232,7 +232,7 @@ class Offer < ActiveRecord::Base
 
   delegate :balance, :pending_earnings, :name, :cs_contact_email, :approved_publisher?, :rev_share, :to => :partner, :prefix => true
   delegate :name, :id, :formatted_active_gamer_count, :protocol_handler, :to => :app, :prefix => true, :allow_nil => true
-  memoize :partner_balance, :app_formatted_active_gamer_count, :app_protocol_handler
+  memoize :partner_balance, :app_formatted_active_gamer_count, :app_protocol_handler, :app_name
 
   alias_method :events, :offer_events
   alias_method :random, :rand
