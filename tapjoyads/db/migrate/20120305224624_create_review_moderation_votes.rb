7333d2ecf9f8a708fded4ffaae0a4d8cb7285f10
bertrand.kuo <bert@tapjoy.com>
2012-04-24 16:59:46 -0700

    review_moderation_votes changed to use guid, hash rockets prettified

--- tapjoyads/db/migrate/20120305224624_create_review_moderation_votes.rb
+++ tapjoyads/db/migrate/20120305224624_create_review_moderation_votes.rb
@@ -1,12 +1,14 @@
 class CreateReviewModerationVotes < ActiveRecord::Migration
   def self.up
-    create_table :review_moderation_votes do |t|
-      t.string :app_review_id, :limit=>36, :null=>false
-      t.string :gamer_id, :limit=>36, :null=>false
-      t.string :type, :limit=>32
+    create_table :review_moderation_votes, :id => false do |t|
+      t.guid    :id, :null => false
+      t.string  :app_review_id, :limit => 36, :null => false
+      t.string  :gamer_id, :limit => 36, :null => false
+      t.string  :type, :limit => 32
       t.integer :value
       t.timestamps
     end
+    add_index :review_moderation_votes, :id, :unique => true
     add_index :review_moderation_votes, [:type, :app_review_id]
     add_index :review_moderation_votes, [:type, :gamer_id]
 
