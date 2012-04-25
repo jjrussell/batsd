1f37ab00c0f89066acdc3314079869795190966c
bertrand.kuo <bert@tapjoy.com>
2012-04-25 15:04:10 -0700

    adding index to cover updated_at date

--- tapjoyads/db/migrate/20120419190829_add_is_blank_to_app_reviews.rb
+++ tapjoyads/db/migrate/20120419190829_add_is_blank_to_app_reviews.rb
@@ -2,7 +2,7 @@ class AddIsBlankToAppReviews < ActiveRecord::Migration
   def self.up
     add_column :app_reviews, :is_blank, :boolean, :default=>false
     add_index :app_reviews,
-              [:app_review_id, :updated_at,:is_blank ],
+              [:app_metadata_id, :updated_at,:is_blank ],
               { :name => "app_reviews_get_app",
                :order => {:updated_at => :desc} }
     AppReview.connection.execute 'UPDATE app_reviews set is_blank=true where text=""'
