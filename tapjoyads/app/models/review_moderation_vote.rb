345f61e6635f8a5c0e4125b3fa7f3a46583a7f7b
bertrand.kuo <bert@tapjoy.com>
2012-04-26 16:32:28 -0700

    moving stuff to extra_attributes

--- tapjoyads/app/models/review_moderation_vote.rb
+++ tapjoyads/app/models/review_moderation_vote.rb
@@ -5,14 +5,35 @@ end
 
 class HelpfulVote < ReviewModerationVote
   belongs_to :app_review, :counter_cache => :helpful_votes_count
-  belongs_to :gamer, :counter_cache => :helpful_votes_count
+  belongs_to :gamer  #, :counter_cache => :helpful_votes_count
 
-  def after_create
+  after_create :incr_sum_on_app_review
+  after_create :incr_count_on_gamer
+
+  before_destroy :decr_sum_on_app_review
+  before_destroy :decr_count_on_gamer
+
+  def incr_count_on_gamer
+    gamer.extra_attributes[:helpful_votes_count] ||= 0
+    gamer.extra_attributes[:helpful_votes_count] += 1
+    gamer.save
+
+  end
+
+  def decr_count_on_gamer
+    gamer.extra_attributes[:helpful_votes_count] ||= 0
+    gamer.extra_attributes[:helpful_votes_count] -= 1
+    gamer.extra_attributes[:helpful_votes_count] = 0 if gamer.extra_attributes[:helpful_votes_count] < 0
+    gamer.save
+
+  end
+
+  def incr_sum_on_app_review
     app_review.helpful_values_sum = app_review.helpful_values_sum + self.value
     app_review.save
   end
 
-  def after_destroy
+  def decr_sum_on_app_review
     app_review.helpful_values_sum = app_review.helpful_values_sum - self.value
     app_review.save
   end
@@ -20,5 +41,22 @@ end
 
 class BuryVote < ReviewModerationVote
   belongs_to :app_review, :counter_cache => :bury_votes_count
-  belongs_to :gamer, :counter_cache => :bury_votes_count
+  belongs_to :gamer #, :counter_cache => :bury_votes_count
+
+  after_create :incr_count_on_gamer
+  before_destroy :decr_count_on_gamer
+
+  def incr_count_on_gamer
+    gamer.extra_attributes[:bury_votes_count] ||= 0
+    gamer.extra_attributes[:bury_votes_count] += 1
+    gamer.save
+  end
+
+  def decr_count_on_gamer
+    gamer.extra_attributes[:bury_votes_count] ||= 0
+    gamer.extra_attributes[:bury_votes_count] -= 1
+    gamer.extra_attributes[:bury_votes_count] = 0 if gamer.extra_attributes[:bury_votes_count] < 0
+    gamer.save
+  end
+
 end
