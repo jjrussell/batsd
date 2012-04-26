a3636ea4c6134f8115413d4f4c049fb6c900b6b3
Aaron Daniel <amdtech@me.com>
2012-04-26 12:49:17 -0700

    These are permanent redirects

--- tapjoyads/app/controllers/documents_controller.rb
+++ tapjoyads/app/controllers/documents_controller.rb
@@ -3,7 +3,7 @@ class DocumentsController < WebsiteController
   protect_from_forgery
 
   def privacy
-    redirect_to 'http://info.tapjoy.com/about-tapjoy/privacy-policy'
+    redirect_to 'http://info.tapjoy.com/about-tapjoy/privacy-policy', :status => :moved_permanently
   end
 
   def privacy_mobile
