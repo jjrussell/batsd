7a77cd13936ce6dc6de2345f80a06ab48bc61144
Hwan-Joon Choi <hwanjoon@tapjoy.com>
2012-04-26 16:25:32 -0700

    Fix migration

--- tapjoyads/app/models/review_moderation_vote.rb
+++ tapjoyads/app/models/review_moderation_vote.rb
@@ -5,7 +5,7 @@ end
 
 class HelpfulVote < ReviewModerationVote
   belongs_to :app_review, :counter_cache => :helpful_votes_count
-  belongs_to :gamer, :counter_cache => :helpful_votes_count
+  belongs_to :gamer
 
   def after_create
     app_review.helpful_values_sum = app_review.helpful_values_sum + self.value
@@ -20,5 +20,5 @@ end
 
 class BuryVote < ReviewModerationVote
   belongs_to :app_review, :counter_cache => :bury_votes_count
-  belongs_to :gamer, :counter_cache => :bury_votes_count
+  belongs_to :gamer
 end
