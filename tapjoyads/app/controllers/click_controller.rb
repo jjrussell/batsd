e4f05cb7f8bbcb5409417ccc89e246fce87322de
Amir Manji <amanji75@gmail.com>
2012-04-27 15:41:15 -0700

    add mac_address to test rewards

--- tapjoyads/app/controllers/click_controller.rb
+++ tapjoyads/app/controllers/click_controller.rb
@@ -78,6 +78,7 @@ class ClickController < ApplicationController
     test_reward.publisher_app_id  = params[:publisher_app_id]
     test_reward.advertiser_app_id = params[:publisher_app_id]
     test_reward.offer_id          = params[:publisher_app_id]
+    test_reward.mac_address       = params[:mac_address]
     test_reward.currency_reward   = @currency.get_reward_amount(@test_offer)
     test_reward.publisher_amount  = 0
     test_reward.advertiser_amount = 0
@@ -100,6 +101,7 @@ class ClickController < ApplicationController
     test_reward.publisher_app_id  = params[:publisher_app_id]
     test_reward.advertiser_app_id = params[:publisher_app_id]
     test_reward.offer_id          = params[:publisher_app_id]
+    test_reward.mac_address       = params[:mac_address]
     test_reward.currency_reward   = @currency.get_reward_amount(@offer)
     test_reward.publisher_amount  = 0
     test_reward.advertiser_amount = 0
