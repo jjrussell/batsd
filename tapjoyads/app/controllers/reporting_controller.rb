7f1c250867811457d41240d655e867c2905657be
James Huang <jhuang1982@gmail.com>
2012-04-30 15:14:53 -0700

    1 line

--- tapjoyads/app/controllers/reporting_controller.rb
+++ tapjoyads/app/controllers/reporting_controller.rb
@@ -82,9 +82,7 @@ class ReportingController < WebsiteController
   private
 
   def show_update_notice
-    if Delayed.show_in_duration?
-      flash.now[:notice] = 'Reporting for Tapjoy.com and In-App offerwalls is tracked separately after 5/1/2012'
-    end
+    flash.now[:notice] = 'Reporting for Tapjoy.com and In-App offerwalls is tracked separately after 5/1/2012' if Delayed.show_in_duration?
   end
 
   def find_offer
