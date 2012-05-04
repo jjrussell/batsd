492f3237c7d69e3a60f3fec396eea29792142647
Jeremy Chotiner <jeremy.chotiner@tapjoy.com>
2012-05-04 15:15:53 -0400

    update var name

--- tapjoyads/app/controllers/games/confirmations_controller.rb
+++ tapjoyads/app/controllers/games/confirmations_controller.rb
@@ -1,24 +1,24 @@
 class Games::ConfirmationsController < GamesController
 
   def create
-    device_info = params[:data] ? ObjectEncryptor.decrypt(params[:data]) : {}
-    device_info[:token] = params[:token] if params[:token]
-    @gamer = Gamer.find_by_confirmation_token(device_info[:token])
+    data = params[:data] ? ObjectEncryptor.decrypt(params[:data]) : {}
+    data[:token] = params[:token] if params[:token]
+    @gamer = Gamer.find_by_confirmation_token(data[:token])
     path = games_root_path
     if @gamer.present? and @gamer.confirmed_at?
         flash[:notice] = 'Email address already confirmed.'
     elsif @gamer.present? && @gamer.confirm!
       flash[:notice] = 'Email address confirmed.'
-      if device_info[:content]
+      if data[:content]
         path = games_root_path(:utm_campaign => 'welcome_email',
                                :utm_medium   => 'email',
                                :utm_source   => 'tapjoy',
-                               :utm_content  => device_info[:content],
+                               :utm_content  => data[:content],
                                :data         => params[:data])
       end
 
-      if device_info[:content] == 'confirm_only'
-        message = {:gamer_id => @gamer.id, :email_type => 'post_confirm'}.merge(device_info)
+      if data[:content] == 'confirm_only'
+        message = {:gamer_id => @gamer.id, :email_type => 'post_confirm'}.merge(data)
         Sqs.send_message(QueueNames::SEND_WELCOME_EMAILS, Base64::encode64(Marshal.dump(message)))
       end
     else
