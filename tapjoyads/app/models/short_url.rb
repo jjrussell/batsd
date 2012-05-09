6472b43bf2927597323291e5b5b502728b3ead6c
Jeremy Chotiner <jeremy.chotiner@tapjoy.com>
2012-05-09 15:23:36 -0400

    clean up some syntax

--- tapjoyads/app/models/short_url.rb
+++ tapjoyads/app/models/short_url.rb
@@ -4,19 +4,21 @@ class ShortUrl < ActiveRecord::Base
   validates_presence_of :token
   validates_uniqueness_of :token
 
+  before_validation :generate_token, :on => :create
+
   BASE_PATH = "#{WEBSITE_URL}/confirm/redirect/"
 
   def self.shorten(long_url, expiry = nil, token = nil)
-    short_url =  self.new(:long_url => long_url, :expiry => expiry, :token => token)
+    short_url = self.new(:long_url => long_url, :expiry => expiry, :token => token)
     short_url.save
     short_url
   end
 
-  def before_validation_on_create
+  def generate_token
     self.token = Authlogic::Random.friendly_token if self.new_record? and self.token.nil?
   end
 
   def url
-    return BASE_PATH + token
+    BASE_PATH + token
   end
 end
