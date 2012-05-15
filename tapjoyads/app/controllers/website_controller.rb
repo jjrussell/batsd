40692d2704917301e457e22557f6c9f3215938ad
Adam Sumner <adam.sumner@tapjoy.com>
2012-05-15 15:45:50 -0700

    Changed wording as requested by @limmster.

--- tapjoyads/app/controllers/website_controller.rb
+++ tapjoyads/app/controllers/website_controller.rb
@@ -18,9 +18,10 @@ class WebsiteController < ApplicationController
                     If you have any questions/concerns, please contact <a href='mailto:support@tapjoy.com'>support@tapjoy.com</a>.
                     <br><br>Read more at our blog: <a href='http://blog.tapjoy.com/for-developers/tapjoy-sdk-update/'>http://blog.tapjoy.com/for-developers/tapjoy-sdk-update/</a>"
 
-  VIDEO_OFFERS_NOTICE = "Please contact your Tapjoy contact or <a href='mailto:support@tapjoy.com'>support@tapjoy.com</a> to learn more about publishing our video ads
-                         in your app and/or running video campaigns in our network. Ask us about our current video promotions!
-                         Please also note, we can now track views to install reporting."
+  VIDEO_OFFERS_NOTICE = "A new iOS and Android SDK (v8.2.0) update is now available here for both Publishers and 
+                        Advertisers. Moving forward, please update the Tapjoy SDK for all apps you're submitting 
+                        to either Apple or Google. There are a number of important updates - please view our change 
+                        log for all the details. If you have any questions/concerns, please contact <a href='mailto:support@tapjoy.com'>support@tapjoy.com</a>."
 
   def sanitize_currency_params(object, fields)
     unless object.nil?
