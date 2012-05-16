b4efd7a75decf401272081dc27ec9dc29d1c1dd9
Brian Stebar II <brian.stebar@tapjoy.com>
2012-05-16 15:22:51 -0400

    Removed the old commented out stuff

--- tapjoyads/app/helpers/tapjoy_mailer_helper.rb
+++ tapjoyads/app/helpers/tapjoy_mailer_helper.rb
@@ -1,6 +1,5 @@
 module TapjoyMailerHelper
   def support_request_field(name, value)
-    #{}"<p><strong>#{name.to_s.humanize}: </strong>#{value.present? ? value : "N/A"}</p>"
     content_tag :p do
       content_tag(:strong, "#{name.to_s.humanize}: ") +
       "#{value.present? ? value : "N/A"}"
