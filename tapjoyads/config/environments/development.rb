b39395a2e128e9a89b700b23d4001f4f346baeb2
Jeff Dickey <jeff@dickey.xxx>
2012-05-16 16:03:58 -0700

    added job routes

--- tapjoyads/config/environments/development.rb
+++ tapjoyads/config/environments/development.rb
@@ -24,7 +24,7 @@ Tapjoyad::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
-  %w( api dashboard website web legacy ).each do |route|
+  %w( api job dashboard website web legacy ).each do |route|
     config.paths.config.routes << Rails.root.join("config/routes/#{route}.rb")
   end
 end
