a70b50a86b66afa82bb095580d5ada83fb6c6fe1
bertrand.kuo <bert@tapjoy.com>
2012-05-16 12:26:00 -0700

    fixed routes, added routing spec

--- tapjoyads/spec/routing/tjm_routing_spec.rb
+++ tapjoyads/spec/routing/tjm_routing_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'routes for tjm' do
+describe 'routes for tjm', :type => :routing do
   it 'should be routable' do
     get('translations/test_filename-hashyhashhash.js')
   end
