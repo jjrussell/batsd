1df55b0f22b73388cc8b53c1764d8bce39f70f76
Simon Yu <simon.yu@tapjoy.com>
2012-05-21 14:48:08 -0700

    Remove the extra files.

--- tapjoyads/console/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '1f1e6eb1f8d3e8957841762abe047e4db6a61e6cce396b889c3335cde21319979281f49de6896c90d0696cbdd70e46c277b1b4c8b7204cbeb42a4e2bb88415d3';
