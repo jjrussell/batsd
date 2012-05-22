2e3d8d095c2577239f7ab2bd6e66c5bad81320e9
Brian Stebar II <brian.stebar@tapjoy.com>
2012-05-22 15:58:42 -0400

    Specs for gamer management tool

--- tapjoyads/spec/controllers/dashboard/tools/gamers_controller_spec.rb
+++ tapjoyads/spec/controllers/dashboard/tools/gamers_controller_spec.rb
@@ -11,7 +11,7 @@ describe Dashboard::Tools::GamersController do
         user = Factory :customer_service_user
         login_as user
 
-        get(:index)
+        get :index
       end
 
       it "allows access" do
@@ -24,7 +24,7 @@ describe Dashboard::Tools::GamersController do
         user = Factory :account_mgr_user
         login_as user
 
-        get(:index)
+        get :index
       end
 
       it "allows access" do
@@ -37,7 +37,58 @@ describe Dashboard::Tools::GamersController do
         user = Factory :partner_user
         login_as user
 
-        get(:index)
+        get :index
+      end
+
+      it "disallows access" do
+        response.should_not be_success
+      end
+    end
+  end
+
+  describe "#show" do
+    before :each do
+      @gamer = Factory :gamer
+      @params = { :id => @gamer.id }
+    end
+
+    context "when logged in as customer service" do
+      before :each do
+        user = Factory :customer_service_user
+        login_as user
+      end
+
+      it "allows access" do
+        get(:show, @params)
+        response.should be_success
+      end
+
+      it "creates a gamer_profile for the gamer if one doesn't exist" do
+        @gamer.gamer_profile.should be_nil
+        get(:show, @params)
+        assigns(:gamer).gamer_profile.should_not be_nil
+      end
+    end
+
+    context "when logged in as an account manager" do
+      before :each do
+        user = Factory :account_mgr_user
+        login_as user
+
+        get(:show, @params)
+      end
+
+      it "allows access" do
+        response.should be_success
+      end
+    end
+
+    context "when logged in as a partner" do
+      before :each do
+        user = Factory :partner_user
+        login_as user
+
+        get(:show, @params)
       end
 
       it "disallows access" do
