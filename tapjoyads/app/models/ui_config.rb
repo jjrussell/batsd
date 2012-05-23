4ba197e1ddc589a77e45899d8bfe23503fbe0cd7
Van Pham <van.pham@yahoo.com>
2012-05-23 10:35:05 -0700

    update to distributed

--- tapjoyads/app/models/ui_config.rb
+++ tapjoyads/app/models/ui_config.rb
@@ -1,15 +1,15 @@
 class UiConfig
 
   def self.is_fb_signup_hidden
-    Mc.get('tjm.ui.hide_fb_signup')
+    Mc.distributed_get('tjm.ui.hide_fb_signup')
   end
 
   def self.hide_fb_signup
-    Mc.put('tjm.ui.hide_fb_signup', true)
+    Mc.distributed_put('tjm.ui.hide_fb_signup', true)
   end
 
   def self.show_fb_signup
-    Mc.delete('tjm.ui.hide_fb_signup')
+    Mc.distributed_delete('tjm.ui.hide_fb_signup')
   end
 
 end
