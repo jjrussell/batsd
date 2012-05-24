bc0abd6eb72f7d61800a52c1bea6d444d8b7d5c9
Jeff Dickey <jeff@dickey.xxx>
2012-05-24 14:53:58 -0700

    fixed offer events on tools

--- tapjoyads/app/controllers/dashboard/tools/offer_events_controller.rb
+++ tapjoyads/app/controllers/dashboard/tools/offer_events_controller.rb
@@ -1,5 +1,58 @@
 class Dashboard::Tools::OfferEventsController < Dashboard::DashboardController
   current_tab :tools
+  before_filter :setup
+
+  def index
+    if params[:filter] == 'all'
+      @event_scope = 'all'
+    elsif params[:filter] == 'completed'
+      @event_scope = 'completed'
+    elsif params[:filter] == 'disabled'
+      @event_scope = 'disabled'
+    else
+      @event_scope = 'upcoming'
+    end
+
+    @offer_events = offer_events_scope.send(@event_scope)
+  end
+
+  def new
+    @offer_event = new_offer_event
+  end
+
+  def create
+    @offer_event = new_offer_event
+    log_activity(@offer_event)
+    @offer_event.attributes = params[:offer_event]
+    if @offer_event.save
+      flash[:notice] = "Created Event for #{@offer_event.offer.name}."
+      redirect_to :action => 'index' and return
+    else
+      flash.now[:error] = "Event could not be created. #{@offer_event.errors[:base]}"
+      render :new and return
+    end
+  end
+
+  def edit
+    flash.now[:warning] = 'You are viewing a Scheduled Event that has already been run or disabled.' unless @offer_event.editable?
+  end
+
+  def update
+    log_activity(@offer_event)
+    if @offer_event.update_attributes(params[:offer_event])
+      flash[:notice] = "Updated Event for #{@offer_event.offer.name}"
+      redirect_to :action => 'index' and return
+    else
+      flash.now[:error] = "Event could not be updated. #{@offer_event.errors[:base]}"
+      render :edit and return
+    end
+  end
+
+  def destroy
+    log_activity(@offer_event)
+    @offer_event.disable!
+    redirect_to :action => 'index' and return
+  end
 
   private
 
