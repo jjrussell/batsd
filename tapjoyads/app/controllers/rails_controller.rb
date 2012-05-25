72e1b78a543f169bf4798fd4f79391118cbdc28d
James Logsdon <jlo@tapjoy.com>
2012-05-25 15:55:59 -0400

    ensure the hash presented by rails/info is ordered the same

--- tapjoyads/app/controllers/rails_controller.rb
+++ tapjoyads/app/controllers/rails_controller.rb
@@ -1,15 +1,16 @@
 class RailsController < ApplicationController
   def info
-    info = {
-      :environment => Rails.env,
-      :git_rev => GIT_REV,
-      :git_branch => GIT_BRANCH,
-      :host_name => HOSTNAME,
-      :time_now => Time.now,
-      :time_zone_now => Time.zone.now,
-      :time_zone => Time.zone.name,
-      :machine_type => MACHINE_TYPE
-    }
+    # Ruby 1.8 hashes are not ordered, so you can't simply wrap the original hash :(
+    info = ActiveSupport::OrderedHash.new
+    info[:environment]   = Rails.env
+    info[:git_rev]       = GIT_REV
+    info[:git_branch]    = GIT_BRANCH
+    info[:host_name]     = HOSTNAME
+    info[:time_now]      = Time.now
+    info[:time_zone_now] = Time.zone.now
+    info[:time_zone]     = Time.zone.name
+    info[:machine_type]  = MACHINE_TYPE
+
     render :json => info
   end
 end
