97b2666c93e00c39357f984b96d2d281a6faadfc
Jeff Dickey <jeff@dickey.xxx>
2012-05-25 18:26:59 -0700

    removed ssl_requirement

--- tapjoyads/app/controllers/agency_api_controller.rb
+++ tapjoyads/app/controllers/agency_api_controller.rb
@@ -1,6 +1,4 @@
 class AgencyApiController < ApplicationController
-  include SslRequirement
-
   skip_before_filter :fix_params, :reject_banned_ips
   skip_before_filter :verify_authenticity_token
 
