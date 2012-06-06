ff3d8f3cd0fae2fb22f9f143eb3761280de8e937
Michael Wheeler <michael.wheeler@tapjoy.com>
2012-06-06 12:34:10 -0700

    Added a dev-mode cache bust for translations

--- tapjoyads/app/controllers/games_controller.rb
+++ tapjoyads/app/controllers/games_controller.rb
@@ -32,7 +32,8 @@ class GamesController < ApplicationController
   end
 
   def get_locale_filename
-    "#{I18n.locale}-#{t('hash',:locale => I18n.default_locale)}#{t('hash')}"
+    dev_bust = Rails.env.development? || Rails.env.test? ? Time.now.to_i : ""
+    "#{I18n.locale}-#{t('hash',:locale => I18n.default_locale)}#{t('hash')}#{dev_bust}"
   end
 
   def get_language_codes
