32efda5ff82dd0c324e3f4dc156dc8559dacfdef
Eric Tipton <eric.tipton@tapjoy.com>
2012-06-12 19:13:19 -0400

    Revert "clean up some syntax"
    This reverts commit 6472b43bf2927597323291e5b5b502728b3ead6c.

--- tapjoyads/app/models/short_url.rb
+++ tapjoyads/app/models/short_url.rb
@@ -4,21 +4,19 @@ class ShortUrl < ActiveRecord::Base
   validates_presence_of :token
   validates_uniqueness_of :token
 
-  before_validation :generate_token, :on => :create
-
   BASE_PATH = "#{WEBSITE_URL}/confirm/redirect/"
 
   def self.shorten(long_url, expiry = nil, token = nil)
-    short_url = self.new(:long_url => long_url, :expiry => expiry, :token => token)
+    short_url =  self.new(:long_url => long_url, :expiry => expiry, :token => token)
     short_url.save
     short_url
   end
 
-  def generate_token
+  def before_validation_on_create
     self.token = Authlogic::Random.friendly_token if self.new_record? and self.token.nil?
   end
 
   def url
-    BASE_PATH + token
+    return BASE_PATH + token
   end
 end
