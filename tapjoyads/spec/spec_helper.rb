6f0f95e4d1e90e45ff696960ff028a2f335f010d
bertrand.kuo <bert@tapjoy.com>
2012-06-13 16:42:38 -0700

    Garbage collection throttled to once per 5 seconds,  for 30% faster specs.

--- tapjoyads/spec/spec_helper.rb
+++ tapjoyads/spec/spec_helper.rb
@@ -1,6 +1,26 @@
 require 'rubygems'
 require 'spork'
 
+unless defined?(DeferredGarbageCollection)
+  class DeferredGarbageCollection
+    DEFERRED_GC_THRESHOLD = 5.0
+    @@last_gc_run = Time.now
+
+    def self.start
+      GC.disable if DEFERRED_GC_THRESHOLD > 0
+    end
+
+    def self.reconsider
+      if DEFERRED_GC_THRESHOLD > 0 && Time.now - @@last_gc_run >= DEFERRED_GC_THRESHOLD
+        GC.enable
+        GC.start
+        GC.disable
+        @@last_gc_run = Time.now
+      end
+    end
+  end
+end
+
 Spork.prefork do
   require 'pry'
   ENV["RAILS_ENV"] ||= 'test'
@@ -21,12 +41,18 @@ Spork.prefork do
     config.include(SpecHelpers)
     config.include(DashboardHelpers)
     config.include(Authlogic::TestCase)
+    config.before(:all) do
+      DeferredGarbageCollection.start
+    end
     config.before(:each) do
       Resolv.stub!(:getaddress=>'1.1.1.1')
       RightAws::SdbInterface.stub!(:new => FakeSdb.new)
       SimpledbResource.reset_connection
       AWS::S3.stub!(:new => FakeS3.new)
       Sqs.stub(:send_message)
+    end
+    config.after(:all) do
+      DeferredGarbageCollection.reconsider
     end
   end
 end
