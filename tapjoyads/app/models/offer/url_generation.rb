4f60364e789fa3c28f201e72a8d555158b0d4ecc
Amir Manji <amanji75@gmail.com>
2012-06-13 04:08:27 -0700

    quick deeplink fix

--- tapjoyads/app/models/offer/url_generation.rb
+++ tapjoyads/app/models/offer/url_generation.rb
@@ -95,7 +95,7 @@ module Offer::UrlGeneration
     when 'DeeplinkOffer'
       params = { :udid => udid, :id => currency.id, :click_key => click_key }
       data=ObjectEncryptor.encrypt(params)
-      final_url = "#{WEBSITE_URL}/games/earn?data=#{data}"
+      final_url = "#{WEBSITE_URL}/earn?data=#{data}"
     end
     final_url
   end
