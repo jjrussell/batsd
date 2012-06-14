941133649b419e62458085f15d5bc55990e84938
Jeff Dickey <jeff@dickey.xxx>
2012-06-14 11:42:11 -0700

    cleaned up locale function

--- tapjoyads/app/controllers/application_controller.rb
+++ tapjoyads/app/controllers/application_controller.rb
@@ -81,21 +81,16 @@ class ApplicationController < ActionController::Base
   end
 
   def set_locale
+    I18n.locale = nil
     language_code = params[:language_code]
-    if language_code
+    if language_code.present?
       language_code.downcase!
-      I18n.locale = nil
+      language_code = language_code.split('-').first if language_code['-']
       if I18n.available_locales.collect(&:to_s).include?(language_code)
         I18n.locale = language_code
-      elsif language_code.present? && language_code['-']
-        language_code = language_code.split('-').first
-        if I18n.available_locales.collect(&:to_s).include?(language_code)
-          I18n.locale = language_code
-        end
       end
-    else
-      I18n.locale = I18n.default_locale
     end
+    I18n.locale ||= I18n.default_locale
   end
 
   def lookup_udid
