eac31aa6f858b284cbc9f3c7883b3fc09313c462
Brian Stebar II <brian.stebar@tapjoy.com>
2012-06-18 12:08:04 -0400

    Added another context

--- tapjoyads/spec/support/contexts/logged_in_as_a_customer_service_user.rb
+++ tapjoyads/spec/support/contexts/logged_in_as_a_customer_service_user.rb
@@ -1,7 +1,7 @@
 shared_context "logged in as a customer service user" do
   let(:user) { user = FactoryGirl.create(:customer_service_user)
-              FactoryGirl.create(:partner, :pending_earnings => 10000, :balance => 10000, :users => [user])
-              user }
+               FactoryGirl.create(:partner, :pending_earnings => 10000, :balance => 10000, :users => [user])
+               user }
 
   before(:each) do
     activate_authlogic
