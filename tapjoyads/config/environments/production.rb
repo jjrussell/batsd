1477396239210a917a914b200b7dccb4ef358915
Van Pham <van.pham@yahoo.com>
2012-06-20 15:07:33 -0700

    add external domain

--- tapjoyads/config/environments/production.rb
+++ tapjoyads/config/environments/production.rb
@@ -79,7 +79,7 @@ SPROCKETS_CONFIG = {
 
 RUN_MODE_PREFIX = ''
 API_URL = local_config['api_url'] || 'https://ws.tapjoyads.com'
-API_URL_EXT = local_config['api_url'] || 'https://ws-ext.tapjoyads.com'
+API_URL_EXT = local_config['api_url_ext'] || 'https://ws-ext.tapjoyads.com'
 
 DASHBOARD_URL = local_config['dashboard_url'] || 'https://dashboard.tapjoy.com'
 WEBSITE_URL = local_config['website_url'] || 'https://www.tapjoy.com'
