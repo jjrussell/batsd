f321ff0bd3f3141aaef2b41159db3fb26d536d17
Adam Sumner <adam.sumner@tapjoy.com>
2012-06-19 18:59:07 -0700

    Add verifier.

--- tapjoyads/app/controllers/user_events_controller.rb
+++ tapjoyads/app/controllers/user_events_controller.rb
@@ -1,25 +1,35 @@
 class UserEventsController < ApplicationController
 
+  USER_EVENT_SALT = '(jdfj(DF(#{&FD@<SDXEDDD^sf3ef3s}'
+
+  before_filter :check_params
+
   def create
-    verify_params([:app_id, :udid, :event_type_id])
-    unless params_valid?
-      return render :text => t('user_event.error.bad_params') , :status => :precondition_failed
-    end
     event = UserEvent.new(params)
     if event.valid?
       event.save
-      render :text => t('user_event.success.created') , :status => :created
+      render :text => t('user_event.success.created'), :status => :created
     else
-      render :text => t('user_event.error.bad_event') , :status => :not_acceptable
+      render :text => t('user_event.error.bad_event'), :status => :not_acceptable
     end
   end
 
   private
 
-  def params_valid?
-    app           = App.find_in_cache(params[:app_id])
-    #Using Integer(n) raises exceptions when n isn't a valid int, instead of .to_i(), which will return 0
-    event_type    = UserEvent::EVENT_TYPE_IDS[Integer(params[:event_type_id])] rescue nil
-    app.present? && event_type.present?
+  def check_params
+    render :text => t('user_event.error.bad_verifier'), :status => :precondition_failed and return unless verified?
+    app = App.find_in_cache(params[:app_id])
+    #Using Integer(n) raises exceptions when n isn't a valid int, instead of n.to_i(), which would return 0
+    event_type = UserEvent::EVENT_TYPE_IDS[Integer(params[:event_type_id])] rescue nil
+    render :text => t('user_event.error.bad_params'), :status => :precondition_failed unless app && event_type
   end
+
+  def verified?
+    verify_params([ :verifier, :app_id, :udid, :event_type_id ])
+    verifier = params.delete(:verifier)
+    params_string = params.map { |key, val| "#{key}=#{val}" }.join('&')
+    verifier == Digest::SHA1.digest(USER_EVENT_SALT + params_string)
+  end
+
+
 end
