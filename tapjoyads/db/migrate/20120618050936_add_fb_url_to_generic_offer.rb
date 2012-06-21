3185bdfdfe62f7b95d7554aa8eff7bc860b16d05
Van Pham <van.pham@yahoo.com>
2012-06-21 15:10:51 -0700

    remove unused stuff

--- tapjoyads/db/migrate/20120618050936_add_fb_url_to_generic_offer.rb
+++
@@ -1,9 +0,0 @@
-class AddFbUrlToGenericOffer < ActiveRecord::Migration
-  def self.up
-    add_column :generic_offers, :fb_url, :string
-  end
-
-  def self.down
-    remove_column :generic_offers, :fb_url
-  end
-end
