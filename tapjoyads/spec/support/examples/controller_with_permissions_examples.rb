5be6c68d24ce625c112b66c124e1168986b11ed6
Brian Stebar II <brian.stebar@tapjoy.com>
2012-06-22 17:37:44 -0400

    Switched to using declarative_authorizations permissions checks directly since using `be_success` isn't very reliable for checking permissions

--- tapjoyads/spec/support/examples/controller_with_permissions_examples.rb
+++ tapjoyads/spec/support/examples/controller_with_permissions_examples.rb
@@ -1,25 +1,21 @@
 shared_examples "a controller with permissions" do
   PERMISSIONS_MAP.each_pair do |action, map|
-    describe "##{action}" do
-      let(:params) { hash = map[:params].present? ? map[:params] : {}
-                     Hash[hash.map do |k,v|
-                       v = v.call if v.is_a?(Proc)
-                       [k,v]
-                     end] }
-      
-      map[:permissions].each_pair do |user_type, permission|
-        context "when logged in as #{user_type.to_s.humanize.downcase} user" do
-          include_context "logged in as user type", user_type
-          if permission
-            it "allows access" do
-              get action, params
-              response.should be_success
-            end
-          else
-            it "disallows access" do
-              get action, params
-              response.should_not be_success
-            end
+    let(:params) { hash = map[:params].present? ? map[:params] : {}
+                   Hash[hash.map do |k,v|
+                     v = v.call if v.is_a?(Proc)
+                     [k,v]
+                   end] }
+    
+    map[:permissions].each_pair do |user_type, permission|
+      context "when logged in as #{user_type.to_s.humanize.downcase} user, ##{action}" do
+        include_context "logged in as user type", user_type
+        if permission
+          it "allows access" do
+            controller.should be_permitted_to action
+          end
+        else
+          it "disallows access" do
+            controller.should_not be_permitted_to action
           end
         end
       end
