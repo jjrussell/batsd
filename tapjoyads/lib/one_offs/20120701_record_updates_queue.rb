5c4980298ce71c7656eacbcfbf982819c50e1ad9
Eric Tipton <eric.tipton@tapjoy.com>
2012-07-01 22:08:17 -0500

    oops, need the job… unsure about # of seconds

--- tapjoyads/lib/one_offs/20120701_record_updates_queue.rb
+++ tapjoyads/lib/one_offs/20120701_record_updates_queue.rb
@@ -1,5 +1,6 @@
 class OneOffs
   def self.add_record_updates_queue
     Sqs.create_queue('RecordUpdates', 30)
+    Job.create!(:active => true, :job_type => 'queue', :controller => 'queue_record_updates', :action => 'index', :frequency => 'interval', :seconds => '0')
   end
 end
