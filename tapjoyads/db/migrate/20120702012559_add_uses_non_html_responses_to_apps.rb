123641a351bbab435d041898d810041979121bfe
Eric Tipton <eric.tipton@tapjoy.com>
2012-07-01 22:34:45 -0500

    we'll use another PR for using the queue for the new column

--- tapjoyads/db/migrate/20120702012559_add_uses_non_html_responses_to_apps.rb
+++ tapjoyads/db/migrate/20120702012559_add_uses_non_html_responses_to_apps.rb
@@ -1,6 +1,8 @@
 class AddUsesNonHtmlResponsesToApps < ActiveRecord::Migration
   def self.up
     add_column :apps, :uses_non_html_responses, :boolean, :null => false, :default => false
+
+    App.cache_all
   end
 
   def self.down
