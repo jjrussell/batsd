631d337724b2aa4cdda7b81c8dae37b917ed26a2
Eric Tipton <eric.tipton@tapjoy.com>
2012-07-03 15:37:31 -0400

    changes per code review

--- tapjoyads/db/migrate/20120702012559_add_uses_non_html_responses_to_apps.rb
+++ tapjoyads/db/migrate/20120702012559_add_uses_non_html_responses_to_apps.rb
@@ -1,8 +1,6 @@
 class AddUsesNonHtmlResponsesToApps < ActiveRecord::Migration
   def self.up
     add_column :apps, :uses_non_html_responses, :boolean, :null => false, :default => false
-
-    App.cache_all
   end
 
   def self.down
