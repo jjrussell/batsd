3a53cf918fa0cd4d4c4ac4df091d573d44e7e17f
Eric Tipton <eric.tipton@tapjoy.com>
2012-07-06 02:14:07 -0400

    add back pre-commit hook script

--- tapjoyads/setup_repo.sh
+++ tapjoyads/setup_repo.sh
@@ -1,4 +1,5 @@
-cp -v config/newrelic-test.yml config/newrelic.yml
-cp -v config/database-default.yml config/database.yml
-cp -v config/local-default.yml config/local.yml
+cp -iv config/newrelic-test.yml config/newrelic.yml
+cp -iv config/database-default.yml config/database.yml
+cp -iv config/local-default.yml config/local.yml
+ln -ivs ../../tapjoyads/config/pre-commit ../.git/hooks/pre-commit
 [ -f "data/GeoIPCity.dat" ] || curl http://s3.amazonaws.com/dev_tapjoy/rails_env/GeoLiteCity.dat.gz | gunzip > data/GeoIPCity.dat
