a5da67dadb1f90046b146acde8d59497565e4f72
Hwan-Joon Choi <hwanjoon@tapjoy.com>
2012-07-10 21:53:08 -0700

    Revert "Disable the submit button, per an implied suggestion by @hc5duke"
    This reverts commit fff259f36e573492193dcc6a1b63681e1aab7195.

--- tapjoyads/public/javascripts/application.js
+++ tapjoyads/public/javascripts/application.js
@@ -3,15 +3,15 @@ if (typeof(console) == "undefined") console={log:$.noop};
 
 function formatCurrency(field) {
   var value = field.val();
-  var form  = field.closest('form');
+  //var form  = field.closest('form');
   var allowNegative = field.hasClass('allow_negative');
   if(value.match(/^\s*\$?\d+(,\d{3})*(\.\d+)?\s*$/) || (allowNegative && /^\s*\-?\s*\$?\d+(,\d{3})*(\.\d+)?\s*$/)) {
     field.val(numberToCurrency(stringToNumber(value, field.hasClass('allow_negative'))));
     field.removeClass('error');
-    form.find('input[type=submit]').attr('disabled', false);
+    //form.find('input[type=submit]').attr('disabled', false);
   } else {
     field.addClass('error');
-    form.find('input[type=submit]').attr('disabled', true);
+    //form.find('input[type=submit]').attr('disabled', true);
   }
 }
 
