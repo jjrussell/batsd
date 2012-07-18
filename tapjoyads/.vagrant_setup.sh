d7dd10dfc3c65442bba23a8d04427a5a2616d6d9
dickeyxxx <jeff@dickey.xxx>
2012-07-18 12:37:00 -0400

    whoops, not tapjoyads

--- tapjoyads/.vagrant_setup.sh
+++ tapjoyads/.vagrant_setup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-cd /vagrant/tapjoyads
+cd /vagrant
 
 sudo apt-get update
 sudo apt-get install libcurl4-openssl-dev gettext libsasl2-dev redis-server imagemagick libmagickwand-dev -y
 
 # Setup repo
-cp -v /vagrant/tapjoyads/config/newrelic-test.yml /vagrant/tapjoyads/config/newrelic.yml
-cp -v /vagrant/tapjoyads/config/database-default.yml /vagrant/tapjoyads/config/database.yml
-cp -v /vagrant/tapjoyads/config/local-default.yml /vagrant/tapjoyads/config/local.yml
-cp -v /vagrant/tapjoyads/config/local-default.yml /vagrant/tapjoyads//config/local.yml
-[ -f "/vagrant/tapjoyads/data/GeoIPCity.dat" ] || curl http://s3.amazonaws.com/dev_tapjoy/rails_env/GeoLiteCity.dat.gz | gunzip > /vagrant/tapjoyads/data/GeoIPCity.dat
-touch /vagrant/tapjoyads/data/GeoIPCity.version
-mkdir -p /vagrant/tapjoyads/tmp
+cp -v /vagrant/config/newrelic-test.yml /vagrant/config/newrelic.yml
+cp -v /vagrant/config/database-default.yml /vagrant/config/database.yml
+cp -v /vagrant/config/local-default.yml /vagrant/config/local.yml
+cp -v /vagrant/config/local-default.yml /vagrant//config/local.yml
+[ -f "/vagrant/data/GeoIPCity.dat" ] || curl http://s3.amazonaws.com/dev_rails_env/GeoLiteCity.dat.gz | gunzip > /vagrant/data/GeoIPCity.dat
+touch /vagrant/data/GeoIPCity.version
+mkdir -p /vagrant/tmp
