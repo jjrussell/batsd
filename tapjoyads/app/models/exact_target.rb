2603e061c6d8f15ac94a90aa7737c85fb18b4d17
Brian Stebar II <brian.stebar@tapjoy.com>
2012-07-18 15:27:35 -0400

    More progress

--- tapjoyads/app/models/exact_target.rb
+++ tapjoyads/app/models/exact_target.rb
@@ -22,12 +22,13 @@ class ExactTarget
       :account_id     => ExactTarget::ET_TAPJOY_CONSUMER_ACCOUNT_ID,
       :interaction_id => ExactTarget::ET_TJM_WELCOME_EMAIL_ID,
       :subscriber     => "brian.stebar@tapjoy.com",
+      :priority       => "High",
       :custom_fields  => {
         :android_device           => 0,
         :confirmation_url         => "https://www.tapjoy.com/confirm?token=CONFIRMATION_TOKEN",
         :currency_id              => "CURRENCY_ID",
         :currency_name            => "GOLD COINS",
-        :facebook_signup          => 1,
+        :facebook_signup          => 0,
         :gamer_email              => "gamer_email@tapjoy.com",
         :linked                   => 1,
         :publisher_icon_url       => "https://s3.amazonaws.com/tapjoy/icons/SIZE/ICON_ID.jpg",
@@ -58,20 +59,23 @@ class ExactTarget
       soap.body = triggered_email_soap_body(data)
     end
 
-    if response.success?
-      response.to_array(:create_response, :results)
-    end
+    response
+
+    # if response.success?
+    #   response.to_array(:create_response, :results)
+    # end
   end
 
   def triggered_email_soap_body(data)
     xml = Builder::XmlMarkup.new
     namespaces = { "xmlns:par" => "http://exacttarget.com/wsdl/partnerAPI" }
 
-    xml.par :Objects, namespaces, "xsi:type" => "par:TriggeredSend" do |xml|
-      xml.par :Options do |xml|
-        xml.par :Priority, data[:priority] if data[:priority].present?
-      end
+    # xml.par :Options do |xml|
+    #   xml.par :RequestType, "Asynchronous"
+    #   xml.par :QueuePriority, "High"
+    # end
 
+    xml.par :Objects, namespaces, "xsi:type" => "par:TriggeredSend" do |xml|
       xml.par :Client do |xml|
         xml.par :ID, data[:account_id]
       end
