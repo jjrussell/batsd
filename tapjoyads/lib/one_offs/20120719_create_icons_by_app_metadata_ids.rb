4e54e8c556af25b10a50a44685f7626ec561ad5a
James Huang <jhuang1982@gmail.com>
2012-07-19 15:57:18 -0700

    fix bug

--- tapjoyads/lib/one_offs/20120719_create_icons_by_app_metadata_ids.rb
+++ tapjoyads/lib/one_offs/20120719_create_icons_by_app_metadata_ids.rb
@@ -9,13 +9,15 @@ class OneOffs
       bucket  = S3.bucket(BucketNames::TAPJOY)
 
       ['src', '256', '114', '57'].each do |path|
-        origin_icon_blob = bucket.objects["icons/#{path}/#{old_icon_id}.jpg"]
-        next if !origin_icon_blob.exists?
+        origin_icon_obj = bucket.objects["icons/#{path}/#{old_icon_id}.jpg"].read
+        next if !origin_icon_obj.exists?
+        origin_icon_blob = origin_icon_obj.read
         bucket.objects["icons/#{path}/#{new_icon_id}.jpg"].write(:data => origin_icon_blob, :acl => :public_read)
 
         if path == '57'
-          origin_icon_blob = bucket.objects["icons/57/#{old_icon_id}.png"]
-          next if !origin_icon_blob.exists?
+          origin_icon_obj = bucket.objects["icons/57/#{old_icon_id}.png"]
+          next if !origin_icon_obj.exists?
+          origin_icon_blob = origin_icon_obj.read
           bucket.objects["icons/57/#{new_icon_id}.png"].write(:data => origin_icon_blob, :acl => :public_read)
         end
       end
