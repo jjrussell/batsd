191a125274c467b6dba1fe0181a5ad078f0a807c
Brian Stebar II <brian.stebar@tapjoy.com>
2012-07-19 11:00:33 -0400

    Added app_ids to ALREADY_COMPLETED_IDS

--- tapjoyads/app/models/offer/rejecting.rb
+++ tapjoyads/app/models/offer/rejecting.rb
@@ -88,6 +88,9 @@ module Offer::Rejecting
     %w(523bde45-1abb-487c-8cef-3e32a189034f 43adf090-10e4-4775-a006-00301e5df1eb) => %w(523bde45-1abb-487c-8cef-3e32a189034f 43adf090-10e4-4775-a006-00301e5df1eb),
     # Crickler 2 iOS
     %w(2533d812-1685-4f44-b121-a7126bd83ba8 1dd2a86d-42d0-454f-94d4-5b79a02b52cf) => %w(2533d812-1685-4f44-b121-a7126bd83ba8 1dd2a86d-42d0-454f-94d4-5b79a02b52cf),
+    # CoffeeTable - Catalog Shopping for iPad
+    %w(5dfbe67b-8033-4b8f-8db7-349838d7eb57 86ba87a2-c605-4fac-bb6a-e1983f9de44e c0ffa993-9ad4-48bc-9e74-a43316cc80e1 27acb266-7f9b-424d-97da-c05ea1fe58ce) =>
+    %w(5dfbe67b-8033-4b8f-8db7-349838d7eb57 86ba87a2-c605-4fac-bb6a-e1983f9de44e c0ffa993-9ad4-48bc-9e74-a43316cc80e1 27acb266-7f9b-424d-97da-c05ea1fe58ce),
   }
 
   TAPJOY_GAMES_RETARGETED_OFFERS = ['2107dd6a-a8b7-4e31-a52b-57a1a74ddbc1', '12b7ea33-8fde-4297-bae9-b7cb444897dc', '8183ce57-8ee4-46c0-ab50-4b10862e2a27']
