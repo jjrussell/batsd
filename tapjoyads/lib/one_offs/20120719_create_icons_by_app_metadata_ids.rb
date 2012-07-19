9bcbafab028147e9719c9ced7450ea4ec1c43452
James Huang <jhuang1982@gmail.com>
2012-07-19 16:13:19 -0700

    buuugs

--- tapjoyads/lib/one_offs/20120719_create_icons_by_app_metadata_ids.rb
+++ tapjoyads/lib/one_offs/20120719_create_icons_by_app_metadata_ids.rb
@@ -1,15 +1,13 @@
 class OneOffs
   def self.create_icons_by_app_metadata_ids
+    print "begin"
     App.live.each do |app|
-      next if app.primary_offer.blank?
-      metadata_id = app.primary_offer.app_metadata_id
-
       old_icon_id = Offer.hashed_icon_id(app.id)
-      new_icon_id = Offer.hashed_icon_id(metadata_id)
+      new_icon_id = Offer.hashed_icon_id(app.primary_app_metadata.id)
       bucket  = S3.bucket(BucketNames::TAPJOY)
 
       ['src', '256', '114', '57'].each do |path|
-        origin_icon_obj = bucket.objects["icons/#{path}/#{old_icon_id}.jpg"].read
+        origin_icon_obj = bucket.objects["icons/#{path}/#{old_icon_id}.jpg"]
         next if !origin_icon_obj.exists?
         origin_icon_blob = origin_icon_obj.read
         bucket.objects["icons/#{path}/#{new_icon_id}.jpg"].write(:data => origin_icon_blob, :acl => :public_read)
@@ -21,6 +19,7 @@ class OneOffs
           bucket.objects["icons/57/#{new_icon_id}.png"].write(:data => origin_icon_blob, :acl => :public_read)
         end
       end
+      break
     end
   end
 
