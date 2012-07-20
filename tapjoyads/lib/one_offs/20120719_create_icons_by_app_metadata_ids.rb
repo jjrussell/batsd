e6c8c6fd3ed3771c2cf5d6e306e5f7271f714b8f
James Huang <jhuang1982@gmail.com>
2012-07-20 15:52:59 -0700

    copy_to instead of read, write

--- tapjoyads/lib/one_offs/20120719_create_icons_by_app_metadata_ids.rb
+++ tapjoyads/lib/one_offs/20120719_create_icons_by_app_metadata_ids.rb
@@ -2,8 +2,10 @@ class OneOffs
   def self.create_icons_by_app_metadata_ids
     app_count = 0
     icon_count = 0
-
+    start_time = Time.now
+    
     App.live.each do |app|
+      puts "#{app_count}: #{app.name}"
       app_count += 1
       old_icon_id = Offer.hashed_icon_id(app.id)
       new_icon_id = Offer.hashed_icon_id(app.primary_app_metadata.id)
@@ -11,27 +13,23 @@ class OneOffs
 
       ['src', '256', '114', '57'].each do |path|
         origin_icon_obj = bucket.objects["icons/#{path}/#{old_icon_id}.jpg"]
-        if origin_icon_obj.exists?
-          origin_icon_blob = origin_icon_obj.read
-          bucket.objects["icons/#{path}/#{new_icon_id}.jpg"].write(:data => origin_icon_blob, :acl => :public_read)
-          icon_count += 1
-        else
+        begin
+          bucket.objects["icons/#{path}/#{old_icon_id}.jpg"].copy_to(AWS::S3::S3Object.new(bucket,"icons/#{path}/#{new_icon_id}.jpg",  {:acl => :public_read }))
+        rescue
           puts "error: App: #{app.name}, can't find icons/#{path}/#{old_icon_id}.jpg"
         end
 
         if path == '57'
-          origin_icon_obj = bucket.objects["icons/57/#{old_icon_id}.png"]
-          if origin_icon_obj.exists?
-            origin_icon_blob = origin_icon_obj.read
-            bucket.objects["icons/57/#{new_icon_id}.png"].write(:data => origin_icon_blob, :acl => :public_read)
-            icon_count += 1
-          else
+          begin
+            bucket.objects["icons/57/#{old_icon_id}.png"].copy_to(AWS::S3::S3Object.new(bucket,"icons/57/#{new_icon_id}.png",  {:acl => :public_read }))
+          rescue
             puts "error: App: #{app.name}, can't find icons/57/#{old_icon_id}.png"
           end
         end
       end
     end
     puts "total app prcessed: #{app_count}, icon copied: #{icon_count}"
+    puts "End time: #{start_time}, end time : #{Time.now}"
   end
 
 end
