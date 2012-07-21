29286ab61abcfeca5896edebdb4728bbc128dd12
Bert Kuo <bert@tapjoy.com>
2012-07-21 00:17:20 -0700

    enhanced FakeSDB, removed enable_sdb from all specs besides simpledb_resource_spec.

--- tapjoyads/lib/fake_sdb.rb
+++ tapjoyads/lib/fake_sdb.rb
@@ -4,22 +4,28 @@ class FakeSdb
 
   def put_attributes(domain, key, attrs_to_put, attrs_to_replace = {}, expected_attrs = {})
     existing_attrs = fake_sdb_data(domain)[key] || {}
-    expected_attrs.each do |k, v|
-      if k == 'version'
-        current_version = existing_attrs[k]
-        if current_version && current_version.first.to_i != v.to_i + 1
-          raise Simpledb::ExpectedAttributeError
-        end
-      elsif existing_attrs[k] != v
-        raise Simpledb::ExpectedAttributeError
+    unless expected_attrs.empty?
+      raise Simpledb::MultipleExistsConditionsError if expected_attrs.size > 1
+      expected_key = expected_attrs.first.first
+      expected_value = expected_attrs.first.last
+
+      #  This seems to match behavior in RightAWS's SdbInterface,  if not Amazon's developer guide at:
+      #  http://docs.amazonwebservices.com/AmazonSimpleDB/latest/DeveloperGuide/SDB_API_PutAttributes.html
+      #  RightAWS code seems to reject expected_attrs  that do not also exist in attrs_to_put
+      #  Probably worth investigating at some point.
+      existing = get_attributes(domain, key)[:attributes]
+
+      if attrs_to_put.keys.include?(expected_key)
+        raise Simpledb::ExpectedAttributeError if expected_value.nil? && existing[expected_key].present?
+        raise Simpledb::ExpectedAttributeError if expected_value != existing[expected_key].try(:first)
       end
     end
-    fake_sdb_data(domain)[key] = existing_attrs.merge(attrs_to_put)
+    fake_sdb_data(domain)[key] = existing_attrs.merge(attrs_to_put.dup)
   end
 
-  def get_attributes(domain, key, something, consistent)
+  def get_attributes(domain, key, attribute_name=nil, consistent=false)
     attributes = fake_sdb_data(domain)[key] || {}
-    { :attributes => attributes }
+    { :attributes => attributes.dup }
   end
 
   def delete_attributes(domain, key, attrs_to_delete = {}, expected_attrs = {})
@@ -54,7 +60,7 @@ class FakeSdb
     case options[:select].first
     when '*'
       array_results = final_results.map { |k, v| { k => v } }
-      { :items => array_results }
+      { :items => array_results.dup }
     when 'count(*)'
       { :items => [ { 'Domain' => { 'Count' => [ final_results.count ] } } ] }
     end
@@ -76,10 +82,14 @@ class FakeSdb
       when /^WHERE$/i
         options[:where] ||= []
         options_array = []
-        options[:where] << [ options_array ]
+        options[:where] << options_array
       when /^OR$/i
         options_array = []
         options[:where] << :or << options_array
+      when /^LIMIT$/i
+        options_array = []
+        options[:limit] ||= [options_array]
+      when /^ORDER BY/i
       else
         options_array << word.gsub('`', '').gsub("'", '')
       end
