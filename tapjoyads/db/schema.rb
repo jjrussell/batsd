c6921ab3512eb0425aa37724e28418a9b9f6d243
amirmanji <amanji75@gmail.com>
2012-07-26 19:50:27 -0400

    fuck this up

--- tapjoyads/db/schema.rb
+++ tapjoyads/db/schema.rb
@@ -1,3 +1,6 @@
+FUCK YOU BRO
+
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
