1fe37fadbeff3e4c2221a9b12de3e982e88b4985
James Logsdon <jlo@tapjoy.com>
2012-07-26 13:46:01 -0400

    Spec for VideoButton#update_tracking_offer hook

--- tapjoyads/spec/models/video_button_spec.rb
+++ tapjoyads/spec/models/video_button_spec.rb
@@ -30,4 +30,27 @@ describe VideoButton do
       @video_button.xml_for_offer.should == offer.third_party_data
     end
   end
+
+  describe '#update_tracking_offer' do
+    it 'is called when a record is saved' do
+      @video_button.should_receive(:update_tracking_offer)
+      @video_button.save
+    end
+
+    it 'updates the tracking offer if #tracking_item_options is not nil' do
+      @video_button.stub(:tracking_item_options => {})
+      offer = @video_button.tracking_offer
+      offer.should_receive(:update_attributes)
+      @video_button.stub(:tracking_offer => offer)
+      @video_button.send(:update_tracking_offer)
+    end
+
+    it 'does not update the tracking offer if #tracking_item_options is nil' do
+      @video_button.stub(:tracking_item_options => nil)
+      offer = @video_button.tracking_offer
+      offer.should_not_receive(:update_attributes)
+      @video_button.stub(:tracking_offer => offer)
+      @video_button.send(:update_tracking_offer)
+    end
+  end
 end
