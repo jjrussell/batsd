c524ab89e3f9a0ce1170444c3fac8fe7f71823e1
James Logsdon <jlo@tapjoy.com>
2012-07-26 13:47:22 -0400

    Refactor VideoButton spec

--- tapjoyads/spec/models/video_button_spec.rb
+++ tapjoyads/spec/models/video_button_spec.rb
@@ -12,45 +12,43 @@ describe VideoButton do
     it { should validate_numericality_of :ordinal }
   end
 
-  before :each do
-    @video_button = FactoryGirl.create(:video_button)
-  end
+  subject { FactoryGirl.create(:video_button) }
 
   it "is by default enabled" do
-    @video_button.enabled.should == true
+    subject.enabled.should == true
   end
 
   it "updates third party data on offer based on video button" do
-    video_offer = VideoOffer.find(@video_button.video_offer_id)
+    video_offer = VideoOffer.find(subject.video_offer_id)
     offer = video_offer.primary_offer
 
-    @video_button.xml_for_offer.should == offer.third_party_data
+    subject.xml_for_offer.should == offer.third_party_data
 
     video_offer.offers.each do |offer|
-      @video_button.xml_for_offer.should == offer.third_party_data
+      subject.xml_for_offer.should == offer.third_party_data
     end
   end
 
   describe '#update_tracking_offer' do
     it 'is called when a record is saved' do
-      @video_button.should_receive(:update_tracking_offer)
-      @video_button.save
+      subject.should_receive(:update_tracking_offer)
+      subject.save
     end
 
     it 'updates the tracking offer if #tracking_item_options is not nil' do
-      @video_button.stub(:tracking_item_options => {})
-      offer = @video_button.tracking_offer
+      subject.stub(:tracking_item_options => {})
+      offer = subject.tracking_offer
       offer.should_receive(:update_attributes)
-      @video_button.stub(:tracking_offer => offer)
-      @video_button.send(:update_tracking_offer)
+      subject.stub(:tracking_offer => offer)
+      subject.send(:update_tracking_offer)
     end
 
     it 'does not update the tracking offer if #tracking_item_options is nil' do
-      @video_button.stub(:tracking_item_options => nil)
-      offer = @video_button.tracking_offer
+      subject.stub(:tracking_item_options => nil)
+      offer = subject.tracking_offer
       offer.should_not_receive(:update_attributes)
-      @video_button.stub(:tracking_offer => offer)
-      @video_button.send(:update_tracking_offer)
+      subject.stub(:tracking_offer => offer)
+      subject.send(:update_tracking_offer)
     end
   end
 end
