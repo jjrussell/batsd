fdfaead77c257e83b47ae541ae914ce0f12cb90f
Eric Tipton <eric.tipton@tapjoy.com>
2012-07-26 23:45:27 -0400

    test

--- tapjoyads/spec/models/offer_spec.rb
+++ tapjoyads/spec/models/offer_spec.rb
@@ -429,10 +429,11 @@ describe Offer do
     describe "url generation" do
       describe '#complete_action_url' do
         it "should substitute tokens in the URL" do
-          @offer.url = 'https://example.com/complete/TAPJOY_GENERIC?source=TAPJOY_GENERIC_SOURCE'
+          @offer.url = 'https://example.com/complete/TAPJOY_GENERIC?source=TAPJOY_GENERIC_SOURCE&uid=TAPJOY_EXTERNAL_UID'
           source = @offer.source_token('12345')
+          uid = Device.advertiser_device_id('x', @offer.partner_id)
           options = {:click_key => 'abcdefg', :udid => 'x', :publisher_app_id => '12345', :currency => 'zxy'}
-          @offer.complete_action_url(options).should == "https://example.com/complete/abcdefg?source=#{source}"
+          @offer.complete_action_url(options).should == "https://example.com/complete/abcdefg?source=#{source}&uid=#{udid}"
         end
       end
     end
