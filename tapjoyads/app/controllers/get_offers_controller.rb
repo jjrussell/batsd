794dc1c5390d915bd2f3531928a8d43b2a3c304c
farm.cp@gmail.com <cfarm@Christopher-Farms-MacBook-Pro.local>
2012-08-02 14:19:46 -0700

    ie sucks

--- tapjoyads/app/controllers/get_offers_controller.rb
+++ tapjoyads/app/controllers/get_offers_controller.rb
@@ -88,6 +88,8 @@ include GetOffersHelper
         @publisher_app.queue_update_attributes(:uses_non_html_responses => true)
       end
       render :json => @final.to_json, :callback => params[:callback] and return
+    elsif params[:device_type] == 'windows'
+      render :template => 'get_offers/webpage' and return
     else
       render :template => 'get_offers/webpage_redesign' and return
     end
