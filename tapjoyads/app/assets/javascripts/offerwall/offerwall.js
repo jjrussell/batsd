05b16d442b52d56e7a263eb8b9381e53dacc6eb1
Van Pham <van.pham@yahoo.com>
2012-08-02 22:35:59 -0700

    remove function

--- tapjoyads/app/assets/javascripts/offerwall/offerwall.js
+++ tapjoyads/app/assets/javascripts/offerwall/offerwall.js
@@ -464,23 +464,10 @@
 
     type: function(obj){
       return !obj || obj == null ? 'null' : toString.call(obj).split(' ').pop().replace(']', '').toLowerCase();
-    },
-
-    addCommas: function(nStr) {
-      nStr += '';
-      x = nStr.split('.');
-      x1 = x[0];
-      x2 = x.length > 1 ? '.' + x[1] : '';
-      var rgx = /(\d+)(\d{3})/;
-      while (rgx.test(x1)) {
-        x1 = x1.replace(rgx, '$1' + ',' + '$2');
-      }
-      return x1 + x2;
     }
   };
 
   $.ready(function(){
     $.init();
   });
-
 })(window, data);
