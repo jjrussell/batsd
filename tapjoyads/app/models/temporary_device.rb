3469bb0250153ceadc7f97470a6c1f9aab3e4fc0
Inderpreet Singh <inderpreet.singh@tapjoy.com>
2012-08-03 12:05:25 -0700

    Save publisher user id and display multiplier in temporary devices

--- tapjoyads/app/models/temporary_device.rb
+++ tapjoyads/app/models/temporary_device.rb
@@ -2,6 +2,8 @@ class TemporaryDevice < SimpledbShardedResource
   self.num_domains = NUM_TEMPORARY_DEVICE_DOMAINS
 
   self.sdb_attr :apps, :type => :json, :default_value => {}
+  self.sdb_attr :publisher_user_ids, :type => :json, :default_value => {}, :cgi_escape => true
+  self.sdb_attr :display_multipliers, :type => :json, :default_value => {}, :cgi_escape => true
 
   def dynamic_domain_name
      domain_number = @key.matz_silly_hash % NUM_TEMPORARY_DEVICE_DOMAINS
