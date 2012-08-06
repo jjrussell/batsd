3e53b7bdcc6e943015157f374aa71184e5334a8d
James Logsdon <jlo@tapjoy.com>
2012-08-06 12:39:46 -0400

    Spec the float coercion of some attribute readers

--- tapjoyads/spec/models/reseller_spec.rb
+++ tapjoyads/spec/models/reseller_spec.rb
@@ -13,4 +13,20 @@ describe Reseller do
     it { should validate_numericality_of :rev_share }
     it { should validate_numericality_of :reseller_rev_share }
   end
+
+  %w(pending_earnings next_payout_amount).each do |attribute|
+    describe "\##{attribute}" do
+      it "reads the '#{attribute}' attribute" do
+        subject.should_receive(:read_attribute).with(attribute)
+        subject.send(attribute.to_sym)
+      end
+
+      it 'coerces the attribute to a float' do
+        response = '1.0'
+        response.should_receive(:to_f)
+        subject.stub(:read_attribute => response)
+        subject.send(attribute.to_sym)
+      end
+    end
+  end
 end
