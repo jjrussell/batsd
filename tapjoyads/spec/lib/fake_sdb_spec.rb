b840cac34b5a290d0a5bdfefc4968945cb0e44f2
Kyle Brett <kyle@kylebrett.com>
2012-08-08 10:51:49 -0700

    FakeSdb   * Added support for comparison operators besides equality   * Added support for logical operators (and/or)   * Added support for parentheses.  (Basic… don't push it)   * Result set and inserted data are properly dup'd and safe to modify

--- tapjoyads/spec/lib/fake_sdb_spec.rb
+++ tapjoyads/spec/lib/fake_sdb_spec.rb
@@ -15,6 +15,38 @@ describe FakeSdb do
       'sing' => ['wrong']
     })
 
+    db.put_attributes('foo', 'three', {
+      'ping' => ['pong'],
+      'bing' => ['wrong'],
+      'sing' => ['wrong']
+    })
+
+    db.put_attributes('foo', 'four', {
+      'ping' => ['pong'],
+      'bing' => ['bong'],
+      'sing' => ['wrong']
+    })
+
+    db.put_attributes('ints', 'one', {
+      'val' => [1]
+    })
+
+    db.put_attributes('ints', 'two', {
+      'val' => [2]
+    })
+
+    db.put_attributes('ints', 'three', {
+      'val' => [3]
+    })
+
+    db.put_attributes('ints', 'four', {
+      'val' => [4]
+    })
+
+    db.put_attributes('ints', 'five', {
+      'val' => [5]
+    })
+
     db
   }
 
@@ -22,37 +54,160 @@ describe FakeSdb do
     subject.instance_variable_get(:@fake_sdb_data)
   }
 
-  describe 'query on a single column' do
-    describe 'with a single condition' do
-      it 'handles equality' do
-        rows = subject.select(
-          %{select * from foo where ping = 'pong'}, nil, nil
-        )[:items]
+  describe 'with no conditions' do
+    it 'returns all rows' do
+      rows = subject.select('select * from foo')[:items]
+      rows.size.should == 4
+    end
+  end
 
-        # This output format seems delicate, so let's
-        # be sure we neeeever break it
-        rows.size.should == 1
-        rows[0].keys.should == ['one']
-        rows[0]['one'].should == data['foo']['one']
-        rows.should == [{
-          'one' => {
-            'bing' => ['bong'],
-            'sing' => ['song'],
-            'ping' => ['pong']
-          }
-        }]
-      end
+  describe 'with a single condition' do
+    it 'handles equality' do
+      rows = subject.select(
+        %{select * from foo where ping = 'pong'}
+      )[:items]
+
+      # This output format seems delicate, so let's
+      # be sure we neeeever break it
+      # rows are returned in an undefined order
+      # which makes this spec a little ridiculous
+      rows.size.should == 3
+      rows.collect(&:keys).flatten.sort.should == %w{four one three}
+      rows.sort { |a, b| a.keys[0] <=> b.keys[0] }.should == [{
+        'four' => data['foo']['four']
+      }, {
+        'one' => data['foo']['one']
+      }, {
+        'three' => data['foo']['three']
+      }]
     end
 
-    describe 'with one `and` operator' do
-      it 'handles equality' do
-        rows = subject.select(
-          %{select * from foo where ping = 'pong' and bing = 'bong'}, nil, nil
-        )[:items]
+    it 'handles inequality' do
+      rows = subject.select(
+        %{select * from foo where ping != 'pong'}
+      )[:items]
 
-        rows.size.should == 1
-        rows[0].keys.should == ['one']
-      end
+      rows.size.should == 1
+      rows.collect(&:keys).flatten.sort.should == %w{two}
+
+      rows = subject.select(
+        %{select * from ints where val >= 4}
+      )[:items]
+
+      rows.size.should == 2
+      rows.collect(&:keys).flatten.sort.should == %w{five four}
+    end
+  end
+
+  describe 'with one `and` operator' do
+    it 'handles equality' do
+      rows = subject.select(
+        %{select * from foo where ping = 'pong' and bing = 'bong'}
+      )[:items]
+
+      rows.size.should == 2
+      rows.collect(&:keys).flatten.sort.should == %w{four one}
+
+      rows = subject.select(
+        %{select * from ints where val = 4}
+      )[:items]
+
+      rows.size.should == 1
+      rows.collect(&:keys).flatten.sort.should == %w{four}
+    end
+
+    it 'can mix equality and inequality' do
+      rows = subject.select(
+        %{select * from foo where ping = 'pong' and bing != 'bong'}
+      )[:items]
+
+      rows.size.should == 1
+
+      rows = subject.select(
+        %{select * from ints where val < 2 or val > 4}
+      )[:items]
+
+      rows.collect(&:keys).flatten.sort.should == %w{five one}
+    end
+
+    it 'can count' do
+      result = subject.select(%{select count(*) from foo where ping = 'pong' and bing = 'bong'})
+      result[:items][0]['Domain']['Count'].should == [2]
+    end
+  end
+
+  describe 'with one `or` operator' do
+    it 'handles equality' do
+      rows = subject.select(
+        %{select * from foo where ping = 'wrong' or sing = 'song'}, nil, nil
+      )[:items]
+
+      rows.size.should == 2
+      rows.collect(&:keys).flatten.sort.should == %w{one two}
+    end
+
+    it 'can mix equality and inequality' do
+      rows = subject.select(
+        %{select * from foo where ping = 'pong' or bing != 'bong'}
+      )[:items]
+
+      rows.size.should == 4
+    end
+
+    it 'can count' do
+      result = subject.select(%{select count(*) from foo where ping = 'pong' or bing != 'bong'})
+      result[:items][0]['Domain']['Count'].should == [4]
+    end
+  end
+
+  describe 'with parentheses' do
+    it 'handles the simplest case' do
+      rows = subject.select(
+        %{select * from foo where (ping = 'pong')}
+      )[:items].collect(&:keys).flatten.sort.should == %w{four one three}
+    end
+
+    it 'disambiguates queries' do
+      subject.select(
+        %{select * from foo where (ping = 'pong' and bing = 'bong') or sing = 'wrong'}
+      )[:items].size.should == 4
+
+      subject.select(
+        %{select * from foo where ping = 'pong' and (bing = 'bong' or sing = 'wrong')}
+      )[:items].size.should == 2
+    end
+  end
+
+  describe 'result set' do
+    it 'is safe to modify' do
+      record = subject.select(
+        %{select * from foo where ping = 'pong' and sing = 'song'}, nil, nil
+      )[:items].first
+
+      # add a column
+      record['one']['thing'] = 'thong'
+      record['one'].delete('ping')
+
+      data['foo']['one'].should_not have_key('thing')
+      data['foo']['one'].should have_key('ping')
+
+      record['one'].should have_key('thing')
+      record['one'].should_not have_key('ping')
+    end
+  end
+
+  describe 'inserted data' do
+    it 'may be modified after insertion' do
+      record = {'key' => ['val']}
+
+      subject.put_attributes('safe', 'data', {
+        'key' => 'val'
+      })
+
+      record['key'][0] << 'OMG MODIFIED'
+
+      subject.select('select * from safe')[:items][0]['data'].should == {'key' => 'val'}
+      record.should == {'key' => ['valOMG MODIFIED']}
     end
   end
 end
