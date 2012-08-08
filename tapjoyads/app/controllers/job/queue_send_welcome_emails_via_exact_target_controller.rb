f15b62beb516ba3f733e6d87a9df09b3e62dd967
Brian Stebar II <brian.stebar@tapjoy.com>
2012-08-08 10:11:37 -0700

    Re-added legacy code and created separate ET email job.
    After speaking with @amdtech, we decided to break the deployment process into two stages.  First, we'll deploy the ExactTarget code and corresponding queue job.  (*Note*: the ET job is separate from the original email job.  This will allow us to manually switch back to using SendGrid without having to revert the new code.)  After we verify that the ExactTarget code is working properly, we'll clean out the legacy code again.

---
+++ tapjoyads/app/controllers/job/queue_send_welcome_emails_via_exact_target_controller.rb
@@ -0,0 +1,25 @@
+class Job::QueueSendWelcomeEmailsControllerViaExactTarget < Job::SqsReaderController
+
+  def initialize
+    super QueueNames::SEND_WELCOME_EMAILS
+    ##
+    ## TODO: Consolidate all email queues for ET emails into one ET_EMAIL_QUEUE
+    ##
+  end
+
+  private
+
+  def on_message(message)
+    ##
+    ## TODO: Set up an ExactTarget status checking job to store whether or not ET is up.
+    ##       Then modify this job to check the stored status before trying to send mail.
+    ##
+    device_info = Marshal.restore(Base64::decode64(message.body))
+
+    gamer = Gamer.find(device_info.delete(:gamer_id))
+    email_type = device_info.delete(:email_type) || 'welcome'
+
+    mailer = TransactionalMailer.new
+    mailer.send("#{email_type}_email", gamer, device_info)
+  end
+end
