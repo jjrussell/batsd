f00301594c7ad02b3b6a2bd1f3f065b988e2e685
Daniel Ho <tapdan@tapjoy.com>
2012-08-10 14:32:09 -0700

    use job queue to cache offer list of different cache key

---
+++ tapjoyads/app/controllers/job/queue_cache_optimized_offer_list_controller.rb
@@ -0,0 +1,10 @@
+class Job::QueueCacheOptimizedOfferListController < Job::SqsReaderController
+  def initialize
+    super QueueNames::CACHE_OPTIMIZED_OFFER_LIST
+  end
+
+  def on_message(message)
+    s3_optimization_key = JSON.parse(message.body)
+    OptimizedOfferList.cache_offer_list(s3_optimization_key)
+  end
+end
