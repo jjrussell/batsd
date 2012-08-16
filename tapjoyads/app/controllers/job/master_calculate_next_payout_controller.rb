8093ba8c7bbf2abc48ff9bb992b0bf346039494f
Brian Sanders <brian.sanders@tapjoy.com>
2012-08-16 18:05:01 -0400

    Fix test hack

--- tapjoyads/app/controllers/job/master_calculate_next_payout_controller.rb
+++ tapjoyads/app/controllers/job/master_calculate_next_payout_controller.rb
@@ -4,9 +4,9 @@ class Job::MasterCalculateNextPayoutController < Job::JobController
   after_filter :save_activity_logs, :only => [:index]
 
   def index
-    total_count = 10.0
+    partners = Partner.to_calculate_next_payout_amount
     failure_count = 0
-    Partner.to_calculate_next_payout_amount.each do |partner|
+    partners.each do |partner|
       begin
         partner.next_payout_amount = Partner.calculate_next_payout_amount(partner.id)
         if partner.payout_threshold_confirmation && partner.next_payout_amount >= partner.payout_threshold
@@ -17,7 +17,7 @@ class Job::MasterCalculateNextPayoutController < Job::JobController
       rescue => exception
         failure_count += 1
         Airbrake.notify(exception, airbrake_request_data)
-        if failure_count.to_f / total_count  > MAX_FAILURE_PERCENTAGE
+        if failure_count.to_f / partners.count.to_f  > MAX_FAILURE_PERCENTAGE
           raise "Maximum tolerable failure count exceeded. Last failure: " + exception
         end
       end
