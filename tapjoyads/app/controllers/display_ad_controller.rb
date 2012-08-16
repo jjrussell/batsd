fd00e888bf12f9a5e9c4abf87d9c8abc67f6a48f
Eric Tipton <eric.tipton@tapjoy.com>
2012-08-15 20:47:30 -0400

    these methods should've been added awhile ago...

--- tapjoyads/app/controllers/display_ad_controller.rb
+++ tapjoyads/app/controllers/display_ad_controller.rb
@@ -67,7 +67,7 @@ class DisplayAdController < ApplicationController
     # For SDK version <= 8.3.0, use high-res (aka 2x) version of 320x50 ad
     # (except certain scenarios)
     if ((params[:size].blank? || (params[:size] == '320x50' &&
-        params[:library_version].to_s.version_less_than_or_equal_to?('8.3.0'))) &&
+        params[:library_version].to_s.version_less_than?('8.3.0'))) &&
         params[:action] != 'webview' && request.format != :json &&
         params[:app_id] != '6b69461a-949a-49ba-b612-94c8e7589642') # TextFree
       params[:size] = '640x100'
