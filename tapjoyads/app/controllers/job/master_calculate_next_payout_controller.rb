015954dd96f676f75983f4882cde6be0d1a3397f
Brian Sanders <brian.sanders@tapjoy.com>
2012-08-16 17:59:23 -0400

    Job master_calculate_next_payout has been failing for any invalid Partner object; added a level of failure tolerance

--- tapjoyads/app/controllers/job/master_calculate_next_payout_controller.rb
+++ tapjoyads/app/controllers/job/master_calculate_next_payout_controller.rb
@@ -1,15 +1,26 @@
 class Job::MasterCalculateNextPayoutController < Job::JobController
+  MAX_FAILURE_PERCENTAGE = 0.10
+
   after_filter :save_activity_logs, :only => [:index]
 
   def index
+    total_count = 10.0
+    failure_count = 0
     Partner.to_calculate_next_payout_amount.each do |partner|
-
-      partner.next_payout_amount = Partner.calculate_next_payout_amount(partner.id)
-      if partner.payout_threshold_confirmation && partner.next_payout_amount >= partner.payout_threshold
-        log_activity(partner)
-        partner.payout_threshold_confirmation = false
+      begin
+        partner.next_payout_amount = Partner.calculate_next_payout_amount(partner.id)
+        if partner.payout_threshold_confirmation && partner.next_payout_amount >= partner.payout_threshold
+          log_activity(partner)
+          partner.payout_threshold_confirmation = false
+        end
+        partner.save!
+      rescue => exception
+        failure_count += 1
+        Airbrake.notify(exception, airbrake_request_data)
+        if failure_count.to_f / total_count  > MAX_FAILURE_PERCENTAGE
+          raise "Maximum tolerable failure count exceeded. Last failure: " + exception
+        end
       end
-      partner.save!
     end
 
     render :text => 'ok'
