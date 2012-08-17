7614a164bf32c72d1a875dd5040c038093d03e85
Hwan-Joon Choi <hwanjoon@tapjoy.com>
2012-08-16 22:21:05 -0700

    Clean things up a little bit
    * combined all regional tokens into a single hash
    * defaults to US

--- tapjoyads/lib/linkshare.rb
+++ tapjoyads/lib/linkshare.rb
@@ -3,39 +3,46 @@ class Linkshare
   LINKSHARE_COUNTRIES = Set.new(%w( JP CA ))
   DGM_COUNTRIES = Set.new(%w( NW AU ))
 
-  TRADEDOUBLER_TOKENS = { 'AR' => 'AR2145609',
-                          'BR' => 'BR2145607',
-                          'GB' => 'GB2079767',
-                          'UK' => 'UK2145605',
-                          'BE' => 'BE2081187',
-                          'BR' => 'BR2081199',
-                          'CH' => 'CH2081198',
-                          'DE' => 'DE2145621',
-                          'ES' => 'ES2081197',
-                          'FR' => 'FR2081184',
-                          'IE' => 'IE2081195',
-                          'IT' => 'IT2081189',
-                          'NL' => 'NL2081194',
-                          'SE' => 'SE2081185'}
-  LINKSHARE_TOKENS =    { 'JP' => 'Qr66oOu*yBY',
-                          'US' => 'OxXMC6MRBt4',
-                          'CA' => 'OxXMC6MRBt4'}
-  DGM_TOKENS =          { 'NW' => '37022',
-                          'AU' => '37022'}
+  REGION_TOKENS = {
+    'AR' => 'AR2145609',
+    'BR' => 'BR2145607',
+    'GB' => 'GB2079767',
+    'UK' => 'UK2145605',
+    'BE' => 'BE2081187',
+    'BR' => 'BR2081199',
+    'CH' => 'CH2081198',
+    'DE' => 'DE2145621',
+    'ES' => 'ES2081197',
+    'FR' => 'FR2081184',
+    'IE' => 'IE2081195',
+    'IT' => 'IT2081189',
+    'NL' => 'NL2081194',
+    'SE' => 'SE2081185',
+    'JP' => 'Qr66oOu*yBY',
+    'US' => 'OxXMC6MRBt4',
+    'CA' => 'OxXMC6MRBt4',
+    'NW' => '37022',
+    'AU' => '37022',
+  }
 
-  def self.add_params(url, country = nil)
+  def self.add_params(url, country = 'US')
+    country = country.to_s.upcase
+    token   = REGION_TOKENS[country.upcase] || REGION_TOKENS['US']
+
     if url =~ /^http:\/\/itunes\.apple\.com/
       if TRADEDOUBLER_COUNTRIES.include?(country)
-        url_params = "partnerId=2003&tduid=#{App::TRADEDOUBLER_TOKENS[country.upcase]}"
+        url_params = "partnerId=2003&tduid=#{token}"
       elsif LINKSHARE_COUNTRIES.include?(country)
-        url_params = "partnerId=30&siteID=#{LINKSHARE_TOKENS[country.upcase]}"
+        url_params = "partnerId=30&siteID=#{token}"
       elsif DGM_COUNTRIES.include?(country)
-        url_params = "partnerId=1002&affToken=#{DGM_TOKENS[country.upcase]}"
+        url_params = "partnerId=1002&affToken=#{token}"
       end
 
-      #if country is not in any of the above lists, do we default to a country?
-      url += url =~ /^http:\/\/itunes\.apple\.com\/.*\?/ ? '&' + url_params : '?' + url_params unless url_params.nil?
+      separator = url =~ /\?/ ? '&' : '?'
+
+      "#{url}#{separator}#{url_params}"
+    else
+      url
     end
-    url
   end
 end
