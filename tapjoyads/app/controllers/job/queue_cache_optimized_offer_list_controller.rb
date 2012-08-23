3beb556436f8df5aaec4a5dffd7dd3e17449b1bc
Daniel Ho <tapdan@tapjoy.com>
2012-08-23 12:22:22 -0700

    remove puts statements

--- tapjoyads/app/controllers/job/queue_cache_optimized_offer_list_controller.rb
+++ tapjoyads/app/controllers/job/queue_cache_optimized_offer_list_controller.rb
@@ -7,7 +7,6 @@ class Job::QueueCacheOptimizedOfferListController < Job::SqsReaderController
 
   def on_message(message)
     s3_optimization_key = message.body
-    puts "job queue controller cache called -- #{s3_optimization_key}"
     OptimizedOfferList.cache_offer_list(s3_optimization_key)
   end
 end
