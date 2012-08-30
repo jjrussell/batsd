76b6257460b614c03fe31ebb8608c7f41b3ac748
Adam Sumner <adam.sumner@tapjoy.com>
2012-08-30 15:35:44 -0700

    Remove unwanted verifications and @enabled.

--- tapjoyads/app/controllers/dashboard/non_rewarded_controller.rb
+++ tapjoyads/app/controllers/dashboard/non_rewarded_controller.rb
@@ -13,14 +13,12 @@ class Dashboard::NonRewardedController < Dashboard::DashboardController
     unless @currency
       @currency = @app.build_non_rewarded
       @currency.save!
-      @enabled = @currency.tapjoy_enabled
     end
   end
 
   def toggle
-    @currency.tapjoy_enabled = !@enabled
+    @currency.tapjoy_enabled = !@currency.tapjoy_enabled
     if @currency.save
-      @enabled = !@enabled
       flash[:notice] = "Non-rewarded has been #{@currency.tapjoy_enabled ? 'enabled' : 'disabled'}."
     else
       flash.now[:error] = "Could not #{@currency.tapjoy_enabled ? 'enable' : 'disable'} non-rewarded."
@@ -43,12 +41,9 @@ class Dashboard::NonRewardedController < Dashboard::DashboardController
   end
 
   def setup
-    verify_params([ :app_id ])
     @app      = App.find(params[:app_id])
     @partner  = @app.partner
     @currency = @app.non_rewarded
-    @enabled  = @currency.try(:tapjoy_enabled)
-    verify_records([ @app, @partner ])
   end
 
 end
