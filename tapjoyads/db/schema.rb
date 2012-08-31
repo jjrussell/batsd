8bc03811150d77011e3d29252d2fb1af3370a7be
amirmanji <amanji75@gmail.com>
2012-08-30 17:25:41 -0700

    fix schema file

--- tapjoyads/db/schema.rb
+++ tapjoyads/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 2011071101140341) do
+ActiveRecord::Schema.define(:version => 20120731133709) do
 
   create_table "action_offers", :id => false, :force => true do |t|
     t.string   "id",                               :limit => 36,                    :null => false
