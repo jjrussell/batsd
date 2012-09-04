c07bce591c6a87c01b9ebfd4cb8015e714331e6b
Neal Wiggins <neal.wiggins@tapjoy.com>
2012-09-04 11:43:04 -0700

    Fix to only use one domain

--- tapjoyads/app/models/cached_offer_list.rb
+++ tapjoyads/app/models/cached_offer_list.rb
@@ -1,6 +1,6 @@
-class CachedOfferList < SimpledbShardedResource
+class CachedOfferList < SimpledbResource
   TYPES = %w(native optimized native-fallback)
-  self.num_domains = NUM_CACHED_OFFER_LIST_DOMAINS
+  self.domain_name = 'cached_offer_list'
 
   self.sdb_attr :generated_at, :type => :time
   self.sdb_attr :cached_at, :type => :time
@@ -9,8 +9,4 @@ class CachedOfferList < SimpledbShardedResource
   self.sdb_attr :memcached_key
   self.sdb_attr :offer_list, :type => :json #my thinking is that this would be good for a multi-dimensional array
 
-  def dynamic_domain_name
-    domain_number = @key.matz_silly_hash % NUM_CACHED_OFFER_LIST_DOMAINS
-    "cached_offer_list_#{domain_number}"
-  end
 end
--- tapjoyads/config/environments/production.rb
+++ tapjoyads/config/environments/production.rb
@@ -103,7 +103,6 @@ NUM_GAME_STATE_MAPPING_DOMAINS = 10
 NUM_PUBLISHER_USER_DOMAINS = 50
 NUM_CONVERSION_ATTEMPT_DOMAINS = 50
 NUM_RISK_PROFILE_DOMAINS = 100
-NUM_CACHED_OFFER_LIST_DOMAINS = 50
 
 mail_chimp = YAML::load_file("#{Rails.root}/config/mail_chimp.yaml")['production']
 MAIL_CHIMP_API_KEY = mail_chimp['api_key']
