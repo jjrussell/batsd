404c171a18d9fd6e0d348482ee2737fff97848b4
James Huang <jhuang1982@gmail.com>
2012-09-05 15:45:54 -0700

    adding one off

---
+++ tapjoyads/lib/one_offs/20120905_fix_action_offer_icon_ids.rb
@@ -0,0 +1,10 @@
+class OneOffs
+  def self.fix_action_offer_icon_ids
+    ActionOffer.all.each do |action_offer|
+      action_offer.offers.each do |offer|
+        offer.icon_id_override = offer.app_metadata_id
+        offer.save! if offer.icon_id_override_changed?
+      end
+    end
+  end
+end
