43a07c211ae526e7837769cf4a3667397f9581b0
dickeyxxx <jeff@dickey.xxx>
2012-09-06 17:55:38 -0700

    Revert "Merge pull request #3415 from dickeyxxx/i18n"
    This reverts commit c3322ebc06f3fb7d1345d03d64fedb6d5f002c76, reversing
    changes made to bb63d81cc4865f5db93e08235167dc41b63240b3.

--- tapjoyads/config/initializers/localeapp.rb
+++
@@ -1,9 +0,0 @@
-if Rails.env.development?
-  require 'localeapp/rails'
-
-  Localeapp.configure do |config|
-    config.api_key = 'EcebMF7f8Xu3MaV8DzXHixGxSsUZDihb6EpndNtvmUqjuICCum'
-    config.polling_environments = []
-    config.sending_environments = [:development]
-  end
-end
