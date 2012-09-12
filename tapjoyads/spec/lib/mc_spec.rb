b41e1c2a83da59c7a3284213b4e655daea768233
Aaron Daniel <amdtech@me.com>
2012-09-12 09:46:33 -0700

    Use rspec 2 shared example goodness

--- tapjoyads/spec/lib/mc_spec.rb
+++ tapjoyads/spec/lib/mc_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
 
-describe Mc do
+shared_examples_for "a cache" do
+  let(:memcache) { described_class }
+
   describe '.get' do
     context 'for a non-existant type' do
       it 'raises a NameError when loading the constant' do
         error = ArgumentError.new("undefined class/module SomethingFake")
         cache = Memcached.new
         cache.stub(:get).with('key1').and_raise(error)
-        lambda { Mc.get('key1', false, [cache]) }.should raise_error(NameError)
+        lambda { memcache.get('key1', false, [cache]) }.should raise_error(NameError)
       end
     end
 
@@ -17,7 +19,7 @@ describe Mc do
           cache = Memcached.new
           cache.stub(:get).with('key1').and_raise(Memcached::NotFound)
           cache.stub(:get).with('key2').and_return('val')
-          Mc.get(['key1', 'key2'], false, [cache]).should == 'val'
+          memcache.get(['key1', 'key2'], false, [cache]).should == 'val'
         end
 
         it 'only adds last key found' do
@@ -28,7 +30,7 @@ describe Mc do
           cache.should_receive(:get).with('key2').and_return('val')
           missing_cache.should_not_receive(:add).with('key1', 'val', 1.week.to_i)
           missing_cache.should_receive(:add).with('key2', 'val', 1.week.to_i)
-          Mc.get(['key1', 'key2'], false, [missing_cache, cache]).should == 'val'
+          memcache.get(['key1', 'key2'], false, [missing_cache, cache]).should == 'val'
         end
       end
 
@@ -36,7 +38,7 @@ describe Mc do
         it 'returns yield' do
           cache = Memcached.new
           cache.stub(:get).with('key1').and_raise(Memcached::NotFound)
-          res = Mc.get(['key1', 'key2']) do
+          res = memcache.get(['key1', 'key2']) do
             'yielded_val'
           end
 
@@ -47,22 +49,22 @@ describe Mc do
   end
 
   it "gets and saves" do
-    Mc.put('key', 'value')
-    Mc.get('key').should == 'value'
+    memcache.put('key', 'value')
+    memcache.get('key').should == 'value'
 
-    val = Mc.get('non-key') do
+    val = memcache.get('non-key') do
       'bar'
     end
 
     val.should == 'bar'
 
-    val = Mc.get_and_put('foo2') do
+    val = memcache.get_and_put('foo2') do
       'bar2'
     end
 
     val.should == 'bar2'
 
-    val = Mc.get_and_put('foo2') do
+    val = memcache.get_and_put('foo2') do
       'not accessed'
     end
     val.should == 'bar2'
@@ -72,7 +74,7 @@ describe Mc do
     thread_list = []
     10.times do |i|
       thread = Thread.new do
-        Mc.put("thread-#{i}", "#{i}", true)
+        memcache.put("thread-#{i}", "#{i}", true)
       end
       thread_list.push(thread)
     end
@@ -82,50 +84,50 @@ describe Mc do
     end
 
     10.times do |i|
-      Mc.get("thread-#{i}", true).should == "#{i}"
+      memcache.get("thread-#{i}", true).should == "#{i}"
     end
   end
 
   it "increments count" do
     key = 'foocount'
     key2 = 'foocount2'
-    Mc.get_count(key).should == 0
+    memcache.get_count(key).should == 0
 
-    Mc.increment_count(key).should == 1
+    memcache.increment_count(key).should == 1
 
     5.times do
-      Mc.increment_count(key)
+      memcache.increment_count(key)
     end
 
-    Mc.get_count(key).should == 6
+    memcache.get_count(key).should == 6
 
-    Mc.increment_count(key, false, 1.week, 10)
-    Mc.get_count(key).should == 16
+    memcache.increment_count(key, false, 1.week, 10)
+    memcache.get_count(key).should == 16
 
-    Mc.get_count(key2).should == 0
-    Mc.increment_count(key2, false, 1.week, -5).should == -5
-    Mc.get_count(key2).should == -5
-    Mc.increment_count(key2, false, 1.week, -1).should == -6
-    Mc.increment_count(key2, false, 1.week, 2).should == -4
-    Mc.increment_count(key2, false, 1.week, 7).should == 3
+    memcache.get_count(key2).should == 0
+    memcache.increment_count(key2, false, 1.week, -5).should == -5
+    memcache.get_count(key2).should == -5
+    memcache.increment_count(key2, false, 1.week, -1).should == -6
+    memcache.increment_count(key2, false, 1.week, 2).should == -4
+    memcache.increment_count(key2, false, 1.week, 7).should == 3
   end
 
   it "compares and swaps" do
 
     thread_list = []
     expected_val = ''
-    Mc.compare_and_swap('foo', true) do |mc_val|
+    memcache.compare_and_swap('foo', true) do |mc_val|
       'a'
     end
 
-    Mc.get('foo').should == 'a'
+    memcache.get('foo').should == 'a'
 
     # Verify retries can occur up to 2 times
     retries = 0
-    Mc.compare_and_swap('foo', true) do |mc_val|
+    memcache.compare_and_swap('foo', true) do |mc_val|
       if retries < 2
         retries += 1;
-        Mc.compare_and_swap('foo', true) do |mc_val|
+        memcache.compare_and_swap('foo', true) do |mc_val|
           mc_val + 'a'
         end
       end
@@ -134,16 +136,16 @@ describe Mc do
     end
 
     retries.should == 2
-    Mc.get('foo').should == 'aaaa'
+    memcache.get('foo').should == 'aaaa'
 
     # Can't retry more than 2 times
-    Mc.put('foo', 'a')
+    memcache.put('foo', 'a')
     lambda do
       retries = 0
-      Mc.compare_and_swap('foo', true) do |mc_val|
+      memcache.compare_and_swap('foo', true) do |mc_val|
         if retries < 3
           retries += 1;
-          Mc.compare_and_swap('foo', true) do |mc_val|
+          memcache.compare_and_swap('foo', true) do |mc_val|
             mc_val + 'a'
           end
         end
@@ -152,6 +154,14 @@ describe Mc do
       end
     end.should raise_error(Memcached::ConnectionDataExists)
     retries.should == 3
-    Mc.get('foo').should == 'aaaa'
+    memcache.get('foo').should == 'aaaa'
   end
+end
+
+describe Mc do
+  it_behaves_like "a cache"
+end
+
+describe SimpledbCache do
+  it_behaves_like "a cache"
 end
