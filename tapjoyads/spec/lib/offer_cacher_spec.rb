4102b01841eeb8343c180e2c52be568ea36f3314
Daniel Ho <tapdan@tapjoy.com>
2012-09-18 14:32:25 -0700

    Added test when rank boost overrides rank score

--- tapjoyads/spec/lib/offer_cacher_spec.rb
+++ tapjoyads/spec/lib/offer_cacher_spec.rb
@@ -42,13 +42,31 @@ describe OfferCacher do
       class << self
         alias_method :distributed_put, :original_distributed_put
         alias_method :distributed_get_and_put, :original_distributed_get_and_put
+        remove_method :cache_test_distributed_put
+        remove_method :cache_test_distributed_get_and_put
       end
     end
   end
 
-  it "caches sorted offers prerejected", :sorted do
-    OfferCacher.cache_offers_prerejected(@offers, Offer::CLASSIC_OFFER_TYPE, false)
-    offers = OfferCacher.get_offers_prerejected(Offer::CLASSIC_OFFER_TYPE, @platform, false, @device_type)
-    offers.map {|o| o.rank_score}.should == @ranks.sort.reverse
+  describe "#get_offers_prerejected" do
+    it "caches sorted offers prerejected", :sorted do
+      OfferCacher.cache_offers_prerejected(@offers, Offer::CLASSIC_OFFER_TYPE, false)
+      offers = OfferCacher.get_offers_prerejected(Offer::CLASSIC_OFFER_TYPE, @platform, false, @device_type)
+      offers.map {|o| o.rank_score}.should == @ranks.sort.reverse
+    end
+
+    it "caches sorted offers prerejected overriden by rank boost", :sorted, :rank_boost do
+      overriden_offer = @offers[2]  # override offer with rank score of 3.0
+      rank_boost = RankBoost.create(:offer_id => overriden_offer.id,
+                                    :start_time => Time.now, :end_time => Time.now + 1.hour,
+                                    :amount => 30)
+      overriden_offer.publisher_app_whitelist = "0001"
+      overriden_offer.calculate_rank_boost!
+      overriden_offer.save
+
+      OfferCacher.cache_offers_prerejected(@offers, Offer::CLASSIC_OFFER_TYPE, false)
+      offers = OfferCacher.get_offers_prerejected(Offer::CLASSIC_OFFER_TYPE, @platform, false, @device_type)
+      offers.map {|o| o.rank_score}.should == [30, 5.0, 4.0, 2.0, 1.0]
+    end
   end
 end
