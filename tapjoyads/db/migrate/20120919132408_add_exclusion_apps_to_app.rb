c15a0396f4a842283b4a3ae8c11995e0b501bfc4
James Huang <jhuang1982@gmail.com>
2012-09-19 14:11:57 -0700

    remove db migrate file

--- tapjoyads/db/migrate/20120919132408_add_exclusion_apps_to_app.rb
+++
@@ -1,9 +0,0 @@
-class AddExclusionAppsToApp < ActiveRecord::Migration
-  def self.up
-    add_column :apps, :exclusion_apps, :text, :null => false, :default => ''
-  end
-
-  def self.down
-    remove_column :apps, :exclusion_apps
-  end
-end
