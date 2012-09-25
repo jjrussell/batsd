c1d1d84fadd5fe9735cee9e4109f17b1ccc4e52c
Hwan-Joon Choi <hwanjoon@tapjoy.com>
2012-09-25 19:41:48 -0400

    apparently this is used in a helper
    This reverts commit daac48033728bc53acf4b7d9be90e9b11f5b9100.

--- tapjoyads/app/controllers/offer_age_gating_controller.rb
+++ tapjoyads/app/controllers/offer_age_gating_controller.rb
@@ -14,6 +14,7 @@ class OfferAgeGatingController < ApplicationController
 
   # passing params[:date_of_birth] to click controller for web_request logging
   def redirect_to_click
+    @now = Time.zone.now
     @currency = Currency.find_in_cache(params[:currency_id])
     @publisher_app = App.find_in_cache(params[:app_id])
 
