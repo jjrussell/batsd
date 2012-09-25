536adf7f24120e3341879d33ad57c9989cb76b62
Matt Carbone <matt.carbone@gmail.com>
2012-09-25 14:27:12 -0400

    I18n notification message and title

--- tapjoyads/app/controllers/job/queue_conversion_notification_controller.rb
+++ tapjoyads/app/controllers/job/queue_conversion_notification_controller.rb
@@ -11,14 +11,14 @@ class Job::QueueConversionNotificationsController < Job::SqsReaderController
   def on_message(message)
     @reward = Reward.find(message.body, :consistent => true)
     raise "Reward not found: #{message.body}" if @reward.nil?
-    
+
     begin
       @publisher_app = App.find_by_id(@reward.publisher_app_id)
 
       @notification = NotificationsClient::Notification.new({
         :app_id => @reward.publisher_app_id, 
-        :title => "Reward Notification",
-        :message => "Your reward from #{@publisher_app.name} is available!",
+        :title => I18n.t('queue_conversion_notifications_controller.notification.title', :default => "Reward Notification"),
+        :message => I18n.t('queue_conversion_notifications_controller.notification.message', :default => "Your reward from #{@publisher_app.name} is available!"),
         :device_id => @reward.udid
       })
       @notification.deliver
