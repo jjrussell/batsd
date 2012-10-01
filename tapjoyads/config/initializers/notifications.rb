ea6b2fe9d3cd9b9f932ae5fc3f4db96519e618c8
deanxorr <dean.dieker@tapjoy.com>
2012-10-01 14:25:27 -0400

    updated notifications.rb to be correct for test dev production, added a staging case

--- tapjoyads/config/initializers/notifications.rb
+++ tapjoyads/config/initializers/notifications.rb
@@ -1,14 +1,19 @@
 case Rails.env
 when "test"
-	Tapjoyad::Application.config.notifications_url = 'notifications.tapjoy.com'
-	NotificationsClient.set_notifications_host("notifications.tapjoy.com")
+	Tapjoyad::Application.config.notifications_url = 'http://localhost:5000'
+	NotificationsClient.set_notifications_host("http://localhost:5000")
 	Tapjoyad::Application.config.signage_secret = ENV['SIGNAGE_SECRET']	
 when "development"
-	Tapjoyad::Application.config.notifications_url = 'notifications.tapjoy.com'
-	NotificationsClient.set_notifications_host("notifications.tapjoy.com")
-	Tapjoyad::Application.config.signage_secret = ENV['SIGNAGE_SECRET']
-when "production"
 	Tapjoyad::Application.config.notifications_url = 'http://localhost:5000'
 	NotificationsClient.set_notifications_host("http://localhost:5000")
 	Tapjoyad::Application.config.signage_secret = ENV['SIGNAGE_SECRET']
+when "production"
+	Tapjoyad::Application.config.notifications_url = 'notifications.tapjoy.com'
+	NotificationsClient.set_notifications_host("notifications.tapjoy.com")
+	Tapjoyad::Application.config.signage_secret = ENV['SIGNAGE_SECRET']
+when "staging"
+	Tapjoyad::Application.config.notifications_url = 'qa.notifications.tapjoy.com'
+	NotificationsClient.set_notifications_host("qa.notifications.tapjoy.com")
+	Tapjoyad::Application.config.signage_secret = ENV['SIGNAGE_SECRET']
+qa.notifications.tapjoy.com	
 end
