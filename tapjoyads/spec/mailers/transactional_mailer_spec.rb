2ad269954e1dfc8f6d054f583e61e7776ed4accb
Brian Stebar II <brian.stebar@tapjoy.com>
2012-10-23 10:14:52 -0400

    Fixed specs

--- tapjoyads/spec/mailers/transactional_mailer_spec.rb
+++ tapjoyads/spec/mailers/transactional_mailer_spec.rb
@@ -9,11 +9,72 @@ describe TransactionalMailer do
   ## TODO: Spec out build_data_for_tjm_welcome_email
   ##
 
-  ##
-  ## TODO: Spec out welcome_email
-  ##
+  describe '#welcome_email' do
+    before :each do
+      @gamer = FactoryGirl.create(:gamer)
+    end
 
-  ##
-  ## TODO: Spec out post_confirm_email
-  ##
+    context 'when a gamer with an invalid email address' do
+      before :each do
+        @gamer.email = 'invalid@tapjoyed.com'
+        @gamer.save
+      end
+
+      around :each do |example|
+        VCR.use_cassette('exact_target/send_triggered_email/invalid_email') do
+          example.run
+        end
+      end
+
+      it "it sets the email_invalid flag on the gamer" do
+        @mailer.welcome_email(@gamer)
+        @gamer.email_invalid.should be_true
+      end
+    end
+
+    context 'when a gamer has a valid email address' do
+      around :each do |example|
+        VCR.use_cassette('exact_target/send_triggered_email/valid_email') do
+          example.run
+        end
+      end
+
+      it "doesn't set the email_invalid flag on gamer" do
+        @mailer.welcome_email(@gamer)
+        @gamer.email_invalid.should_not be_true
+      end
+    end
+  end
+
+  describe '#post_confirm_email' do
+    before :each do
+      @gamer = FactoryGirl.create(:gamer)
+    end
+
+    context 'when a gamer with an invalid email address' do
+      before :each do
+        @gamer.email = 'invalid@tapjoyed.com'
+        @gamer.save
+      end
+
+      around :each do |example|
+        VCR.use_cassette('exact_target/send_triggered_email/invalid_email') do
+          example.run
+        end
+      end
+
+      it "it sets the email_invalid flag on the gamer" do
+        @mailer.post_confirm_email(@gamer)
+        @gamer.email_invalid.should be_true
+      end
+    end
+
+    context 'when a gamer has a valid email address' do
+      around :each do |example|
+        VCR.use_cassette('exact_target/send_triggered_email/valid_email') do
+          example.run
+        end
+      end
+    end
+  end
 end
