f6ffd0156fd361a3ad109735d70c38be6f371a30
Matt Carbone <matt.carbone@gmail.com>
2012-10-23 10:57:49 -0400

    custom message text for PPI offers

--- tapjoyads/app/controllers/job/queue_conversion_notification_controller.rb
+++ tapjoyads/app/controllers/job/queue_conversion_notification_controller.rb
@@ -21,12 +21,10 @@ class Job::QueueConversionNotificationsController < Job::SqsReaderController
         {:namespace => 'idfa',       :identifier => @device.idfa}
       ].reject{ |a| a[:identifier].nil?  }
 
-      @publisher_app = App.find_in_cache(@reward.publisher_app_id)
-
       @notification = NotificationsClient::Notification.new({
         :app_id => @reward.publisher_app_id, 
         :title => I18n.t('queue_conversion_notifications_controller.notification.title', :default => "Reward Notification"),
-        :message => I18n.t('queue_conversion_notifications_controller.notification.message', :default => "Your reward from #{@publisher_app.name} is available!"),
+        :message => message_text,
         :device_aliases => device_aliases
       })
       @notification.deliver
@@ -35,5 +33,32 @@ class Job::QueueConversionNotificationsController < Job::SqsReaderController
     rescue NotificationsClient::NetworkException => e
       Rails.logger.error "A notification network error occurred: #{e.status_code}"
     end
+  end
+
+private
+  def message_text
+    case @reward.offer.item_type
+    when "App"
+      #PPI
+      i18n_message("You earned #{currency.name} by downloading #{advertiser_app.name}")
+    else
+      i18n_message("Your #{currency.name} in #{publisher_app.name} is available!")
+    end
+  end
+
+  def i18n_message(s)
+    I18n.t('queue_conversion_notifications_controller.notification.message', :default => s)
+  end
+
+  def publisher_app
+    App.find_in_cache(@reward.publisher_app_id)
+  end
+
+  def advertiser_app
+    App.find_in_cache(@reward.advertiser_app_id)
+  end
+
+  def currency
+    Currency.find_in_cache(@reward.currency_id)
   end
 end
--- tapjoyads/spec/controllers/job/queue_conversion_notification_controller_spec.rb
+++ tapjoyads/spec/controllers/job/queue_conversion_notification_controller_spec.rb
@@ -3,21 +3,22 @@ require 'spec_helper'
 describe Job::QueueConversionNotificationsController do
   before :each do
    @controller.should_receive(:authenticate).at_least(:once).and_return(true)
-
+   @currency = Factory.create(:currency)
    @publisher_app = FactoryGirl.create(:app)
-   advertiser_app = FactoryGirl.create(:app)
-   @offer = advertiser_app.primary_offer
+   @advertiser_app = FactoryGirl.create(:app)
+   @offer = @advertiser_app.primary_offer
 
    @reward = FactoryGirl.create(:reward,
       :type                  => 'offer',
       :publisher_app_id      => @publisher_app.id,
-      :advertiser_app_id     => advertiser_app.id,
+      :advertiser_app_id     => @advertiser_app.id,
       :offer_id              => @offer.id,
       :publisher_partner_id  => @publisher_app.partner_id,
-      :advertiser_partner_id => advertiser_app.partner_id,
+      :advertiser_partner_id => @advertiser_app.partner_id,
       :publisher_amount      => 1,
       :advertiser_amount     => 1,
       :tapjoy_amount         => 1,
+      :currency_id           => @currency.id,
       :udid                  => 'udid'
     )
 
@@ -35,29 +36,22 @@ describe Job::QueueConversionNotificationsController do
   end
 
   context "sending" do
-    it "should find reward" do
+    before(:each) do
       NotificationsClient::Notification.any_instance.stub(:deliver)
+    end
 
+    it "should find reward" do
       Reward.should_receive(:find).with(@reward.key, :consistent => true).and_return(@reward)
       get(:run_job, :message => @reward.key)
     end
 
     it "should create a Notification object" do
-      NotificationsClient::Notification.any_instance.stub(:deliver)
-
       NotificationsClient::Notification.should_receive(:new).with({
         :app_id => @reward.publisher_app_id,
         :title => "Reward Notification",
-        :message => "Your reward from #{@publisher_app.name} is available!",
+        :message => "You earned #{@currency.name} by downloading #{@advertiser_app.name}",
         :device_aliases => [{:namespace => 'android_id', :identifier => 'android_id'}, {:namespace => 'idfa', :identifier => 'idfa'}]
       }).and_return(@notification)
-
-      get(:run_job, :message => @reward.key)
-    end
-
-    it "should deliver a notification" do
-      NotificationsClient::Notification.any_instance.stub(:deliver)
-      NotificationsClient::Notification.any_instance.should_receive(:deliver)
 
       get(:run_job, :message => @reward.key)
     end
