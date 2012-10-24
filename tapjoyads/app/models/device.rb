4fbe5635690a5f0994367151b2e36c12ed4808bc
Inderpreet Singh <inderpreet.singh@tapjoy.com>
2012-10-24 15:54:54 -0700

    Fix specs

--- tapjoyads/app/models/device.rb
+++ tapjoyads/app/models/device.rb
@@ -252,7 +252,7 @@ class Device < SimpledbShardedResource
   end
 
   def recommendations(options = {})
-    RecommendationList.new(options.merge(:device_id => key)).apps
+    RecommendationList.new(options.merge(:device_id => self.id)).apps
   end
 
   def gamers
