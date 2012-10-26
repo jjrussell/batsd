55dacef78a5a592dd3c836798e69e1e16e3dcc0a
Weston Jossey <weston.jossey@gmail.com>
2012-10-26 12:59:54 -0400

    Use protobuffers if possible

--- tapjoyads/config/initializers/riak.rb
+++ tapjoyads/config/initializers/riak.rb
@@ -1,6 +1,6 @@
 Riak.escaper = "cgi"
 if Rails.env.production?  
-  $riak = Riak::Client.new(:nodes => RIAK_NODES)
+  $riak = Riak::Client.new(:protocol => 'pbc', :nodes => RIAK_NODES)
 else
   $riak = Riak::Client.new
 end
