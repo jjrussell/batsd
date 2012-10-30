499c7a696a711be42d8ddb1eb5934ebbe84e45e8
Matt Carbone <matt.carbone@gmail.com>
2012-10-30 10:30:53 -0400

    spec failure & rollback when setting app_secret_key

--- tapjoyads/spec/controllers/dashboard/push_controller_spec.rb
+++ tapjoyads/spec/controllers/dashboard/push_controller_spec.rb
@@ -11,6 +11,8 @@ describe Dashboard::PushController do
     )
     @app = FactoryGirl.create(:app, :partner => @partner, :notifications_enabled => false)
     login_as(@user)
+
+    NotificationsClient::App.any_instance.stub(:update)
   end
  
   describe 'update' do
@@ -26,6 +28,12 @@ describe Dashboard::PushController do
     it 'should redirect to push index' do
       do_request
       response.should redirect_to(app_push_index_path(@app))
+    end
+
+    it 'should rollback if setting app_secret fails' do
+      NotificationsClient::App.any_instance.should_receive(:update).and_raise
+      do_request
+      @app.reload.notifications_enabled.should == false
     end
   end
 end
