f6b7aa5fc175de57d773f6215c33a01e89eb6a16
amirmanji <amanji75@gmail.com>
2012-11-01 19:00:20 -0700

    enable featured video when rewarded app installs are hidden

--- tapjoyads/app/models/offer_list.rb
+++ tapjoyads/app/models/offer_list.rb
@@ -26,6 +26,8 @@ class OfferList
     @algorithm                  = options.delete(:algorithm)
     @algorithm_options          = options.delete(:algorithm_options)
 
+    raise "Unknown options #{options.keys.join(', ')}" unless options.empty?
+
     @algorithm_options ||= {}
 
     @currency ||= Currency.find_in_cache(currency_id) if currency_id.present?
@@ -50,12 +52,10 @@ class OfferList
       @store_whitelist << @app_store_name if AppStore.find(@app_store_name).exclusive?
     end
 
-    raise "Unknown options #{options.keys.join(', ')}" unless options.empty?
-
-    if @hide_rewarded_app_installs
+    if @currency && !@currency.rewarded?
       @type = case @type
       when Offer::DEFAULT_OFFER_TYPE
-        (@currency && !@currency.rewarded?) ? Offer::NON_REWARDED_BACKFILLED_OFFER_TYPE : @type
+        Offer::NON_REWARDED_BACKFILLED_OFFER_TYPE
       when Offer::FEATURED_OFFER_TYPE
         Offer::NON_REWARDED_FEATURED_OFFER_TYPE
       when Offer::FEATURED_BACKFILLED_OFFER_TYPE
