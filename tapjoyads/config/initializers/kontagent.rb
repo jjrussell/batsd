abcdfe0f89aed035304d5fc616d810b7ca2f1a8f
jweissman <joseph.weissman@tapjoy.com>
2012-11-05 14:03:45 -0500

    Update: newlines on config files

--- tapjoyads/config/initializers/kontagent.rb
+++ tapjoyads/config/initializers/kontagent.rb
@@ -1,3 +1,3 @@
 require 'kontagent'
 KT_CONFIG = YAML.load_file("#{Rails.root}/config/kontagent.yml")[Rails.env]
-Kontagent::Base.configure(KT_CONFIG) #['username'], KT_CONFIG['password'], KT_CONFIG['endpoint'])
+Kontagent::Base.configure(KT_CONFIG)
