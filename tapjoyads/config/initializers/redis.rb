2005206fc091fa6d8a859c6af1ba44318000ba9d
Aaron Daniel <amdtech@me.com>
2012-11-08 16:22:13 -0800

    New permanent redis db connection

--- tapjoyads/config/initializers/redis.rb
+++ tapjoyads/config/initializers/redis.rb
@@ -1,5 +1,6 @@
 if Rails.env.production?
   $redis = Redis.new(:host => 'redis.tapjoy.net')
+  $perma_redis = Redis.new(:host => 'redis.tapjoy.net', :db => 1)
   $redis_read = Redis.new(:host => 'alpha.redis.tapjoy.net')
 else
   $redis_read = $redis = Redis.new
