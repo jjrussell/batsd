b96cad4fb6807a011c4e6bd863feac2c3f79df0e
Aaron Daniel <amdtech@me.com>
2012-11-09 13:18:45 -0800

    Switch analtics lgoger to the right cluster

--- tapjoyads/config/environments/production.rb
+++ tapjoyads/config/environments/production.rb
@@ -61,6 +61,14 @@ rescue Errno::ENOENT
   local_config = {}
 end
 
+CACHE_SERVERS = {}
+CACHE_SERVERS[:analytics_logger] = [
+  'tj-prod-20120424.fqfjqv.0001.use1.cache.amazonaws.com',
+  'tj-prod-20120424.fqfjqv.0002.use1.cache.amazonaws.com',
+  'tj-prod-20120424.fqfjqv.0003.use1.cache.amazonaws.com',
+  'tj-prod-20120424.fqfjqv.0004.use1.cache.amazonaws.com',
+  'tj-prod-20120424.fqfjqv.0005.use1.cache.amazonaws.com'
+]
 MEMCACHE_SERVERS = [
   'localhost:11210' # couchbase stats cluster
 ]
