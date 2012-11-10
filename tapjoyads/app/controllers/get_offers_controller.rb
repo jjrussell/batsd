18a8ea611630a685435f051529bd56b96fde782f
Daniel Ho <tapdan@tapjoy.com>
2012-11-10 02:57:30 +0100

    Show test video offer as long as device is a test device for the currency
    Checks SDK version to determine whether to return test_video offer or not
    
    Checks test_video params in video_offer_ids for showing test_video_offer

--- tapjoyads/app/controllers/get_offers_controller.rb
+++ tapjoyads/app/controllers/get_offers_controller.rb
@@ -36,7 +36,10 @@ class GetOffersController < ApplicationController
     @sdk9plus = library_version >= '9'
     if @currency.has_test_device?(params[:udid])
       @test_offers = [ @publisher_app.test_offer ]
-      if params[:all_videos] || params[:video_offer_ids].to_s.split(',').include?('test_video')
+      requires_videos_param = library_version < '8.3'
+      if !requires_videos_param ||
+           params[:all_videos] ||
+           params[:video_offer_ids].to_s.split(',').include?('test_video')
         @test_offers << @publisher_app.test_video_offer.primary_offer
       end
     end
