1ca3c58a44d2388c9cc8b50370e4906f3b1b200f
Daniel Ho <tapdan@tapjoy.com>
2012-11-16 03:23:59 +0100

    Moved tests from click_controller_spec to coupon_instructions_controller_spec where they belong

--- tapjoyads/spec/controllers/coupon_instructions_controller_spec.rb
+++ tapjoyads/spec/controllers/coupon_instructions_controller_spec.rb
@@ -53,6 +53,19 @@ describe CouponInstructionsController do
           fill_in 'email_address', :with => 'tapjoy@tapjoy.com'
           click_button('Send Coupon')
         end
+
+        it 'goes to complete action' do
+          response.should render_template('coupons/complete')
+        end
+        it 'responds with 200' do
+          response.should be_success
+        end
+        it 'has success on the page' do
+          page.has_content?('Success')
+        end
+        it 'has coupon\'s name' do
+          page.has_content?(@coupon.name)
+        end
         it 'should have a POST request to the create action' do
           response.should be_success
         end
