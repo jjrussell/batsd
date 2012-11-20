37104bd11c2786d53d836e4557db2bee2abf1529
Matt Nguyen <mnguyen916@gmail.com>
2012-11-19 17:57:58 -0800

    Refactor

--- tapjoyads/app/controllers/package_names_controller.rb
+++ tapjoyads/app/controllers/package_names_controller.rb
@@ -1,14 +1,13 @@
 class PackageNamesController < ApplicationController
   def index
     return unless verify_params([:udid, :package_names])
-    device = Device.new(:key => params[:udid])
+    device = Device.find(params[:udid])
     package_names = params[:package_names].split(',').map(&:strip).reject(&:empty?).uniq
     @call_success = true
     @new_refresh_interval = 1.week
 
     web_request = WebRequest.new
     web_request.put_values('package_names', params, ip_address, geoip_data, request.headers['User-Agent'])
-    web_request.truncated_package_names = false
 
     package_names.each do |package_name|
       web_request.package_names = package_name
