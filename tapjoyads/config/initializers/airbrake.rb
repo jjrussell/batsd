daa56c6eb5908196b424d6ebf19f2f7d043b6e4c
Kyle Brett <kyle@kylebrett.com>
2012-11-30 13:57:30 -0500

    Upgrade Airbrake + hack in evented http requests

--- tapjoyads/config/initializers/airbrake.rb
+++ tapjoyads/config/initializers/airbrake.rb
@@ -1,3 +1,48 @@
+class Airbrake::Sender
+  private
+  # Airbrake uses this method to get a Net::HTTP::Proxy object it can
+  # use to execute its HTTP request.  So let's sneak our own object in there
+  # TODO SSL (we don't use it though)
+  alias_method :setup_http_connection_dashkb, :setup_http_connection
+
+  def setup_http_connection
+    return setup_http_connection_dashkb unless ENV['ASYNC'] && EM.reactor_running?
+    Object.new.tap do |http|
+      http.instance_variable_set(:@delegate, self)
+
+      def http.method_missing(m, *args, &block)
+        @delegate.send(m, *args, &block)
+      end
+
+      def http.post(path, data, headers)
+        begin
+          req = EM::HttpRequest.new("http://#{url.host}", {
+            connect_timeout: http_open_timeout,
+            inactivity_timeout: http_read_timeout
+          }).post({
+            path: path,
+            body: data,
+            head: headers
+          })
+        rescue => e
+          Rails.logger.info "Error reporting exception to airbrake: #{e}"
+        end
+
+        raise 'no response from airbrake' unless (req.response_header.status.to_i > 0 rescue nil)
+
+        # Get proper http response class for status
+        klass = Net::HTTPResponse::CODE_TO_OBJ[req.response_header.status.to_s]
+
+        # Build + return, with shim for #body since we have it already
+        klass.new('1.1', req.response_header.status, req.response).tap do |response|
+          response.instance_variable_set(:@body, req.response)
+          def response.body; @body; end
+        end
+      end
+    end
+  end
+end
+
 Airbrake.configure do |config|
   config.api_key = '54ad56ce4f8a75e5b413ab3e325d88f9'
   config.ignore_by_filter do |exception_data|
