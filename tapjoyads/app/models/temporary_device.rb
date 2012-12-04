1a5bbc7dba65b77b703ba8b27d04383d3f025ff9
Weston Jossey <weston.jossey@gmail.com>
2012-12-04 11:43:11 -0500

    Add missing include

--- tapjoyads/app/models/temporary_device.rb
+++ tapjoyads/app/models/temporary_device.rb
@@ -1,5 +1,6 @@
 class TemporaryDevice < SimpledbShardedResource
   #Note the that domain name is "td" to save on bytes since device keys are in memory for Riak
+  include RiakMirror
   mirror_configuration :riak_bucket_name => "td"
 
   self.num_domains = NUM_TEMPORARY_DEVICE_DOMAINS
