3a56a50741dfcf013b42fe3e4b5ba17a0faf69ac
Weston Jossey <weston.jossey@gmail.com>
2012-12-04 12:09:55 -0500

    Revert "Use riak as primary read server for publisher_users"
    This reverts commit f88e4cb323655b06995e02a0c5ad8f7c9c974421.

--- tapjoyads/app/models/publisher_user.rb
+++ tapjoyads/app/models/publisher_user.rb
@@ -1,6 +1,6 @@
 class PublisherUser < SimpledbShardedResource
   include RiakMirror
-  mirror_configuration :riak_bucket_name => "publisher_users", :read_from_riak => true
+  mirror_configuration :riak_bucket_name => "publisher_users"
 
   MAX_UDIDS = 5
 
