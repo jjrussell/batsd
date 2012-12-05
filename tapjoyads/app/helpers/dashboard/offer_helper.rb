5f456f61750e7b4c70e7ae131c36e55b31761682
Andrew Kasper <andrew.kasper@tapjoy.com>
2012-12-05 15:05:46 -0500

    Unify and simplify offer name display in search results, statz, edit offer, active campaigns, and partner list views

--- tapjoyads/app/helpers/dashboard/offer_helper.rb
+++ tapjoyads/app/helpers/dashboard/offer_helper.rb
@@ -1,16 +1,27 @@
 module Dashboard::OfferHelper
   def offer_title_for(offer)
-    "#{offer.name} [#{offer_title_suffix_for(offer)}]"
+    "#{offer.name} #{offer_title_suffix_for(offer)}"
   end
 
   def offer_title_suffix_for(offer)
-    if offer.main? && offer.rewarded && !offer.featured?
-      'Main'
-    elsif offer.main?
-      "Main #{offer_type_description_for(offer)}"
+    offer_suffix_for(offer) + main_offer_description_for(offer)
+  end
+
+  def offer_suffix_for(offer)
+    offer.name_suffix.present? ? "(#{offer.name_suffix})" : ''
+  end
+
+  def main_offer_description_for(offer)
+    if offer.main?
+      if offer.rewarded && !offer.featured?
+        ' [Main]'
+      else
+        " [Main #{offer_type_description_for(offer)}]"
+      end
     else
-      offer.name_suffix
+      ''
     end
+
   end
 
   def offer_type_description_for(offer)
