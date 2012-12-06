d6b1ca28d3fac064c317795f790bf88cd55ef4b9
Kyle Brett <kyle@kylebrett.com>
2012-12-05 16:48:45 -0800

    Riak: hack EM::Synchrony::TCPSocket into protocol buffers backend

--- tapjoyads/config/initializers/riak.rb
+++ tapjoyads/config/initializers/riak.rb
@@ -1,5 +1,19 @@
+RIAK_NODES ||= [{host: ENV['RIAK_HOST']}] || []
+DEVICE_RIAK_NODES ||= RIAK_NODES
+
+if ENV['ASYNC']
+  class Riak::Client::ProtobuffsBackend
+    private
+    def new_socket
+      EM::Synchrony::TCPSocket.new(@node.host, @node.pb_port).tap do |socket|
+        socket.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_NODELAY, true)
+      end
+    end
+  end
+end
+
 Riak.escaper = "cgi"
-if Rails.env.production?
+if Rails.env.production? || (ENV['ASYNC'] && RIAK_NODES.any? && DEVICE_RIAK_NODES.any?)
   $riak = Riak::Client.new(:protocol => 'pbc', :nodes => RIAK_NODES)
   $riak_devices = Riak::Client.new(:protocol => 'pbc', :nodes => DEVICE_RIAK_NODES)
 else
