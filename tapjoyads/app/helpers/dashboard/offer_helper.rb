6e4683d862b89527b141c130ab6e49bd5b8019cc
Eric Tipton <eric.tipton@tapjoy.com>
2012-12-11 18:33:50 -0500

    Revert "Merge pull request #4272 from akasper/associated_offer_names"
    This reverts commit 97a2e5dedd0a3488ff62c392740ef15495815901, reversing
    changes made to 4acc637d73b8ad166a321a0bd64972f80f157dab.

--- tapjoyads/app/helpers/dashboard/offer_helper.rb
+++ tapjoyads/app/helpers/dashboard/offer_helper.rb
@@ -1,27 +1,16 @@
 module Dashboard::OfferHelper
   def offer_title_for(offer)
-    "#{offer.name} #{offer_title_suffix_for(offer)}"
+    "#{offer.name} [#{offer_title_suffix_for(offer)}]"
   end
 
   def offer_title_suffix_for(offer)
-    offer_suffix_for(offer) + main_offer_description_for(offer)
-  end
-
-  def offer_suffix_for(offer)
-    offer.name_suffix.present? ? "(#{offer.name_suffix})" : ''
-  end
-
-  def main_offer_description_for(offer)
-    if offer.main?
-      if offer.rewarded && !offer.featured?
-        ' [Main]'
-      else
-        " [Main #{offer_type_description_for(offer)}]"
-      end
+    if offer.main? && offer.rewarded && !offer.featured?
+      'Main'
+    elsif offer.main?
+      "Main #{offer_type_description_for(offer)}"
     else
-      ''
+      offer.name_suffix
     end
-
   end
 
   def offer_type_description_for(offer)
