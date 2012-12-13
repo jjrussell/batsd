08076feee0c59179de62749d3ec155a70f6758ac
jweissman <joseph.weissman@tapjoy.com>
2012-12-13 17:30:52 -0500

    Cleanup: commented code in specs

--- tapjoyads/spec/controllers/dashboard/kontagent_controller_spec.rb
+++ tapjoyads/spec/controllers/dashboard/kontagent_controller_spec.rb
@@ -124,7 +124,6 @@ describe Dashboard::KontagentController do
       partner.stub    :kontagent_enabled => true,  :kontagent_subdomain => subdomain
       controller.stub :current_partner => partner, :current_user => user
 
-      #put :update, :id => integration_request.id
       post :resync
 
       response.should redirect_to(:action => :index)
