1817ad8d2fe7c7026f617f575de3fe31096b3580
Aaron Daniel <amdtech@me.com>
2012-12-13 16:51:44 -0800

    Need mysql access for the job_runner

--- tapjoyads/lib/jobs.rb
+++ tapjoyads/lib/jobs.rb
@@ -21,4 +21,7 @@ root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 require File.join(root, 'config', 'boot')
 require File.join(root, 'config', 'environment')
 
+ActiveRecord::Base.connection.disconnect!
+ActiveRecord::Base.establish_connection('job_runner')
+
 JobRunner.start
