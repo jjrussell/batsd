495041fb2f8355168b7bfa3723c4404a04a013d6
Aaron Daniel <amdtech@me.com>
2012-12-14 18:40:52 -0800

    Typo and render for expire_currency_sales

--- tapjoyads/app/controllers/job/master_expire_currency_sales_controller.rb
+++ tapjoyads/app/controllers/job/master_expire_currency_sales_controller.rb
@@ -1,7 +1,9 @@
-class Job::MasterExpireCurrencySalesController < Jobs::JobController
+class Job::MasterExpireCurrencySalesController < Job::JobController
   def index
     CurrencySale.past.each do |sale|
       sale.currency.cache if sale.end_time > 2.hours.ago
     end
+
+    render :text => 'ok'
   end
 end
