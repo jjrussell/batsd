e2eb8bfbd46d70a6ce04289b772b5f9cd677a081
Andrew Kasper <andrew.kasper@tapjoy.com>
2012-12-20 17:56:50 -0500

    More graceful handling of links in apps dropdown.

--- tapjoyads/app/assets/javascripts/tapjoy/lazy_loaded_dropdown.js
+++ tapjoyads/app/assets/javascripts/tapjoy/lazy_loaded_dropdown.js
@@ -1,65 +1,16 @@
-function dropdownify(offers, element) {
-  for(var i in offers) {
-    var offer = offers[i]
-    var html =
-    "<div id='" + offer.id + "' class='app' title='" + offer.name + "'>" +
-      "<span class='app_name'>" + titleFor(offer) + "</span>" +
-      "<img alt='" + offer.platform + "_flat' class='platform_icon' src='/images/" + offer.platform_icon_url + "'>" +
-      "<input disabled='disabled' id='app_id' name='app_id' type='hidden' value='" + offer.name + "'>" +
-      linkTo(offer) +
-    "</div>";
-
-    element.append(html);
-  };
-  return false;
-}
-
-function titleFor(offer) {
-  return "<div class='offer_name'>" +
-    "<div class='icon'>" +
-      "<img alt='' height='24' width='24' id='icon' src='" + offer.icon_url + "'>" +
-    "</div>" +
-    "<h1 class='name'>" + offer.name + "</h1>" +
-    "<div class='descriptors'>" + offer.descriptors.join(', ') + "</div>" +
-  "</div>";
-}
-
-function linkTo(offer) {
-  return "<a href='" + urlFor(offer) + "' class='hidden'>#</a>";
-}
-
-function urlFor(offer) {
-  return '/dashboard/reporting/' + offer.id
-}
-
-function isSelected(offer) {
-  offer.id == ('#selected_app .app .offer_name').attr('id');
-}
-
-function selected(offer) {
-  if(isSelected(offer)) {
-    return ' selected';
-  } else
-  {
-    return '';
-  }
-}
-
-function fetchDropdownOffers(element) {
-  var selectedID = $('#selected_app .offer_name').attr('id');
-  $('#selected_app img.platform_icon').after('<img id="app_dropdown_loading_icon" alt="loading" src="/images/load-circle.gif">');
+function populateDropdownOffers(element) {
+  insertLoadingIcon();
   $.get('/dashboard/offers', { count: true }, function(response) {
     var count = parseInt(response);
-    var partnerOfferPath = '/dashboard/offers';
     var pageSize = 100;
     var page = 0;
 
-    checkFinishedLoading(count);
+    monitorFinishedLoading(count);
 
     for(null; (page*pageSize) <= count; page++) {
       $.ajax({
-        url: partnerOfferPath,
-        data: {page_size: pageSize, page: page, source: 'dropdown', selected: selectedID },
+        url: partnerOfferPath(),
+        data: {page_size: pageSize, page: page, source: 'dropdown', selected: selectedID() },
         success: function(response, status, xhr) {
           dropdownify(response.data, element);
         },
@@ -76,19 +27,94 @@ function fetchDropdownOffers(element) {
       });
     }
   });
+};
 
-  $('.app').each(function(){
+function dropdownify(offers, element) {
+  for(var i in offers) {
+    var offer = offers[i]
+    var html =
+    '<div id="' + offer.id + '" class="app" title="' + safeOfferName(offer) + '">' +
+      "<span class='app_name'>" + titleFor(offer) + "</span>" +
+      "<img alt='" + offer.platform + "_flat' class='platform_icon' src='/images/" + offer.platform_icon_url + "'>" +
+      "<input disabled='disabled' id='app_id' name='app_id' type='hidden' value='" + safeOfferName(offer) + "'>" +
+      linkTo(offer) +
+    "</div>";
+
+    element.append(html);
+    addLinkBehaviorTo($('#' + offer.id + '.app').not('selected'));
+  };
+  return false;
+}
+
+function titleFor(offer) {
+  return "<div class='offer_name'>" +
+    "<div class='icon'>" +
+      "<img alt='' height='24' width='24' id='icon' src='" + offer.icon_url + "'>" +
+    "</div>" +
+    "<h1 class='name'>" + offer.name + "</h1>" +
+    "<div class='descriptors'>" + offer.descriptors.join(', ') + "</div>" +
+  "</div>";
+}
+
+function safeOfferName(offer) {
+  return offer.name.replace("\'", "\\'");
+}
+
+function linkTo(offer) {
+  return "<a href='" + urlFor(offer) + "' class='hidden'>#</a>";
+}
+
+function urlFor(offer) {
+  return '/dashboard/reporting/' + offer.id
+}
+
+function isSelected(offer) {
+  offer.id == ('#selected_app .app .offer_name').attr('id');
+}
+
+function selected(offer) {
+  return (isSelected(offer) ? ' selected' : '');
+}
+
+function selectedID() {
+  return $('#selected_app .offer_name').attr('id');
+}
+
+function partnerOfferPath() {
+  return '/dashboard/offers';
+}
+
+function monitorFinishedLoading(count) {
+  var k = setInterval(function () {
+    if($('#apps_box .app').size() >= count-1) {
+      removeLoadingIcon();
+      clearInterval(k);
+    }
+  }, 100);
+}
+
+function insertLoadingIcon() {
+  $('#selected_app img.platform_icon').after('<img id="app_dropdown_loading_icon" alt="loading" src="/images/load-circle.gif">');
+  return false;
+}
+
+function removeLoadingIcon() {
+  $('#app_dropdown_loading_icon').remove();
+}
+
+function markSelected(element, selectedID) {
+  $('.app').each(function() {
     if($(this).attr('id') == selectedID) {
       $(this).addClass('selected');
     }
   });
-};
+}
 
-function checkFinishedLoading(count) {
-  var k = setInterval(function () {
-    if($('#apps_box .app').size() >= count-1) {
-      $('#app_dropdown_loading_icon').remove();
-      clearInterval();
-    }
-  }, 1000);
+function addLinkBehaviorTo(element) {
+  element.click(function(e) {
+    $('.app.selected').removeClass('selected');
+    $(this).addClass('selected');
+    $('#selected_app_text').html($(this).html());
+    location.href = $(this).children('a').attr('href');
+  });
 }
