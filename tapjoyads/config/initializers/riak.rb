4c27ddcad8e7e002f0c891f8661e604208711435
Eric Abbott <eric.abbott@tapjoy.com>
2012-12-22 14:32:02 -0500

    Write to second device cluster. Leave on SDB for now.

--- tapjoyads/config/initializers/riak.rb
+++ tapjoyads/config/initializers/riak.rb
@@ -1,6 +1,7 @@
 Riak.escaper = "cgi"
 if Rails.env.production?
   $riak = Riak::Client.new(:protocol => 'pbc', :nodes => RIAK_NODES)
+  $riak_devices = Riak::Client.new(:protocol => 'pbc', :nodes => DEVICE_RIAK_NODES)
 else
-  $riak = Riak::Client.new
+  $riak = $riak_devices = Riak::Client.new
 end
