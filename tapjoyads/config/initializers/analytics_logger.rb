3360244c5f582eb932a89b7c99611675bf0ebfca
Chris Gerber <chris.gerber@tapjoy.com>
2013-01-02 16:21:09 -0500

    Removed the audit_message_counts setting for analytics_logger

--- tapjoyads/config/initializers/analytics_logger.rb
+++ tapjoyads/config/initializers/analytics_logger.rb
@@ -1,6 +1,5 @@
 AnalyticsLogger.tap do |config|
   config.amqp_url = AMQP_URL
   config.logger.level = ANALYTICS_LOGGER_LEVEL
-  config.audit_message_counts = true
   config.memcache_servers = CACHE_SERVERS[:analytics_logger]
 end
