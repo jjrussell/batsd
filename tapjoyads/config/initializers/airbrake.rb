31cf11f34392f0c106156bbbec72746f4f81fcc9
Aaron Daniel <amdtech@me.com>
2013-01-07 18:46:59 -0800

    New airbrake app for connect

--- tapjoyads/config/initializers/airbrake.rb
+++ tapjoyads/config/initializers/airbrake.rb
@@ -44,7 +44,7 @@ class Airbrake::Sender
 end
 
 Airbrake.configure do |config|
-  config.api_key = '54ad56ce4f8a75e5b413ab3e325d88f9'
+  config.api_key = 'bdf26b75f95c3ca09c91f1d8d6491327'
   config.ignore_by_filter do |exception_data|
     if exception_data[:error_class] == 'RightAws::AwsError'
       true if exception_data[:error_message] =~ /^(ServiceUnavailable|SignatureDoesNotMatch)/
